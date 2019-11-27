95de4cf88e3d18851bf6849b50625bb08a792a43
Dan Green <danngreen1@gmail.com>
2019-11-26 17:07:13 -0800

    oversampler: fixed bug and simplified Log2Int

--- src/filter.hh
+++ src/filter.hh
@@ -1,24 +1,9 @@
 #include <stm32f7xx.h>
 
-constexpr uint32_t Log2Int(uint32_t x) {
-    return 
-    x==(1UL<<16) ? 16 :
-    x==(1UL<<15) ? 15 :
-    x==16384 ? 14 :
-    x==8192 ? 13 :
-    x==4096 ? 12 :
-    x==2048 ? 11 :
-    x==1024 ? 10 :
-    x==512 ? 9 :
-    x==256 ? 8 :
-    x==128 ? 7 :
-    x==64 ? 6 :
-    x==32 ? 5 :
-    x==16 ? 4 :
-    x==8 ? 3 :
-    x==4 ? 2 :
-    x==2 ? 1 :
-    x==1 ? 0 : 0;
+constexpr uint32_t Log2Int(const uint32_t x) {
+    for (int i=0; i<32; i++)
+        if (x==(1UL<<i)) return i;
+    return 0;
 }
 
 template<class T, int size>
@@ -28,9 +13,10 @@ public:
     Oversampler() {}
     void add_val(T newval) {
         buff_ += newval;
-        if (++idx_>size) {
-            idx_ = 0;
+        if (++idx_>=size) {
             val_ = buff_ >> oversample_shift_;
+            idx_ = 0;
+            buff_ = 0;
         }
     }
     T val() {return val_;}
