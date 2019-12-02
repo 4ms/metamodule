4fcdbce26cf9d66be0817e0cf796daa250b0c49a
Dan Green <danngreen1@gmail.com>
2019-12-01 20:57:13 -0800

    TimPWM: refactored with LL instead of HAL
    Updated leds.hh to use new refactoring
    Added LL c file for tim

--- src/drivers/tim_pwm.hh
+++ src/drivers/tim_pwm.hh
@@ -2,143 +2,170 @@
 #include <stm32f7xx.h>
 #include "system.hh"
 #include "colors.hh"
+#include "stm32f7xx_ll_tim.h"
 
-//Todo Re-do this with LL
+enum class TimChannelNum {
+    _1=LL_TIM_CHANNEL_CH1,
+    _1N=LL_TIM_CHANNEL_CH1N,
+    _2=LL_TIM_CHANNEL_CH2,
+    _2N=LL_TIM_CHANNEL_CH2N,
+    _3=LL_TIM_CHANNEL_CH3,
+    _3N=LL_TIM_CHANNEL_CH3N,
+    _4=LL_TIM_CHANNEL_CH4,
+    _5=LL_TIM_CHANNEL_CH5,
+    _6=LL_TIM_CHANNEL_CH6
+};
+
 
 // #define _debugconst_ const
 #define _debugconst_
 
-//Todo: use local static like ADC, to prevent user from constructding TimPwm's with same TIMx multiple times
-//
-//It'll be much easier without HAL, since we just pass a TIM_TypeDef * around
-//
-//Problem with current implementation:
-//class xyz { TIM_HandleTypeDef th; 
-//              myfunc() { TimPwm timer1{TIM1}; th = timer1.get_htim(); }
-// };  
-// th will be a ref to a deleted object when myfunc() exits!
-//
-class TimPwm {
-friend class TimPwmLed;
-public:
-    TimPwm(TIM_TypeDef *TIM) {
-        System::enable_tim_rcc(TIM);
+class TimPwmPeriph;
+class TimPwmChannel;
+class NoLed;
 
-        htim_.Instance = TIM;
-        htim_.Init.Prescaler = 1;
-        htim_.Init.Period = 256;
-        htim_.Init.ClockDivision = 0;
-        htim_.Init.CounterMode = TIM_COUNTERMODE_UP;
-        htim_.Init.RepetitionCounter = 0;
-        htim_.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-        HAL_TIM_PWM_Init(&htim_);
-    }
 
-    // static TIM_HandleTypeDef PeriphInstance(TIM_TypeDef *TIM) {return ;}
-
+class TimPwmPeriph {
+friend class TimPwmChannel;
 private:
-    TIM_HandleTypeDef& get_htim() {return htim_;}
-    TIM_HandleTypeDef htim_;
+    static void init_periph_once(TIM_TypeDef *TIM, 
+                                uint32_t period=256,
+                                uint16_t prescaler=0, 
+                                uint32_t clock_division=0)
+    {
+        static bool did_init[16]={false};
+
+        uint8_t tim_i = System::tim_periph_to_num(TIM);
+        if (!tim_i) return; //Todo: check exception implications of using assert
+
+        tim_i--;
+        if (!did_init[tim_i]) {
+            System::enable_tim_rcc(TIM);
+            LL_TIM_InitTypeDef timinit = {
+                .Prescaler = prescaler,
+                .CounterMode = LL_TIM_COUNTERMODE_UP,
+                .Autoreload = period,
+                .ClockDivision = clock_division,
+                .RepetitionCounter = 0
+            };
+            LL_TIM_Init(TIM, &timinit); 
+            LL_TIM_DisableARRPreload(TIM);
+
+            did_init[tim_i] = true;
+        }
+    }
 };
 
 enum class TimPwmOutType {Norm, N};
 
-class TimPwmLed {
-protected:
-    TimPwmLed() : htim_(nohtim_) {}
+// TimPwmChannel: output PWM on a pin
+// automatically initializes the given TIM peripheral if it hasn't been initialized yet
+// Usage:
+// TimPwmChannel myPWMOutput {TIM1, TimChannelNum::_2, GPIO_PIN_3, GPIOA, 
+class TimPwmChannel {
 public:
-    TimPwmLed(  TimPwm& timpwm, 
-                const uint32_t channel, 
-                const uint16_t pin, 
+    TimPwmChannel(  TIM_TypeDef* const TIM,
+                TimChannelNum const channel,
+                uint16_t const pin, 
                 GPIO_TypeDef * const port, 
-                const uint8_t af, 
-                TimPwmOutType out_n = TimPwmOutType::Norm)
-    : htim_(timpwm.get_htim()), 
-      channel_(channel), 
-      pin_(pin, port, PinMode::ALT, PinPull::NONE, PinSpeed::MEDIUM, af) 
+                uint8_t const af, 
+                TimPwmOutType out_n = TimPwmOutType::Norm,
+                uint32_t period=256,
+                uint16_t prescaler=0, 
+                uint32_t clock_division=0)
+    : TIM_(TIM),
+      channel_(channel)
     {
-        init_();
+        Pin output_pin{pin, port, PinMode::ALT, PinPull::NONE, PinSpeed::MEDIUM, af};
+
+        TimPwmPeriph::init_periph_once(TIM, period, prescaler, clock_division);
+
+        if (channel_==TimChannelNum::_1N)
+            channel_base_ = TimChannelNum::_1;
+        else if (channel_== TimChannelNum::_2N)
+            channel_base_ = TimChannelNum::_2;
+        else if (channel_== TimChannelNum::_3N)
+            channel_base_ = TimChannelNum::_3;
+        else 
+            channel_base_ = channel_;
+
+        LL_TIM_OC_InitTypeDef tim_oc
+         = {
+            .OCMode = LL_TIM_OCMODE_PWM1,
+            .OCState = out_n==TimPwmOutType::Norm ? LL_TIM_OCSTATE_ENABLE : LL_TIM_OCSTATE_DISABLE,
+            .OCNState = out_n==TimPwmOutType::Norm ? LL_TIM_OCSTATE_DISABLE : LL_TIM_OCSTATE_ENABLE,
+            .CompareValue = 0,
+            .OCPolarity = LL_TIM_OCPOLARITY_LOW,
+            .OCNPolarity = LL_TIM_OCPOLARITY_HIGH,
+            .OCIdleState = LL_TIM_OCIDLESTATE_LOW,
+            .OCNIdleState = LL_TIM_OCIDLESTATE_LOW,
+        };
+
+        LL_TIM_OC_Init(TIM_, static_cast<uint32_t>(channel_base_), &tim_oc);
+        LL_TIM_CC_EnableChannel(TIM_, static_cast<uint32_t>(channel_));
+        LL_TIM_EnableCounter(TIM_);
     }
 
-    TimPwmLed(  TIM_HandleTypeDef& htim, 
-                const uint32_t channel, 
-                const uint16_t pin, 
-                GPIO_TypeDef * const port, 
-                const uint8_t af, 
-                TimPwmOutType out_n = TimPwmOutType::Norm)
-    : htim_(htim), 
-      channel_(channel), 
-      pin_(pin, port, PinMode::ALT, PinPull::NONE, PinSpeed::MEDIUM, af) 
-    {
-        init_();
+    constexpr void set_brightness(uint32_t val) const {
+        _set_timer_ccr(TIM_, channel_base_, val);
     }
 
-    //Todo: finish this type of initialization (passing TIMx and getting a local static Instance)
-    // TimPwmLed(TIM_TypeDef *TIM, const uint32_t channel, const uint16_t pin, GPIO_TypeDef * const port, const uint8_t af)
-    // : htim_(TimPwm::PeriphInstance(TIM)), channel_(channel), pin_(pin, port, PinMode::ALT, PinPull::NONE, PinSpeed::MEDIUM, af) {
-    //     init_();
-    // }
+protected:
+    TimPwmChannel() : TIM_(nullptr) {}
 
-    void set_brightness(uint32_t val) _debugconst_ {
-        __HAL_TIM_SET_COMPARE(&htim_, channel_, val);
+private: 
+    constexpr void _set_timer_ccr(TIM_TypeDef *TIMx, TimChannelNum channel, uint32_t val) const {
+        switch (channel) {
+            case TimChannelNum::_1: TIMx->CCR1 = val; break;
+            case TimChannelNum::_2: TIMx->CCR2 = val; break;
+            case TimChannelNum::_3: TIMx->CCR3 = val; break;
+            case TimChannelNum::_4: TIMx->CCR4 = val; break;
+            case TimChannelNum::_5: TIMx->CCR5 = val; break;
+            default:            TIMx->CCR6 = val; break;
+        }
     }
 
-    private: void init_() {
-        TIM_OC_InitTypeDef tim_oc;
-        tim_oc.OCMode = TIM_OCMODE_PWM1;
-        tim_oc.OCPolarity = TIM_OCPOLARITY_LOW;
-        tim_oc.OCFastMode = TIM_OCFAST_DISABLE;
-        tim_oc.OCNPolarity = TIM_OCNPOLARITY_HIGH;
-        tim_oc.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-        tim_oc.OCIdleState = TIM_OCIDLESTATE_RESET;
-        tim_oc.Pulse = 0;
-        HAL_TIM_PWM_ConfigChannel(&htim_, &tim_oc, channel_);
-        HAL_TIM_PWM_Start(&htim_, channel_);
-    }
-
-private:
-    Pin pin_;
-    TIM_HandleTypeDef& htim_;
-    TIM_HandleTypeDef nohtim_;
-    uint32_t channel_;
+    TIM_TypeDef* const TIM_;
+    TimChannelNum channel_;
+    TimChannelNum channel_base_;
 };
 
-class NoLed : public TimPwmLed {
+class NoLed : public TimPwmChannel {
 public:
     NoLed() {}
-    void set_brightness(uint32_t val) {}
+    void set_brightness(uint32_t val) const {}
 };
 
 struct RgbLed {
-    RgbLed(TimPwmLed r_LED, TimPwmLed g_LED, TimPwmLed b_LED)
+    RgbLed(TimPwmChannel r_LED, TimPwmChannel g_LED, TimPwmChannel b_LED)
     : r_(r_LED), g_(g_LED), b_(b_LED) {}
 
-    void set_background(Color col) {background_color_ = col;}
-    void set_solid(Color col) {solid_color_ = col;}
+    void set_background(Color const &col) {background_color_ = col;}
+    void set_solid(Color const &col) {solid_color_ = col;}
 
-    void flash(Color c, uint32_t flash_freq = 100) {
+    void flash(Color const &c, uint32_t const flash_freq = 100) {
         flash_color_ = c;
         flash_phase_ = 0xFFFFFFFF;
         flash_freq_ = flash_freq; // divided by update_rate?
     }
 
     // freq in secs
-    void set_glow(Color c, uint32_t freq = 0x00FFFFFF) {
+    void set_glow(Color const &c, uint32_t const freq = 0x00FFFFFF) {
         glow_color_ = c;
         osc_.set_frequency(freq); //divided by update_rate?
     }
 
-    void set_color(Color col) _debugconst_ {
+    constexpr void set_color(Color const &col) const {
         r_.set_brightness(col.red());
         g_.set_brightness(col.green());
         b_.set_brightness(col.blue());
     }
-    void set_color(Color col, uint8_t brightness) _debugconst_ {
+    void set_color(Color const &col, uint8_t const brightness) const {
         r_.set_brightness((col.red() * brightness)>>8);
         g_.set_brightness((col.green() * brightness)>>8);
         b_.set_brightness((col.blue() * brightness)>>8);
     }
-    void set_color(Color col, float brightness) _debugconst_ {
+    void set_color(Color const &col, float const brightness) const {
         r_.set_brightness(col.red() * brightness);
         g_.set_brightness(col.green() * brightness);
         b_.set_brightness(col.blue() * brightness);
@@ -157,7 +184,7 @@ struct RgbLed {
     }
 
 private:
-    TimPwmLed r_, g_, b_;
+    TimPwmChannel const r_, g_, b_;
     TriangleOscillator<89478> osc_;
     Color background_color_ = Colors::off;
     Color solid_color_ = Colors::off;
@@ -167,3 +194,6 @@ private:
     uint32_t flash_phase_ = 0;
 //    Color::Adjustment& color_cal_;
 };
+
+
+
--- src/leds.hh
+++ src/leds.hh
@@ -8,96 +8,58 @@
 class LedCtl {
 
 public:
-    LedCtl() {
-        //Freq1: PC9, PC8, PB3
-        // Pin<NORMAL> r(OUTPUT, GPIO_PIN_9, GPIOC);
-        // Pin<NORMAL> g(OUTPUT, GPIO_PIN_8, GPIOC);
-        // Pin<NORMAL> b(OUTPUT, GPIO_PIN_3, GPIOB);
-        // r.high(); r.low();
-        // g.high(); g.low();
-        // b.high(); b.low();
-
-        //Res1: PA11, PA10, PB5
-        // Pin<NORMAL> r(OUTPUT, GPIO_PIN_11, GPIOA);
-        // Pin<NORMAL> g(OUTPUT, GPIO_PIN_10, GPIOA);
-        // Pin<NORMAL> b(OUTPUT, GPIO_PIN_5, GPIOB);
-
-        // Pin<NORMAL> m5r(OUTPUT, GPIO_PIN_6, GPIOB);
-        // Pin<NORMAL> m5b(OUTPUT, GPIO_PIN_7, GPIOB);
-        // Pin<NORMAL> m4r(OUTPUT, GPIO_PIN_8, GPIOB);
-        // Pin<NORMAL> m4b(OUTPUT, GPIO_PIN_9, GPIOB);
-        // Pin<NORMAL> m3r(OUTPUT, GPIO_PIN_0, GPIOA);
-        // Pin<NORMAL> m3b(OUTPUT, GPIO_PIN_1, GPIOA);
-        // Pin<NORMAL> m2r(OUTPUT, GPIO_PIN_2, GPIOA);
-        // Pin<NORMAL> m2b(OUTPUT, GPIO_PIN_3, GPIOA);
-        // Pin<NORMAL> m1r(OUTPUT, GPIO_PIN_4, GPIOA);
-        // Pin<NORMAL> m1b(OUTPUT, GPIO_PIN_5, GPIOA);
-
-        // Pin<NORMAL> freq2g(OUTPUT, GPIO_PIN_6, GPIOA);
-        // Pin<NORMAL> freq2b(OUTPUT, GPIO_PIN_7, GPIOA);
-        // Pin<NORMAL> res2g(OUTPUT, GPIO_PIN_0, GPIOB);
-        // Pin<NORMAL> res2b(OUTPUT, GPIO_PIN_1, GPIOB);
-    }
+    LedCtl() {    }
 
     //Todo: only update if glowing or fading
     void update() {
         freq1.refresh();
         res1.refresh();
-        // freq2.refresh();
-        // res2.refresh();
-        // mode[0].refresh();
-        // mode[1].refresh();
-        // mode[2].refresh();
-        // mode[3].refresh();
-        // mode[4].refresh();
+        freq2.refresh();
+        res2.refresh();
+        mode[0].refresh();
+        mode[1].refresh();
+        mode[2].refresh();
+        mode[3].refresh();
+        mode[4].refresh();
     }
-
-private:
-    //Todo: automatically construct these when a TimPwmLed is constructed with TIMx
-    TimPwm tim1 {TIM1};
-    TimPwm tim2 {TIM2};
-    TimPwm tim3 {TIM3};
-    TimPwm tim4 {TIM4};
-    TimPwm tim5 {TIM5};
-    TimPwm tim8 {TIM8};
 
 public:
     _debugconst_ RgbLed freq1 {
-        {tim8, TIM_CHANNEL_4, GPIO_PIN_9, GPIOC, GPIO_AF3_TIM8},
-        {tim8, TIM_CHANNEL_3, GPIO_PIN_8, GPIOC, GPIO_AF3_TIM8},
-        {tim2, TIM_CHANNEL_2, GPIO_PIN_3, GPIOB, GPIO_AF1_TIM2}};
+        {TIM8, TimChannelNum::_4, GPIO_PIN_9, GPIOC, LL_GPIO_AF_3},
+        {TIM8, TimChannelNum::_3, GPIO_PIN_8, GPIOC, LL_GPIO_AF_3},
+        {TIM2, TimChannelNum::_2, GPIO_PIN_3, GPIOB, LL_GPIO_AF_1}};
 
     _debugconst_ RgbLed res1 {
-        {tim1, TIM_CHANNEL_4, GPIO_PIN_11, GPIOA, GPIO_AF1_TIM1},
-        {tim1, TIM_CHANNEL_3, GPIO_PIN_10, GPIOA, GPIO_AF1_TIM1},
-        {tim3, TIM_CHANNEL_2, GPIO_PIN_5, GPIOB, GPIO_AF2_TIM3}};
+        {TIM1, TimChannelNum::_4, GPIO_PIN_11, GPIOA, LL_GPIO_AF_1},
+        {TIM1, TimChannelNum::_3, GPIO_PIN_10, GPIOA, LL_GPIO_AF_1},
+        {TIM3, TimChannelNum::_2, GPIO_PIN_5, GPIOB, LL_GPIO_AF_2}};
 
     _debugconst_ RgbLed freq2 {
         NoLedElement,
-        {tim3, TIM_CHANNEL_1, GPIO_PIN_6, GPIOA, GPIO_AF2_TIM3},
-        {tim8, TIM_CHANNEL_1, GPIO_PIN_7, GPIOA, GPIO_AF3_TIM8, TimPwmOutType::N}};
+        {TIM3, TimChannelNum::_1, GPIO_PIN_6, GPIOA, LL_GPIO_AF_2},
+        {TIM8, TimChannelNum::_1, GPIO_PIN_7, GPIOA, LL_GPIO_AF_3, TimPwmOutType::N}};
 
     _debugconst_ RgbLed res2 {
         NoLedElement,
-        {tim3, TIM_CHANNEL_3, GPIO_PIN_0, GPIOB, GPIO_AF2_TIM3},
-        {tim3, TIM_CHANNEL_4, GPIO_PIN_1, GPIOB, GPIO_AF2_TIM3}};
+        {TIM3, TimChannelNum::_3, GPIO_PIN_0, GPIOB, LL_GPIO_AF_2},
+        {TIM3, TimChannelNum::_4, GPIO_PIN_1, GPIOB, LL_GPIO_AF_2}};
 
     _debugconst_ RgbLed mode[5] {
-        {{tim4, TIM_CHANNEL_1, GPIO_PIN_6, GPIOB, GPIO_AF2_TIM4},
+        {{TIM4, TimChannelNum::_1, GPIO_PIN_6, GPIOB, LL_GPIO_AF_2},
          NoLedElement,
-         {tim4, TIM_CHANNEL_2, GPIO_PIN_7, GPIOB, GPIO_AF2_TIM4}},
+         {TIM4, TimChannelNum::_2, GPIO_PIN_7, GPIOB, LL_GPIO_AF_2}},
 
-        {{tim4, TIM_CHANNEL_3, GPIO_PIN_8, GPIOB, GPIO_AF2_TIM4},
+        {{TIM4, TimChannelNum::_3, GPIO_PIN_8, GPIOB, LL_GPIO_AF_2},
          NoLedElement,
-         {tim4, TIM_CHANNEL_4, GPIO_PIN_9, GPIOB, GPIO_AF2_TIM4}},
+         {TIM4, TimChannelNum::_4, GPIO_PIN_9, GPIOB, LL_GPIO_AF_2}},
 
-        {{tim2, TIM_CHANNEL_1, GPIO_PIN_0, GPIOA, GPIO_AF1_TIM2},
+        {{TIM2, TimChannelNum::_1, GPIO_PIN_0, GPIOA, LL_GPIO_AF_1},
          NoLedElement,
-         {tim5, TIM_CHANNEL_2, GPIO_PIN_1, GPIOA, GPIO_AF2_TIM5}},
+         {TIM5, TimChannelNum::_2, GPIO_PIN_1, GPIOA, LL_GPIO_AF_2}},
 
-        {{tim2, TIM_CHANNEL_3, GPIO_PIN_2, GPIOA, GPIO_AF1_TIM2},
+        {{TIM2, TimChannelNum::_3, GPIO_PIN_2, GPIOA, LL_GPIO_AF_1},
          NoLedElement,
-         {tim2, TIM_CHANNEL_4, GPIO_PIN_3, GPIOA, GPIO_AF1_TIM2}},
+         {TIM2, TimChannelNum::_4, GPIO_PIN_3, GPIOA, LL_GPIO_AF_1}},
 
         {NoLedElement,  //DAC0 PA4
          NoLedElement,
