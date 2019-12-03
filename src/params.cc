3680a9c82ea7433f12d1241ce51ad0dda5b99b12
Dan Green <danngreen1@gmail.com>
2019-12-02 16:50:40 -0800

    params: bug fix, wrong CV channel read

--- src/params.cc
+++ src/params.cc
@@ -2,7 +2,7 @@
 
 void Params::update(Controls& controls) {
     freq[0] = controls.freq1CV.get() / 4095.0f;
-    freq[1] = controls.freq1CV.get() / 4095.0f;
+    freq[1] = controls.freq2CV.get() / 4095.0f;
     res[0] = controls.res1CV.get() / 4095.0f;
     res[1] = controls.res2CV.get() / 4095.0f;
 }
