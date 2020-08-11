a75ae6003e9dd861f6aa15116be0c4376c934676
Dan Green <danngreen1@gmail.com>
2020-08-11 16:00:28 -0700

    Fixed case with pin.hh
    Using LL macros instead of direct register access

--- src/drivers/pin.hh
+++ src/drivers/pin.hh
@@ -24,35 +24,34 @@
 #include "stm32f7xx_ll_gpio.h"
 #include "stm32f7xx_ll_bus.h"
 
-//Todo: CamelCase, not ALL CAPS
 enum class PinPolarity { 
-    NORMAL, 
-    INVERTED 
+    Normal, 
+    Inverted 
 };
 
 enum class PinMode : uint32_t { 
-    INPUT = LL_GPIO_MODE_INPUT, 
-    OUTPUT = LL_GPIO_MODE_OUTPUT,
-    ANALOG = LL_GPIO_MODE_ANALOG, 
-    ALT = LL_GPIO_MODE_ALTERNATE 
+    Input = LL_GPIO_MODE_INPUT, 
+    Output = LL_GPIO_MODE_OUTPUT,
+    Analog = LL_GPIO_MODE_ANALOG, 
+    Alt = LL_GPIO_MODE_ALTERNATE 
 };
 
 enum class PinPull : uint32_t { 
-    UP = LL_GPIO_PULL_UP,
-    DOWN = LL_GPIO_PULL_DOWN,
-    NONE = LL_GPIO_PULL_NO
+    Up = LL_GPIO_PULL_UP,
+    Down = LL_GPIO_PULL_DOWN,
+    None = LL_GPIO_PULL_NO
 };
 
 enum class PinSpeed : uint32_t { 
-    LOW = LL_GPIO_SPEED_FREQ_LOW, 
-    MEDIUM = LL_GPIO_SPEED_FREQ_MEDIUM, 
-    HIGH = LL_GPIO_SPEED_FREQ_HIGH, 
-    VERY_HIGH = LL_GPIO_SPEED_FREQ_VERY_HIGH 
+    Low = LL_GPIO_SPEED_FREQ_LOW, 
+    Medium = LL_GPIO_SPEED_FREQ_MEDIUM, 
+    High = LL_GPIO_SPEED_FREQ_HIGH, 
+    VeryHigh = LL_GPIO_SPEED_FREQ_VERY_HIGH 
 };
 
 enum class PinOType : uint32_t { 
-    PUSHPULL = LL_GPIO_OUTPUT_PUSHPULL, 
-    OPENDRAIN = LL_GPIO_OUTPUT_OPENDRAIN
+    PushPull = LL_GPIO_OUTPUT_PUSHPULL, 
+    OpenDrain = LL_GPIO_OUTPUT_OPENDRAIN
 };
 
 enum class GPIO : uint32_t {
@@ -75,168 +74,104 @@ enum class GPIO : uint32_t {
     #endif
 };
 
-//Todo: does auto pin work? Might save an asm extend to use uint32_t
-template<GPIO port, uint8_t pin, PinPolarity polarity=PinPolarity::NORMAL> 
+template<GPIO port, uint8_t pin, PinPolarity polarity=PinPolarity::Normal> 
 struct PinL {
     static_assert((pin <= 15) && (pin >= 0), "GPIO pin number must be in the range 0 - 15");
 
-    volatile auto GPIOPort(GPIO port_) {
+    auto GPIOPort(GPIO port_) {
         return reinterpret_cast<GPIO_TypeDef *>(port_);
+    }
+    template<class X>
+    auto LL(X p) {
+        return static_cast<uint32_t>(p);
     }
 
     PinL() = default;
 
-    constexpr PinL(PinMode m){
+    constexpr PinL(PinMode m) {
         System::enable_gpio_rcc(GPIOPort(port));
         mode(m);
     }
 
-    constexpr PinL(PinMode m, PinOType o){
+    constexpr PinL(PinMode m, PinOType o) {
         System::enable_gpio_rcc(GPIOPort(port));
         mode(m);
         out_type(o);
     }
 
-    constexpr PinL(PinMode m, PinPull pp){
+    constexpr PinL(PinMode m, PinPull pp) {
         System::enable_gpio_rcc(GPIOPort(port));
         mode(m);
         pull(pp);
     }
 
-    constexpr PinL(PinMode m, uint8_t af, PinPull pp=PinPull::NONE){
+    constexpr PinL(PinMode m, uint8_t af, PinPull pp=PinPull::None) {
         System::enable_gpio_rcc(GPIOPort(port));
         mode(m);
         alt_func(af);
         pull(pp);
     }
 
-    operator bool(){
+    operator bool() {
         return read();
     }
 
-    PinL& operator=(bool val){
+    PinL& operator=(bool val) {
         write(val);
         return *this;
     }
 
-    void write(bool val){
+    void write(bool val) {
         if(val)
             high();
         else
             low();
     }
-    bool read(){
-        return (GPIOPort(port)->IDR & (1<<pin) ? true : false);
-        // Todo: compare to:
-        // return LL_GPIO_IsInputPinSet(GPIOPort(port), 1<<pin);
+    bool read() {
+        return LL_GPIO_IsInputPinSet(GPIOPort(port), 1<<pin);
+    }
+    bool is_on() {
+        return (polarity==PinPolarity::Inverted) ? !read() : read();
     }
 
-    void high(){
-        GPIOPort(port)->BSRR = 1 << pin;
-        // Todo: compare to:
-        // LL_GPIO_SetOutputPin(GPIOPort(port), 1<<pin);
+    void high() {
+        LL_GPIO_SetOutputPin(GPIOPort(port), 1<<pin);
     }
-
-    void low(){
-        GPIOPort(port)->BSRR = 1<<(pin+16);
-        // Todo: compare to:
-        // LL_GPIO_ResetOutputPin(GPIOPort(port), 1<<pin);
+    void low() {
+        LL_GPIO_ResetOutputPin(GPIOPort(port), 1<<pin);
     }
-
-    void toggle(){
-        GPIOPort(port)->ODR ^= 1<<pin;
-        // Todo: compare to:
-        // LL_GPIO_TogglePin(GPIOPort(port), 1<<pin);
+    void toggle() {
+        LL_GPIO_TogglePin(GPIOPort(port), 1<<pin);
     }
-
-    //on and off 
-    void on() {if (polarity==PinPolarity::INVERTED) low(); else high();}
-    void off() {if (polarity==PinPolarity::INVERTED) high(); else low();}
-    void set_to(uint32_t v) {if (v) on(); else off();}
-
-    bool is_on() {return (polarity==PinPolarity::INVERTED) ? !read() : read();}
-
+    void on() {
+        if (polarity==PinPolarity::Inverted) low(); else high();
+    }
+    void off() {
+        if (polarity==PinPolarity::Inverted) high(); else low();
+    }
+    void set_to(uint32_t v) {
+        if (v) on(); else off();
+    }
+    void set_to(bool v) {
+        if (v) on(); else off();
+    }
 
     void mode(PinMode m){
-        LL_GPIO_SetPinMode(GPIOPort(port), 1<<pin, static_cast<uint32_t>(m));
+        LL_GPIO_SetPinMode(GPIOPort(port), 1<<pin, LL(m));
     }
-
     void out_type(PinOType o){
-        LL_GPIO_SetPinOutputType(GPIOPort(port), 1<<pin, static_cast<uint32_t>(o));
+        LL_GPIO_SetPinOutputType(GPIOPort(port), 1<<pin, LL(o));
     }
-
     void out_speed(PinSpeed s){
-        LL_GPIO_SetPinSpeed(GPIOPort(port), 1<<pin, static_cast<uint32_t>(s));
+        LL_GPIO_SetPinSpeed(GPIOPort(port), 1<<pin, LL(s));
     }
-
     void pull(PinPull p){
-        LL_GPIO_SetPinPull(GPIOPort(port), 1<<pin, static_cast<uint32_t>(p));
+        LL_GPIO_SetPinPull(GPIOPort(port), 1<<pin, LL(p));
     }
-
     void alt_func(const uint8_t af){
-//        if (pin >= LL_GPIO_PIN_8)
-        if (pin >= 8)
+       if (pin >= LL_GPIO_PIN_8)
             LL_GPIO_SetAFPin_8_15(GPIOPort(port), 1<<pin, af);
         else
             LL_GPIO_SetAFPin_0_7(GPIOPort(port), 1<<pin, af);
     }
 };   
-
-//GPIO Pin driver
-template<PinPolarity polarity=PinPolarity::NORMAL>
-class PinPolarized {
-public :
-    PinPolarized() {}
-    PinPolarized( uint16_t const pin, 
-                    GPIO_TypeDef * const port, 
-                    PinMode const mode, 
-                    PinPull const pull = PinPull::NONE,
-                    PinSpeed const speed = PinSpeed::MEDIUM,
-                    uint8_t const af = 0
-    )
-    : pin_(pin), port_(port)
-    {
-        System::enable_gpio_rcc(port_);
-        auto mode_ = mode == PinMode::INPUT  ? LL_GPIO_MODE_INPUT :
-                     mode == PinMode::OUTPUT ? LL_GPIO_MODE_OUTPUT :
-                     mode == PinMode::ANALOG ? LL_GPIO_MODE_ANALOG :
-                     mode == PinMode::ALT    ? LL_GPIO_MODE_ALTERNATE :
-                                               LL_GPIO_MODE_OUTPUT;
-        auto pull_ = pull == PinPull::UP    ? GPIO_PULLUP :
-                     pull == PinPull::DOWN  ? GPIO_PULLDOWN :
-                                              GPIO_NOPULL;
-        auto speed_ = speed == PinSpeed::LOW        ? GPIO_SPEED_FREQ_LOW :
-                      speed == PinSpeed::MEDIUM     ? GPIO_SPEED_FREQ_MEDIUM :
-                      speed == PinSpeed::HIGH       ? GPIO_SPEED_FREQ_HIGH :
-                      speed == PinSpeed::VERY_HIGH  ? GPIO_SPEED_FREQ_VERY_HIGH :
-                                                      GPIO_SPEED_FREQ_MEDIUM;
-        LL_GPIO_SetPinMode(port_, pin_, mode_);
-        LL_GPIO_SetPinPull(port_, pin_, pull_);
-        LL_GPIO_SetPinSpeed(port_, pin_, speed_);
-        // if (mode_==PinMode::OUTPUT)
-        LL_GPIO_SetPinOutputType(port_, pin_, LL_GPIO_OUTPUT_PUSHPULL); //OD not supported by class
-        if (mode==PinMode::ALT) {
-            if (pin_ >= LL_GPIO_PIN_8)
-                LL_GPIO_SetAFPin_8_15(port_, pin_, af);
-            else
-                LL_GPIO_SetAFPin_0_7(port_, pin_, af);
-        }
-    }
-
-    void high() {port_->BSRR = pin_;}
-    void low() {port_->BSRR = (uint32_t)pin_ << 16;}
-    void on() {if (polarity==PinPolarity::INVERTED) low(); else high();}
-    void off() {if (polarity==PinPolarity::INVERTED) high(); else low();}
-    void set(uint32_t v) {if (v) on(); else off();}
-
-    bool read_raw() {return ((port_->IDR) & pin_) ? true : false;}
-    uint8_t is_on() {return (polarity==PinPolarity::INVERTED) ? !read_raw() : read_raw();}
-
-private:
-    uint16_t pin_;
-    GPIO_TypeDef* port_;
-
-};
-
-using Pin = PinPolarized<PinPolarity::NORMAL>;
-using PinInverted = PinPolarized<PinPolarity::INVERTED>;
