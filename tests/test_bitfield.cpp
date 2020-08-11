c10269e78421d5a8ee6c81056e66aaa44707c2fe
Dan Green <danngreen1@gmail.com>
2020-08-11 16:10:48 -0700

    Removed comments from test_bitfield.cpp

--- tests/test_bitfield.cpp
+++ tests/test_bitfield.cpp
@@ -33,38 +33,3 @@ TEST(bitfield_tests, two_bits_correct)
 	LONGS_EQUAL(bf8.bitfield(BIT0, BIT4), BIT0 + BIT4);
 }
 
-/*
-template <int Z>
-class SomePeriph {
-public:
-    SomePeriph() : a(Z) {}
-    int a;
-};
-
-template <int Z, int Y>
-class SomeOtherPeriph {
-public:
-    SomeOtherPeriph() : a(Z), b(Y) {
-        //init Peripheral Z, channel Y
-    }
-    int a, b;
-};
-
-
-template <typename X>
-auto constexpr create_SomePeriph(X x) {
-    return SomePeriph<x()>();
-}
-
-template <typename X>
-auto constexpr create_SomeOtherPeriph(X x) {
-    return SomeOtherPeriph<std::get<0>(x()), std::get<1>(x())>();
-}
-
-void test2() {
-  auto Channel1 = create_SomePeriph([] { return 1; });
-  auto Channel2 = create_SomePeriph([] { return 2; });
-  //
-  auto OtherChannel3 = create_SomeOtherPeriph([] { return std::make_tuple(ADC2_BASE, 3); });
-}
-*/
