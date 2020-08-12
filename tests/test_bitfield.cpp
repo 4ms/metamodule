835d4f64822f74b57e34f0460affa781b0e5f5e7
Dan Green <danngreen1@gmail.com>
2020-08-12 10:13:27 -0700

    Another bitfield test for three parameters

--- tests/test_bitfield.cpp
+++ tests/test_bitfield.cpp
@@ -41,6 +41,11 @@ TEST(bitfield_tests, two_single_bits_equals_OR)
 	LONGS_EQUAL(bf8.bitfield(BIT8_3, BIT8_7), BIT8_3 | BIT8_7);
 }
 
+TEST(bitfield_tests, three_parameters_equals_OR)
+{
+	LONGS_EQUAL(bf8.bitfield(BIT8_0, BIT8_2, BIT8_3), BIT8_0 | BIT8_2 | BIT8_3);
+}
+
 TEST(bitfield_tests, overlapping_bits_equals_OR)
 {
 	uint8_t num1 = 0b1100;
@@ -51,7 +56,7 @@ TEST(bitfield_tests, overlapping_bits_equals_OR)
 TEST(bitfield_tests, overflow_ignores_higher_bits)
 {
 	uint32_t big_num = 0x89ABCD5A;
-	uint32_t big_num2 = 0x89ABCDE6;
+	uint32_t big_num2 = 0x12345678;
 	uint8_t bottom_byte = static_cast<uint8_t>(0x000000FF & big_num);
 	uint8_t bottom_byte2 = static_cast<uint8_t>(0x000000FF & big_num2);
 	LONGS_EQUAL(bf8.bitfield(big_num, big_num2), bottom_byte | bottom_byte2);
