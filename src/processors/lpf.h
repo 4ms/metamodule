17edadc83a21ec54fbd7fde60f587626b0ec7e0e
Dan Green <danngreen1@gmail.com>
2020-08-24 21:41:21 -0700

    lpf: using constrain() instead of std::min/max
    Also using lower-case suffix "f" for floats

--- src/processors/lpf.h
+++ src/processors/lpf.h
@@ -12,7 +12,7 @@ public:
 	{
 		float output = 0;
 		float fSlow0 = (1.0f / q);
-		float fSlow1 = tan((fConst0 * cutoff)); //todo: will this compile on stm32?
+		float fSlow1 = tan((fConst0 * cutoff));
 		float fSlow2 = (1.0f / fSlow1);
 		float fSlow3 = (1.0f / (((fSlow0 + fSlow2) / fSlow1) + 1.0f));
 		float fSlow4 = (((fSlow2 - fSlow0) / fSlow1) + 1.0f);
@@ -28,24 +28,26 @@ public:
 	{
 		cutoff = 200;
 		q = 1;
-		for (int l0 = 0; (l0 < 3); l0 = (l0 + 1)) {
+		for (int l0 = 0; l0 < 3; l0++) {
 			fRec0[l0] = 0.0f;
 		}
 	}
 
 	virtual void set_param(int param_id, float val)
 	{
-		const float minCutoff = 20.F;
-		const float maxCutoff = 20000.F;
+		const float minCutoff = 20.f;
+		const float maxCutoff = 20000.f;
 		if (param_id == 0)
-			cutoff = map_value(val, 0.F, 1.F, minCutoff, maxCutoff);
+			cutoff = map_value(val, 0.f, 1.f, minCutoff, maxCutoff);
 		if (param_id == 1)
-			q = map_value(val, 0.F, 1.F, 0.2F, 40.F);
+			q = map_value(val, 0.f, 1.f, 0.2F, 40.f);
 	}
 	virtual void set_samplerate(float sr)
 	{
-		fConst0 = (3.14159274F / std::min<float>(192000.0F, std::max<float>(1.0F, float(sr))));
+		fConst0 = 3.14159274f / constrain(sr, 1.0f, 192000.0f);
 	}
+
+	//~LowPassFilter() {}
 
 private:
 	float cutoff;
