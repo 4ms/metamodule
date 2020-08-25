dcc9119463ad5a395e2a48f3b08db040f6039034
Dan Green <danngreen1@gmail.com>
2020-08-24 21:44:00 -0700

    Removed stdint.h include from audio_processor

--- src/processors/audio_processor.hh
+++ src/processors/audio_processor.hh
@@ -1,9 +1,8 @@
 #pragma once
-#include <stdint.h>
 
 struct AudioProcessor {
 	virtual float update(float in) = 0;
 	virtual void set_samplerate(float sr) = 0;
 	virtual void set_param(int param_id, float val) = 0;
+	//virtual ~AudioProcessor() = 0;
 };
-
