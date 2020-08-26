4181bd90b3fd7f912dd5cf8d817312fca8d2bf67
Ben Davis <bendedavis@gmail.com>
2020-08-26 16:04:01 -0700

    Added bit crusher effect

--- src/processors/bit_crush.h
+++ src/processors/bit_crush.h
@@ -1,9 +1,46 @@
 #pragma once
 #include "audio_processor.hh"
+#include "tools.h"
 
-struct BitCrusher : public AudioProcessor {
-	virtual float update(float in) { return 0;}
-	virtual void set_samplerate(float sr) {}
-	virtual void set_param(int param_id, float val) {}
+class BitCrusher : public AudioProcessor {
+public:
+	virtual float update(float input)
+	{
+		phaccu += reducedSampleRate / currentSampleRate;
+		if (phaccu > 1.0f) {
+
+			int quantizedVal = input * powf(2, bitDepth);
+			float bitReduced = (float)quantizedVal / (powf(2, bitDepth));
+
+			sampledOutput = bitReduced;
+
+			phaccu -= 1.0f;
+		}
+		return (sampledOutput);
+	}
+
+	BitCrusher()
+	{
+	}
+
+	virtual void set_param(int param_id, float val)
+	{
+		if (param_id == 0) {
+			reducedSampleRate = map_value(val, 0.0f, 1.0f, 1.0f, currentSampleRate / 2.0f);
+		}
+		if (param_id == 1) {
+			bitDepth = map_value(val, 0.0f, 1.0f, 1, 16);
+		}
+	}
+	virtual void set_samplerate(float sr)
+	{
+		currentSampleRate = sr;
+	}
+
+private:
+	float reducedSampleRate = 48000.0f;
+	int bitDepth = 16;
+	float phaccu = 0;
+	float currentSampleRate;
+	float sampledOutput;
 };
-
