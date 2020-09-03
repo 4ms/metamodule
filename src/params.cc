aec3b360ed65eb43c485b69d7db38c86b74355d7
Dan Green <danngreen1@gmail.com>
2020-09-02 21:43:43 -0700

    Params: testing rotary button by using it to advance mode LEDs
    Disabled touch pads

--- src/params.cc
+++ src/params.cc
@@ -1,21 +1,21 @@
-#include "ui.hh"
 #include "math.hh"
+#include "ui.hh"
 
-void Params::update() {
-    freq[0] = controls.freq1_cv.get() / 4095.0f;
-    freq[1] = controls.freq2_cv.get() / 4095.0f;
+void Params::update()
+{
+	freq[0] = controls.freq1_cv.get() / 4095.0f;
+	freq[1] = controls.freq2_cv.get() / 4095.0f;
 
-    res[0] = controls.res1_cv.get() / 4095.0f;
-    res[1] = controls.res2_cv.get() / 4095.0f;
+	res[0] = controls.res1_cv.get() / 4095.0f;
+	res[1] = controls.res2_cv.get() / 4095.0f;
 
-    if (controls.pads.just_touched(0)) {
-        mode[0] = wrap<5>(mode[0]+1);
-    }
-    if (controls.pads.just_touched(1)) {
-        mode[1] = wrap<5>(mode[1]+1);
-    }
+	//if (controls.pads.just_touched(0)) {
+	if (controls.rotary_button[0].is_just_released()) {
+		mode[0] = wrap<kNumFX>(mode[0] + 1);
+	}
+	// if (controls.pads.just_touched(1)) {
+	if (controls.rotary_button[1].is_just_released()) {
+		mode[1] = wrap<kNumFX>(mode[1] + 1);
+	}
 }
 
-void Params::read() {
-
-}
