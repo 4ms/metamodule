dd5076ce8a6bdc1aaa4c4efd3de4312e0221782f
Ben Davis <bendedavis@gmail.com>
2020-09-03 16:38:18 -0700

    Updated expo decay class

--- src/processors/tools.h
+++ src/processors/tools.h
@@ -26,52 +26,61 @@ using namespace MathTools;
 
 class ExpDecay {
 public:
-	float decayTime=10;
+	float decayTime = 10;
 
 	ExpDecay()
 	{
-		for (int i = 0; i < 2; i++) {
-			fRec0[i] = 0;
-		}
+		attackTime = 10;
 	}
 
 	virtual void set_samplerate(float sr)
 	{
-		fConst0 = 1.0f / constrain(sr, 1.0f, 192000.0f);
+		sampleRate = sr;
+	}
+
+	float update(float input)
+	{
+		lastSample = currentSample;
+		currentSample = input;
+
+		if (currentSample > lastSample) {
+			direction = 1;
+		}
+		else if (currentSample < lastSample) {
+			direction = 0;
+		}
+
+		if (direction == 0)
+			slewedOut += (input - slewedOut) * (1000.0f / decayTime / sampleRate);
+		else {
+			slewedOut += (input - slewedOut) * (1000.0f / attackTime / sampleRate);
+		}
+
+		return (slewedOut);
+	}
+
+private:
+	float sampleRate;
+	float slewedOut = 0;
+	int direction;
+	float lastSample = 0;
+	float currentSample = 0;
+	float attackTime;
+};
+
+class CubicNonlinearDist {
+public:
+	CubicNonlinearDist()
+	{
 	}
 
 	float update(float input)
 	{
 		float output = 0;
-		float fSlow0 = (0.00100000005f * float(decayTime));
-		int iSlow1 = (fabsf(fSlow0) < 0.00000001f); //1.1920929e-07f);
-		float fSlow2 = (iSlow1 ? 0.0f : expf((0.0f - (fConst0 / (iSlow1 ? 1.0f : fSlow0)))));
-		float fSlow3 = (1.0f - fSlow2);
-		float fTemp0 = fabsf(input);
-		fRec0[0] = fmax(fTemp0, ((fRec0[1] * fSlow2) + (fTemp0 * fSlow3)));
-		output = fRec0[0];
-		fRec0[1] = fRec0[0];
+		float fTemp0 = constrain(input, -1.0f, 1.0f);
+		output = fTemp0 * (1.0f - (0.333333343f * fTemp0 * fTemp0));
 		return output;
 	}
 
-private:
-	float fConst0;
-	float fRec0[2];
-};
-
-class CubicNonlinearDist {
-public:
-CubicNonlinearDist()
-{
-
-}
-
-float update(float input)
-{
-	float output =0;
-	float fTemp0 = constrain(input,-1.0f,1.0f);
-			output = fTemp0 * (1.0f - (0.333333343f * fTemp0*fTemp0));
-			return output;
-}
 private:
 };
