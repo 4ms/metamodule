94dab3ff02dc31aac937effd22b8ac4fc7fe0188
Dan Green <danngreen1@gmail.com>
2020-09-02 21:43:16 -0700

    Controls: rotary_button is a DebouncedSwitch. Updates in Control::read()

--- src/controls.cc
+++ src/controls.cc
@@ -1,6 +1,6 @@
-#include "stm32f7xx_ll_dma.h"
 #include "controls.hh"
 #include "debug.hh"
+#include "stm32f7xx_ll_dma.h"
 
 //"Thread" Controls::read()
 //Read CV jacks ---> update oversampler
@@ -11,50 +11,48 @@
 
 void Controls::read()
 {
-    Debug::set_0(true);
-    
-    freq1_cv.read();
-    freq2_cv.read();
-    res1_cv.read();
-    res2_cv.read();
-    pads.check_alert_received();
+	Debug::set_0(true);
+
+	freq1_cv.read();
+	freq2_cv.read();
+	res1_cv.read();
+	res2_cv.read();
+	pads.check_alert_received();
+
+	rotary_button[0].update();
+	rotary_button[1].update();
 
 	Debug::set_0(false);
 
-    //read_rotary
-    // Controls::rotary_turn[0] = read_rotary(0);
-    //todo: use queue, from SWN
+	//read_rotary
+	// rotary[0].read();
+}
 
-    //read_button
-    //Todo: same as SEG
-    // uint32_t new_state = get_button_state(0);
-    // if (Controls::rotary_button[0].state && !new_state)
-    // {
-    //     rotary_button[0].just_pressed = 1;
-    //     //..
-    // }
+void Controls::begin()
+{
+	pads.begin();
 }
 
 Controls::Controls()
 {
-    //Todo: Think of a more elegant solution than to either 
-    //   1) call the start_dma() method of just one ADC channel per ADC periph,
-    //or 2) call every ADC channels' start_dma() method but make AdcPeriph::start_dma() static so it can check if it's already been started
-    //or 3) start the dma with the AdcPeriph<ADC_1>::start_dma(...) format
+	//Todo: Think of a more elegant solution than to either
+	//   1) call the start_dma() method of just one ADC channel per ADC periph,
+	//or 2) call every ADC channels' start_dma() method but make AdcPeriph::start_dma() static so it can check if it's already been started
+	//or 3) start the dma with the AdcPeriph<ADC_1>::start_dma(...) format
 
-    freq1_cv.start_dma(DMA2, LL_DMA_STREAM_4, LL_DMA_CHANNEL_0, DMA2_Stream4_IRQn);
-    // AdcPeriph<ADC_1>::start_dma(DMA2, LL_DMA_STREAM_4, LL_DMA_CHANNEL_0, DMA2_Stream4_IRQn);
+	freq1_cv.start_dma(DMA2, LL_DMA_STREAM_4, LL_DMA_CHANNEL_0, DMA2_Stream4_IRQn);
+	// AdcPeriph<ADC_1>::start_dma(DMA2, LL_DMA_STREAM_4, LL_DMA_CHANNEL_0, DMA2_Stream4_IRQn);
 }
 
 //every 11.6us (86.2kHz), ~400ns
 extern "C" void DMA2_Stream4_IRQHandler(void)
 {
-    if (LL_DMA_IsActiveFlag_TC4(DMA2)) {
-        LL_DMA_ClearFlag_TC4(DMA2);
-        Controls::read();
-    }
-    if (LL_DMA_IsActiveFlag_TE4(DMA2)) {
-        LL_DMA_ClearFlag_TE4(DMA2);
-        //Todo: Handle ADC DMA error
-    }
+	if (LL_DMA_IsActiveFlag_TC4(DMA2)) {
+		LL_DMA_ClearFlag_TC4(DMA2);
+		Controls::read();
+	}
+	if (LL_DMA_IsActiveFlag_TE4(DMA2)) {
+		LL_DMA_ClearFlag_TE4(DMA2);
+		//Todo: Handle ADC DMA error
+	}
 }
