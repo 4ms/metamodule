7031956d360efb2c7d047ead9762a3f9a25eb459
Dan Green <danngreen1@gmail.com>
2020-09-04 16:10:45 -0700

    Made jack sense objects into arrays

--- src/controls.hh
+++ src/controls.hh
@@ -21,12 +21,11 @@ struct Controls {
 	static inline AnalogIn<ADC_1, AdcChanNum::_12> freq2_cv{GPIO::C, 2};
 	static inline AnalogIn<ADC_1, AdcChanNum::_13> res2_cv{GPIO::C, 3};
 
-	static inline JackSense freq2_sense{GPIO::C, 14};
-	static inline JackSense res2_sense{GPIO::C, 4};
-	static inline JackSense in1_sense{GPIO::C, 13};
-	static inline JackSense in2_sense{GPIO::C, 15};
+	static inline JackSense freq_sense[2] = {{GPIO::C, 14}, {GPIO::C, 14}}; //FixMe: only channel 2 is on p1
+	static inline JackSense res_sense[2] = {{GPIO::C, 4}, {GPIO::C, 4}};
+	static inline JackSense in_sense[2] = {{GPIO::C, 13}, {GPIO::C, 15}};
 
-	static inline TouchCtl pads;
+    static inline TouchCtl pads;
 	static inline int32_t rotary_turn[2];
 	static inline DebouncedSwitch rotary_button[2] = {
 		{GPIO::D, 2},
