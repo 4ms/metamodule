e524bdb9bd12d01cbfe8d9d81f88ba1dc439602b
Dan Green <danngreen1@gmail.com>
2020-09-08 22:22:24 -0700

    Separated RgbLed from PwmLed
    Formatted tim_pwm.hh

--- src/drivers/tim_pwm.hh
+++ src/drivers/tim_pwm.hh
@@ -1,30 +1,28 @@
 #pragma once
-#include <stm32f7xx.h>
-#include "system.hh"
-#include "colors.hh"
 #include "pin.hh"
 #include "stm32f7xx_ll_tim.h"
 
 enum class TimChannelNum {
-    _1  = LL_TIM_CHANNEL_CH1,
-    _1N = LL_TIM_CHANNEL_CH1N,
-    _2  = LL_TIM_CHANNEL_CH2,
-    _2N = LL_TIM_CHANNEL_CH2N,
-    _3  = LL_TIM_CHANNEL_CH3,
-    _3N = LL_TIM_CHANNEL_CH3N,
-    _4  = LL_TIM_CHANNEL_CH4,
-    _5  = LL_TIM_CHANNEL_CH5,
-    _6  = LL_TIM_CHANNEL_CH6
+	_1 = LL_TIM_CHANNEL_CH1,
+	_1N = LL_TIM_CHANNEL_CH1N,
+	_2 = LL_TIM_CHANNEL_CH2,
+	_2N = LL_TIM_CHANNEL_CH2N,
+	_3 = LL_TIM_CHANNEL_CH3,
+	_3N = LL_TIM_CHANNEL_CH3N,
+	_4 = LL_TIM_CHANNEL_CH4,
+	_5 = LL_TIM_CHANNEL_CH5,
+	_6 = LL_TIM_CHANNEL_CH6
 };
 
 class TimPwmPeriph {
-friend class TimPwmChannel;
+	friend class TimPwmChannel;
+
 private:
-    static void init_periph_once(TIM_TypeDef *TIM, uint32_t period=256, uint16_t prescaler=0, uint32_t clock_division=0);
+	static void init_periph_once(TIM_TypeDef *TIM, uint32_t period = 256, uint16_t prescaler = 0, uint32_t clock_division = 0);
 };
 
-// TimPwmChannel: output PWM on a pin
-// automatically initializes the given TIM peripheral if it hasn't been initialized yet
+// TimPwmChannel: output PWM on a pin.
+// Automatically initializes the given TIM peripheral if it hasn't been initialized yet
 // Usage: (default period of timer is 256)
 //      TimPwmChannel myPWM {TIM2, TimChannelNum::_2, GPIO_PIN_3, GPIOB, LL_GPIO_AF_1};
 //      // output starts immediately
@@ -41,81 +39,88 @@ private:
 // myPWMOutput.set_periph_clock_division(0);
 // myPWMOutput.init_periph();
 // myPWMOutput.start_output();
-// myPWMOutput.stop_output();t
+// myPWMOutput.stop_output();
 //Todo: add separate methods for setting pin, TIM, channel, period, prescaler, clock_div
 
 class TimPwmChannel {
 public:
-    TimPwmChannel(  TIM_TypeDef* const TIM,
-                    TimChannelNum const channel,
-                    uint32_t period=256,
-                    uint16_t prescaler=1, 
-                    uint32_t clock_division=0)
-    : TIM_(TIM),
-      channel_(channel)
-    {
-        TimPwmPeriph::init_periph_once(TIM, period, prescaler, clock_division);
+	TimPwmChannel(TIM_TypeDef *const TIM,
+				  TimChannelNum const channel,
+				  uint32_t period = 256,
+				  uint16_t prescaler = 1,
+				  uint32_t clock_division = 0)
+		: TIM_(TIM)
+		, channel_(channel)
+	{
+		TimPwmPeriph::init_periph_once(TIM, period, prescaler, clock_division);
 
-        bool inverted_channel;
-        if (channel_==TimChannelNum::_1N) {
-            channel_base_ = TimChannelNum::_1;
-            inverted_channel = true;
-        }
-        else if (channel_== TimChannelNum::_2N) {
-            channel_base_ = TimChannelNum::_2;
-            inverted_channel = true;
-        }
-        else if (channel_== TimChannelNum::_3N) {
-            channel_base_ = TimChannelNum::_3;
-            inverted_channel = true;
-        }
-        else {
-            channel_base_ = channel_;
-            inverted_channel = false;
-        }
+		bool inverted_channel;
+		if (channel_ == TimChannelNum::_1N) {
+			channel_base_ = TimChannelNum::_1;
+			inverted_channel = true;
+		}
+		else if (channel_ == TimChannelNum::_2N) {
+			channel_base_ = TimChannelNum::_2;
+			inverted_channel = true;
+		}
+		else if (channel_ == TimChannelNum::_3N) {
+			channel_base_ = TimChannelNum::_3;
+			inverted_channel = true;
+		}
+		else {
+			channel_base_ = channel_;
+			inverted_channel = false;
+		}
 
-        LL_TIM_OC_InitTypeDef tim_oc = {
-            .OCMode = LL_TIM_OCMODE_PWM1,
-            .OCState = inverted_channel ? LL_TIM_OCSTATE_DISABLE : LL_TIM_OCSTATE_ENABLE,
-            .OCNState = inverted_channel ? LL_TIM_OCSTATE_ENABLE : LL_TIM_OCSTATE_DISABLE,
-            .CompareValue = 0,
-            .OCPolarity = LL_TIM_OCPOLARITY_LOW,
-            .OCNPolarity = LL_TIM_OCPOLARITY_LOW,
-            .OCIdleState = LL_TIM_OCIDLESTATE_LOW,
-            .OCNIdleState = LL_TIM_OCIDLESTATE_LOW,
-        };
+		LL_TIM_OC_InitTypeDef tim_oc = {
+			.OCMode = LL_TIM_OCMODE_PWM1,
+			.OCState = inverted_channel ? LL_TIM_OCSTATE_DISABLE : LL_TIM_OCSTATE_ENABLE,
+			.OCNState = inverted_channel ? LL_TIM_OCSTATE_ENABLE : LL_TIM_OCSTATE_DISABLE,
+			.CompareValue = 0,
+			.OCPolarity = LL_TIM_OCPOLARITY_LOW,
+			.OCNPolarity = LL_TIM_OCPOLARITY_LOW,
+			.OCIdleState = LL_TIM_OCIDLESTATE_LOW,
+			.OCNIdleState = LL_TIM_OCIDLESTATE_LOW,
+		};
 
-        LL_TIM_OC_Init(TIM_, static_cast<uint32_t>(channel_base_), &tim_oc);
-        LL_TIM_CC_EnableChannel(TIM_, static_cast<uint32_t>(channel_));
-        LL_TIM_EnableCounter(TIM_);
-    }
+		LL_TIM_OC_Init(TIM_, static_cast<uint32_t>(channel_base_), &tim_oc);
+		LL_TIM_CC_EnableChannel(TIM_, static_cast<uint32_t>(channel_));
+		LL_TIM_EnableCounter(TIM_);
+	}
 
-    constexpr void set_output_level(uint32_t val) const {
-        _set_timer_ccr(TIM_, channel_base_, val);
-    }
-    void start_output() const {
-        LL_TIM_CC_EnableChannel(TIM_, static_cast<uint32_t>(channel_));
-    }
-    void stop_output() const {
-        LL_TIM_CC_DisableChannel(TIM_, static_cast<uint32_t>(channel_));
-    }
+	constexpr void set_output_level(uint32_t val) const
+	{
+		_set_timer_ccr(TIM_, channel_base_, val);
+	}
+	void start_output() const
+	{
+		LL_TIM_CC_EnableChannel(TIM_, static_cast<uint32_t>(channel_));
+	}
+	void stop_output() const
+	{
+		LL_TIM_CC_DisableChannel(TIM_, static_cast<uint32_t>(channel_));
+	}
 
 protected:
-    TimPwmChannel() : TIM_(nullptr) {}
+	TimPwmChannel()
+		: TIM_(nullptr)
+	{
+	}
 
-private: 
-    constexpr void _set_timer_ccr(TIM_TypeDef *TIMx, TimChannelNum channel, uint32_t val) const {
-        switch (channel) {
-            case TimChannelNum::_1: TIMx->CCR1 = val; break;
-            case TimChannelNum::_2: TIMx->CCR2 = val; break;
-            case TimChannelNum::_3: TIMx->CCR3 = val; break;
-            case TimChannelNum::_4: TIMx->CCR4 = val; break;
-            case TimChannelNum::_5: TIMx->CCR5 = val; break;
-            default:            TIMx->CCR6 = val; break;
-        }
-    }
+private:
+	constexpr void _set_timer_ccr(TIM_TypeDef *TIMx, TimChannelNum channel, uint32_t val) const
+	{
+		switch (channel) {
+			case TimChannelNum::_1: TIMx->CCR1 = val; break;
+			case TimChannelNum::_2: TIMx->CCR2 = val; break;
+			case TimChannelNum::_3: TIMx->CCR3 = val; break;
+			case TimChannelNum::_4: TIMx->CCR4 = val; break;
+			case TimChannelNum::_5: TIMx->CCR5 = val; break;
+			default: TIMx->CCR6 = val; break;
+		}
+	}
 
-    TIM_TypeDef* const TIM_;
-    TimChannelNum channel_;
-    TimChannelNum channel_base_;
+	TIM_TypeDef *const TIM_;
+	TimChannelNum channel_;
+	TimChannelNum channel_base_;
 };
--- src/leds.hh
+++ src/leds.hh
@@ -1,13 +1,13 @@
 #pragma once
 #include "colors.hh"
-#include "oscs.hh"
 #include "pin.hh"
+#include "rgbled.hh"
+#include "stm32f7xx.h"
 #include "tim_pwm.hh"
-#include <stm32f7xx.h>
 
-class NoLed : public TimPwmChannel {
+class NoPwmLed : public TimPwmChannel {
 public:
-	NoLed() {}
+	NoPwmLed() {}
 	void set_output_level(uint32_t val) const {}
 };
 
@@ -23,87 +23,9 @@ private:
 	uint8_t dac_chan_;
 };
 
-struct PwmRgbLed {
-	PwmRgbLed(TimPwmChannel r_LED, TimPwmChannel g_LED, TimPwmChannel b_LED)
-		: r_(r_LED)
-		, g_(g_LED)
-		, b_(b_LED)
-	{
-	}
-
-	void set_background(Color const &col) { background_color_ = col; }
-	void blend_background(Color const col)
-	{
-		background_color_ = background_color_.blend(col);
-	}
-	void add_background(Color const col)
-	{
-		background_color_ = background_color_ + col;
-	}
-	//void set_solid(Color const &col) { solid_color_ = col; }
-
-	void flash(Color const &c, uint32_t const flash_freq = 100)
-	{
-		flash_color_ = c;
-		flash_phase_ = 0xFFFFFFFF;
-		flash_freq_ = flash_freq; // divided by update_rate?
-	}
-
-	// freq in secs
-	void set_glow(Color const &c, uint32_t const freq = 0x00FFFFFF)
-	{
-		glow_color_ = c;
-		osc_.set_frequency(freq); //divided by update_rate?
-	}
-
-	constexpr void set_color(Color const &col) const
-	{
-		r_.set_output_level(col.red());
-		g_.set_output_level(col.green());
-		b_.set_output_level(col.blue());
-	}
-	void set_color(Color const &col, uint8_t const brightness) const
-	{
-		r_.set_output_level((col.red() * brightness) >> 8);
-		g_.set_output_level((col.green() * brightness) >> 8);
-		b_.set_output_level((col.blue() * brightness) >> 8);
-	}
-	void set_color(Color const &col, float const brightness) const
-	{
-		r_.set_output_level(col.red() * brightness);
-		g_.set_output_level(col.green() * brightness);
-		b_.set_output_level(col.blue() * brightness);
-	}
-
-	//Todo: don't waste cycles updating if nothing's changed
-	void refresh()
-	{
-		Color c = background_color_;
-		// if (solid_color_ != Colors::off) c = solid_color_;
-		// c = c.blend(glow_color_, osc_.Process());
-		// c = c.blend(flash_color_, flash_phase_);
-		// c = c.adjust(color_cal_);
-		set_color(c);
-		if (flash_phase_ > flash_freq_)
-			flash_phase_ -= flash_freq_;
-		else
-			flash_phase_ = 0;
-	}
-
-private:
-	TimPwmChannel const r_, g_, b_;
-	TriangleOscillator<89478> osc_;
-	Color background_color_ = Colors::off;
-	Color solid_color_ = Colors::off;
-	Color flash_color_ = Colors::white;
-	Color glow_color_ = Colors::red;
-	uint32_t flash_freq_ = 100;
-	uint32_t flash_phase_ = 0;
-	//    Color::Adjustment& color_cal_;
-};
+using PwmRgbLed = RgbLed<TimPwmChannel>;
 
 class LedCtl {
-
 public:
 	LedCtl()
 	{
@@ -144,46 +66,44 @@ public:
 public:
 	PwmRgbLed freq[2] = {
 		{{TIM8, TimChannelNum::_4},
-		{TIM8, TimChannelNum::_3},
-		{TIM2, TimChannelNum::_2}},
+		 {TIM8, TimChannelNum::_3},
+		 {TIM2, TimChannelNum::_2}},
 		{NoLedElement,
-		{TIM3, TimChannelNum::_1},
-		{TIM8, TimChannelNum::_1N}}
-	};
+		 {TIM3, TimChannelNum::_1},
+		 {TIM8, TimChannelNum::_1N}}};
 
 	PwmRgbLed res[2] = {
 		{{TIM1, TimChannelNum::_4},
-		{TIM1, TimChannelNum::_3},
-		{TIM3, TimChannelNum::_2}},
+		 {TIM1, TimChannelNum::_3},
+		 {TIM3, TimChannelNum::_2}},
 
 		{NoLedElement,
-		{TIM3, TimChannelNum::_3},
-		{TIM3, TimChannelNum::_4}}
-	};
+		 {TIM3, TimChannelNum::_3},
+		 {TIM3, TimChannelNum::_4}}};
 
 	PwmRgbLed mode[5]{
 		{NoLedElement, //DAC0 PA4
-		NoLedElement,
-		NoLedElement}, //DAC1 PA5
+		 NoLedElement,
+		 NoLedElement}, //DAC1 PA5
 
 		{{TIM2, TimChannelNum::_3},
-		NoLedElement,
-		{TIM2, TimChannelNum::_4}},
+		 NoLedElement,
+		 {TIM2, TimChannelNum::_4}},
 
 		{{TIM2, TimChannelNum::_1},
-		NoLedElement,
-		{TIM5, TimChannelNum::_2}},
+		 NoLedElement,
+		 {TIM5, TimChannelNum::_2}},
 
 		{{TIM4, TimChannelNum::_3},
-		NoLedElement,
-		{TIM4, TimChannelNum::_4}},
+		 NoLedElement,
+		 {TIM4, TimChannelNum::_4}},
 
 		{{TIM4, TimChannelNum::_1},
-		NoLedElement,
-		{TIM4, TimChannelNum::_2}},
+		 NoLedElement,
+		 {TIM4, TimChannelNum::_2}},
 	};
 
 private:
-	NoLed NoLedElement;
+	NoPwmLed NoLedElement;
 };
 
