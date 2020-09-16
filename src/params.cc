cbf4184c6140815a9fe1fb22a4e95d831d7f15af
Dan Green <danngreen1@gmail.com>
2020-09-16 11:35:03 -0700

    Added HAL SAI files

--- src/params.cc
+++ src/params.cc
@@ -14,7 +14,6 @@ void Params::update()
 		freq[i] = constrain((freq_knob_pos[i] / kFreqScaling) + (controls.freq_cv[i].get() / 4095.0f), 0.f, 1.f);
 		res[i] = constrain((res_knob_pos[i] / kResScaling) + (controls.res_cv[i].get() / 4095.0f), 0.f, 1.f);
 
-		//if (controls.pads.just_touched(i)) {
 		if (controls.rotary_button[i].is_just_released()) {
 			mode[i] = wrap<kNumFX>(mode[i] + 1);
 		}
