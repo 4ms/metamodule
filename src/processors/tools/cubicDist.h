e1c7fc4678271f428ab61b578f285f97be0fef98
Ben Davis <bendedavis@gmail.com>
2020-09-17 15:19:21 -0700

    Moved cubic distortion class to processor tools

---
+++ src/processors/tools/cubicDist.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class CubicNonlinearDist {
+public:
+	float update(float input)
+	{
+		float output = 0;
+		float fTemp0 = constrain(input, -1.0f, 1.0f);
+		output = fTemp0 * (1.0f - (0.333333343f * fTemp0 * fTemp0));
+		return output;
+	}
+};
