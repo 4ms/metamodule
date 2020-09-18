5a5c091454ed1bc09a122e49d789fef7fd555f3d
Ben Davis <bendedavis@gmail.com>
2020-09-18 13:36:00 -0700

    Delay line added, phaser test

--- src/processors/phaser.h
+++ src/processors/phaser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "audio_processor.hh"
+#include "math.hh"
 #include <cmath>
 
 using namespace MathTools;
@@ -9,26 +10,26 @@ class Phaser : public AudioProcessor {
 public:
 	virtual float update(float input)
 	{
-		float output = 0;
+		//output = interpolate(input, delay->update(input), 0.5f);
+		float output = input;
 		return output;
 	}
 
 	Phaser()
 	{
+		//delay = new DelayLine(1000);
 	}
 
 	virtual void set_param(int param_id, float val)
 	{
-		if (param_id == 0)
-			//
-			if (param_id == 1)
-		//
+		if (param_id == 0) {}
+		if (param_id == 1) {}
 	}
 	virtual void set_samplerate(float sr)
 	{
+		//delay->set_samplerate(sr);
 	}
 
-	//~LowPassFilter() {}
-
 private:
+	//DelayLine *delay;
 };
--- src/processors/tools/delayLine.h
+++ src/processors/tools/delayLine.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "math.hh"
+
 class DelayLine {
 public:
+	float delayTimeMS = 500;
+
 	DelayLine(float maxTimeMS)
 	{
-		maxSamples = (maxTimeMS / 1000.0f) * sampleRate;
-		delayBuffer = new float[]
+		maxSamples = ((maxTimeMS / 1000.0f) * sampleRate) + 1;
+		delayBuffer = new float[maxSamples];
 	}
 
 	virtual void set_samplerate(float sr)
@@ -15,10 +19,28 @@ public:
 
 	float update(float input)
 	{
+		delayBuffer[writeIndex] = input;
+		readIndex = writeIndex - ((delayTimeMS / 1000.0f) * sampleRate);
+		if (readIndex < 0)
+			readIndex += maxSamples;
+		read1 = readIndex;
+		read2 = (read1 + 1) % maxSamples;
+		float readFraction = readIndex - read1;
+		output = interpolate(read1, read2, readFraction);
+		writeIndex++;
+		writeIndex %= maxSamples;
+		return (output);
 	}
 
 private:
 	int maxSamples;
 	float *delayBuffer;
-	float sampleRate;
+	float sampleRate = 44100;
+
+	float output = 0;
+
+	unsigned long writeIndex;
+	float readIndex;
+	unsigned long read1;
+	unsigned long read2;
 };
