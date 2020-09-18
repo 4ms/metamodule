d5efb74f1986b1af7e328e3cc895a92ef5907d41
Dan Green <danngreen1@gmail.com>
2020-09-17 21:41:43 -0700

    Timekeeper is init/constructed with a config struct
    is_running flag created

--- src/drivers/timekeeper.cc
+++ src/drivers/timekeeper.cc
@@ -3,51 +3,50 @@
 #include "stm32xx.h"
 
 Timekeeper::Timekeeper()
+	: is_running(false)
 {}
 
-void Timekeeper::init(TIM_TypeDef *TIMx,
-					  uint32_t period_us,
-					  std::function<void(void)> func,
-					  uint32_t priority1,
-					  uint32_t priority2)
+void Timekeeper::init(const TimekeeperConfig &config, std::function<void(void)> func)
 {
-	timx = TIMx;
-	irqn = System::tim_periph_to_IRQn(timx);
+	set_periph(config.TIMx);
 	task_func = func;
 	register_task(func);
 	System::enable_tim_rcc(timx);
-	set_timing(period_us, priority1, priority2);
+	set_timing(config.period_ns, config.priority1, config.priority2);
 }
 
-Timekeeper::Timekeeper(TIM_TypeDef *TIMx,
-					   uint32_t period_us,
-					   std::function<void(void)> func,
-					   uint32_t priority1,
-					   uint32_t priority2)
-	: timx(TIMx)
+Timekeeper::Timekeeper(const TimekeeperConfig &config, std::function<void(void)> func)
+	: timx(config.TIMx)
 	, irqn(System::tim_periph_to_IRQn(timx))
 	, task_func(func)
+	, is_running(false)
 {
 	register_task(func);
 	System::enable_tim_rcc(timx);
-	set_timing(period_us, priority1, priority2);
+	set_timing(config.period_ns, config.priority1, config.priority2);
 }
 
 void Timekeeper::start()
 {
-	HAL_NVIC_EnableIRQ(irqn);
+	is_running = true;
 }
 
 void Timekeeper::stop()
 {
-	HAL_NVIC_DisableIRQ(irqn);
+	is_running = false;
 }
 
-void Timekeeper::set_timing(uint32_t period_us, uint32_t priority1, uint32_t priority2)
+void Timekeeper::set_periph(TIM_TypeDef *TIMx)
+{
+	timx = TIMx;
+	irqn = System::tim_periph_to_IRQn(timx);
+}
+
+void Timekeeper::set_timing(uint32_t period_ns, uint32_t priority1, uint32_t priority2)
 {
 	uint32_t sysfreq_Hz = System::tim_periph_max_freq(timx);
-	float sysfreq_us = 1000000.f / sysfreq_Hz;
-	uint32_t period_clocks = period_us / sysfreq_us;
+	float sysfreq_ns = 1000000000.f / sysfreq_Hz;
+	uint32_t period_clocks = period_ns / sysfreq_ns;
 
 	uint32_t prescaler = 0;
 	while ((period_clocks / (prescaler + 1)) > 65535) {
@@ -67,6 +66,9 @@ void Timekeeper::set_timing(uint32_t period_us, uint32_t priority1, uint32_t pri
 		clock_division = 255;
 	}
 
+	HAL_NVIC_SetPriority(irqn, priority1, priority2);
+	HAL_NVIC_EnableIRQ(irqn);
+
 	TIM_HandleTypeDef tim;
 	tim.Instance = timx;
 	tim.Init.Period = period_clocks;
@@ -76,9 +78,7 @@ void Timekeeper::set_timing(uint32_t period_us, uint32_t priority1, uint32_t pri
 	tim.Init.RepetitionCounter = 0;
 	tim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 
-	HAL_NVIC_SetPriority(irqn, priority1, priority2);
 	HAL_TIM_Base_Init(&tim);
-
 	HAL_TIM_Base_Start_IT(&tim);
 }
 
@@ -90,12 +90,25 @@ void Timekeeper::register_task(std::function<void(void)> func)
 		[this]() {
 			if (tim_update_IT_is_set()) {
 				if (tim_update_IT_is_source()) {
-					if (task_func)
+					if (is_running && task_func) //Todo: what is the cost of std::function operator bool
 						task_func();
 				}
 				tim_update_IT_clear();
 			}
 		});
+}
+
+bool Timekeeper::tim_update_IT_is_set() const
+{
+	return (timx->SR & TIM_IT_UPDATE) == TIM_IT_UPDATE;
+}
+bool Timekeeper::tim_update_IT_is_source() const
+{
+	return (timx->DIER & TIM_IT_UPDATE) == TIM_IT_UPDATE;
+}
+void Timekeeper::tim_update_IT_clear() const
+{
+	timx->SR &= ~(TIM_IT_UPDATE);
 }
 
 //Todo: allows us to double-up on the IRQs that have two TIM UP (TIM1/TIM10, and TIM8/TIM13 share an ISR)
--- src/drivers/timekeeper.hh
+++ src/drivers/timekeeper.hh
@@ -2,15 +2,18 @@
 #include "system.hh"
 #include <functional>
 
+struct TimekeeperConfig {
+	TIM_TypeDef *TIMx;
+	uint32_t period_ns;
+	uint32_t priority1;
+	uint32_t priority2;
+};
+
 class Timekeeper {
 public:
-	Timekeeper(TIM_TypeDef *TIMx, uint32_t period_us, std::function<void(void)> func, uint32_t priority1 = 1, uint32_t priority2 = 1);
-
 	Timekeeper();
-	void init(TIM_TypeDef *TIMx, uint32_t period_us, std::function<void(void)> func, uint32_t priority1 = 1, uint32_t priority2 = 1);
-
-	void set_timing(uint32_t period_us, uint32_t priority1 = 1, uint32_t priority2 = 1);
-	void register_task(std::function<void(void)> func);
+	Timekeeper(const TimekeeperConfig &config, std::function<void(void)> func);
+	void init(const TimekeeperConfig &config, std::function<void(void)> func);
 
 	void start();
 	void stop();
@@ -18,19 +21,15 @@ public:
 private:
 	TIM_TypeDef *timx;
 	IRQn_Type irqn;
+	bool is_running;
 	std::function<void(void)> task_func;
 
-	bool tim_update_IT_is_set()
-	{
-		return (timx->SR & TIM_IT_UPDATE) == TIM_IT_UPDATE;
-	}
-	bool tim_update_IT_is_source()
-	{
-		return (timx->DIER & TIM_IT_UPDATE) == TIM_IT_UPDATE;
-	}
-	void tim_update_IT_clear()
-	{
-		timx->SR &= ~(TIM_IT_UPDATE);
-	}
+	void set_periph(TIM_TypeDef *timx);
+	void set_timing(uint32_t period_ns, uint32_t priority1 = 3, uint32_t priority2 = 3);
+	void register_task(std::function<void(void)> func);
+
+	bool tim_update_IT_is_set() const;
+	bool tim_update_IT_is_source() const;
+	void tim_update_IT_clear() const;
 };
 
