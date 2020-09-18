fad53ede5296c2cd158d89a11d62340a78259b90
Dan Green <danngreen1@gmail.com>
2020-09-17 21:53:49 -0700

    Formatted pca9685 driver

--- src/drivers/pca9685_led_driver.cc
+++ src/drivers/pca9685_led_driver.cc
@@ -5,10 +5,9 @@ PCA9685Driver::PCA9685Driver(I2CPeriph &i2c, const DMAConfig &dma_defs)
 	, num_chips_(kNumLedDriverChips)
 	, dma_defs_(dma_defs)
 	, dma_(*this)
-{
-}
+{}
 
-//Reset chip registers
+// Reset chip registers
 LEDDriverError PCA9685Driver::start()
 {
 	uint8_t driverAddr;
@@ -18,7 +17,7 @@ LEDDriverError PCA9685Driver::start()
 		err = reset_chip(driverAddr);
 		if (err != LEDDriverError::None)
 			return (err);
-		//Todo return failed driver#: | ((driverAddr+1)<<4));
+		// Todo return failed driver#: | ((driverAddr+1)<<4));
 	}
 
 	return LEDDriverError::None;
@@ -28,14 +27,15 @@ LEDDriverError PCA9685Driver::start()
 // (not implemented in I2CPeriph yet)
 void PCA9685Driver::start_it_mode()
 {
-	//Todo: check if start() has been called, and call it if not
+	// Todo: check if start() has been called, and call it if not
 	i2cp_.enable_IT(2, 2);
 }
 
-// Start transferring via DMA, given a frame buffer and the hardware configuration
+// Start transferring via DMA, given a frame buffer and the hardware
+// configuration
 LEDDriverError PCA9685Driver::start_dma_mode()
 {
-	//Todo: check if start() has been called, and call it if not
+	// Todo: check if start() has been called, and call it if not
 	start_it_mode();
 	return dma_.start_dma(dma_defs_);
 }
@@ -50,7 +50,7 @@ LEDDriverError PCA9685Driver::start_dma_mode()
 LEDDriverError PCA9685Driver::set_single_led(uint8_t led_element_number, uint16_t brightness)
 {
 	uint8_t driver_addr;
-	uint8_t data[5]; //2 bytes for on time + 2 bytes for off time + 1 byte for LED address
+	uint8_t data[5];
 
 	if (led_element_number >= (num_chips_ * 16))
 		return LEDDriverError::BAD_LED_PARAM;
@@ -60,18 +60,18 @@ LEDDriverError PCA9685Driver::set_single_led(uint8_t led_element_number, uint16_
 
 	driver_addr = I2C_BASE_ADDRESS | (driver_addr << 1);
 
-	data[0] = REG_LED0 + (led_element_number * 4); //4 registers per LED element
-	data[1] = 0;								   //on-time = 0
+	uint8_t registers_per_led = 4;
+	data[0] = REG_LED0 + (led_element_number * registers_per_led);
+	data[1] = 0; // on-time = 0
 	data[2] = 0;
-	data[3] = brightness & 0xFF; //off-time = brightness
+	data[3] = brightness & 0xFF; // off-time = brightness
 	data[4] = brightness >> 8;
 
-	//Todo: write_IT()
+	// Todo: write_IT()
 	auto err = i2cp_.write(driver_addr, data, 5);
 
-	return (err == I2CPeriph::Error::I2C_NO_ERR)
-			   ? LEDDriverError::None
-			   : LEDDriverError::SET_LED_ERR;
+	return (err == I2CPeriph::Error::I2C_NO_ERR) ? LEDDriverError::None
+												 : LEDDriverError::SET_LED_ERR;
 }
 
 // Sets color of an RGB LED
@@ -85,7 +85,8 @@ LEDDriverError PCA9685Driver::set_single_led(uint8_t led_element_number, uint16_
 // led_number==9 refers to PWM pins 12,13,14 of PCA9685 chip with address 1
 //
 // Note: This function cannot access pin 15
-LEDDriverError PCA9685Driver::set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue)
+LEDDriverError
+PCA9685Driver::set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue)
 {
 	uint8_t driverAddr;
 	uint8_t data[13];
@@ -96,31 +97,31 @@ LEDDriverError PCA9685Driver::set_rgb_led(uint8_t led_number, uint16_t c_red, ui
 	driverAddr = get_chip_num(led_number);
 	led_number = led_number - (driverAddr * 5);
 
-	//12 registers per LED (4 registers per LED element) = 12*16 registers per driver
+	// 12 registers per LED (4 registers per LED element) = 12*16 registers per
+	// driver
 	data[0] = REG_LED0 + (led_number * 12);
-	data[1] = 0; //on-time = 0
+	data[1] = 0; // on-time = 0
 	data[2] = 0;
-	data[3] = c_red & 0xFF; //off-time = brightness
+	data[3] = c_red & 0xFF; // off-time = brightness
 	data[4] = c_red >> 8;
 
-	data[5] = 0; //on-time = 0
+	data[5] = 0; // on-time = 0
 	data[6] = 0;
-	data[7] = c_green & 0xFF; //off-time = brightness
+	data[7] = c_green & 0xFF; // off-time = brightness
 	data[8] = c_green >> 8;
 
-	data[9] = 0; //on-time = 0
+	data[9] = 0; // on-time = 0
 	data[10] = 0;
-	data[11] = c_blue & 0xFF; //off-time = brightness
+	data[11] = c_blue & 0xFF; // off-time = brightness
 	data[12] = c_blue >> 8;
 
 	driverAddr = I2C_BASE_ADDRESS | (driverAddr << 1);
 
-	//Todo: write_IT()
+	// Todo: write_IT()
 	auto err = i2cp_.write(driverAddr, data, 13);
 
-	return (err == I2CPeriph::Error::I2C_NO_ERR)
-			   ? LEDDriverError::None
-			   : LEDDriverError::I2C_XMIT_TIMEOUT;
+	return (err == I2CPeriph::Error::I2C_NO_ERR) ? LEDDriverError::None
+												 : LEDDriverError::I2C_XMIT_TIMEOUT;
 }
 
 LEDDriverError PCA9685Driver::reset_chip(uint8_t driverAddr)
@@ -128,22 +129,28 @@ LEDDriverError PCA9685Driver::reset_chip(uint8_t driverAddr)
 	LEDDriverError err;
 
 	err = write_register(driverAddr, REG_MODE1, 0b00000000); // clear sleep mode
-	if (err != LEDDriverError::None) return err;
+	if (err != LEDDriverError::None)
+		return err;
 	HAL_Delay(1);
 
-	err = write_register(driverAddr, REG_MODE1, 0b10000000); //start reset mode
-	if (err != LEDDriverError::None) return err;
+	err = write_register(driverAddr, REG_MODE1, 0b10000000); // start reset mode
+	if (err != LEDDriverError::None)
+		return err;
 	HAL_Delay(1);
 
-	err = write_register(driverAddr, REG_MODE1, 0b00100000); //auto increment
-	if (err != LEDDriverError::None) return err;
-	err = write_register(driverAddr, REG_MODE2, 0b00010001); // INVERT=1, OUTDRV=0, OUTNE=01
-	if (err != LEDDriverError::None) return err;
+	err = write_register(driverAddr, REG_MODE1, 0b00100000); // auto increment
+	if (err != LEDDriverError::None)
+		return err;
+	err = write_register(driverAddr, REG_MODE2,
+						 0b00010001); // INVERT=1, OUTDRV=0, OUTNE=01
+	if (err != LEDDriverError::None)
+		return err;
 
 	return LEDDriverError::None;
 }
 
-LEDDriverError PCA9685Driver::write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue)
+LEDDriverError
+PCA9685Driver::write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue)
 {
 	uint8_t data[2] = {registerAddr, registerValue};
 
@@ -151,12 +158,12 @@ LEDDriverError PCA9685Driver::write_register(uint8_t driverAddr, uint8_t registe
 
 	auto err = i2cp_.write(driverAddr, data, 2);
 
-	return (err == I2CPeriph::Error::I2C_NO_ERR)
-			   ? LEDDriverError::None
-			   : LEDDriverError::I2C_XMIT_TIMEOUT;
+	return (err == I2CPeriph::Error::I2C_NO_ERR) ? LEDDriverError::None
+												 : LEDDriverError::I2C_XMIT_TIMEOUT;
 }
 
-//returns led element number of the red element of the given RGB LED id (green is red + 1, blue = red + 2)
+// returns led element number of the red element of the given RGB LED id (green
+// is red + 1, blue = red + 2)
 uint8_t PCA9685Driver::get_red_led_element_id(uint8_t rgb_led_id)
 {
 	return (rgb_led_id * 3) + (rgb_led_id / kNumRGBLEDsPerChip);
