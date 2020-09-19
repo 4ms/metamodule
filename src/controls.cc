0fb2a9e0d9efd5b199c17e3ae5f6cac725941090
Dan Green <danngreen1@gmail.com>
2020-09-18 18:30:26 -0700

    Split ADC arrays up (UPDATE FOR DUALOP PCB VALUES)
    ...because of the template class, two ADCs of different channels can't be in an array. To be fixed.
    Also Controls reads buttons and jack sense.

--- src/controls.cc
+++ src/controls.cc
@@ -7,18 +7,25 @@ void Controls::read()
 {
 	Debug::set_0(true);
 
+	freq_cv0.read();
+	res_cv0.read();
+	freq_cv1.read();
+	res_cv1.read();
 	for (uint32_t i = 0; i < 2; i++) {
-		freq_cv[i].read();
-		res_cv[i].read();
 		rotary[i].update();
 		rotary_button[i].update();
+		mode_button[i].update();
+		freq_sense[i].update();
+		res_sense[i].update();
+		in_sense[i].update();
+		out_sense[i].update();
 	}
 	Debug::set_0(false);
 }
 
 void Controls::begin()
 {
-	//?? Todo: is this needed?
+	read_controls_task.start();
 }
 
 Controls::Controls()
@@ -41,8 +48,6 @@ Controls::Controls()
 		.priority2 = 3,
 	};
 
-	read_controls_task.init(
-		kControlReadTaskConfig,
-		[this]() { read(); });
+	read_controls_task.init(kControlReadTaskConfig, [this]() { read(); });
 }
 
--- src/controls.hh
+++ src/controls.hh
@@ -10,8 +10,10 @@
 using JackSense = DebouncedSwitch;
 
 struct Controls {
-	static inline AnalogIn<ADC_1, AdcChanNum::_10> freq_cv[2] = {{GPIO::A, 2}, {GPIO::A, 0}};
-	static inline AnalogIn<ADC_1, AdcChanNum::_11> res_cv[2] = {{GPIO::A, 3}, {GPIO::A, 1}};
+	static inline AnalogIn<ADC_1, AdcChanNum::_11> freq_cv0 = {GPIO::C, 1}; // LFO CV
+	static inline AnalogIn<ADC_1, AdcChanNum::_13> res_cv0 = {GPIO::C, 3};	// Depth CV ok
+	static inline AnalogIn<ADC_1, AdcChanNum::_10> freq_cv1 = {GPIO::C, 0}; // Lat CV ok
+	static inline AnalogIn<ADC_1, AdcChanNum::_15> res_cv1 = {GPIO::C, 5};	// WTSpread CV ok
 
 	static inline JackSense freq_sense[2] = {{GPIO::C, 4}, {GPIO::B, 1}};
 	static inline JackSense res_sense[2] = {{GPIO::C, 5}, {GPIO::B, 0}};
