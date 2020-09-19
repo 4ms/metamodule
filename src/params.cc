525901c3f81a974b93f1595e4a08eb193fbd3f7c
Dan Green <danngreen1@gmail.com>
2020-09-18 18:31:28 -0700

    params uses new cv values (non-arrays)

--- src/params.cc
+++ src/params.cc
@@ -5,16 +5,19 @@ using namespace MathTools;
 
 void Params::update()
 {
+	uint16_t freqcv[2] = {controls.freq_cv0.get(), controls.freq_cv1.get()};
+	uint16_t rescv[2] = {controls.res_cv0.get(), controls.res_cv1.get()};
+
 	for (int i = 0; i < 2; i++) {
 		float rotary_motion = static_cast<float>(controls.rotary[i].read());
 		if (controls.rotary_button[i].is_pressed())
 			res_knob_pos[i] += rotary_motion;
 		else
 			freq_knob_pos[i] += rotary_motion;
-		freq[i] = constrain((freq_knob_pos[i] / kFreqScaling) + (controls.freq_cv[i].get() / 4095.0f), 0.f, 1.f);
-		res[i] = constrain((res_knob_pos[i] / kResScaling) + (controls.res_cv[i].get() / 4095.0f), 0.f, 1.f);
+		freq[i] = constrain((freq_knob_pos[i] / kFreqScaling) + (freqcv[i] / 4095.0f), 0.f, 1.f);
+		res[i] = constrain((res_knob_pos[i] / kResScaling) + (rescv[i] / 4095.0f), 0.f, 1.f);
 
-		if (controls.rotary_button[i].is_just_released()) {
+		if (controls.mode_button[i].is_just_released()) {
 			mode[i] = wrap<kNumFX>(mode[i] + 1);
 		}
 	}
