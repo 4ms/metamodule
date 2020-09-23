cfa0f1b65f3185b28084e1322825c49549089fbc
Ben Davis <bendedavis@gmail.com>
2020-09-23 13:03:55 -0700

    last change introduced offset, rolled back

--- src/processors/tools/cubicDist.h
+++ src/processors/tools/cubicDist.h
@@ -7,6 +7,6 @@ public:
 		float output = 0;
 		float fTemp0 = constrain(input, -1.0f, 1.0f);
 		output = fTemp0 * (1.0f - (0.333333343f * fTemp0 * fTemp0));
-		return output * 1.5f;
+		return output;
 	}
 };
