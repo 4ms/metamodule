eb08d4efd122d7731949e3827a7f10ed03127adf
Dan Green <danngreen1@gmail.com>
2020-09-25 10:17:40 -0700

    Removed float operator[] for interp_array (doesn't work)

--- src/util/interp_array.hh
+++ src/util/interp_array.hh
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>
 template<typename T, unsigned int Size>
 struct InterpArray {
 	T data[Size];
@@ -13,10 +12,6 @@ struct InterpArray {
 	{
 		return data[index];
 	}
-	const T operator[](const float index) const
-	{
-		return interp(index);
-	}
 	const T interp(const float index) const
 	{
 		int idx0 = static_cast<int>(index);
@@ -28,9 +23,4 @@ struct InterpArray {
 		return data[idx0] + (data[idx1] - data[idx0]) * phase;
 	}
 };
-/*
-		InterpArray<float, 512> fold1;
-		foldSamp[1] = fold1.interp(lookupIndex);
-		foldSamp[2] = fold2.interp(lookupIndex);
-		foldSamp[3] = fold3.interp(lookupIndex);
-*/
+
