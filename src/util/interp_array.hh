746f026b939f35adeff5f716b72d4c5be8540f93
Dan Green <danngreen1@gmail.com>
2020-09-25 11:14:23 -0700

    interp_array passes tests. Has interp() and interp_by_index()

--- src/util/interp_array.hh
+++ src/util/interp_array.hh
@@ -1,35 +1,25 @@
 #pragma once
 #include <cstddef>
+#include <array>
 
 template<typename T, unsigned int Size>
-struct InterpArray {
-	T data[Size];
-
-	InterpArray() = default;
-
-	InterpArray(T initval)
+struct InterpArray : public std::array<T, Size> {
+	
+	constexpr float interp_by_index(const float index) const
 	{
-		for (size_t i = 0; i < Size; i++)
-			data[i] = initval;
-	}
-
-	T &operator[](const unsigned int index)
-	{
-		return data[index];
-	}
-	const T operator[](const unsigned int index) const
-	{
-		return data[index];
-	}
-	const T interp(const float index) const
-	{
-		int idx0 = static_cast<int>(index);
+		unsigned int idx0 = static_cast<unsigned int>(index);
 		float phase = index - static_cast<float>(idx0);
-		int idx1 = idx0 + 1;
+		unsigned int idx1 = idx0 + 1;
 		if (idx1 >= Size)
 			idx1 = 0;
 
-		return data[idx0] + (data[idx1] - data[idx0]) * phase;
+		return (*this)[idx0] + ((*this)[idx1] - (*this)[idx0]) * phase;
+	}
+
+	constexpr float interp(const float phase) const
+	{
+		float const index = phase * (Size-1);
+		return interp_by_index(index);
 	}
 };
 
