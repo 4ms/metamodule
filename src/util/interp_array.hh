5f1f7b72108f973cb81c90a2b7daedf22ce98517
Dan Green <danngreen1@gmail.com>
2020-09-25 10:04:12 -0700

    Interp_array class

--- src/util/interp_array.hh
+++ src/util/interp_array.hh
@@ -1,3 +1,33 @@
+#pragma once
+
+#include <array>
+template<typename T, unsigned int Size>
+struct InterpArray {
+	T data[Size];
+
+	T &operator[](const unsigned int index)
+	{
+		return data[index];
+	}
+	const T operator[](const unsigned int index) const
+	{
+		return data[index];
+	}
+	const T operator[](const float index) const
+	{
+		return interp(index);
+	}
+	const T interp(const float index) const
+	{
+		int idx0 = static_cast<int>(index);
+		float phase = index - static_cast<float>(idx0);
+		int idx1 = idx0 + 1;
+		if (idx1 >= Size)
+			idx1 = 0;
+
+		return data[idx0] + (data[idx1] - data[idx0]) * phase;
+	}
+};
 /*
 		InterpArray<float, 512> fold1;
 		foldSamp[1] = fold1.interp(lookupIndex);
