798b4767b320286379c5c8f34064ae13426387bb
Dan Green <danngreen1@gmail.com>
2020-10-07 17:21:31 -0700

    Rotaries don't go past 0 or 1

--- src/params.cc
+++ src/params.cc
@@ -10,12 +10,15 @@ void Params::update()
 
 	for (int i = 0; i < 2; i++) {
 		float rotary_motion = static_cast<float>(controls.rotary[i].read());
-		if (controls.rotary_button[i].is_pressed())
-			res_knob_pos[i] += rotary_motion;
-		else
-			freq_knob_pos[i] += rotary_motion;
-		freq[i] = constrain((freq_knob_pos[i] / kFreqScaling) + (freqcv[i] / 4095.0f), 0.f, 1.f);
-		res[i] = constrain((res_knob_pos[i] / kResScaling) + (rescv[i] / 4095.0f), 0.f, 1.f);
+		if (rotary_motion != 0.f) {
+			if (controls.rotary_button[i].is_pressed())
+				res_knob_pos[i] = constrain(res_knob_pos[i] + (rotary_motion / kFreqScaling), 0.f, 1.f);
+			else
+				freq_knob_pos[i] = constrain(freq_knob_pos[i] + (rotary_motion / kResScaling), 0.f, 1.f);
+		}
+
+		freq[i] = constrain(freq_knob_pos[i] + (freqcv[i] / 4095.0f) - 0.5f, 0.f, 1.f);
+		res[i] = constrain(res_knob_pos[i] + (rescv[i] / 4095.0f) - 0.5f, 0.f, 1.f);
 
 		if (controls.mode_button[i].is_just_released()) {
 			mode[i] = wrap<kNumFX>(mode[i] + 1);
--- src/params.hh
+++ src/params.hh
@@ -6,22 +6,22 @@ class Params {
 public:
 	Params(Controls &c)
 		: controls(c)
-	{
-	}
+	{}
 
 	float freq[2];
 	float res[2];
 	uint8_t mode[2] = {0};
-	float freq_knob_pos[2] = {50, 50};
-	float res_knob_pos[2] = {0, 0};
+	float freq_knob_pos[2] = {0.5f, 0.5f};
+	float res_knob_pos[2] = {0.5f, 0.5f};
 
 public:
 	void update();
 	Controls &controls;
 
 private:
-	static const inline float kFreqScaling = 100.0f; //Todo: figure out what this should equal, and put it somewhere sensible
+	static const inline float kFreqScaling =
+		100.0f; // Todo: figure out what this should equal, and put it somewhere sensible
 	static const inline float kResScaling = 100.0f;
-	static const inline uint8_t kNumFX = 5; //Todo: how to enforce this matches size of AudioFXList?
+	static const inline uint8_t kNumFX = 5; // Todo: how to enforce this matches size of AudioFXList?
 };
 
