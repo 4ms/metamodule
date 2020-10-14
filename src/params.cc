0795d1a185b1b88609a58b852ea78e0ae97a9b71
Ben Davis <bendedavis@gmail.com>
2020-10-14 12:22:50 -0700

    removed offset for parameters

--- src/params.cc
+++ src/params.cc
@@ -9,9 +9,9 @@ void Params::update()
 	uint32_t freqcv[2];
 	uint32_t rescv[2];
 	freqcv[0] = controls.freq_sense[0].is_pressed() ? 0U : controls.freq_cv0.get();
-	freqcv[1] = controls.freq_sense[1].is_pressed() ? 0U: controls.freq_cv1.get();
-	rescv[0] = controls.res_sense[0].is_pressed() ? 0U: controls.res_cv0.get();
-	rescv[1] = controls.res_sense[1].is_pressed() ? 0U: controls.res_cv1.get();
+	freqcv[1] = controls.freq_sense[1].is_pressed() ? 0U : controls.freq_cv1.get();
+	rescv[0] = controls.res_sense[0].is_pressed() ? 0U : controls.res_cv0.get();
+	rescv[1] = controls.res_sense[1].is_pressed() ? 0U : controls.res_cv1.get();
 
 	for (int i = 0; i < 2; i++) {
 		float rotary_motion = static_cast<float>(controls.rotary[i].read());
@@ -22,8 +22,8 @@ void Params::update()
 				freq_knob_pos[i] = constrain(freq_knob_pos[i] + (rotary_motion / kResScaling), 0.f, 1.f);
 		}
 
-		freq[i] = constrain(freq_knob_pos[i] + (freqcv[i] / 4095.0f) - 0.5f, 0.f, 1.f);
-		res[i] = constrain(res_knob_pos[i] + (rescv[i] / 4095.0f) - 0.5f, 0.f, 1.f);
+		freq[i] = constrain(freq_knob_pos[i] + (freqcv[i] / 4095.0f), 0.f, 1.f);
+		res[i] = constrain(res_knob_pos[i] + (rescv[i] / 4095.0f), 0.f, 1.f);
 
 		if (controls.mode_button[i].is_just_released()) {
 			mode[i] = wrap<kNumFX>(mode[i] + 1);
@@ -31,4 +31,3 @@ void Params::update()
 	}
 	// Debug::set_1(false);
 }
-
