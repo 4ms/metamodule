9814d4bbc1ae28d2470aedd1a6497b8ab9aedbcd
Dan Green <danngreen1@gmail.com>
2020-10-22 12:31:01 -0700

    cv jacks using oversampler size of 16

--- src/controls.hh
+++ src/controls.hh
@@ -10,19 +10,19 @@
 using JackSense = DebouncedSwitch;
 
 struct Controls {
-	static inline AnalogIn<AdcPeriphNum::_1, AdcChanNum::_2> freq_cv0 = {GPIO::A, 2};
-	static inline AnalogIn<AdcPeriphNum::_1, AdcChanNum::_3> res_cv0 = {GPIO::A, 3};
-	static inline AnalogIn<AdcPeriphNum::_1, AdcChanNum::_0> freq_cv1 = {GPIO::A, 0};
-	static inline AnalogIn<AdcPeriphNum::_1, AdcChanNum::_1> res_cv1 = {GPIO::A, 1};
+	AnalogIn<AdcPeriphNum::_1, AdcChanNum::_2, 16> freq_cv0 = {GPIO::A, 2};
+	AnalogIn<AdcPeriphNum::_1, AdcChanNum::_3, 16> res_cv0 = {GPIO::A, 3};
+	AnalogIn<AdcPeriphNum::_1, AdcChanNum::_0, 16> freq_cv1 = {GPIO::A, 0};
+	AnalogIn<AdcPeriphNum::_1, AdcChanNum::_1, 16> res_cv1 = {GPIO::A, 1};
 
-	static inline JackSense freq_sense[2] = {{GPIO::C, 4, PinPolarity::Normal}, {GPIO::B, 1, PinPolarity::Normal}};
-	static inline JackSense res_sense[2] = {{GPIO::C, 5, PinPolarity::Normal}, {GPIO::B, 0, PinPolarity::Normal}};
-	static inline JackSense in_sense[2] = {{GPIO::G, 12, PinPolarity::Normal}, {GPIO::G, 11, PinPolarity::Normal}};
-	static inline JackSense out_sense[2] = {{GPIO::D, 7, PinPolarity::Normal}, {GPIO::G, 10, PinPolarity::Normal}};
+	JackSense freq_sense[2] = {{GPIO::C, 4, PinPolarity::Normal}, {GPIO::B, 1, PinPolarity::Normal}};
+	JackSense res_sense[2] = {{GPIO::C, 5, PinPolarity::Normal}, {GPIO::B, 0, PinPolarity::Normal}};
+	JackSense in_sense[2] = {{GPIO::G, 12, PinPolarity::Normal}, {GPIO::G, 11, PinPolarity::Normal}};
+	JackSense out_sense[2] = {{GPIO::D, 7, PinPolarity::Normal}, {GPIO::G, 10, PinPolarity::Normal}};
 
-	static inline RotaryEncoder<RotaryFullStep> rotary[2] = {{GPIO::A, 7, GPIO::A, 6}, {GPIO::C, 7, GPIO::C, 6}};
-	static inline DebouncedSwitch rotary_button[2] = {{GPIO::A, 5}, {GPIO::B, 9}};
-	static inline DebouncedSwitch mode_button[2] = {{GPIO::C, 14}, {GPIO::C, 15}};
+	RotaryEncoder<RotaryFullStep> rotary[2] = {{GPIO::A, 7, GPIO::A, 6}, {GPIO::C, 7, GPIO::C, 6}};
+	DebouncedSwitch rotary_button[2] = {{GPIO::A, 5}, {GPIO::B, 9}};
+	DebouncedSwitch mode_button[2] = {{GPIO::C, 14}, {GPIO::C, 15}};
 
 	Controls();
 	void read();
