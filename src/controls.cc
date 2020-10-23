8fe42bf5a638e444bf985b115c564bcb67871b61
Dan Green <danngreen1@gmail.com>
2020-10-23 10:27:26 -0700

    Moved debug function for measuring controls read timing, to a separate function

--- src/controls.cc
+++ src/controls.cc
@@ -30,21 +30,23 @@ void Controls::start()
 Controls::Controls()
 {
 	CVADCPeriph::init_dma(ADC_DMA_conf, adc_buffer);
-
-	// Debug: use this to read the DMA timing with an external pin:
-	// InterruptManager::registerISR(ADC_DMA_conf.IRQn, []() {
-	// 	uint32_t tmpisr = DMA2->LISR;
-	// 	if (LL_DMA_IsActiveFlag_TC0(DMA2)) {
-	// 		if (LL_DMA_IsEnabledIT_TC(DMA2, LL_DMA_STREAM_0)) {
-	// 			Debug::toggle_2();
-	// 		}
-	// 		LL_DMA_ClearFlag_TC0(DMA2);
-	// 	}
-	// });
-	// CVADCPeriph::enable_DMA_IT();
-
+	// debug_control_read_timing();
 	CVADCPeriph::start_adc();
 
 	read_controls_task.init(control_read_tim_conf, [this]() { read(); });
 }
 
+static void debug_control_read_timing()
+{
+	// Debug: use this to read the DMA timing with an external pin:
+	InterruptManager::registerISR(ADC_DMA_conf.IRQn, []() {
+		if (LL_DMA_IsActiveFlag_TC0(DMA2)) {
+			if (LL_DMA_IsEnabledIT_TC(DMA2, LL_DMA_STREAM_0)) {
+				// set this to whatever pin your scope is connected to:
+				Debug::toggle_2();
+			}
+			LL_DMA_ClearFlag_TC0(DMA2);
+		}
+	});
+	CVADCPeriph::enable_DMA_IT();
+}
