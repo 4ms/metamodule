ad21a74f122703a62f40d699a3ce26f49731f32a
Dan Green <danngreen1@gmail.com>
2020-11-02 17:12:24 -0800

    using new will go to an infinite loop (so we make sure we don't use it)

--- src/sys/new.cc
+++ src/sys/new.cc
@@ -43,15 +43,18 @@ void operator delete[](void *p, std::nothrow_t) noexcept
 
 extern "C" size_t _sbrk(int incr)
 {
+	while (1) {
+		;
+		//force infinite loop
+	}
 	extern char _fmc_start; // Defined by the linker
 	// extern char _end; // Defined by the linker
 	static char *heap_end;
 	char *prev_heap_end;
 
 	if (heap_end == 0) {
-		//force HARD FAULT
+		heap_end = &_fmc_start;
 		//heap_end = (char *)(0x20014000);
-		//heap_end = &_fmc_start;
 		// heap_end = &_end;
 	}
 	prev_heap_end = heap_end;
