093d70afd1b4f0207ba126f1d196bdb3dbce60ef
Dan Green <danngreen1@gmail.com>
2020-11-06 16:08:00 -0800

    Using sane initial values

--- src/processors/resonator.h
+++ src/processors/resonator.h
@@ -17,7 +17,7 @@ class Resonator : public AudioProcessor {
 	using Buffer = float[3];
 
 	Coefs coefs[2];
-	Buffer buff[2];
+	Buffer buff[2] = {{0,0,0},{0,0,0}};
 	float tap_volume[2];
 
 	float run_filter(float input, Coefs &c, Buffer &b)
@@ -34,36 +34,24 @@ public:
 		float out_a = run_filter(input, coefs[0], buff[0]);
 		float out_b = run_filter(out_a, coefs[1], buff[1]);
 
-		// bufa[2] = (c0_a * bufa[1] + c1 * bufa[0]) - c2_a * input;
-		// bufa[0] = bufa[0] - (c1 * bufa[2]);
-
-		// bufa[1] = bufa[2];
-		// float out_a =  bufa[1];
-
-		// buf[2] = (c0 * buf[1] + c1 * buf[0]) - c2  * out_a;
-		// buf[0] = buf[0] - (c1 * buf[2]);
-
-		// buf[1] = buf[2];
-		// float out_b = buf[1];
-
 		float output = (tap_volume[0] * out_a) - out_b; // output of filter two needs to be inverted to avoid phase cancellation
 		return output;
 	}
 
 	Resonator()
 	{
-		freq = 0.1;
-		res = 0.5;
+		freq = 50.f;
+		res = 0.05f;
 		calc_coefs();
 	}
 
 	virtual void set_param(int param_id, float val)
 	{
 		const float minFreq = 20.0f;
-		const float maxFreq = 20000.f;
+		const float maxFreq = 15000.f;
 		if (param_id == 0) {
 			freq = expTable.interp(val);
-			freq = map_value(val, 1.f, 32.f, minFreq, maxFreq);
+			freq = map_value(freq, 1.f, 32.f, minFreq, maxFreq);
 		}
 		else if (param_id == 1) {
 			res = val;
@@ -80,11 +68,12 @@ public:
 		// Q/RESONANCE: c0 = 1 - 2/(decay * samplerate), where decay is around 0.01 to 4.0
 		// Todo: samplerate should be in this calculation
 		float res0 = MathTools::constrain(res * 2.f, 0.f, 1.f);
-		coefs[0].c0 = 1.003225f - expTable.closest(1.0f - res0) / 310.f; // * 96000/samplerate ??
-		//coefs[0].c0 = 1.0f - 2.f/(samplerate * expTable.closest(res));
+		//coefs[0].c0 = 1.003225f - expTable.interp(1.0f - res0) / 310.f; // * 96000/samplerate ??
+		coefs[0].c0 = 1.0f - 2.f/(samplerate * ((expTable.closest(res0)-0.9f)/32.f));
 
 		float res1 = res < 0.952f ? 0.244f : (0.244f + (res - 0.952f) * 15.f);
-		coefs[1].c0 = 1.003225f - expTable.closest(1.0f - res1) / 310.f;
+		//coefs[1].c0 = 1.003225f - expTable.closest(1.0f - res1) / 310.f;
+		coefs[1].c0 = 1.0f - 2.f/(samplerate * ((expTable.closest(res1)-0.9f)/32.f));
 
 		// FREQ: c1 = 2 * pi * freq / samplerate
 		coefs[0].c1 = 2.f * M_PI * freq / samplerate;
@@ -103,10 +92,14 @@ public:
 		tap_volume[1] = 1.0f - tap_volume[0];
 		tap_volume[1] *= boost_table.closest(res1 - 0.220f);
 
-		coefs[0].c2 = (0.003f * coefs[0].c1) - (0.1f * coefs[0].c0) + 0.102f;
+		const float highfreqboost = 0.4f;
+		const float high_res_cut = 0.1f;
+		coefs[0].c2 = (highfreqboost * coefs[0].c1) - (high_res_cut * coefs[0].c0) + 0.102f;
+		//coefs[0].c2 *= 6.0f;
 
-		coefs[1].c2 = (0.003f * coefs[1].c1) - (0.1f * coefs[1].c0) + 0.102f;
+		coefs[1].c2 = (highfreqboost * coefs[1].c1) - (high_res_cut * coefs[1].c0) + 0.102f;
 		coefs[1].c2 *= tap_volume[1];
+		//coefs[1].c2 *= 6.0f;
 	}
 
 private:
