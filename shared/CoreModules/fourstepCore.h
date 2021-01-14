ff6fcc0d5d17fb70ae3e0f44767043a1e396b72c
Ben Davis <bendedavis@gmail.com>
2021-01-14 13:41:15 -0800

    added four step seq tests

--- shared/CoreModules/fourstepCore.h
+++ shared/CoreModules/fourstepCore.h
@@ -2,8 +2,8 @@
 
 #include "CoreModules/moduleTypes.h"
 #include "coreProcessor.h"
-#include "math.hh"
 #include "processors/stepsequencer.h"
+#include "util/math.hh"
 
 using namespace MathTools;
 
