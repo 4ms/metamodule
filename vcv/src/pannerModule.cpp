360b48f3f20497f8e761e745f795cc35aab627a1
Ben Davis <bendedavis@gmail.com>
2021-01-22 09:18:20 -0800

    added panner module

---
+++ vcv/src/pannerModule.cpp
@@ -0,0 +1,41 @@
+#include "CommModule.h"
+#include "CommWidget.h"
+#include "math.hh"
+
+struct PannerModule : CommModule {
+
+	enum ParamIds { PAN_PARAM, NUM_PARAMS };
+	enum InputIds { SIGNAL_INPUT, NUM_INPUTS };
+	enum OutputIds { LEFT_OUTPUT, RIGHT_OUTPUT, NUM_OUTPUTS };
+	enum LightIds { NUM_LIGHTS };
+
+	PannerModule()
+	{
+		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		core = ModuleFactory::create("PANNER");
+		selfID.typeID = "PANNER";
+	}
+};
+
+struct PannerWidget : CommModuleWidget {
+
+	PannerModule *mainModule;
+
+	Label *valueLabel;
+	Label *recLabel;
+
+	PannerWidget(PannerModule *module)
+	{
+		setModule(module);
+		mainModule = module;
+
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/4hp.svg")));
+
+		addLabeledKnob("PAN", PannerModule::PAN_PARAM, {0, 0});
+		addLabeledInput("IN", PannerModule::SIGNAL_INPUT, {0, 2});
+		addLabeledOutput("LEFT", PannerModule::LEFT_OUTPUT, {0, 1});
+		addLabeledOutput("RIGHT", PannerModule::RIGHT_OUTPUT, {0, 0});
+	}
+};
+
+Model *modelPanner = createModel<PannerModule, PannerWidget>("PANNER");
