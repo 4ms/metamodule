47daaa52644471e41bc5b9715badad770650b994
Dan Green <danngreen1@gmail.com>
2021-01-26 15:48:21 -0800

    Added PhaseAccum to shared/util

--- shared/util/oscs.hh
+++ shared/util/oscs.hh
@@ -35,3 +35,32 @@ private:
 	uint32_t increment_;
 };
 
+template<int update_rate>
+struct PhaseAccum {
+	PhaseAccum(uint32_t freq)
+		: increment_(freq * (max_ / update_rate))
+	{}
+	PhaseAccum()
+		: increment_(0)
+	{}
+	uint32_t Process()
+	{
+		phase_ += increment_;
+		return phase_;
+	}
+
+	void set_frequency(uint32_t freq)
+	{
+		increment_ = freq * (max_ / update_rate);
+	}
+	void set_phase(uint32_t phase)
+	{
+		phase_ = phase;
+	}
+
+private:
+	static constexpr uint32_t max_ = 0xFFFFFFFF;
+	uint32_t phase_ = 0;
+	uint32_t increment_;
+};
+
