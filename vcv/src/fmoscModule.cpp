c9e1a6af09b42e44b1786878972a76d95b9b61e9
Ben Davis <bendedavis@gmail.com>
2021-01-26 10:35:07 -0800

    added index knob and cv

--- vcv/src/fmoscModule.cpp
+++ vcv/src/fmoscModule.cpp
@@ -4,8 +4,8 @@
 
 struct FmoscModule : CommModule {
 
-	enum ParamIds { COARSE_TUNE_PARAM, INDEX_PARAM, RATIO_PARAM, NUM_PARAMS };
-	enum InputIds { NUM_INPUTS };
+	enum ParamIds { COARSE_TUNE_PARAM, INDEX_PARAM, RATIO_COARSE_PARAM, RATIO_FINE_PARAM, NUM_PARAMS };
+	enum InputIds { INDEX_INPUT, NUM_INPUTS };
 	enum OutputIds { MAIN_OUTPUT, NUM_OUTPUTS };
 	enum LightIds { NUM_LIGHTS };
 
@@ -14,6 +14,8 @@ struct FmoscModule : CommModule {
 		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		core = ModuleFactory::create("FMOSC");
 		selfID.typeID = "FMOSC";
+
+		inputJacks[FmoscModule::INDEX_INPUT]->scale = [](float f) { return f / 5.0f; };
 	}
 };
 
@@ -33,7 +35,10 @@ struct FmoscWidget : CommModuleWidget {
 
 		addLabeledKnob("PITCH", FmoscModule::COARSE_TUNE_PARAM, {0, 0});
 		addLabeledKnob("INDEX", FmoscModule::INDEX_PARAM, {1, 0});
-		addLabeledKnob("RATIO", FmoscModule::RATIO_PARAM, {0, 1});
+		addLabeledKnob("RATIO C", FmoscModule::RATIO_COARSE_PARAM, {0, 1});
+		addLabeledKnob("RATIO F", FmoscModule::RATIO_FINE_PARAM, {1, 1});
+
+		addLabeledInput("INDEX", FmoscModule::INDEX_INPUT, {0, 0});
 
 		addLabeledOutput("OUT", FmoscModule::MAIN_OUTPUT, {2, 0});
 
