1a4295288623ebaf44b5b2f274bda1e3a0782a0e
Dan Green <danngreen1@gmail.com>
2021-01-26 15:46:59 -0800

    started sdram conf for 48LC chip

---
+++ H7/src/conf/sdram_48LC16M16_6A_conf.hh
@@ -0,0 +1,26 @@
+#pragma once
+#include "drivers/sdram_config_struct.hh"
+
+const SDRAMTimingConfig SDRAM_timing_48LC16M16_6A = {
+	.tRC_ns = 63,
+	.tRP_ns = 21,
+	.tMRD_ns = 14,
+	.tXSR_ns = 65,
+	.tRAS_ns = 42,
+	.tWR_ns = 14,
+	.tRCD_ns = 21,
+
+	.max_freq_MHz = 143,
+	.CAS_latency = 3,
+	.refresh_ms = 64,
+	.read_burst_enabled = true,
+	.read_pipe_delay = 2,
+};
+
+const SDRAMArchConfig SDRAM_arch_48LC16M16_6A = {
+	.num_internal_banks = 4,
+	.bus_width_bits = 16,
+	.column_bits = 12, //? or 13?
+	.row_bits = 9,
+	.num_rows = 8192,
+};
