ce415b539f22effd0422358c4ee949d80ffc9706
Dan Green <danngreen1@gmail.com>
2021-01-27 15:09:06 -0800

    I2C pins set

--- H7/src/conf/i2c_conf.hh
+++ H7/src/conf/i2c_conf.hh
@@ -3,13 +3,13 @@
 
 const I2CConfig i2c_conf = {
 	.I2Cx = I2C1,
-	.SCL = {GPIO::B, 8, GPIO_AF4_I2C1},
+	.SCL = {GPIO::B, 6, GPIO_AF4_I2C1},
 	.SDA = {GPIO::B, 7, GPIO_AF4_I2C1},
 	.timing =
 		{
-			.PRESC = 8 * 2, // tPREC = 216MHz / 8 = 27MHz
+			.PRESC = 18 * 2, // tPREC = 480MHz / 18 = 26.7MHz
 			.SCLDEL_SDADEL = 0b00010001,
-			.SCLH = 17, // 27MHz / (17+17) = 794kHz
+			.SCLH = 17, // 26.7MHz / (17+17) = 784kHz
 			.SCLL = 17,
 		},
 };
