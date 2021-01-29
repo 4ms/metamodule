014c73f2a29ce62e4dab786678d5020e2ef7985c
Ben Davis <bendedavis@gmail.com>
2021-01-29 11:17:10 -0800

    added curve controls

--- vcv/src/complexenvelopeModule.cpp
+++ vcv/src/complexenvelopeModule.cpp
@@ -4,7 +4,17 @@
 
 struct ComplexenvelopeModule : CommModule {
 
-	enum ParamIds { ATTACK_PARAM, HOLD_PARAM, DECAY_PARAM, SUSTAIN_PARAM, RELEASE_PARAM, NUM_PARAMS };
+	enum ParamIds {
+		ATTACK_PARAM,
+		HOLD_PARAM,
+		DECAY_PARAM,
+		SUSTAIN_PARAM,
+		RELEASE_PARAM,
+		ATTACK_CURVE_PARAM,
+		DECAY_CURVE_PARAM,
+		RELEASE_CURVE_PARAM,
+		NUM_PARAMS
+	};
 	enum InputIds { GATE_INPUT, NUM_INPUTS };
 	enum OutputIds { SIGNAL_OUTPUT, NUM_OUTPUTS };
 	enum LightIds { NUM_LIGHTS };
@@ -34,10 +44,15 @@ struct ComplexenvelopeWidget : CommModuleWidget {
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/8hpTemplate.svg")));
 
 		addLabeledKnob("ATTACK", ComplexenvelopeModule::ATTACK_PARAM, {0, 0});
-		addLabeledKnob("HOLD", ComplexenvelopeModule::HOLD_PARAM, {1, 0});
 		addLabeledKnob("DECAY", ComplexenvelopeModule::DECAY_PARAM, {0, 1});
-		addLabeledKnob("SUSTAIN", ComplexenvelopeModule::SUSTAIN_PARAM, {1, 1});
 		addLabeledKnob("RELEASE", ComplexenvelopeModule::RELEASE_PARAM, {0, 2});
+
+		addLabeledKnob("CURVE", ComplexenvelopeModule::ATTACK_CURVE_PARAM, {1, 0});
+		addLabeledKnob("CURVE", ComplexenvelopeModule::DECAY_CURVE_PARAM, {1, 1});
+		addLabeledKnob("CURVE", ComplexenvelopeModule::RELEASE_CURVE_PARAM, {1, 2});
+
+		addLabeledKnob("HOLD", ComplexenvelopeModule::HOLD_PARAM, {0, 3});
+		addLabeledKnob("SUSTAIN", ComplexenvelopeModule::SUSTAIN_PARAM, {1, 3});
 
 		addLabeledOutput("OUT", ComplexenvelopeModule::SIGNAL_OUTPUT, {0, 0});
 		addLabeledInput("IN", ComplexenvelopeModule::GATE_INPUT, {0, 1});
