02a3a33043570cbd8336e7475f8c52566072f1c0
Ben Davis <bendedavis@gmail.com>
2021-02-01 14:25:54 -0800

    tweaked envelope times

--- shared/CoreModules/drumCore.h
+++ shared/CoreModules/drumCore.h
@@ -3,8 +3,9 @@
 #include "CoreModules/moduleTypes.h"
 #include "coreProcessor.h"
 #include "math.hh"
-#include "processors/macroEnvelope.h"
+#include "processors/envelope.h"
 #include "processors/twoOpFMOscillator.h"
+#include "util/math_tables.hh"
 
 using namespace MathTools;
 
@@ -12,35 +13,58 @@ class DrumCore : public CoreProcessor {
 public:
 	virtual void update(void) override
 	{
-		float pitchEnvelopeOut = 0;
-		float toneEnvelopeOut = 0;
-		float noiseEnvelopeOut = 0;
-		float fmEnvelopeOut = 0;
+		osc.set_frequency(baseFrequency + (envelopes[pitchEnvelope].update(gateIn) * 4000.0f * (pitchAmount*pitchAmount)));
+		osc.modAmount = envelopes[fmEnvelope].update(gateIn) * fmAmount;
+		auto noiseOut = randomNumber(-1.0f, 1.0f) * envelopes[noiseEnvelope].update(gateIn);
 
-		pitchEnvelopeOut = pitchEnvelope.update(gateIn);
-		toneEnvelopeOut = toneEnvelope.update(gateIn);
-		noiseEnvelopeOut = noiseEnvelope.update(gateIn);
-		fmEnvelopeOut = fmEnvelope.update(gateIn);
-
-		osc.set_frequency(baseFrequency + (pitchEnvelopeOut * 2000.0f * pitchAmount));
-		osc.modAmount = fmEnvelopeOut * fmAmount;
-		auto noiseOut = randomNumber(-1.0f, 1.0f) * noiseEnvelopeOut;
-
-		auto toneOutput = osc.update() * toneEnvelopeOut;
+		auto toneOutput = osc.update() * envelopes[toneEnvelope].update(gateIn);
 
 		drumOutput = interpolate(toneOutput, noiseOut, noiseBlend);
 	}
 
-	DrumCore() {}
+	DrumCore()
+	{
+		for (int i = 0; i < 4; i++) {
+			envelopes[i].sustainEnable = false;
+			envelopes[i].set_attack_curve(0);
+			envelopes[i].set_decay_curve(0);
+			envelopes[i].set_release_curve(0.5f);
+			envelopes[i].set_sustain(0.2f);
+		}
+
+		envelopes[toneEnvelope].set_envelope_time(0, 1);
+		envelopes[toneEnvelope].set_envelope_time(1, 50);
+		envelopes[toneEnvelope].set_envelope_time(2, 100);
+		envelopes[toneEnvelope].set_envelope_time(3, 2000);
+		envelopes[toneEnvelope].set_release_curve(1.0f);
+
+		envelopes[fmEnvelope].set_envelope_time(0, 1);
+		envelopes[fmEnvelope].set_envelope_time(1, 0);
+		envelopes[fmEnvelope].set_envelope_time(2, 300);
+		envelopes[fmEnvelope].set_envelope_time(3, 700);
+
+		envelopes[noiseEnvelope].set_envelope_time(0, 1);
+		envelopes[noiseEnvelope].set_envelope_time(1, 0);
+		envelopes[noiseEnvelope].set_envelope_time(2, 30);
+		envelopes[noiseEnvelope].set_envelope_time(3, 700);
+		envelopes[noiseEnvelope].set_release_curve(0.0f);
+
+		envelopes[pitchEnvelope].set_envelope_time(0, 5.0);
+		envelopes[pitchEnvelope].set_envelope_time(1, 0);
+		envelopes[pitchEnvelope].set_envelope_time(2, 50);
+		envelopes[pitchEnvelope].set_envelope_time(3, 2000);
+	}
 
 	virtual void set_param(int const param_id, const float val) override
 	{
 		switch (param_id) {
 			case 0:
-				baseFrequency = map_value(val, 0.0f, 1.0f, 20.0f, 1000.0f);
+				baseFrequency = map_value(val, 0.0f, 1.0f, 10.0f, 1000.0f);
 				break;
-			case 1:
-				pitchEnvelope.set_time(val);
+			case 1: // pitch envelope
+				envelopes[pitchEnvelope].set_envelope_time(2, pitchDecayTimes.interp(val));
+				envelopes[pitchEnvelope].set_envelope_time(3, pitchReleaseTimes.interp(val));
+				envelopes[pitchEnvelope].set_sustain(pitchBreakPoint.interp(val));
 				break;
 			case 2:
 				pitchAmount = val;
@@ -48,17 +72,27 @@ public:
 			case 3:
 				osc.ratioFine = map_value(val, 0.0f, 1.0f, 1.0f, 16.0f);
 				break;
-			case 4:
-				fmEnvelope.set_time(val);
+			case 4: // fm envelope
+				envelopes[fmEnvelope].set_envelope_time(0, map_value(val, 0.0f, 1.0f, 1.0f, 100.0f));
+				envelopes[fmEnvelope].set_envelope_time(2, map_value(val, 0.0f, 1.0f, 10.0f, 8000.0f));
+				envelopes[fmEnvelope].set_envelope_time(3, map_value(val, 0.0f, 1.0f, 10.0f, 3000.0f));
+				envelopes[fmEnvelope].set_sustain(map_value(val, 0.0f, 1.0f, 0.0f, 0.3f));
 				break;
 			case 5:
 				fmAmount = val;
 				break;
-			case 6:
-				toneEnvelope.set_time(val);
+			case 6: // tone envelope
+				envelopes[toneEnvelope].set_envelope_time(0, toneAttackTimes.interp(val));
+				envelopes[toneEnvelope].set_envelope_time(1, toneHoldTimes.interp(val));
+				envelopes[toneEnvelope].set_envelope_time(2, toneDecayTimes.interp(val));
+				envelopes[toneEnvelope].set_envelope_time(3, toneReleaseTimes.interp(val));
+				envelopes[toneEnvelope].set_sustain(toneBreakPoint.interp(val));
 				break;
-			case 7:
-				noiseEnvelope.set_time(val);
+			case 7: // noise envelope
+				envelopes[noiseEnvelope].set_envelope_time(0, map_value(val, 0.0f, 1.0f, 1.0f, 50.0f));
+				envelopes[noiseEnvelope].set_envelope_time(2, map_value(val, 0.0f, 1.0f, 30.0f, 100.0f));
+				envelopes[noiseEnvelope].set_envelope_time(3, map_value(val, 0.0f, 1.0f, 100.0f, 3000.0f));
+				envelopes[noiseEnvelope].set_sustain(map_value(val, 0.0f, 1.0f, 0.0f, 0.25f));
 				break;
 			case 8:
 				noiseBlend = val;
@@ -67,10 +101,9 @@ public:
 	}
 	virtual void set_samplerate(const float sr) override
 	{
-		pitchEnvelope.set_samplerate(sr);
-		toneEnvelope.set_samplerate(sr);
-		noiseEnvelope.set_samplerate(sr);
-		fmEnvelope.set_samplerate(sr);
+		for (int i = 0; i < 4; i++) {
+			envelopes[i].set_samplerate(sr);
+		}
 		osc.set_samplerate(sr);
 	}
 
@@ -103,10 +136,9 @@ public:
 	static inline bool s_registered = ModuleFactory::registerModuleType(typeID, description, create);
 
 private:
-	MacroEnvelope pitchEnvelope{false};
-	MacroEnvelope fmEnvelope{false};
-	MacroEnvelope toneEnvelope{false};
-	MacroEnvelope noiseEnvelope{false};
+	enum { pitchEnvelope, fmEnvelope, toneEnvelope, noiseEnvelope };
+
+	Envelope envelopes[4];
 	TwoOpFM osc;
 
 	float gateIn = 0;
@@ -115,4 +147,14 @@ private:
 	float noiseBlend = 0.5f;
 	float pitchAmount = 0;
 	float fmAmount = 0;
+
+	InterpArray<float, 4> pitchDecayTimes = {10, 10, 200, 500};
+	InterpArray<float, 4> pitchBreakPoint = {0, 0.1, 0.2, 1};
+	InterpArray<float, 4> pitchReleaseTimes = {50, 300, 500, 3000};
+
+	InterpArray<float, 7> toneAttackTimes = {1, 1, 3, 5, 7, 9, 20};
+	InterpArray<float, 6> toneHoldTimes = {0, 20, 50, 70, 100, 600};
+	InterpArray<float, 3> toneDecayTimes = {10, 200, 600};
+	InterpArray<float, 4> toneBreakPoint = {0.1, 0.2, 0.8};
+	InterpArray<float, 3> toneReleaseTimes = {10, 500, 4000};
 };
