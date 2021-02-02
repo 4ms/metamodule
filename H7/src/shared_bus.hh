39241d00935f23ffc6c9d3bdd3c1ebaaeea910af
Dan Green <danngreen1@gmail.com>
2021-02-01 22:23:34 -0800

    Updated mdrivlib with new namespacing. Removed SharedBus

--- H7/src/shared_bus.hh
+++ H7/src/shared_bus.hh
@@ -1,32 +1,13 @@
-#pragma once
-#include "conf/i2c_conf.hh"
-#include "drivers/i2c.hh"
-#include "drivers/pin.hh"
+// #pragma once
+// #include "drivers/i2c.hh"
 
-class SharedBus {
-public:
-	static inline I2CPeriph i2c;
+// class SharedBus {
+// public:
+// 	static inline I2CPeriph i2c;
 
-	SharedBus(const I2CConfig &defs)
-	{
-		Pin sda{defs.SDA.gpio,
-				defs.SDA.pin,
-				PinMode::Alt,
-				defs.SDA.af,
-				PinPull::Up,
-				PinPolarity::Normal,
-				PinSpeed::High,
-				PinOType::OpenDrain};
-		Pin scl{defs.SCL.gpio,
-				defs.SCL.pin,
-				PinMode::Alt,
-				defs.SCL.af,
-				PinPull::Up,
-				PinPolarity::Normal,
-				PinSpeed::High,
-				PinOType::OpenDrain};
-
-		i2c.init(defs.I2Cx, defs.timing);
-	}
-};
+// 	SharedBus(const I2CConfig &defs)
+// 	{
+// 		i2c.init(defs);
+// 	}
+// };
 
