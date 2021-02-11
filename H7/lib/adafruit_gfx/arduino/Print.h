e23e95ff5d89963a504ef276dbba60ffbb1dd870
Dan Green <danngreen1@gmail.com>
2021-02-10 17:12:14 -0800

    adafruit: formatted print.cpp/h

--- H7/lib/adafruit_gfx/arduino/Print.h
+++ H7/lib/adafruit_gfx/arduino/Print.h
@@ -20,9 +20,10 @@
 #ifndef Print_h
 #define Print_h
 
+#include <cstring>
 #include <inttypes.h>
-#include <stdio.h> // for size_t
 #include <stdarg.h> // for printf
+#include <stdio.h>	// for size_t
 
 // #include "WString.h"
 #include "Printable.h"
@@ -36,76 +37,80 @@
 #define SUPPORT_LONGLONG
 
 class Print {
-  private:
-    int write_error;
-    size_t printNumber(unsigned long, uint8_t);
+private:
+	int write_error;
+	size_t printNumber(unsigned long, uint8_t);
 #ifdef SUPPORT_LONGLONG
-    void printLLNumber(uint64_t, uint8_t);
+	void printLLNumber(uint64_t, uint8_t);
 #endif
-    size_t printFloat(double, uint8_t);
-  protected:
-    void setWriteError(int err = 1)
-    {
-      write_error = err;
-    }
-  public:
-    Print() : write_error(0) {}
+	size_t printFloat(double, uint8_t);
 
-    int getWriteError()
-    {
-      return write_error;
-    }
-    void clearWriteError()
-    {
-      setWriteError(0);
-    }
+protected:
+	void setWriteError(int err = 1)
+	{
+		write_error = err;
+	}
 
-    virtual size_t write(uint8_t) = 0;
-    size_t write(const char *str)
-    {
-      if (str == NULL) {
-        return 0;
-      }
-      return write((const uint8_t *)str, strlen(str));
-    }
-    virtual size_t write(const uint8_t *buffer, size_t size);
-    size_t write(const char *buffer, size_t size)
-    {
-      return write((const uint8_t *)buffer, size);
-    }
+public:
+	Print()
+		: write_error(0)
+	{}
+
+	int getWriteError()
+	{
+		return write_error;
+	}
+	void clearWriteError()
+	{
+		setWriteError(0);
+	}
+
+	virtual size_t write(uint8_t) = 0;
+	size_t write(const char *str)
+	{
+		if (str == NULL) {
+			return 0;
+		}
+		return write((const uint8_t *)str, strlen(str));
+	}
+	virtual size_t write(const uint8_t *buffer, size_t size);
+	size_t write(const char *buffer, size_t size)
+	{
+		return write((const uint8_t *)buffer, size);
+	}
 
 	// size_t print(const __FlashStringHelper *);
 	// size_t print(const String &);
-    size_t print(const char[]);
-    size_t print(char);
-    size_t print(unsigned char, int = DEC);
-    size_t print(int, int = DEC);
-    size_t print(unsigned int, int = DEC);
-    size_t print(long, int = DEC);
-    size_t print(unsigned long, int = DEC);
-    size_t print(double, int = 2);
-    size_t print(const Printable &);
+	size_t print(const char[]);
+	size_t print(char);
+	size_t print(unsigned char, int = DEC);
+	size_t print(int, int = DEC);
+	size_t print(unsigned int, int = DEC);
+	size_t print(long, int = DEC);
+	size_t print(unsigned long, int = DEC);
+	size_t print(double, int = 2);
+	size_t print(const Printable &);
 
 	// size_t println(const __FlashStringHelper *);
 	// size_t println(const String &s);
-    size_t println(const char[]);
-    size_t println(char);
-    size_t println(unsigned char, int = DEC);
-    size_t println(int, int = DEC);
-    size_t println(unsigned int, int = DEC);
-    size_t println(long, int = DEC);
-    size_t println(unsigned long, int = DEC);
-    size_t println(double, int = 2);
-    size_t println(const Printable &);
-    size_t println(void);
+	size_t println(const char[]);
+	size_t println(char);
+	size_t println(unsigned char, int = DEC);
+	size_t println(int, int = DEC);
+	size_t println(unsigned int, int = DEC);
+	size_t println(long, int = DEC);
+	size_t println(unsigned long, int = DEC);
+	size_t println(double, int = 2);
+	size_t println(const Printable &);
+	size_t println(void);
 #ifdef SUPPORT_LONGLONG
-    void println(int64_t, uint8_t = DEC);
-    void print(int64_t, uint8_t = DEC);
-    void println(uint64_t, uint8_t = DEC);
-    void print(uint64_t, uint8_t = DEC);
+	void println(int64_t, uint8_t = DEC);
+	void print(int64_t, uint8_t = DEC);
+	void println(uint64_t, uint8_t = DEC);
+	void print(uint64_t, uint8_t = DEC);
 #endif
 
-    int printf(const char *format, ...);
+	int printf(const char *format, ...);
 	// int printf(const __FlashStringHelper *format, ...);
 };
 
