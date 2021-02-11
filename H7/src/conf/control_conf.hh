bd29eedbb8c692e887b4abc381172fe9af3ac71b
Dan Green <danngreen1@gmail.com>
2021-02-10 17:08:48 -0800

    slowing down SPI ADC

--- H7/src/conf/control_conf.hh
+++ H7/src/conf/control_conf.hh
@@ -23,7 +23,7 @@ constexpr SpiConfig<kSpiAdcConfPeriphNum, kSpiAdcConfNumChips> spi_adc_conf = {
 		},
 
 	.use_hardware_ss = false,
-	.clock_division = 64,
+	.clock_division = 256,
 	.data_size = 16,
 	.data_dir = SpiDataDir::Duplex,
 };
