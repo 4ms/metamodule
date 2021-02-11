421c91c7c5a2724a2ce56f80a6a041e97da6676a
Dan Green <danngreen1@gmail.com>
2021-02-10 17:08:36 -0800

    Separated Debouncer and state machine

--- shared/util/debouncer.hh
+++ shared/util/debouncer.hh
@@ -1,28 +1,17 @@
+#pragma once
 #include <cstdint>
 
-template<unsigned RisingEdgePattern = 0x0000000F, unsigned FallingEdgePattern = 0xFFFFFFF0>
-struct Debouncer {
-	Debouncer()
-		: debounce_state_{0}
-		, is_high_{false}
+struct Button {
+	Button()
+		: is_high_{false}
 		, got_rising_edge_{false}
 		, got_falling_edge_{false}
 	{}
 
-	void register_state(unsigned new_state)
+	bool is_high() const
 	{
-		debounce_state_ = (debounce_state_ << 1) | new_state;
-		if (debounce_state_ == RisingEdgePattern) {
-			got_rising_edge_ = true;
-			is_high_ = true;
-			got_falling_edge_ = false;
-		} else if (debounce_state_ == FallingEdgePattern) {
-			got_rising_edge_ = false;
-			is_high_ = false;
-			got_falling_edge_ = true;
-		}
+		return is_high_;
 	}
-
 	bool is_pressed() const
 	{
 		return is_high_;
@@ -36,6 +25,10 @@ struct Debouncer {
 		} else
 			return false;
 	}
+	bool just_went_high()
+	{
+		return is_just_pressed();
+	}
 
 	bool is_just_released()
 	{
@@ -45,11 +38,47 @@ struct Debouncer {
 		} else
 			return false;
 	}
+	bool just_went_low()
+	{
+		return is_just_released();
+	}
 
-private:
+	void register_rising_edge()
+	{
+		is_high_ = true;
+		got_rising_edge_ = true;
+		got_falling_edge_ = false;
+	}
+	void register_falling_edge()
+	{
+		is_high_ = false;
+		got_falling_edge_ = true;
+		got_rising_edge_ = false;
+	}
+
+protected:
 	bool is_high_;
 	bool got_rising_edge_;
 	bool got_falling_edge_;
+};
+
+template<unsigned RisingEdgePattern = 0x0000000F, unsigned FallingEdgePattern = 0xFFFFFFF0>
+struct Debouncer : Button {
+	Debouncer()
+		: debounce_state_{0}
+	{}
+
+	void register_state(unsigned new_state)
+	{
+		debounce_state_ = (debounce_state_ << 1) | new_state;
+		if (debounce_state_ == RisingEdgePattern) {
+			register_rising_edge();
+		} else if (debounce_state_ == FallingEdgePattern) {
+			register_falling_edge();
+		}
+	}
+
+private:
 	unsigned debounce_state_;
 };
 
