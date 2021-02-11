5f842947583e44ee222cb1b8cdd68f9a6071f008
Dan Green <danngreen1@gmail.com>
2021-02-10 17:09:44 -0800

    Screen boots up with a demo, slow but works

--- H7/src/screen.cc
+++ H7/src/screen.cc
@@ -1,93 +1,12 @@
+#define PROGMEM
 #include "screen.hh"
-#include "screen_demo_pic.h"
-// const unsigned int pic[19200];
-
-#define ST_CMD_DELAY 0x80 // special signifier for command lists
-
-#define ST77XX_NOP 0x00
-#define ST77XX_SWRESET 0x01
-#define ST77XX_RDDID 0x04
-#define ST77XX_RDDST 0x09
-
-#define ST77XX_SLPIN 0x10
-#define ST77XX_SLPOUT 0x11
-#define ST77XX_PTLON 0x12
-#define ST77XX_NORON 0x13
-
-#define ST77XX_INVOFF 0x20
-#define ST77XX_INVON 0x21
-#define ST77XX_DISPOFF 0x28
-#define ST77XX_DISPON 0x29
-#define ST77XX_CASET 0x2A
-#define ST77XX_RASET 0x2B
-#define ST77XX_RAMWR 0x2C
-#define ST77XX_RAMRD 0x2E
-
-#define ST77XX_PTLAR 0x30
-#define ST77XX_TEOFF 0x34
-#define ST77XX_TEON 0x35
-#define ST77XX_MADCTL 0x36
-#define ST77XX_COLMOD 0x3A
-
-#define ST77XX_MADCTL_MY 0x80
-#define ST77XX_MADCTL_MX 0x40
-#define ST77XX_MADCTL_MV 0x20
-#define ST77XX_MADCTL_ML 0x10
-#define ST77XX_MADCTL_RGB 0x00
-
-#define ST77XX_RDID1 0xDA
-#define ST77XX_RDID2 0xDB
-#define ST77XX_RDID3 0xDC
-#define ST77XX_RDID4 0xDD
-
-// Some ready-made 16-bit ('565') color settings:
-#define ST77XX_BLACK 0x0000
-#define ST77XX_WHITE 0xFFFF
-#define ST77XX_RED 0xF800
-#define ST77XX_GREEN 0x07E0
-#define ST77XX_BLUE 0x001F
-#define ST77XX_CYAN 0x07FF
-#define ST77XX_MAGENTA 0xF81F
-#define ST77XX_YELLOW 0xFFE0
-#define ST77XX_ORANGE 0xFC00
-
-// clang-format off
-
-static const uint8_t generic_st7789[] =  {                
-									// Init commands for 7789 screens
-    9,                              //  9 commands in list:
-    ST77XX_SWRESET,   ST_CMD_DELAY, //  1: Software reset, no args, w/delay
-      150,                          //     ~150 ms delay
-    ST77XX_SLPOUT ,   ST_CMD_DELAY, //  2: Out of sleep mode, no args, w/delay
-      10,                           //      10 ms delay
-    ST77XX_COLMOD , 1+ST_CMD_DELAY, //  3: Set color mode, 1 arg + delay:
-      0x55,                         //     16-bit color
-      10,                           //     10 ms delay
-    ST77XX_MADCTL , 1,              //  4: Mem access ctrl (directions), 1 arg:
-      0x08,                         //     Row/col addr, bottom-top refresh
-    ST77XX_CASET  , 4,              //  5: Column addr set, 4 args, no delay:
-      0x00,
-      0,        					//     XSTART = 0
-      0,
-      240,  						//     XEND = 240
-    ST77XX_RASET  , 4,              //  6: Row addr set, 4 args, no delay:
-      0x00,
-      0,             				//     YSTART = 0
-      320>>8,
-      320&0xFF, 			 		//     YEND = 320
-    ST77XX_INVON  ,   ST_CMD_DELAY, //  7: hack
-      10,
-    ST77XX_NORON  ,   ST_CMD_DELAY, //  8: Normal display on, no args, w/delay
-      10,                           //     10 ms delay
-    ST77XX_DISPON ,   ST_CMD_DELAY, //  9: Main screen turn on, no args, delay
-      10 
-};
-
-// clang-format on
+#include "Fonts/FreeSans12pt7b.h"
+#include "Fonts/FreeSansBold12pt7b.h"
+#include "Fonts/FreeSansBold18pt7b.h"
+#include "drivers/spi_screen_ST77XX.hh"
 
 void Screen::init()
 {
-
 	if ((window_width == 240) && (window_height == 240)) { // 1.3" and 1.54" displays
 		_colstart = 0;
 		_rowstart = 80;
@@ -100,128 +19,124 @@ void Screen::init()
 	}
 
 	init_display(generic_st7789);
-	set_rotation(0);
-	return;
-
-	// RES = 0;
-	// delayms(10);
-	// RES = 1;
-	// delayms(120);
-
-	write<Cmd>(0x36); // MADCTL
-	// if (DERECTION == 0)
-	write<Data>(0x00);
-	// else if (DERECTION == 2)
-	// write<Data>(0x70);
-
-	write<Cmd>(0x3A); // COLMOD
-	write<Data>(0x05);
-
-	write<Cmd>(0xB2); //??
-	write<Data>(0x0C);
-	write<Data>(0x0C);
-	write<Data>(0x00);
-	write<Data>(0x33);
-	write<Data>(0x33);
-
-	write<Cmd>(0xB7);
-	write<Data>(0x35);
-
-	write<Cmd>(0xBB);
-	write<Data>(0x19);
-
-	write<Cmd>(0xC0);
-	write<Data>(0x2C);
-
-	write<Cmd>(0xC2);
-	write<Data>(0x01);
-
-	write<Cmd>(0xC3);
-	write<Data>(0x12);
-
-	write<Cmd>(0xC4);
-	write<Data>(0x20);
-
-	write<Cmd>(0xC6);
-	write<Data>(0x0F);
-
-	write<Cmd>(0xD0);
-	write<Data>(0xA4);
-	write<Data>(0xA1);
-
-	write<Cmd>(0xE0);
-	write<Data>(0xD0);
-	write<Data>(0x04);
-	write<Data>(0x0D);
-	write<Data>(0x11);
-	write<Data>(0x13);
-	write<Data>(0x2B);
-	write<Data>(0x3F);
-	write<Data>(0x54);
-	write<Data>(0x4C);
-	write<Data>(0x18);
-	write<Data>(0x0D);
-	write<Data>(0x0B);
-	write<Data>(0x1F);
-	write<Data>(0x23);
-
-	write<Cmd>(0xE1);
-	write<Data>(0xD0);
-	write<Data>(0x04);
-	write<Data>(0x0C);
-	write<Data>(0x11);
-	write<Data>(0x13);
-	write<Data>(0x2C);
-	write<Data>(0x3F);
-	write<Data>(0x44);
-	write<Data>(0x51);
-	write<Data>(0x2F);
-	write<Data>(0x1F);
-	write<Data>(0x1F);
-	write<Data>(0x20);
-	write<Data>(0x23);
-
-	write<Cmd>(0x21); // INVOFF
-
-	write<Cmd>(0x11); // SLPOUT
-	HAL_Delay(120);
-
-	write<Cmd>(0x29); // DISPON
+	set_rotation(1);
 }
 
 void Screen::demo()
 {
-	int n = 0;
-	set_pos(60, 40, 179, 199);
-	for (int j = 0; j < 160; j++) {
-		for (int i = 0; i < 120; i++) {
-			write<Data>(pic[n++]);
-		}
-	}
+	fillRect(0, 0, 240, 240, ST77XX::BLACK);
+	setFont(&FreeSansBold18pt7b);
+	setTextColor(ST77XX::WHITE);
+	setTextSize(1);
+	setCursor(2, 200);
+	print("MetaModule");
+
+	drawMockupBase();
 }
 
-// static void Screen::sendCommand(uint8_t cmd, const uint8_t *addr, uint8_t numDataBytes)
-// {
-// 	write<Cmd>(cmd);
-// 	while (numDataBytes--) {
-// 		write<Data>(*addr++);
-// 	}
-// }
+class Rgb {
+	uint16_t col;
 
+public:
+	Rgb(uint8_t r, uint8_t g, uint8_t b)
+		: col(((r & 0b011111) << 11) | ((g & 0b111111) << 5) | ((b & 0b011111)))
+	{}
+	uint16_t operator()()
+	{
+		return col;
+	}
+};
+
+static uint16_t bar_x[4], bar_y[4];
+static uint16_t col[4] = {
+	Rgb{20, 56, 12}(),
+	Rgb{12, 40, 28}(),
+	Rgb{30, 12, 12}(),
+	Rgb{12, 12, 31}(),
+};
+
+void Screen::draw_bar(uint16_t level, uint16_t x, uint16_t y, uint16_t col)
+{
+	fillRect(x + level, y, width() / 2 - 10 - level, 12, ST77XX::BLACK);
+	fillRect(x, y, level, 12, col);
+}
+
+void Screen::drawMockupBase()
+{
+	setTextWrap(false);
+
+	uint16_t cur_y = 0;
+
+	int16_t x, y;
+	uint16_t w, h;
+
+	setFont(&FreeSansBold18pt7b);
+	setTextColor(ST77XX::WHITE);
+	getTextBounds("Resonator", 0, 0, &x, &y, &w, &h);
+	setCursor((width() - w) / 2, h);
+	print("Resonator");
+
+	cur_y += h + 15;
+	uint16_t line1 = cur_y;
+
+	setFont(&FreeSansBold12pt7b);
+	setTextColor(col[0]);
+	getTextBounds("Freq 1", 0, 0, &x, &y, &w, &h);
+	setCursor(5, h + cur_y);
+	print("Freq 1");
+	bar_x[0] = 5;
+	bar_y[0] = cur_y + h + 5;
+	draw_bar(75, bar_x[0], bar_y[0], col[0]);
+
+	cur_y += h + 25;
+
+	setFont(&FreeSansBold12pt7b);
+	setTextColor(col[1]);
+	getTextBounds("Res (Q)", 0, 0, &x, &y, &w, &h);
+	setCursor(5, h + cur_y);
+	print("Res (Q)");
+	bar_x[1] = 5;
+	bar_y[1] = cur_y + h + 5;
+	draw_bar(75, 5, cur_y + h + 5, col[1]);
+
+	cur_y = line1;
+
+	setFont(&FreeSansBold12pt7b);
+	setTextColor(col[2]);
+	getTextBounds("Morph", 0, 0, &x, &y, &w, &h);
+	setCursor(width() - 5 - w, h + cur_y);
+	print("Morph");
+	bar_x[2] = width() / 2 + 5;
+	bar_y[2] = cur_y + h + 5;
+	draw_bar(60, width() / 2 + 5, cur_y + h + 5, col[2]);
+
+	cur_y += h + 25;
+
+	setFont(&FreeSansBold12pt7b);
+	setTextColor(col[3]);
+	getTextBounds("Level", 0, 0, &x, &y, &w, &h);
+	setCursor(width() - 5 - w, h + cur_y);
+	print("Level");
+	bar_x[3] = width() / 2 + 5;
+	bar_y[3] = cur_y + h + 5;
+	draw_bar(95, width() / 2 + 5, cur_y + h + 5, col[3]);
+}
+
+// Todo re-write as just a sequence of commands with delays, seriously...
 void Screen::init_display(const uint8_t *addr)
 {
 	uint8_t numCommands, cmd, numArgs;
 	uint16_t ms;
 
-	numCommands = *addr++;			 // Number of commands to follow
-	while (numCommands--) {			 // For each command...
-		cmd = *addr++;				 // Read command
-		numArgs = *addr++;			 // Number of args to follow
-		ms = numArgs & ST_CMD_DELAY; // If hibit set, delay follows args
-		numArgs &= ~ST_CMD_DELAY;	 // Mask out delay bit
-		write<Cmd>(cmd);
+	numCommands = *addr++;					 // Number of commands to follow
+	while (numCommands--) {					 // For each command...
+		cmd = *addr++;						 // Read command
+		numArgs = *addr++;					 // Number of args to follow
+		ms = numArgs & ST77XX::ST_CMD_DELAY; // If hibit set, delay follows args
+		numArgs &= ~ST77XX::ST_CMD_DELAY;	 // Mask out delay bit
+		transmit<Cmd>(cmd);
 		while (numArgs--) {
-			write<Data>(*addr++);
+			transmit<Data>(*addr++);
 		}
 
 		if (ms) {
@@ -236,25 +151,25 @@ void Screen::set_rotation(uint8_t m)
 {
 	uint8_t madctl = 0;
 
-	rotation = m & 3; // can't be higher than 3
+	rotation = m & 3;
 
 	switch (rotation) {
 		case 0:
-			madctl = ST77XX_MADCTL_MX | ST77XX_MADCTL_MY | ST77XX_MADCTL_RGB;
+			madctl = ST77XX::MADCTL_MX | ST77XX::MADCTL_MY | ST77XX::MADCTL_RGB;
 			_xstart = _colstart;
 			_ystart = _rowstart;
 			_width = window_width;
 			_height = window_height;
 			break;
 		case 1:
-			madctl = ST77XX_MADCTL_MY | ST77XX_MADCTL_MV | ST77XX_MADCTL_RGB;
+			madctl = ST77XX::MADCTL_MY | ST77XX::MADCTL_MV | ST77XX::MADCTL_RGB;
 			_xstart = _rowstart;
 			_ystart = _colstart;
 			_height = window_width;
 			_width = window_height;
 			break;
 		case 2:
-			madctl = ST77XX_MADCTL_RGB;
+			madctl = ST77XX::MADCTL_RGB;
 			if ((window_width == 135) && (window_height == 240)) {
 				_xstart = _colstart - 1;
 				_ystart = _rowstart;
@@ -266,7 +181,7 @@ void Screen::set_rotation(uint8_t m)
 			_height = window_height;
 			break;
 		case 3:
-			madctl = ST77XX_MADCTL_MX | ST77XX_MADCTL_MV | ST77XX_MADCTL_RGB;
+			madctl = ST77XX::MADCTL_MX | ST77XX::MADCTL_MV | ST77XX::MADCTL_RGB;
 			if ((window_width == 135) && (window_height == 240)) {
 				_xstart = _rowstart;
 				_ystart = _colstart;
@@ -278,23 +193,28 @@ void Screen::set_rotation(uint8_t m)
 			_width = window_height;
 			break;
 	}
-	write<Cmd>(ST77XX_MADCTL);
-	write<Data>(madctl);
+	transmit<Cmd>(ST77XX::MADCTL);
+	transmit<Data>(madctl);
 }
 
 void Screen::set_pos(uint16_t Xstart, uint16_t Ystart, uint16_t Xend, uint16_t Yend)
 {
-	write<Cmd>(ST77XX_CASET);
-	write<Data>(Xstart >> 8);
-	write<Data>(Xstart);
-	write<Data>(Xend >> 8);
-	write<Data>(Xend);
+	Xstart += _xstart;
+	Ystart += _ystart;
+	Xend += _xstart;
+	Yend += _ystart;
+	transmit<Cmd>(ST77XX::CASET);
+	transmit<Data>(Xstart >> 8);
+	transmit<Data>(Xstart);
+	transmit<Data>(Xend >> 8);
+	transmit<Data>(Xend);
 
-	write<Cmd>(ST77XX_RASET);
-	write<Data>(Ystart >> 8);
-	write<Data>(Ystart);
-	write<Data>(Yend >> 8);
-	write<Data>(Yend);
+	transmit<Cmd>(ST77XX::RASET);
+	transmit<Data>(Ystart >> 8);
+	transmit<Data>(Ystart);
+	transmit<Data>(Yend >> 8);
+	transmit<Data>(Yend);
 
-	write<Cmd>(ST77XX_RAMWR);
+	transmit<Cmd>(ST77XX::RAMWR);
 }
+
--- H7/src/screen.hh
+++ H7/src/screen.hh
@@ -1,5 +1,5 @@
 #pragma once
-// #include "Adafruit_ST7789.h"
+#include "Adafruit_GFX_Library/Adafruit_GFX.h"
 #include "conf/screen_conf.hh"
 #include "drivers/pin.hh"
 #include "drivers/spi.hh"
@@ -8,17 +8,26 @@
 
 class mmooo {};
 
-class Screen : MMScreenDriverT {
+class Screen : public MMScreenDriverT, Adafruit_GFX {
 public:
 	Screen(const MMScreenConf &conf)
-		: MMScreenDriverT{conf.screen_SPI_conf}
+		: Adafruit_GFX{240, 240}
+		, MMScreenDriverT{conf.screen_SPI_conf}
 		, window_height{MMScreenConf::width}
 		, window_width{MMScreenConf::height}
 	{}
 
 	void init();
-	void demo();
 	void set_rotation(uint8_t m);
+
+	virtual void drawPixel(int16_t x, int16_t y, uint16_t color) override
+	{
+		set_pos(x, y, 1, 1);
+		transmit<Data>(color >> 8);
+		transmit<Data>(color & 0xFF);
+	}
+
+	void demo();
 
 private:
 	int window_width;
@@ -33,5 +42,8 @@ private:
 
 	void set_pos(uint16_t Xstart, uint16_t Ystart, uint16_t Xend, uint16_t Yend);
 	void init_display(const uint8_t *addr);
+
+	void drawMockupBase();
+	void draw_bar(uint16_t level, uint16_t x, uint16_t y, uint16_t col);
 };
 
