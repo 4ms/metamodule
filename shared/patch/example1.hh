d808130f3a5f863b45fb99c842578c10159f136f
Dan Green <danngreen1@gmail.com>
2021-02-19 11:50:48 -0800

    freqshift/detune example (crashes as-is). Saved fadedelay example

--- shared/patch/example1.hh
+++ shared/patch/example1.hh
@@ -2,33 +2,26 @@
 static const Patch Example1 = { 
     .modules_used = {
         "PANEL_8",
-        "FADEDELAY",
-        "FADEDELAY",
+        "DETUNE",
+        "FREQSHIFT",
     },
     .num_modules = 3,
 
     .module_nodes = {{
         // PANEL_8
-        {1, 2, 1, 2, 255, 254, 253, 252, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        // FADEDELAY
-        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        // FADEDELAY
+        {2, 1, 2, 1, 255, 254, 253, 252, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // DETUNE
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // FREQSHIFT
+        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
     }},
 
     .nets = {{
         {
             .num_jacks = 2,
             .jacks = {{
-                {.module_id = 0, .jack_id = 0},
-                {.module_id = 2, .jack_id = 0},
-            }},
-        },
-        {
-            .num_jacks = 2,
-            .jacks = {{
-                {.module_id = 0, .jack_id = 1},
                 {.module_id = 1, .jack_id = 0},
+                {.module_id = 0, .jack_id = 1},
             }},
         },
         {
@@ -41,35 +34,40 @@ static const Patch Example1 = {
         {
             .num_jacks = 2,
             .jacks = {{
-                {.module_id = 1, .jack_id = 0},
                 {.module_id = 0, .jack_id = 1},
+                {.module_id = 1, .jack_id = 0},
+            }},
+        },
+        {
+            .num_jacks = 2,
+            .jacks = {{
+                {.module_id = 0, .jack_id = 0},
+                {.module_id = 2, .jack_id = 0},
             }},
         },
     }},
     .num_nets = 4,
 
     .static_knobs = {{
-        {.module_id = 1, .param_id = 0, .value = 0.382500f},
-        {.module_id = 1, .param_id = 1, .value = 0.352500f},
-        {.module_id = 1, .param_id = 2, .value = 0.355500f},
-        {.module_id = 1, .param_id = 3, .value = 0.432000f},
-        {.module_id = 2, .param_id = 0, .value = 0.258000f},
-        {.module_id = 2, .param_id = 1, .value = 0.322500f},
-        {.module_id = 2, .param_id = 2, .value = 0.232500f},
-        {.module_id = 2, .param_id = 3, .value = 0.373500f},
+        {.module_id = 1, .param_id = 0, .value = 0.346500f},
+        {.module_id = 1, .param_id = 1, .value = 0.564000f},
+        {.module_id = 1, .param_id = 2, .value = 0.261000f},
+        {.module_id = 1, .param_id = 3, .value = 0.579000f},
+        {.module_id = 2, .param_id = 0, .value = 0.285000f},
+        {.module_id = 2, .param_id = 1, .value = 0.157500f},
+        {.module_id = 2, .param_id = 2, .value = 0.291000f},
     }},
-    .num_static_knobs = 8,
+    .num_static_knobs = 7,
 
     .mapped_knobs = {{
         {.module_id = 2, .param_id = 0, .panel_knob_id = 0},
         {.module_id = 2, .param_id = 1, .panel_knob_id = 1},
         {.module_id = 2, .param_id = 2, .panel_knob_id = 2},
-        {.module_id = 2, .param_id = 3, .panel_knob_id = 3},
-        {.module_id = 1, .param_id = 0, .panel_knob_id = 4},
-        {.module_id = 1, .param_id = 1, .panel_knob_id = 5},
-        {.module_id = 1, .param_id = 2, .panel_knob_id = 6},
-        {.module_id = 1, .param_id = 3, .panel_knob_id = 7},
+        {.module_id = 1, .param_id = 0, .panel_knob_id = 3},
+        {.module_id = 1, .param_id = 1, .panel_knob_id = 4},
+        {.module_id = 1, .param_id = 2, .panel_knob_id = 5},
+        {.module_id = 1, .param_id = 3, .panel_knob_id = 6},
     }},
-    .num_mapped_knobs = 8,
+    .num_mapped_knobs = 7,
 
 };
---
+++ shared/patch/fadedelay_two.hh
@@ -0,0 +1,82 @@
+#include "patch.hh"
+static const Patch Example1 = {
+	.modules_used =
+		{
+			"PANEL_8",
+			"FADEDELAY",
+			"FADEDELAY",
+		},
+	.num_modules = 3,
+
+	.module_nodes = {{
+		// PANEL_8
+		{
+			1, 2, 1, 2, 255, 254, 253, 252, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		},
+		// FADEDELAY
+		{
+			2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		},
+		// FADEDELAY
+		{
+			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		},
+	}},
+
+	.nets = {{
+		{
+			.num_jacks = 2,
+			.jacks = {{
+				{.module_id = 0, .jack_id = 0},
+				{.module_id = 2, .jack_id = 0},
+			}},
+		},
+		{
+			.num_jacks = 2,
+			.jacks = {{
+				{.module_id = 0, .jack_id = 1},
+				{.module_id = 1, .jack_id = 0},
+			}},
+		},
+		{
+			.num_jacks = 2,
+			.jacks = {{
+				{.module_id = 2, .jack_id = 0},
+				{.module_id = 0, .jack_id = 0},
+			}},
+		},
+		{
+			.num_jacks = 2,
+			.jacks = {{
+				{.module_id = 1, .jack_id = 0},
+				{.module_id = 0, .jack_id = 1},
+			}},
+		},
+	}},
+	.num_nets = 4,
+
+	.static_knobs = {{
+		{.module_id = 1, .param_id = 0, .value = 0.382500f},
+		{.module_id = 1, .param_id = 1, .value = 0.352500f},
+		{.module_id = 1, .param_id = 2, .value = 0.355500f},
+		{.module_id = 1, .param_id = 3, .value = 0.432000f},
+		{.module_id = 2, .param_id = 0, .value = 0.258000f},
+		{.module_id = 2, .param_id = 1, .value = 0.322500f},
+		{.module_id = 2, .param_id = 2, .value = 0.232500f},
+		{.module_id = 2, .param_id = 3, .value = 0.373500f},
+	}},
+	.num_static_knobs = 8,
+
+	.mapped_knobs = {{
+		{.module_id = 2, .param_id = 0, .panel_knob_id = 0},
+		{.module_id = 2, .param_id = 1, .panel_knob_id = 1},
+		{.module_id = 2, .param_id = 2, .panel_knob_id = 2},
+		{.module_id = 2, .param_id = 3, .panel_knob_id = 3},
+		{.module_id = 1, .param_id = 0, .panel_knob_id = 4},
+		{.module_id = 1, .param_id = 1, .panel_knob_id = 5},
+		{.module_id = 1, .param_id = 2, .panel_knob_id = 6},
+		{.module_id = 1, .param_id = 3, .panel_knob_id = 7},
+	}},
+	.num_mapped_knobs = 8,
+
+};
