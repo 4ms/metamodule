0d1a2fcd025277a47e9589526c2225ba154d9cb4
Dan Green <danngreen1@gmail.com>
2021-02-26 09:48:14 -0800

    Makefile has OPTFLAGs for each file in separate file (makefile_opts.mk)

---
+++ H7/makefile_opts.mk
@@ -0,0 +1,18 @@
+#----------------------------------
+# Uncomment to compile unoptimized:
+# $(BUILDDIR)/src/m7/main.o: OPTFLAG = -O0
+# $(BUILDDIR)/src/screen.o: OPTFLAG = -O0
+# $(BUILDDIR)/src/controls.o: OPTFLAG = -O0
+# $(BUILDDIR)/src/audio.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(PERIPH)/src/%.o: OPTFLAG = -O3
+# $(BUILDDIR)/$(PERIPH)/src/stm32h7xx_hal_qspi.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/sdram.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/system.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(PERIPH)/src/stm32h7xx_hal_i2c.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/pca9685_led_driver_dma.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/pca9685_led_driver.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/i2c.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/sai.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(DRIVERLIB)/drivers/qspi_flash_driver.o: OPTFLAG = -O0
+# $(BUILDDIR)/$(SHARED)/CoreModules/pitchshiftCore.o: OPTFLAG = -O0
+#-----------------------------------
