c7ba1526d8da7b7860b8d0cd0fb3d4eb5dbdff81
Dan Green <danngreen1@gmail.com>
2021-02-26 13:43:37 -0800

    ReverbCore/Module: added Ratio knobs for AllPass and Comb
    Widened module to 16HP to make room for CVs and more knobs

--- shared/CoreModules/reverbCore.h
+++ shared/CoreModules/reverbCore.h
@@ -20,7 +20,7 @@ public:
 
 		wetSignal /= static_cast<float>(numComb);
 
-		for (int i = 0; i < numAll; i++) {
+		for (int i = 0; i < numAllpass; i++) {
 			wetSignal = apFilter[i].process(wetSignal);
 		}
 
@@ -29,50 +29,140 @@ public:
 
 	ReverbCore()
 	{
-		for (int i = 0; i < numAll; i++) {
-			apFilter[i].setLength(allTuning[i]);
+		globalAllpassAtten = 1.0f;
+		globalCombAtten = 1.0f;
+		for (int i = 0; i < numAllpass; i++) {
+			manualKnobAllpassAtten[i] = 1.0f;
+			ratioKnobAllpassAtten[i] = 1.0f;
+			apFilter[i].setLength(maxAllpassTuning[i]);
 			apFilter[i].setFeedback(0.6f);
 			apFilter[i].setFadeSpeed(0.001f);
-			currentAllTunning[i] = allTuning[i];
 		}
 
 		for (int i = 0; i < numComb; i++) {
+			manualKnobCombAtten[i] = 1.0f;
+			ratioKnobCombAtten[i] = 1.0f;
 			combFilter[i].setFeedback(0);
-			combFilter[i].setLength(combTuning[i]);
-			currentCombTuning[i] = combTuning[i];
+			combFilter[i].setLength(maxCombTuning[i]);
+			combFilter[i].setFadeSpeed(0.001f);
 		}
+	}
+
+	enum Knobs {
+		Size = 0,
+		Damp = 1,
+		Mix = 2,
+		Time = 3,
+		AllpassRatio = 4,
+		CRatio = 5,
+		Allpass1,
+		Allpass2,
+		Allpass3,
+		Allpass4,
+		C1,
+		C2,
+		C3,
+		C4,
+		C5,
+		C6,
+		C7,
+		C8,
+	};
+
+	float getAllpassTuning(const int id)
+	{
+		return currentAllpassTuning[id];
+	}
+	float getCombTuning(const int id)
+	{
+		return currentCombTuning[id];
+	}
+	void updateAllpassTuning(const int id)
+	{
+		currentAllpassTuning[id] =
+			maxAllpassTuning[id] * globalAllpassAtten * manualKnobAllpassAtten[id] * ratioKnobAllpassAtten[id];
+		if (currentAllpassTuning[id] < 1.f)
+			currentAllpassTuning[id] = 1.f;
+		apFilter[id].setLength(currentAllpassTuning[id]);
+	}
+
+	void updateCombTuning(const int id)
+	{
+		currentCombTuning[id] = maxCombTuning[id] * globalCombAtten * manualKnobCombAtten[id] * ratioKnobCombAtten[id];
+		if (currentCombTuning[id] < 1.f)
+			currentCombTuning[id] = 1.f;
+		combFilter[id].setLength(currentCombTuning[id]);
 	}
 
 	virtual void set_param(int const param_id, const float val) override
 	{
 		switch (param_id) {
-			case 0: // size
+			case Size:
+				globalAllpassAtten = val;
+				globalCombAtten = val;
 				for (int i = 0; i < numComb; i++) {
-					currentCombTuning[i] = combTuning[i] * val;
-					if (currentCombTuning[i] < 1)
-						currentCombTuning[i] = 1;
-					combFilter[i].setLength(currentCombTuning[i]);
+					updateCombTuning(i);
 				}
-				for (int i = 0; i < numAll; i++) {
-					currentAllTunning[i] = allTuning[i] * val;
-					if (currentAllTunning[i] < 1)
-						currentAllTunning[i] = 1;
-					apFilter[i].setLength(currentAllTunning[i]);
+				for (int i = 0; i < numAllpass; i++) {
+					updateAllpassTuning(i);
 				}
 				break;
-			case 1: // damp
+
+			case Damp:
 				for (int i = 0; i < numComb; i++) {
 					combFilter[i].setDamp(val);
 				}
 				break;
-			case 2:
+
+			case Mix:
 				mix = val;
 				break;
-			case 3: // time
+
+			case Time:
 				for (int i = 0; i < numComb; i++) {
 					combFilter[i].setFeedback(map_value(val, 0.0f, 1.0f, 0.8f, 0.99f));
 				}
 				break;
+
+			case AllpassRatio: {
+				int ival = (int)(val * 48);		// 0...48
+				float fval = ival / 12.f + 1.f; // 1..5, steps of 0.08333
+				for (int i = 0; i < numAllpass; i++) {
+					ratioKnobAllpassAtten[i] = i == 0 ? 1.f : ratioKnobAllpassAtten[i - 1] / fval;
+					updateAllpassTuning(i);
+				}
+			} break;
+
+			case CRatio: {
+				int ival = (int)(val * 48);
+				float fval = ival / 12.f + 1.f;
+				for (int i = 0; i < numComb; i++) {
+					ratioKnobCombAtten[i] = i == 0 ? 1.f : ratioKnobCombAtten[i - 1] / fval;
+					updateCombTuning(i);
+				}
+			} break;
+
+			case Allpass1:
+			case Allpass2:
+			case Allpass3:
+			case Allpass4: {
+				int i = param_id - Allpass1;
+				manualKnobAllpassAtten[i] = val;
+				updateAllpassTuning(i);
+			} break;
+
+			case C1:
+			case C2:
+			case C3:
+			case C4:
+			case C5:
+			case C6:
+			case C7:
+			case C8: {
+				int i = param_id - C1;
+				manualKnobCombAtten[i] = val;
+				updateCombTuning(i);
+			} break;
 		}
 	}
 	virtual void set_samplerate(const float sr) override
@@ -112,17 +202,30 @@ private:
 	float signalIn = 0;
 	float signalOut = 0;
 
-	static const int numComb = 8;
-	static const int numAll = 4;
+	static constexpr int numAllpass = 4;
+	static constexpr int numComb = 8;
 
-	static constexpr int allTuning[numAll] = {1248, 812, 358, 125};
-	static constexpr int combTuning[numComb] = {3000, 4003, 4528, 5217, 1206, 2108, 3337, 5003};
+	// static constexpr int defaultAllpassTuning[numAllpass] = {1248, 812, 358, 125};
+	// static constexpr int defaultCombTuning[numComb] = {3000, 4003, 4528, 5217, 1206, 2108, 3337, 5003};
+	static constexpr int maxAllpassTuning[numAllpass] = {6000, 6000, 6000, 6000};
+	static constexpr int maxCombTuning[numComb] = {6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000};
 
-	int currentAllTunning[numAll];
-	int currentCombTuning[numComb];
+	float globalAllpassAtten;
+	float globalCombAtten;
+	float manualKnobAllpassAtten[numAllpass];
+	float manualKnobCombAtten[numComb];
 
+	float ratioKnobAllpassAtten[numAllpass];
+	float ratioKnobCombAtten[numComb];
+	// Todo: Add CV
+	// float ratioCvAllpassAtten[numAllpass];
+	// float ratioCvCombAtten[numAllpass];
+
+	float currentAllpassTuning[numAllpass];
+	float currentCombTuning[numComb];
+
+	AllPass<6000> apFilter[numAllpass];
 	Comb<6000> combFilter[numComb];
-	AllPass<6000> apFilter[numAll];
 
 	float mix = 0;
 };
--- vcv/src/reverbModule.cpp
+++ vcv/src/reverbModule.cpp
@@ -4,7 +4,15 @@
 
 struct ReverbModule : CommModule {
 
-	enum ParamIds { SIZE_PARAM, DAMP_PARAM, MIX_PARAM, TIME_PARAM, NUM_PARAMS };
+	enum ParamIds {
+		SIZE_PARAM,
+		DAMP_PARAM,
+		MIX_PARAM,
+		TIME_PARAM,
+		APRATIO,
+		CRATIO,
+		NUM_PARAMS
+	};
 	enum InputIds { SIGNAL_INPUT, NUM_INPUTS };
 	enum OutputIds { SIGNAL_OUTPUT, NUM_OUTPUTS };
 	enum LightIds { NUM_LIGHTS };
@@ -34,11 +42,15 @@ struct ReverbWidget : CommModuleWidget {
 
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/8hpTemplate.svg")));
 
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/16hpTemplate.svg")));
 		addModuleTitle("VERB");
 		addLabeledKnob("SIZE", ReverbModule::SIZE_PARAM, {0, 0});
-		addLabeledKnob("TIME",ReverbModule::TIME_PARAM,{1,0});
-		addLabeledKnob("DAMP", ReverbModule::DAMP_PARAM, {0, 1});
-		addLabeledKnob("MIX", ReverbModule::MIX_PARAM, {1, 1});
+		addLabeledKnob("TIME", ReverbModule::TIME_PARAM, {1, 0});
+		addLabeledKnob("DAMP", ReverbModule::DAMP_PARAM, {2, 0});
+		addLabeledKnob("MIX", ReverbModule::MIX_PARAM, {3, 0});
+
+		addLabeledKnob("AP Ratio", ReverbModule::APRATIO, {0, 1});
+		addLabeledKnob("Comb Ratio", ReverbModule::CRATIO, {1, 1});
 		addLabeledInput("IN", ReverbModule::SIGNAL_INPUT, {0, 0});
 		addLabeledOutput("OUT", ReverbModule::SIGNAL_OUTPUT, {1, 0});
 	}
