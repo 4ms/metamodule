4ad44b7292ab4ecf9179ca8b540b5d1e87b7ac70
Dan Green <danngreen1@gmail.com>
2021-03-01 18:11:19 -0800

    CM4 core toggles pin when running

--- H7/src/m4/main.cc
+++ H7/src/m4/main.cc
@@ -2,6 +2,9 @@
 #include "drivers/hsem.hh"
 #include "drivers/rcc.hh"
 #include "drivers/stm32xx.h"
+
+#include "debug.hh"
+
 void main(void)
 {
 	target::RCC_Control::HSEM_::set();
@@ -10,19 +13,20 @@ void main(void)
 
 	target::HSEM_::template IER<HSEM_id_mask>::set();
 
-	// HAL_HSEM_ActivateNotification(HSEM_id_mask);
-
 	// Domain D2 goes to STOP mode (Cortex-M4 in deep-sleep) waiting for Cortex-M7 to
 	// perform system initialization (system clock config, external memory configuration.. )
 	HAL_PWREx_ClearPendingEvent();
 	HAL_PWREx_EnterSTOPMode(PWR_MAINREGULATOR_ON, PWR_STOPENTRY_WFE, PWR_D2_DOMAIN);
 
-	__HAL_HSEM_CLEAR_FLAG(HSEM_id_mask);
+	// __HAL_HSEM_CLEAR_FLAG(HSEM_id_mask);
+	target::HSEM_::template ICR<HSEM_id_mask>::set();
 
 	HAL_Init();
 	target::RCC_Control::SYSCFG_::set();
 
 	while (1) {
+		Debug::Pin3::high();
+		Debug::Pin3::low();
 	}
 }
 
