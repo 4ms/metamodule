c0b5930e4db5bfb35b9ce36ed7610306ff57731b
Ben Davis <bendedavis@gmail.com>
2021-03-15 14:17:07 -0700

    v/oct input overrides ratio settings

--- shared/CoreModules/fmoscCore.h
+++ shared/CoreModules/fmoscCore.h
@@ -11,12 +11,19 @@ class FmoscCore : public CoreProcessor {
 public:
 	virtual void update(void) override
 	{
-		fm.set_frequency(basePitch * expTable.interp(constrain(pitchInput, 0.0f, 1.0f)));
+		fm.set_frequency(0, basePitch * setPitchMultiple(pitchInput));
+		if (secondPitchConnected==false) {
+			fm.set_frequency(1, basePitch*ratioFine*ratioCoarse);
+		}
+		else
+		{
+			fm.set_frequency(1,basePitch*setPitchMultiple(secondPitchInput));
+		}
 		totalIndex = constrain(indexCV * indexAmount + indexKnob, 0.0f, 1.0f);
 		float totalShape = constrain(shapeCV * shapeAmount + shapeKnob, 0.0f, 1.0f);
 		fm.shape = totalShape;
 		fm.modAmount = totalIndex;
-		float finalMix = constrain(mixCV+mix,0.0f,1.0f);
+		float finalMix = constrain(mixCV + mix, 0.0f, 1.0f);
 		fm.mix = finalMix;
 		mainOutput = fm.update();
 	}
@@ -33,13 +40,13 @@ public:
 				indexKnob = val;
 				break;
 			case 2:
-				fm.ratioCoarse = map_value(val, 0.0f, 1.0f, 0, 7);
+				ratioCoarse = map_value(val, 0.0f, 1.0f, 0, 7);
 				break;
 			case 3: {
 				if (val < 0.5f)
-					fm.ratioFine = map_value(val, 0.0f, 0.5f, 0.5f, 1.0f);
+					ratioFine = map_value(val, 0.0f, 0.5f, 0.5f, 1.0f);
 				else
-					fm.ratioFine = map_value(val, 0.5f, 1.0f, 1.0f, 2.0f);
+					ratioFine = map_value(val, 0.5f, 1.0f, 1.0f, 2.0f);
 				break;
 			}
 			case 4:
@@ -76,6 +83,9 @@ public:
 			case 3:
 				mixCV = val;
 				break;
+			case 4:
+				secondPitchInput = val;
+				break;
 		}
 	}
 
@@ -90,6 +100,19 @@ public:
 		return output;
 	}
 
+	virtual void mark_input_unpatched(const int input_id) override
+	{
+		if (input_id == 4) {
+			secondPitchConnected = false;
+		}
+	}
+	virtual void mark_input_patched(const int input_id) override
+	{
+		if (input_id == 4) {
+			secondPitchConnected = true;
+		}
+	}
+
 	static std::unique_ptr<CoreProcessor> create()
 	{
 		return std::make_unique<FmoscCore>();
@@ -100,11 +123,15 @@ public:
 
 private:
 	TwoOpFM fm;
+	float ratioCoarse = 1;
+	float ratioFine = 1;
 	float mix = 0;
 	float mixCV = 0;
 	float basePitch = 0;
 	float mainOutput;
 	float indexKnob = 0;
+	float secondPitchInput = 0;
+	bool secondPitchConnected = false;
 	float indexCV = 0;
 	float shapeKnob = 0;
 	float shapeCV = 0;
