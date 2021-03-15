973769e94ed9651e094438a075e9126f7c411256
Ben Davis <bendedavis@gmail.com>
2021-03-15 14:18:07 -0700

    updated FM oscillator to take frequency for both operators

--- shared/processors/twoOpFMOscillator.h
+++ shared/processors/twoOpFMOscillator.h
@@ -8,65 +8,46 @@ private:
 	static constexpr uint32_t max_ = 0xFFFFFFFF;
 
 	uint32_t phaccu[2];
-	const float ratioTable[8] = {0.0625f, 0.125f, 0.25f, 0.5f, 1.0f, 2.0f, 4.0f, 8.0f};
 	int sampleRate = 48000;
 	float sinOut[2];
-	uint32_t increment[2];
-
-	float currentFreq = 1;
-	float lastFreq = 1;
-
-	float currentRatio = 1;
-	float lastRatio = 1;
-
-	void updateIncrement()
-	{
-		increment[0] = currentFreq * (currentRatio) * (max_ / sampleRate);
-	}
 
 public:
-	int ratioCoarse = 4;
-	float ratioFine = 1;
+
 	float modAmount = 0;
 	float shape = 0;
 	float mix = 0;
+	float priFreq=1;
+	float secFreq=1;
 
 	TwoOpFM() {}
 
-	void set_frequency(float _freq)
+	void set_frequency(int channel, float inputFreq)
 	{
-		lastFreq = currentFreq;
-		currentFreq = _freq;
-		if (currentFreq != lastFreq) {
-			updateIncrement();
+		if(channel == 0)
+		priFreq = inputFreq;
+		else if (channel ==1)
+		{
+			secFreq=inputFreq;
 		}
-	}
-
-	void updateRatio()
-	{
-		lastRatio = currentRatio;
-		currentRatio = ratioTable[ratioCoarse] * ratioFine;
-		if (currentRatio != lastRatio) {
-			updateIncrement();
-		}
+		
 	}
 
 	float update(void)
 	{
-		updateRatio();
-		phaccu[0] += increment[0];
-		sinOut[0] = sinTable[phaccu[0] >> 21];
-		auto freqCalc = (currentFreq + (sinOut[0] * modAmount * 4000.0f));
-		increment[1] = freqCalc * (max_ / sampleRate);
-		phaccu[1] += increment[1];
-
+		phaccu[1] += secFreq * (max_/sampleRate);
 		sinOut[1] = sinTable[((phaccu[1] + max_ / 2) & max_) >> 21];
-		auto sqrOut1 = (phaccu[1] > (max_ / 2)) * 2.0f - 1.0f;
-		auto sqrOut2 = (phaccu[0] > (max_ / 2)) * 2.0f - 1.0f;
 
-		auto finalWav1 = MathTools::interpolate(sinOut[1], sqrOut1, shape);
-		auto finalWav2 = MathTools::interpolate(sinOut[0], sqrOut2, shape);
-		return (MathTools::interpolate(finalWav1,finalWav2,mix));
+
+		sinOut[0] = sinTable[phaccu[0] >> 21];
+		auto freqCalc = (priFreq + (sinOut[1] * modAmount * 4000.0f));
+		phaccu[0] += freqCalc*(max_/sampleRate);
+
+		auto sqrOut1 = (phaccu[0] > (max_ / 2)) * 2.0f - 1.0f;
+		auto sqrOut2 = (phaccu[1] > (max_ / 2)) * 2.0f - 1.0f;
+
+		auto finalWav1 = MathTools::interpolate(sinOut[0], sqrOut1, shape);
+		auto finalWav2 = MathTools::interpolate(sinOut[1], sqrOut2, shape);
+		return (MathTools::interpolate(finalWav1, finalWav2, mix));
 	}
 
 	void set_samplerate(float sr)
