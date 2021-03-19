598ab9c05578e4929175adec401b2ed3a92ce441
Ben Davis <bendedavis@gmail.com>
2021-03-19 15:28:34 -0700

    implement slew limiter controls

--- vcv/src/slewlimiterModule.cpp
+++ vcv/src/slewlimiterModule.cpp
@@ -4,23 +4,15 @@
 
 struct SlewlimiterModule : CommModule {
 
-	enum ParamIds {
-		NUM_PARAMS
-	};
-	enum InputIds {
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+	enum ParamIds { RISE_TIME_PARAM, FALL_TIME_PARAM, NUM_PARAMS };
+	enum InputIds { SIGNAL_INPUT, NUM_INPUTS };
+	enum OutputIds { SIGNAL_OUTPUT, NUM_OUTPUTS };
+	enum LightIds { NUM_LIGHTS };
 
 	SlewlimiterModule()
 	{
 		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        core = ModuleFactory::create("SLEWLIMITER");
+		core = ModuleFactory::create("SLEWLIMITER");
 		selfID.typeID = "SLEWLIMITER";
 	}
 };
@@ -38,6 +30,14 @@ struct SlewlimiterWidget : CommModuleWidget {
 		mainModule = module;
 
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/4hp.svg")));
+
+		addModuleTitle("SLEW LIMITER");
+
+		addLabeledKnob("RISE", SlewlimiterModule::RISE_TIME_PARAM, {0, 0});
+		addLabeledKnob("FALL", SlewlimiterModule::FALL_TIME_PARAM, {0, 1});
+
+		addLabeledInput("IN", SlewlimiterModule::SIGNAL_INPUT, {0, 1});
+		addLabeledOutput("OUT", SlewlimiterModule::SIGNAL_OUTPUT, {0, 0});
 	}
 };
 
