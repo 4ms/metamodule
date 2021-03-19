697123264e3482c5853bed55c1d7536d6816507e
Ben Davis <bendedavis@gmail.com>
2021-03-19 15:27:51 -0700

    implemented slew limiter functions

--- shared/CoreModules/slewlimiterCore.h
+++ shared/CoreModules/slewlimiterCore.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "CoreModules/moduleTypes.h"
 #include "coreProcessor.h"
 #include "math.hh"
-#include "CoreModules/moduleTypes.h"
+#include "processors/tools/expDecay.h"
 
 using namespace MathTools;
 
@@ -10,27 +11,33 @@ class SlewlimiterCore : public CoreProcessor {
 public:
 	virtual void update(void) override
 	{
-	
+		signalOutput = slew.update(signalInput);
 	}
 
-	SlewlimiterCore()
-	{
-	}
+	SlewlimiterCore() {}
 
 	virtual void set_param(int const param_id, const float val) override
 	{
 		switch (param_id) {
-			
+			case 0: // rise time
+
+				break;
+			case 1: // fall time
+				slew.decayTime = map_value(val, 0.0f, 1.0f, 1.0f, 2000.0f);
+				break;
 		}
 	}
 	virtual void set_samplerate(const float sr) override
 	{
+		slew.set_samplerate(sr);
 	}
 
 	virtual void set_input(const int input_id, const float val) override
 	{
 		switch (input_id) {
-			
+			case 0:
+				signalInput = val;
+				break;
 		}
 	}
 
@@ -38,7 +45,9 @@ public:
 	{
 		float output = 0;
 		switch (output_id) {
-		
+			case 0:
+				output = signalOutput;
+				break;
 		}
 		return output;
 	}
@@ -52,5 +61,7 @@ public:
 	static inline bool s_registered = ModuleFactory::registerModuleType(typeID, description, create);
 
 private:
-	
+	ExpDecay slew;
+	float signalInput = 0;
+	float signalOutput = 0;
 };
