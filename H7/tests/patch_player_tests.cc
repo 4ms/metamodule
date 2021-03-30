e643a848a6ff66ccc70b9f65bcca1f375c9ed0a4
Dan Green <danngreen1@gmail.com>
2021-03-30 13:03:01 -0700

    Patch player unit tests made cleaner

--- H7/tests/patch_player_tests.cc
+++ H7/tests/patch_player_tests.cc
@@ -23,15 +23,15 @@ static const Patch p = {
 		{
 			.num_jacks = 2,
 			.jacks = {{
-				{.module_id = 0, .jack_id = 1},
-				{.module_id = 5, .jack_id = 3},
+				{.module_id = 0, .jack_id = 1}, // producer: panel jack like "Audio In", or virtual jack like "ADSR Out"
+				{.module_id = 5, .jack_id = 3}, // consumer: panel jack like "CV Out", or virtual jack like "FM In"
 			}},
 		},
 		{
 			.num_jacks = 2,
 			.jacks = {{
 				{.module_id = 3, .jack_id = 6},
-				{.module_id = 0, .jack_id = 2},
+				{.module_id = 0, .jack_id = 1},
 			}},
 		},
 	}},
@@ -40,31 +40,53 @@ static const Patch p = {
 
 TEST_CASE("Panel jack connections")
 {
-	// should see:
-	// panel input#2 (which is Audio Out, for example) --> module 5 input#1
-	// module 3 output#6 --> panel output#4 (which is CV In C, for example)
-
 	PatchPlayer player;
 	player.calc_panel_jack_connections(p);
-	CHECK(player.get_panel_input_connection(0).module_id == 0);
-	CHECK(player.get_panel_input_connection(1).module_id == 5);
-	CHECK(player.get_panel_input_connection(2).module_id == 0);
-	CHECK(player.get_panel_input_connection(3).module_id == 0);
-	CHECK(player.get_panel_input_connection(4).module_id == 0);
-	CHECK(player.get_panel_input_connection(5).module_id == 0);
-	CHECK(player.get_panel_input_connection(6).module_id == 0);
 
-	CHECK(player.get_panel_output_connection(0).module_id == 0);
-	CHECK(player.get_panel_output_connection(1).module_id == 0);
-	CHECK(player.get_panel_output_connection(2).module_id == 3);
-	CHECK(player.get_panel_output_connection(3).module_id == 0);
-	CHECK(player.get_panel_output_connection(4).module_id == 0);
-	CHECK(player.get_panel_output_connection(5).module_id == 0);
-	CHECK(player.get_panel_output_connection(6).module_id == 0);
+	for (int i = 0; i < Panel::NumInJacks; i++) {
+		// printf("in_conns[%d] = {%d, %d}\n", i, player.in_conns[i].module_id, player.in_conns[i].jack_id);
+		if (i != 1) {
+			CHECK(player.get_panel_input_connection(i).module_id == 0);
+			CHECK(player.get_panel_input_connection(i).jack_id == 0);
+		} else {
+			CHECK(player.get_panel_input_connection(i).module_id == 5);
+			CHECK(player.get_panel_input_connection(i).jack_id == 3);
+		}
+	}
+
+	for (int i = 0; i < Panel::NumOutJacks; i++) {
+		// printf("out_conns[%d] = {%d, %d}\n", i, player.out_conns[i].module_id, player.out_conns[i].jack_id);
+		if (i != 1) {
+			CHECK(player.get_panel_output_connection(i).module_id == 0);
+			CHECK(player.get_panel_output_connection(i).jack_id == 0);
+		} else {
+			CHECK(player.get_panel_output_connection(i).module_id == 3);
+			CHECK(player.get_panel_output_connection(i).jack_id == 6);
+		}
+	}
 }
 
 TEST_CASE("Dup module index")
 {
+
+	SUBCASE("Dual LFOs, one panel")
+	{
+		const Patch exampleDualLFO = {
+			.modules_used =
+				{
+					"PANEL_8",
+					"LFOSINE",
+					"LFOSINE",
+				},
+			.num_modules = 3,
+		};
+		PatchPlayer player;
+		player.calc_multiple_module_indicies(exampleDualLFO);
+		CHECK(player.get_multiple_module_index(0) == 0); // PANEL_8
+		CHECK(player.get_multiple_module_index(1) == 1); // LFOSINE 1
+		CHECK(player.get_multiple_module_index(2) == 2); // LFOSINE 2
+	}
+
 	SUBCASE("If there's more than one of a module type, caches correct 'dup index' for each")
 	{
 		PatchPlayer player;
