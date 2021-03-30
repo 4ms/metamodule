f8437638e4e3fbbdb0b3750fbd01c95b8e044f42
Dan Green <danngreen1@gmail.com>
2021-03-30 10:37:17 -0700

    Debugging twoOpFmosc, made phaccu casts explicit

--- shared/processors/twoOpFMOscillator.h
+++ shared/processors/twoOpFMOscillator.h
@@ -12,35 +12,36 @@ private:
 	float sinOut[2];
 
 public:
-
 	float modAmount = 0;
 	float shape = 0;
 	float mix = 0;
-	float priFreq=1;
-	float secFreq=1;
+	float priFreq = 1;
+	float secFreq = 1;
 
 	TwoOpFM() {}
 
 	void set_frequency(int channel, float inputFreq)
 	{
-		if(channel == 0)
-		priFreq = inputFreq;
-		else if (channel ==1)
-		{
-			secFreq=inputFreq;
+		if (channel == 0)
+			priFreq = inputFreq;
+		else if (channel == 1) {
+			secFreq = inputFreq;
 		}
-		
 	}
 
+	// Todo: store inv_samplerate, not sampleRate
 	float update(void)
 	{
-		phaccu[1] += secFreq * (max_/sampleRate);
-		sinOut[1] = sinTable[((phaccu[1] + max_ / 2) & max_) >> 21];
+		auto inv_samplerate = max_ / sampleRate;
 
+		uint32_t increment = secFreq * static_cast<float>(inv_samplerate);
+		phaccu[1] += increment;
+		sinOut[1] = sinTable[((phaccu[1] + max_ / 2) & max_) >> 21];
 
 		sinOut[0] = sinTable[phaccu[0] >> 21];
 		auto freqCalc = (priFreq + (sinOut[1] * modAmount * 4000.0f));
-		phaccu[0] += freqCalc*(max_/sampleRate);
+		uint32_t increment2 = freqCalc * static_cast<float>(inv_samplerate);
+		phaccu[0] += increment2;
 
 		auto sqrOut1 = (phaccu[0] > (max_ / 2)) * 2.0f - 1.0f;
 		auto sqrOut2 = (phaccu[1] > (max_ / 2)) * 2.0f - 1.0f;
