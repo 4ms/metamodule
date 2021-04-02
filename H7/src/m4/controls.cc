eb5109072c63ab97d8b76e54f158bcf4d16c7d56
Dan Green <danngreen1@gmail.com>
2021-04-01 17:10:22 -0700

    Fixed wrong order of gate/clock inputs

--- H7/src/m4/controls.cc
+++ H7/src/m4/controls.cc
@@ -33,9 +33,9 @@ void Controls::update_params()
 
 	cur_params->buttons[0].copy_state(button0);
 	cur_params->buttons[1].copy_state(button1);
-	cur_params->gate_ins[0].copy_state(clock_in);
-	cur_params->gate_ins[1].copy_state(gate_in1);
-	cur_params->gate_ins[2].copy_state(gate_in2);
+	cur_params->gate_ins[0].copy_state(gate_in1);
+	cur_params->gate_ins[1].copy_state(gate_in2);
+	cur_params->gate_ins[2].copy_state(clock_in);
 	cur_params->jack_senses = get_jacksense_reading();
 
 	if (_first_param) {
