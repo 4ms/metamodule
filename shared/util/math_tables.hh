bc6f3fd4953b1cad8c9f2e50d67394fdb8d61225
Ben Davis <bendedavis@gmail.com>
2021-04-05 15:29:01 -0700

    added logTable

--- shared/util/math_tables.hh
+++ shared/util/math_tables.hh
@@ -4,3 +4,4 @@ extern const InterpArray<float, 2048> sinTable;
 extern const InterpArray<float, 2048> tanTable;
 extern const InterpArray<float, 2048> expTable;
 extern const InterpArray<float, 2048> pow9Table;
+extern const InterpArray<float, 2048> logTable;
