7f04d2d6951480d984bb1c29ce94077de23d1d5d
Ben Davis <bendedavis@gmail.com>
2021-04-13 12:18:55 -0700

    updated clk multiply and divide patches

--- shared/patch/integration_tests/test_clkmultiplierCore.hh
+++ shared/patch/integration_tests/test_clkmultiplierCore.hh
@@ -6,26 +6,30 @@ static const Patch test_clkmultiplierCore = {
         "LFOSINE",
         "LFOSINE",
         "CLKMULTIPLIER",
+        "LOGIC",
     },
-    .num_modules = 4,
+    .num_modules = 5,
 
     .module_nodes = {{
         // PANEL_8
-        {1, 255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        {1, 1, 255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
         // LFOSINE
-        {242, 241, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        {243, 242, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
         // LFOSINE
-        {240, 239, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        {241, 240, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
         // CLKMULTIPLIER
         {1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // LOGIC
+        {3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
     }},
 
     .nets = {{
         {
-            .num_jacks = 2,
+            .num_jacks = 3,
             .jacks = {{
-                {.module_id = 1, .jack_id = 0},
+                {.module_id = 4, .jack_id = 1},
                 {.module_id = 3, .jack_id = 0},
+                {.module_id = 0, .jack_id = 1},
             }},
         },
         {
@@ -38,15 +42,22 @@ static const Patch test_clkmultiplierCore = {
         {
             .num_jacks = 2,
             .jacks = {{
+                {.module_id = 1, .jack_id = 0},
+                {.module_id = 4, .jack_id = 0},
+            }},
+        },
+        {
+            .num_jacks = 2,
+            .jacks = {{
                 {.module_id = 3, .jack_id = 0},
                 {.module_id = 0, .jack_id = 0},
             }},
         },
     }},
-    .num_nets = 3,
+    .num_nets = 4,
 
     .static_knobs = {{
-        {.module_id = 1, .param_id = 0, .value = 0.226500f},
+        {.module_id = 1, .param_id = 0, .value = 0.135000f},
         {.module_id = 1, .param_id = 1, .value = 0.000000f},
         {.module_id = 1, .param_id = 2, .value = 1.000000f},
         {.module_id = 2, .param_id = 0, .value = 0.774000f},
