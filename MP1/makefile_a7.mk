1998126ef28e7409f73e3da0595f0e9bd4ab1b88
Dan Green <danngreen1@gmail.com>
2021-04-12 17:10:31 -0700

    Adding MP1 targets to mdrivlib: Pin class and GPIO_X
    simplified u-boot subproject

--- MP1/makefile_a7.mk
+++ MP1/makefile_a7.mk
@@ -2,9 +2,9 @@
 
 $(info Building for A7 core)
 BUILDDIR = $(BUILDDIR_A7)
-LOADFILE = stm32mp15xx_ca7.ld
+LOADFILE = $(DEVICE)/src/stm32mp15xx_ca7.ld
 MCU = #-mcpu=cortex-a7 -mfpu=? -mthumb -mlittle-endian -mfloat-abi=?
-ARCH_CFLAGS = -DCORE_CA7 -DSTM32MP157Cxx #-DARM_MATH_CA7
+ARCH_CFLAGS = -DCORE_CA7 -DSTM32MP157Cxx -DSTM32MP1 #-DARM_MATH_CA7
 CORE_SRC = src/a7
 HAL_CONF_INC = src/a7
 
@@ -12,29 +12,40 @@ OPTFLAG = -O0
 include makefile_opts.mk
 
 ####
-BINARYNAME = app
-UIMAGENAME = bare-arm.uimg
+UIMG  		= $(BUILDDIR)/$(BINARYNAME).uimg
 LOADADDR 	= 0xC2000040
 ENTRYPOINT 	= $(LOADADDR)
-UBOOTDIR =../u-boot-stm32mp-2020.01-r0/build_baremetal
+UBOOTDIR 	= $(LIBDIR)/u-boot
+UBOOTSRCDIR = $(UBOOTDIR)/u-boot-stm32mp1-baremetal
+UBOOTBUILDDIR = $(UBOOTDIR)/build
+UBOOT_MKIMAGE = $(UBOOTBUILDDIR)/tools/mkimage
 #####
 
 SOURCES = $(DRIVERLIB)/drivers/$(STARTUP_CA7) \
-		  app.c
+		  src/a7/app.cc\
+		  $(DRIVERLIB)/drivers/pin.cc
 
 OBJECTS   = $(addprefix $(BUILDDIR)/, $(addsuffix .o, $(basename $(SOURCES))))
 DEPS   	  = $(addprefix $(BUILDDIR)/, $(addsuffix .d, $(basename $(SOURCES))))
 
 INCLUDES = -I. \
-		   -IDrivers/STM32MP1xx_HAL_Driver/Inc \
-		   -IDrivers/CMSIS/Core_A/Include \
-		   -IDrivers/CMSIS/Device/ST/STM32MP1xx/Include \
+		   -Isrc \
+		   -Isrc/a7 \
+		   -Isrc/a7/conf \
+		   -I$(PERIPH)/include \
+		   -I$(CMSIS)/Core_A/Include \
+		   -I$(CMSIS)/Core/Include \
+		   -I$(CMSIS)/include \
+		   -I$(DEVICE)/include \
+		   -I$(DRIVERLIB) \
+		   -I$(DRIVERLIB)/drivers \
 
 AFLAGS = $(MCU)
 
 CFLAGS = -g2 \
 		 -nostdlib \
 		 -nostartfiles \
+		 -DUSE_HAL_DRIVER \
 		 $(ARCH_CFLAGS) \
 		 $(MCU) \
 		 $(INCLUDES) \
@@ -53,7 +64,7 @@ CXXFLAGS = $(CFLAGS) \
 		-Wno-register \
 		-Wno-volatile \
 
-LFLAGS = -T $(DEVICE)/$(LOADFILE) \
+LFLAGS = -T $(LOADFILE) \
 	-Wl,--gc-sections \
 	-Wl,-Map,$(BUILDDIR)/$(BINARYNAME).map,--cref \
 	$(MCU)  \
@@ -78,12 +89,10 @@ ELF 	= $(BUILDDIR)/$(BINARYNAME).elf
 HEX 	= $(BUILDDIR)/$(BINARYNAME).hex
 BIN 	= $(BUILDDIR)/$(BINARYNAME).bin
 
-PHONY: all
+all: Makefile $(ELF) $(UIMG)
 
-all: Makefile $(ELF) $(UIMAGENAME)
-
-$(UIMAGENAME): $(BIN)
-	$(UBOOTDIR)/tools/mkimage -A arm -C none -T kernel -a $(LOADADDR) -e $(ENTRYPOINT) -d $< $@
+$(UIMG): $(BIN) $(UBOOT_MKIMAGE)
+	$(UBOOT_MKIMAGE) -A arm -C none -T kernel -a $(LOADADDR) -e $(ENTRYPOINT) -d $(BIN) $@
 
 $(BUILDDIR)/%.o: %.s
 	@mkdir -p $(dir $@)
@@ -95,12 +104,24 @@ $(BUILDDIR)/%.o: %.c $(BUILDDIR)/%.d
 	$(info Building $< at $(OPTFLAG))
 	$(CC) -c $(DEPFLAGS) $(OPTFLAG) $(CFLAGS) $< -o $@
 
-$(ELF): $(OBJECTS) $(DEVICE)/$(LOADFILE)
+$(BUILDDIR)/%.o: %.cc $(BUILDDIR)/%.d
+	@mkdir -p $(dir $@)
+	$(info Building $< at $(OPTFLAG))
+	$(CXX) -c $(DEPFLAGS) $(OPTFLAG) $(CXXFLAGS) $< -o $@
+
+$(ELF): $(OBJECTS) $(LOADFILE)
 	$(info Linking...)
 	$(LD) $(LFLAGS) -o $@ $(OBJECTS)
 
 $(BIN): $(ELF)
 	$(OBJCPY) -O binary $< $@
+
+$(UBOOT_MKIMAGE): $(UBOOTSRCDIR)
+	cd $(UBOOTSRCDIR) && make KBUILD_OUTPUT=$(PWD)/$(UBOOTBUILDDIR) CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig
+	cd $(UBOOTSRCDIR) && make -j16 KBUILD_OUTPUT=$(PWD)/$(UBOOTBUILDDIR) CROSS_COMPILE=arm-none-eabi- all
+
+uboot_clean:
+	rm -rf $(UBOOTBUILDDIR)
 
 %.d: ;
 
@@ -113,6 +134,7 @@ ifneq "$(MAKECMDGOALS)" "clean"
 endif
 
 .PRECIOUS: $(DEPS) $(OBJECTS) $(ELF)
+.PHONY: all
 
 #Todo: add card commands
 #./create-sd.sh sdcard.img bare-arm.uimg
---
+++ MP1/src/a7/app.cc
@@ -0,0 +1,159 @@
+// #include "stm32mp157cxx_ca7.h"
+// #include "stm32mp1xx_ll_gpio.h"
+// #include "stm32mp1xx_ll_usart.h"
+#include "drivers/pin.hh"
+#include "drivers/stm32xx.h"
+
+#include <stdint.h>
+
+const uint32_t *UART4_ISR = (uint32_t *)0x40010028;
+const uint32_t TXFE = (1 << 23); // FIFO Empty
+const uint32_t TXFNF = (1 << 7); // TX FIFO Not Full
+const uint32_t TC = (1 << 6);	 // Tx Complete
+
+/* GPIO I, pin 8 = red D2 */
+/* GPIO I, pin 9 = green D2 */
+/* GPIO Z, pin 6 = red D1 */
+/* GPIO Z, pin 7 = green D1 */
+/* all LEDs are active low */
+
+void delay_long()
+{
+	uint32_t i = 0x100000;
+	while (i--)
+		;
+}
+
+void soft_breakpoint()
+{
+	volatile int stop = 1;
+	while (stop) {
+		// Attach a debugger and manually change the value at the address of `stop` in RAM from 1 to 0
+	}
+}
+
+void delay_for_uart(void)
+{
+	int i = 255;
+	while (i--)
+		;
+}
+
+uint32_t uart_ready()
+{
+	uint32_t isr = (*UART4_ISR);
+	if (isr & TC)
+		return 1;
+	else
+		return 0;
+}
+
+void write(const char *str)
+{
+	while (*str) {
+		UART4->TDR = *str++;
+		delay_for_uart();
+	}
+}
+
+int main()
+{
+	// Test UART
+	UART4->TDR = 'X';
+	delay_for_uart();
+	UART4->TDR = 'Y';
+	delay_for_uart();
+	UART4->TDR = 'Z';
+	delay_for_uart();
+	UART4->TDR = 'z';
+	delay_for_uart();
+	UART4->TDR = 'z';
+	delay_for_uart();
+	UART4->TDR = '\r';
+	delay_for_uart();
+	UART4->TDR = '\n';
+	delay_for_uart();
+
+	Pin red_LED2{GPIO::I, 8, PinMode::Output};
+	Pin green_LED2{GPIO::I, 9, PinMode::Output};
+	Pin red_LED1{GPIO::Z, 6, PinMode::Output};
+	Pin green_LED1{GPIO::Z, 7, PinMode::Output};
+
+	green_LED1.high();
+	green_LED1.low();
+
+	green_LED2.high();
+	green_LED2.low();
+
+	red_LED1.high();
+	red_LED1.low();
+
+	red_LED2.high();
+	red_LED2.low();
+
+	// RCC->MC_AHB4ENSETR |= RCC_MC_AHB4ENSETR_GPIOIEN;
+	// RCC->MC_AHB5ENSETR |= RCC_MC_AHB5ENSETR_GPIOZEN;
+
+	// LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+	// LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
+	// LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_8, LL_GPIO_SPEED_FREQ_MEDIUM);
+	// LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+	// LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
+	// LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_9, LL_GPIO_SPEED_FREQ_MEDIUM);
+
+	// LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
+	// LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
+
+	// LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
+	// LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
+
+	// LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+	// LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+
+	// LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+	// LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+
+	// Test function calls (tests the stack)
+	const char *s = "Hello world from bare-metal!\r\n";
+	write(s);
+
+	const char *s2 = "And hi to you too!\r\n";
+	while (*s2 != '\0') {
+		UART4->TDR = *s2;
+		s2++;
+		delay_for_uart();
+	}
+
+	while (1) {
+		// LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
+		red_LED1.low();
+		delay_long();
+		red_LED1.high();
+		// LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
+
+		// LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
+		red_LED2.low();
+		delay_long();
+		red_LED2.high();
+		// LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
+
+		// LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+		green_LED1.low();
+		delay_long();
+		green_LED1.high();
+		// LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+
+		// LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+		green_LED2.low();
+		delay_long();
+		green_LED2.high();
+		// LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+	};
+
+	return 0;
+}
+
+void SystemInit(void)
+{
+	return;
+}
