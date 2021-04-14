973f2d003ce777ed2a3d63ebefb14407e9ffc0b4
Dan Green <danngreen1@gmail.com>
2021-04-14 16:18:18 -0700

    Fixed recursive #include in mp1 HAL

--- MP1/lib/stm32/hal/include/stm32mp1xx_hal.h
+++ MP1/lib/stm32/hal/include/stm32mp1xx_hal.h
@@ -27,7 +27,7 @@ extern "C" {
 	#endif
 
 	/* Includes ------------------------------------------------------------------*/
-	#include "stm32mp1xx_hal_def.h"
+	// #include "stm32mp1xx_hal_def.h"
 	#include "stm32mp1xx_hal_conf.h"
 
 /** @addtogroup STM32MP1xx_HAL_Driver
