502eaef42ff483cee398963bc915d5af467ba7f6
Dan Green <danngreen1@gmail.com>
2021-04-13 22:56:34 -0700

    i2c pins set for A7

--- MP1/src/a7/conf/i2c_conf.hh
+++ MP1/src/a7/conf/i2c_conf.hh
@@ -3,26 +3,25 @@
 
 constexpr uint32_t LEDUpdateHz = 100;
 
-const I2CConfig i2c_conf_controls = {
-
-	.I2Cx = I2C3,
-	.SCL = {GPIO::A, 8, GPIO_AF4_I2C3},
-	.SDA = {GPIO::C, 9, GPIO_AF4_I2C3},
-	.timing =
-		{
-			.PRESC = 0x00,
-			.SCLDEL_SDADEL = 0x90,
-			.SCLH = 0x19,
-			.SCLL = 0x45,
-		},
-	.priority1 = 0,
-	.priority2 = 1,
-};
+// const I2CConfig i2c_conf_controls = {
+// .I2Cx = I2C3,
+// .SCL = {GPIO::A, 8, GPIO_AF4_I2C3},
+// .SDA = {GPIO::C, 9, GPIO_AF4_I2C3},
+// .timing =
+// 	{
+// 		.PRESC = 0x00,
+// 		.SCLDEL_SDADEL = 0x90,
+// 		.SCLH = 0x19,
+// 		.SCLL = 0x45,
+// 	},
+// .priority1 = 0,
+// .priority2 = 1,
+// };
 
 const I2CConfig i2c_conf_codec = {
 	.I2Cx = I2C1,
-	.SCL = {GPIO::B, 6, GPIO_AF4_I2C1},
-	.SDA = {GPIO::B, 7, GPIO_AF4_I2C1},
+	.SCL = {GPIO::H, 11, 5},
+	.SDA = {GPIO::H, 12, 5},
 	.timing =
 		{
 			.PRESC = 0x00,
