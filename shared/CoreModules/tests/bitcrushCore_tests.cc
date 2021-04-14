c7971e5cc8525c4a01fd4475ffe38ab794e49e00
Ben Davis <bendedavis@gmail.com>
2021-04-14 12:45:28 -0700

    unit test written for bit reducer

--- shared/CoreModules/tests/bitcrushCore_tests.cc
+++ shared/CoreModules/tests/bitcrushCore_tests.cc
@@ -1,5 +1,6 @@
 #include "CoreModules/bitcrushCore.h"
 #include "doctest.h"
+#include "processors/tools/quantizeBits.h"
 #include <stdint.h>
 
 TEST_CASE("pass through with sample rate and bit rate at max")
@@ -15,4 +16,16 @@ TEST_CASE("pass through with sample rate and bit rate at max")
 
 		CHECK(rand == b.get_output(0));
 	}
+}
+
+TEST_CASE("test that bit reduction calculates properly")
+{
+	BitQuantizer q;
+	q.setSteps(4);
+	q.setMin(-1);
+	q.setMax(1);
+	CHECK(q.update(1) == 1.0);
+	CHECK(q.update(-1) == -1);
+	CHECK(q.update(0.3) == doctest::Approx(0.33333f));
+	CHECK(q.update(-0.5f) == doctest::Approx(-0.33333f));
 }
