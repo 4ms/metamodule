e29bc46278057983a8ba140a9d335771034009b0
Dan Green <danngreen1@gmail.com>
2021-04-15 16:52:06 -0700

    a7 dac conf

--- firmware/src/a7/conf/dac_conf.hh
+++ firmware/src/a7/conf/dac_conf.hh
@@ -10,16 +10,16 @@
 
 struct MM_DACConf : DefaultSpiTransferConf {
 	struct SpiConf : DefaultSpiConf {
-		static constexpr uint16_t PeriphNum = 2;
+		static constexpr uint16_t PeriphNum = 2; // SPI2
 		static constexpr uint16_t NumChips = 2;
 		static constexpr IRQType IRQn = SPI2_IRQn;
 		static constexpr uint16_t priority1 = 1;
 		static constexpr uint16_t priority2 = 3;
-		static constexpr PinNoInit SCLK = {GPIO::B, 13, LL_GPIO_AF_5};
+		static constexpr PinNoInit SCLK = {GPIO::A, 9, LL_GPIO_AF_5};
 		static constexpr PinNoInit COPI = {GPIO::B, 15, LL_GPIO_AF_5};
 		static constexpr PinNoInit CIPO = {GPIO::A, 0};
-		static constexpr PinNoInit CS0 = {GPIO::B, 12, 0};
-		static constexpr PinNoInit CS1 = {GPIO::B, 11, 0};
+		static constexpr PinNoInit CS0 = {GPIO::B, 9, 0};
+		static constexpr PinNoInit CS1 = {GPIO::B, 8, 0};
 		static constexpr bool use_hardware_ss = false;
 		static constexpr uint16_t clock_division = 16;
 		static constexpr uint16_t data_size = 24;
@@ -29,12 +29,13 @@ struct MM_DACConf : DefaultSpiTransferConf {
 		static constexpr uint32_t NumClocksToggleSSInterData = 0;
 	};
 	static constexpr uint32_t NumChannelsPerChip = 2;
-	using AuxPin = FPin<GPIO::B, 14, PinMode::Output>;
+	using AuxPin = FPin<GPIO::E, 3, PinMode::Output>;
 };
 
+// Pin change on SAI LR clock
 const PinChangeConfig DAC_update_conf = {
-	.pin = 4,
-	.port = GPIO::E,
+	.pin = 15,
+	.port = GPIO::A,
 	.on_rising_edge = true,
 	.on_falling_edge = true,
 	.priority1 = 0,
