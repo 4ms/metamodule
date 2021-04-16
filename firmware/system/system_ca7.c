9534306c5ba09fb479a59455fae116cdf4b3ad50
Dan Green <danngreen1@gmail.com>
2021-04-16 15:58:49 -0700

    Added SystemInit() in system_ca7.c

--- firmware/system/system_ca7.c
+++ firmware/system/system_ca7.c
@@ -1,11 +1,12 @@
-#include "drivers/stm32xx.h"
+#include "irq_ctrl.h"
+#include "stm32mp1xx.h"
 
 uint32_t SystemCoreClock = HSE_VALUE;
 
 void SystemInit(void)
 {
-	/* do not use global variables because this function is called before
-	   reaching pre-main. RW section may be overwritten afterwards.          */
+	// do not use global variables because this function is called before
+	// reaching pre-main. RW section may be overwritten afterwards.
 
 	// Invalidate entire Unified TLB
 	__set_TLBIALL(0);
@@ -20,28 +21,21 @@ void SystemInit(void)
 	__DSB();
 	__ISB();
 
-	//  Invalidate data cache
 	L1C_InvalidateDCacheAll();
 
 	// #if ((__FPU_PRESENT == 1) && (__FPU_USED == 1))
-	// Enable FPU
 	__FPU_Enable();
 
-	// Create Translation Table
 	// MMU_CreateTranslationTable();
 
-	// Enable MMU
 	// MMU_Enable();
 
-	// Enable Caches
 	L1C_EnableCaches();
 	L1C_EnableBTAC();
 
 #if (__L2C_PRESENT == 1)
-	// Enable L2C
 	L2C_Enable();
 #endif
 
-	// IRQ Initialize
-	// IRQ_Initialize();
+	IRQ_Initialize();
 }
