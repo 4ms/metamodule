f8490795d60e5f37f2fdaec0a2ae6f94604eaacc
Dan Green <danngreen1@gmail.com>
2021-04-20 16:55:50 -0700

    Using SAI3 instead of SAI4
    Fixes issue where SAI4 only outputs MCLK-- FixMe: Why?

--- firmware/src/a7/conf/codec_sai_conf.hh
+++ firmware/src/a7/conf/codec_sai_conf.hh
@@ -5,27 +5,27 @@
 #include "drivers/stm32xx.h"
 
 const SaiConfig codec_sai_conf = {
-	.sai = SAI4,
-	.tx_block = SAI4_Block_B,
-	.rx_block = SAI4_Block_A,
+	.sai = SAI3,
+	.tx_block = SAI3_Block_A,
+	.rx_block = SAI3_Block_B,
 
-	.mode = SaiConfig::RXMaster,
+	.mode = SaiConfig::TXMaster,
 
 	.dma_init_tx =
 		{
 			.DMAx = DMA2,
-			.stream = DMA2_Stream1,		   // BDMA_Channel0,
-			.channel = DMA_REQUEST_SAI4_B, // BDMA_REQUEST_SAI4_B,
-			.IRQn = DMA2_Stream1_IRQn,	   // BDMA_Channel0_IRQn,
+			.stream = DMA2_Stream0,		   // BDMA_Channel1,
+			.channel = DMA_REQUEST_SAI3_A, // BDMA_REQUEST_SAI4_A,
+			.IRQn = DMA2_Stream0_IRQn,	   // BDMA_Channel1_IRQn,
 			.pri = 1,
 			.subpri = 0,
 		},
 	.dma_init_rx =
 		{
 			.DMAx = DMA2,
-			.stream = DMA2_Stream0,		   // BDMA_Channel1,
-			.channel = DMA_REQUEST_SAI4_A, // BDMA_REQUEST_SAI4_A,
-			.IRQn = DMA2_Stream0_IRQn,	   // BDMA_Channel1_IRQn,
+			.stream = DMA2_Stream1,		   // BDMA_Channel0,
+			.channel = DMA_REQUEST_SAI3_B, // BDMA_REQUEST_SAI4_B,
+			.IRQn = DMA2_Stream1_IRQn,	   // BDMA_Channel0_IRQn,
 			.pri = 1,
 			.subpri = 0,
 		},
@@ -34,9 +34,24 @@ const SaiConfig codec_sai_conf = {
 	.framesize = 32,
 	.samplerate = 48000,
 
-	.MCLK = {GPIO::B, 3, LL_GPIO_AF_12},
-	.SCLK = {GPIO::B, 4, LL_GPIO_AF_4},
-	.LRCLK = {GPIO::A, 15, LL_GPIO_AF_12},
-	.SD_DAC = {GPIO::B, 5, LL_GPIO_AF_10}, // SAI 4 A = RX
-	.SD_ADC = {GPIO::H, 5, LL_GPIO_AF_12}, // SAI 4 B = TX
+	// SAI 1
+	// .MCLK = {GPIO::E, 2, LL_GPIO_AF_6},
+	// .SCLK = {GPIO::E, 5, LL_GPIO_AF_6},
+	// .LRCLK = {GPIO::G, 15, LL_GPIO_AF_6},
+	// .SD_DAC = {GPIO::E, 6, LL_GPIO_AF_6},
+	// .SD_ADC = {GPIO::E, 3, LL_GPIO_AF_6},
+
+	// SAI 4:
+	// .MCLK = {GPIO::B, 3, LL_GPIO_AF_12}, //works
+	// .SCLK = {GPIO::B, 4, LL_GPIO_AF_4}, //does not work (not connected to other PCB)
+	// .LRCLK = {GPIO::A, 15, LL_GPIO_AF_12}, //always high
+	// .SD_DAC = {GPIO::B, 5, LL_GPIO_AF_10}, // SAI 4 SD A //does not work
+	// .SD_ADC = {GPIO::H, 5, LL_GPIO_AF_12}, // SAI 4 SD B
+
+	// SAI 3:
+	.MCLK = {GPIO::D, 15, LL_GPIO_AF_6},
+	.SCLK = {GPIO::D, 0, LL_GPIO_AF_6},
+	.LRCLK = {GPIO::D, 4, LL_GPIO_AF_6},
+	.SD_DAC = {GPIO::D, 1, LL_GPIO_AF_6}, // SD A
+	.SD_ADC = {GPIO::E, 1, LL_GPIO_AF_6}, // SD B
 };
