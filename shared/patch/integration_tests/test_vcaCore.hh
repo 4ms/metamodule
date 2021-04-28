1c21696821310b0fcc46d6d6f4fb5b78c3ac413d
Ben Davis <bendedavis@gmail.com>
2021-04-28 13:10:42 -0700

    added vca core test patch

---
+++ shared/patch/integration_tests/test_vcaCore.hh
@@ -0,0 +1,65 @@
+#include "patch.hh"
+static const Patch test_vcaCore = { 
+    .patch_name = "test_vcaCore",
+    .modules_used = {
+        "PANEL_8",
+        "VCA",
+        "LFOSINE",
+        "ADENV",
+    },
+    .num_modules = 4,
+
+    .module_nodes = {{
+        // PANEL_8
+        {255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // VCA
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // LFOSINE
+        {241, 240, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+        // ADENV
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+    }},
+
+    .nets = {{
+        {
+            .num_jacks = 3,
+            .jacks = {{
+                {.module_id = 2, .jack_id = 0},
+                {.module_id = 1, .jack_id = 0},
+                {.module_id = 3, .jack_id = 0},
+            }},
+        },
+        {
+            .num_jacks = 2,
+            .jacks = {{
+                {.module_id = 3, .jack_id = 0},
+                {.module_id = 1, .jack_id = 1},
+            }},
+        },
+    }},
+    .num_nets = 2,
+
+    .static_knobs = {{
+        {.module_id = 1, .param_id = 0, .value = 0.000000f},
+        {.module_id = 1, .param_id = 1, .value = 1.000000f},
+        {.module_id = 1, .param_id = 2, .value = 0.000000f},
+        {.module_id = 1, .param_id = 3, .value = 1.000000f},
+        {.module_id = 2, .param_id = 0, .value = 0.000000f},
+        {.module_id = 2, .param_id = 1, .value = 0.000000f},
+        {.module_id = 2, .param_id = 2, .value = 1.000000f},
+        {.module_id = 3, .param_id = 0, .value = 0.000000f},
+        {.module_id = 3, .param_id = 1, .value = 0.252000f},
+        {.module_id = 3, .param_id = 2, .value = 0.462000f},
+        {.module_id = 3, .param_id = 3, .value = 1.000000f},
+    }},
+    .num_static_knobs = 11,
+
+    .mapped_knobs = {{
+        {.module_id = 1, .param_id = 0, .panel_knob_id = 0},
+        {.module_id = 1, .param_id = 1, .panel_knob_id = 1},
+        {.module_id = 1, .param_id = 2, .panel_knob_id = 2},
+        {.module_id = 1, .param_id = 3, .panel_knob_id = 3},
+    }},
+    .num_mapped_knobs = 4,
+
+};
