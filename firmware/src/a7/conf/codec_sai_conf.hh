9898064d8ef3bfed0166f655cc505d2065270bac
Dan Green <danngreen1@gmail.com>
2021-04-28 13:02:15 -0700

    SAI and DAC priorities tweaked: DAC 0,0, SAI: 3,3. ISRs do not interrupt each other
    Added user stack end to linker script

--- firmware/src/a7/conf/codec_sai_conf.hh
+++ firmware/src/a7/conf/codec_sai_conf.hh
@@ -17,8 +17,8 @@ const SaiConfig codec_sai_conf = {
 			.stream = DMA2_Stream1,		   // BDMA_Channel1,
 			.channel = DMA_REQUEST_SAI3_A, // BDMA_REQUEST_SAI4_A,
 			.IRQn = DMA2_Stream1_IRQn,	   // BDMA_Channel1_IRQn,
-			.pri = 0,
-			.subpri = 0,
+			.pri = 3,
+			.subpri = 3,
 		},
 	.dma_init_rx =
 		{
@@ -26,29 +26,14 @@ const SaiConfig codec_sai_conf = {
 			.stream = DMA2_Stream2,		   // BDMA_Channel0,
 			.channel = DMA_REQUEST_SAI3_B, // BDMA_REQUEST_SAI4_B,
 			.IRQn = DMA2_Stream2_IRQn,	   // BDMA_Channel0_IRQn,
-			.pri = 0,
-			.subpri = 0,
+			.pri = 3,
+			.subpri = 3,
 		},
 
 	.datasize = SAI_DATASIZE_24,
 	.framesize = 32,
 	.samplerate = 48000,
 
-	// SAI 1
-	// .MCLK = {GPIO::E, 2, LL_GPIO_AF_6},
-	// .SCLK = {GPIO::E, 5, LL_GPIO_AF_6},
-	// .LRCLK = {GPIO::G, 15, LL_GPIO_AF_6},
-	// .SD_DAC = {GPIO::E, 6, LL_GPIO_AF_6},
-	// .SD_ADC = {GPIO::E, 3, LL_GPIO_AF_6},
-
-	// SAI 4:
-	// .MCLK = {GPIO::B, 3, LL_GPIO_AF_12}, //works
-	// .SCLK = {GPIO::B, 4, LL_GPIO_AF_4}, //does not work (not connected to other PCB)
-	// .LRCLK = {GPIO::A, 15, LL_GPIO_AF_12}, //always high
-	// .SD_DAC = {GPIO::B, 5, LL_GPIO_AF_10}, // SAI 4 SD A //does not work
-	// .SD_ADC = {GPIO::H, 5, LL_GPIO_AF_12}, // SAI 4 SD B
-
-	// SAI 3:
 	.MCLK = {GPIO::D, 15, LL_GPIO_AF_6},
 	.SCLK = {GPIO::D, 0, LL_GPIO_AF_6},
 	.LRCLK = {GPIO::D, 4, LL_GPIO_AF_6},
