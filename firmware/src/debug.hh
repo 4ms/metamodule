78dd845a74910faa741d7f0c020b448e850ac072
Dan Green <danngreen1@gmail.com>
2021-05-07 14:31:40 -0700

    Debug Pin3 disabled (conflicts with SAI LRCLK pin)

--- firmware/src/debug.hh
+++ firmware/src/debug.hh
@@ -14,7 +14,7 @@ struct Debug {
 	using Pin0 = FPin<GPIO::D, 5, PinMode::Output>;
 	using Pin1 = FPin<GPIO::D, 6, PinMode::Output>;
 	using Pin2 = FPin<GPIO::E, 15, PinMode::Output>;
-	using Pin3 = FPin<GPIO::D, 4, PinMode::Output>;
+	// using Pin3 = FPin<GPIO::D, 4, PinMode::Output>;
 
 	using red_LED1 = FPin<GPIO::Z, 6, PinMode::Output>;
 	using red_LED2 = FPin<GPIO::I, 8, PinMode::Output>;
@@ -30,7 +30,7 @@ struct Debug {
 	static inline Pin0 pin_0_init;
 	static inline Pin1 pin_1_init;
 	static inline Pin2 pin_2_init;
-	static inline Pin3 pin_3_init;
+	// static inline Pin3 pin_3_init;
 };
 
 #define GCC_OPTIMIZE_OFF __attribute__((optimize("-O0")))
