75cf22d46fcc4a1384826da718fd8c3f92a05151
Dan Green <danngreen1@gmail.com>
2021-05-26 12:58:19 -0700

    static buffers with section defined by GCC attribute need to be not inside a struct
    With -flto, they have to be in root level (inside a namespace is OK), but cannot be in a struct. Bug in GCC10?

--- firmware/src/a7/static_buffers.hh
+++ firmware/src/a7/static_buffers.hh
@@ -1,41 +1,37 @@
 #pragma once
 #include "audio.hh"
 #include "conf/screen_buffer_conf.hh"
-// #include "drivers/mpu.hh"
 #include "leds.hh"
 #include "params.hh"
 
-// Defined in linker:
-// extern char _dma_buffer_start;
-// extern char _dma_buffer_region_size;
-
-// StaticBuffers sets up our regions of memory that have specific requirements in the Cortex M7/M4 architecture.
+// StaticBuffers sets up our regions of memory that have specific requirements in the target chip architecture.
 // The attributes are defined in the linker file.
 // - Audio DMA Buffer must have a static address and be in certain SRAM regions, and be non-cacheable.
-// - Screen framebuffer is large, must be fast, and accessible to M7 and the M4 controlling the MDMA
+// - Screen framebuffer is large, must be fast, and accessible to both cores (and in some cases, DMA friendly)
 // - LED Frame buffer needs to be accessible to both cores
 // - Param Blocks need to be accessible to both cores (speed may be a concern too)
 namespace MetaModule
 {
-struct StaticBuffers {
-	static inline __attribute__((section(".sysram"))) AudioStream::AudioStreamBlock audio_dma_block[4];
-	static inline __attribute__((section(".sysram"))) uint32_t led_frame_buffer[PCA9685Driver::kNumLedsPerChip];
-	static inline __attribute__((section(".sysram"))) MMScreenBufferConf::FrameBufferT screen_framebuf;
+namespace StaticBuffers
+{
+static inline __attribute__((section(".ddma"))) AudioStream::AudioStreamBlock audio_dma_block[4];
+static inline __attribute__((section(".sysram"))) uint32_t led_frame_buffer[PCA9685Driver::kNumLedsPerChip];
+static inline __attribute__((section(".sysram"))) MMScreenBufferConf::FrameBufferT screen_framebuf;
 
-	// FIXME: Why does last few words of param_blocks become 00'd out if its in sysram and appears earlier in this list?
-	// (address 0x2FFDE400 and up is blanked)
-	static inline __attribute__((section(".shared_memory"))) DoubleBufParamBlock param_blocks; // 4380 * 2
+// FIXME: Why does last few words of param_blocks become 00'd out if its in sysram and appears earlier in this list?
+// (address 0x2FFDE400 and up is blanked)
+static inline __attribute__((section(".ddma"))) DoubleBufParamBlock param_blocks; // 4380 * 2
 
-	static void init()
-	{
-		// Todo: why doesn't Params::Params() get called? because it's in a NOLOAD section of memory?
-		for (auto &block : param_blocks) {
-			for (auto &param : block.params) {
-				param.clear();
-			}
-			block.metaparams.clear();
+static void init()
+{
+	// Todo: why doesn't Params::Params() get called? because it's in a NOLOAD section of memory?
+	for (auto &block : param_blocks) {
+		for (auto &param : block.params) {
+			param.clear();
 		}
+		block.metaparams.clear();
 	}
-};
+}
+}; // namespace StaticBuffers
 
 } // namespace MetaModule
