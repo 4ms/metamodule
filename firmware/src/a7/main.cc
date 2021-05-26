190ae38636185acdf2b0e238d7d55fa0fcc1a0e8
Dan Green <danngreen1@gmail.com>
2021-05-26 12:57:06 -0700

    Fix shared ptr addresses being stuck in cache

--- firmware/src/a7/main.cc
+++ firmware/src/a7/main.cc
@@ -61,9 +61,13 @@ void main()
 
 	SharedBus::i2c.deinit();
 
-	SharedMemory::write_address_of(&StaticBuffers::param_blocks, SharedMemory::ParamsPtrLocation);
-	SharedMemory::write_address_of(&StaticBuffers::led_frame_buffer, SharedMemory::LEDFrameBufLocation);
-	auto addr = SharedMemory::write_address_of(&StaticBuffers::screen_framebuf, SharedMemory::ScreenBufLocation);
+	auto addr = SharedMemory::write_address_of(&StaticBuffers::param_blocks, SharedMemory::ParamsPtrLocation);
+	target::SystemCache::clean_dcache_by_addr(addr);
+
+	addr = SharedMemory::write_address_of(&StaticBuffers::led_frame_buffer, SharedMemory::LEDFrameBufLocation);
+	target::SystemCache::clean_dcache_by_addr(addr);
+
+	addr = SharedMemory::write_address_of(&StaticBuffers::screen_framebuf, SharedMemory::ScreenBufLocation);
 	target::SystemCache::clean_dcache_by_addr(addr);
 
 	// Enable ISR for LedBufFrameLock:
