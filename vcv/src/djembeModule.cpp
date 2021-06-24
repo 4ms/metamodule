c7a03fcdd76b0e129d1e6e150c826190c781b4ac
Dan Green <danngreen1@gmail.com>
2021-06-24 14:50:14 -0700

    djembeModule added (WIP)

--- vcv/src/djembeModule.cpp
+++ vcv/src/djembeModule.cpp
@@ -1,27 +1,25 @@
 #include "CommModule.h"
 #include "CommWidget.h"
-#include "math.hh"
+#include "util/math.hh"
 
 struct DjembeModule : CommModule {
 
-	enum ParamIds {
-		NUM_PARAMS
-	};
-	enum InputIds {
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+	enum ParamIds { FREQ, GAIN, SHARPNESS, STRIKE, NUM_PARAMS };
+	enum InputIds { FREQCV, GAINCV, SHARPNESSCV, STRIKECV, TRIGIN, NUM_INPUTS };
+	enum OutputIds { OUT, NUM_OUTPUTS };
+	enum LightIds { NUM_LIGHTS };
 
 	DjembeModule()
 	{
 		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        core = ModuleFactory::create("DJEMBE");
+		core = ModuleFactory::create("DJEMBE");
 		selfID.typeID = "DJEMBE";
+		outputJacks[DjembeModule::OUT]->scale = [](float f) { return f * 8.0f; };
+		inputJacks[DjembeModule::FREQCV]->scale = [](float f) { return f / 5.0f; };
+		inputJacks[DjembeModule::GAINCV]->scale = [](float f) { return f / 5.0f; };
+		inputJacks[DjembeModule::SHARPNESSCV]->scale = [](float f) { return f / 5.0f; };
+		inputJacks[DjembeModule::STRIKECV]->scale = [](float f) { return f / 5.0f; };
+		inputJacks[DjembeModule::TRIGIN]->scale = [](float f) { return f / 5.0f; };
 	}
 };
 
@@ -37,7 +35,22 @@ struct DjembeWidget : CommModuleWidget {
 		setModule(module);
 		mainModule = module;
 
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/4hp.svg")));
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/8hptemplate.svg")));
+
+		addModuleTitle("Djembe");
+
+		addLabeledKnob("Pitch", DjembeModule::FREQ, {0, 0});
+		addLabeledKnob("Sharpness", DjembeModule::SHARPNESS, {1, 0});
+		addLabeledKnob("Hit", DjembeModule::GAIN, {0, 1});
+		addLabeledKnob("StrikeAmt", DjembeModule::STRIKE, {1, 1});
+
+		addLabeledInput("Trigger", DjembeModule::TRIGIN, {0, 2});
+		addLabeledOutput("Out", DjembeModule::OUT, {1, 2});
+
+		addLabeledInput("Pitch", DjembeModule::FREQCV, {0, 1});
+		addLabeledInput("Sharpness", DjembeModule::SHARPNESSCV, {1, 1});
+		addLabeledInput("Hit", DjembeModule::GAINCV, {0, 0});
+		addLabeledInput("StrikeAmt", DjembeModule::STRIKECV, {1, 0});
 	}
 };
 
