680c5501cd83af3e1d5164e2aa900c91cbd7105e
Dan Green <danngreen1@gmail.com>
2021-06-28 16:13:57 -0700

    iirneon cleaned up: all local vars are aligned

--- shared/CoreModules/iirneon.hh
+++ shared/CoreModules/iirneon.hh
@@ -8,7 +8,7 @@ class ParallelBPIIR {
 	// Todo: re-arranging might improve cache performance
 public:
 	float32x4_t __attribute__((aligned(16))) fSlow19202122;
-	float32x4_t __attribute__((aligned(16))) fRec0567_0;
+	// float32x4_t __attribute__((aligned(16))) fRec0567_0;
 	float32x4_t __attribute__((aligned(16))) fRec0567_1;
 	float32x4_t __attribute__((aligned(16))) fRec0567_2;
 	float32x4_t __attribute__((aligned(16))) fConst691215;
@@ -51,27 +51,16 @@ public:
 
 	float calc_4iir(float in)
 	{
-		// fRec0[0] = (in - ((fSlow19 * fRec0[1]) + (fConst6 * fRec0[2])));
-		// fRec5[0] = (in - ((fSlow20 * fRec5[1]) + (fConst9 * fRec5[2])));
-		// fRec6[0] = (in - ((fSlow21 * fRec6[1]) + (fConst12 * fRec6[2])));
-		// fRec7[0] = (in - ((fSlow22 * fRec7[1]) + (fConst15 * fRec7[2])));
-
-		// float32x4_t fRec0567_0;
+		float32x4_t __attribute__((aligned(16))) fRec0567_0;
 		fRec0567_0 = vmulq_f32(fSlow19202122, fRec0567_1);
 		fRec0567_0 = vmlaq_f32(fRec0567_0, fConst691215, fRec0567_2);
 
-		float32x4_t in_v = vdupq_n_f32(in);
+		float32x4_t __attribute__((aligned(16))) in_v = vdupq_n_f32(in);
 		fRec0567_0 = vsubq_f32(in_v, fRec0567_0);
 
-		// signalOut = 0.f;
-		// signalOut += 1.0f * (fRec0[0] - fRec0[2]);
-		// signalOut += 0.25f * (fRec5[0] - fRec5[2]);
-		// signalOut += 0.111111112f * (fRec6[0] - fRec6[2]);
-		// signalOut += 0.0625f * (fRec7[0] - fRec7[2]);
-
-		float32x4_t diff0567_02 = vsubq_f32(fRec0567_0, fRec0567_2);
-		float32x4_t outvec = vmulq_f32(diff0567_02, outmixWeights);
-		float32x2_t sum_tmp1 = vpadd_f32(vget_low_f32(outvec), vget_high_f32(outvec));
+		float32x4_t __attribute__((aligned(16))) diff0567_02 = vsubq_f32(fRec0567_0, fRec0567_2);
+		float32x4_t __attribute__((aligned(16))) outvec = vmulq_f32(diff0567_02, outmixWeights);
+		float32x2_t __attribute__((aligned(16))) sum_tmp1 = vpadd_f32(vget_low_f32(outvec), vget_high_f32(outvec));
 
 		// Todo:
 		// We need to do this:
@@ -89,18 +78,18 @@ public:
 		// std::cout << "fRec0[1] = " << fRec0567_1.v[0] << "\t";
 		// std::cout << "fRec0[2] = " << fRec0567_2.v[0] << "\t";
 
-		fRec0567_2[0] = fRec0567_1[0];
-		fRec0567_2[1] = fRec0567_1[1];
-		fRec0567_2[2] = fRec0567_1[2];
-		fRec0567_2[3] = fRec0567_1[3];
+		// fRec0567_2[0] = fRec0567_1[0];
+		// fRec0567_2[1] = fRec0567_1[1];
+		// fRec0567_2[2] = fRec0567_1[2];
+		// fRec0567_2[3] = fRec0567_1[3];
 
-		fRec0567_1[0] = fRec0567_0[0];
-		fRec0567_1[1] = fRec0567_0[1];
-		fRec0567_1[2] = fRec0567_0[2];
-		fRec0567_1[3] = fRec0567_0[3];
+		// fRec0567_1[0] = fRec0567_0[0];
+		// fRec0567_1[1] = fRec0567_0[1];
+		// fRec0567_1[2] = fRec0567_0[2];
+		// fRec0567_1[3] = fRec0567_0[3];
 
-		// fRec0567_2 = vmaxq_f32(fRec0567_1, fRec0567_1);
-		// fRec0567_1 = vmaxq_f32(fRec0567_0, fRec0567_0);
+		fRec0567_2 = vmaxq_f32(fRec0567_1, fRec0567_1);
+		fRec0567_1 = vmaxq_f32(fRec0567_0, fRec0567_0);
 
 		// Doesn't generate a store:
 		// float rec_1[4] = {vgetq_lane_f32(fRec0567_1, 0),
