2f879c548460ef5a949e497967035ffc5c562913
Dan Green <danngreen1@gmail.com>
2021-07-08 12:59:56 -0700

    Starting new PatchHeader format, and unit tests for MappedParamC

--- firmware/tests/patch_player_tests.cc
+++ firmware/tests/patch_player_tests.cc
@@ -115,3 +115,72 @@ TEST_CASE("Dup module index")
 		}
 	}
 }
+
+TEST_CASE("MappedParam get_mapped_val")
+{
+
+	MappedParamC knob;
+	float mapped_low;
+	float mapped_high;
+	SUBCASE("Normal mappings (partial range)")
+	{
+		mapped_low = 0.3f;
+		mapped_high = 0.8f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.3));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.55));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.8));
+
+		mapped_low = 0.9f;
+		mapped_high = 1.0f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.9));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.95));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(1.0));
+	}
+
+	SUBCASE("Mapped to full range (trivial mapping)")
+	{
+		mapped_low = 0.0f;
+		mapped_high = 1.0f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.0));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.5));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(1.0));
+	}
+
+	SUBCASE("Zero mapping")
+	{
+		mapped_low = 0.0f;
+		mapped_high = 0.0f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.0));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.0));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.0));
+	}
+
+	SUBCASE("Reverse direction mapping (0..1 => 1..0)")
+	{
+		mapped_low = 1.0f;
+		mapped_high = 0.0f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(1.0));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.5));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.0));
+	}
+	SUBCASE("Partial range, reverse direction(0..1 => 1.0..0.5)")
+	{
+		mapped_low = 0.5f;
+		mapped_high = 0.1f;
+		knob.range = mapped_high - mapped_low;
+		knob.offset = mapped_low;
+		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.5));
+		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.3));
+		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.1));
+	}
+}
--- shared/patch/patch.hh
+++ shared/patch/patch.hh
@@ -14,7 +14,6 @@ const int MAX_NODES_IN_PATCH = MAX_JACKS_PER_MODULE * MAX_MODULES_IN_PATCH;
 // FixMe: non-node only code:
 const int MAX_CONNECTIONS_PER_NODE = 4;
 
-// Todo: will this pack into 4 bytes on all systems?
 struct Jack {
 	uint16_t module_id;
 	uint16_t jack_id;
@@ -63,13 +62,6 @@ struct Patch {
 	int num_mapped_knobs;
 };
 
-// Todo: move cached patch info here:
-struct PatchAnalysis {
-	// get_panel_input_connection
-	// get_panel_output_connection
-	// get_duplicate_module_type_id
-};
-
 struct PatchRef {
 	PatchRef(const Patch &p)
 		: patch(p)
@@ -77,3 +69,44 @@ struct PatchRef {
 
 	const Patch &patch;
 };
+
+struct NetC {
+	Jack out;
+	std::array<Jack, MAX_CONNECTIONS_PER_NODE - 1> ins;
+};
+
+struct MappedParamC {
+	uint16_t module_id;
+	uint16_t param_id;
+	uint16_t panel_knob_id;
+	uint16_t curve_type; // reserved for future use
+	float range;
+	float offset;
+
+	// Returns the value of the mapped knob, given the panel knob value
+	// Return value goes from offset to offset+range as panel_val goes from 0 to 1
+	// If range<0 then mapping will be reversed direction
+	float get_mapped_val(float panel_val)
+	{
+		return range * panel_val + offset;
+	}
+};
+
+struct PatchHeader {
+	uint32_t format_version;
+
+	uint16_t name_strlen;
+	uint16_t num_modules;
+	uint16_t num_nets;
+	uint16_t num_static_knobs;
+	uint16_t num_mapped_knobs;
+};
+
+// Following PatchHeader is DATASIZE bytes of data,
+// where DATASIZE = name_strlen + num_modules*sizeof(ModuleTypeSlug) + num_nets*sizeof(NetC)
+// 					+ num_static_knobs*sizeof(StaticParam) + num_mapped_knobs*sizeof(MappedParamC)
+// char patch_name[name_strlen];
+// ModuleTypeSlug modules_used[num_modules];
+// NetC nets[num_nets];
+// StaticParam static_knobs[num_static_knobs];
+// MappedParam mapped_knobs[num_mapped_knobs];
