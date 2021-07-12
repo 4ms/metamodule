854b2ef732846e1dfb237e0453f23f9c7850ff52
Dan Green <danngreen1@gmail.com>
2021-07-12 13:46:09 -0700

    Measured OutputLow/HighRangeVolts scaling values on hardware

--- firmware/src/scalingConfig.hh
+++ firmware/src/scalingConfig.hh
@@ -3,5 +3,5 @@
 static inline constexpr float InputLowRangeVolts = -5.f;
 static inline constexpr float InputHighRangeVolts = 5.f;
 
-static inline constexpr float OutputLowRangeVolts = -5.f;
-static inline constexpr float OutputHighRangeVolts = 5.f;
+static inline constexpr float OutputLowRangeVolts = -7.91367f;
+static inline constexpr float OutputHighRangeVolts = 7.91367f;
