ada9fe20f87345714c9c2b9a936b816732890725
Dan Green <danngreen1@gmail.com>
2021-07-11 18:16:31 -0700

    Unit tests: add tests for duplicate module index, and more...
    check int_cable cable sizes
    cleanups

--- firmware/tests/patch_player_tests.cc
+++ firmware/tests/patch_player_tests.cc
@@ -1,6 +1,5 @@
 #include "doctest.h"
 #include "patch_player.hh"
-#include <iostream>
 
 // The test data in these unit tests was generated using VCV rack to create a .mmpatch binary file.
 // This file is then converted to a raw C-array using:
@@ -61,40 +60,16 @@ TEST_CASE("Simple output jack mapping")
 
 	SUBCASE("Check if mapped_outs[] data was loaded OK")
 	{
-		// These tests are commented out because it doesn't matter if they're true or not,
-		// as long as the output connection data tests pass
-		// But they can be useful in debugging
-		// CHECK(player.mapped_outs[0].panel_jack_id == 1);
-		// CHECK(player.mapped_outs[0].out.module_id == 1);
-		// CHECK(player.mapped_outs[0].out.jack_id == 1);
-
-		// CHECK(player.mapped_outs[1].panel_jack_id == 0);
-		// CHECK(player.mapped_outs[1].out.module_id == 1);
-		// CHECK(player.mapped_outs[1].out.jack_id == 3);
-
-		// CHECK(player.mapped_outs[2].panel_jack_id == 2);
-		// CHECK(player.mapped_outs[2].out.module_id == 2);
-		// CHECK(player.mapped_outs[2].out.jack_id == 1);
-
 		SUBCASE("Check if output connection data is correct")
 		{
-			Jack panel_out_0 = player.get_panel_output_connection(0);
-			CHECK(panel_out_0.module_id == 1);
-			CHECK(panel_out_0.jack_id == 3);
-
-			Jack panel_out_1 = player.get_panel_output_connection(1);
-			CHECK(panel_out_1.module_id == 1);
-			CHECK(panel_out_1.jack_id == 1);
-
-			Jack panel_out_2 = player.get_panel_output_connection(2);
-			CHECK(panel_out_2.module_id == 2);
-			CHECK(panel_out_2.jack_id == 1);
+			CHECK(player.get_panel_output_connection(0) == Jack{1, 3});
+			CHECK(player.get_panel_output_connection(1) == Jack{1, 1});
+			CHECK(player.get_panel_output_connection(2) == Jack{2, 1});
 
 			SUBCASE("Unmapped jacks are connected to 0,0")
 			{
-				Jack panel_out_3 = player.get_panel_output_connection(3);
-				CHECK(panel_out_3.module_id == 0);
-				CHECK(panel_out_3.jack_id == 0);
+				CHECK(player.get_panel_output_connection(3) == Jack{0, 0});
+				CHECK(player.get_panel_output_connection(4) == Jack{0, 0});
 			}
 		}
 	}
@@ -115,6 +90,9 @@ TEST_CASE("Internal cables: single and stacked")
 
 	bool found_cable1 = false;
 	bool found_cable2 = false;
+	int cable1_idx = -1;
+	int cable2_idx = -1;
+
 	Jack end_of_cable = Jack{-1, -1};
 
 	for (int net_i = 0; net_i < ph->num_int_cables; net_i++) {
@@ -124,8 +102,10 @@ TEST_CASE("Internal cables: single and stacked")
 		// {1,3} -> {2,0}
 		if (cable.out == Jack{1, 3}) {
 			if (cable.ins[0] == Jack{2, 0}) {
-				if (cable.ins[1] == end_of_cable)
+				if (cable.ins[1] == end_of_cable) {
 					found_cable1 = true;
+					cable1_idx = net_i;
+				}
 			}
 		}
 
@@ -135,17 +115,28 @@ TEST_CASE("Internal cables: single and stacked")
 			if ((cable.ins[0] == Jack{2, 1} && cable.ins[1] == Jack{2, 2}) ||
 				(cable.ins[0] == Jack{2, 2} && cable.ins[1] == Jack{2, 1}))
 			{
-				if (cable.ins[2] == end_of_cable)
+				if (cable.ins[2] == end_of_cable) {
 					found_cable2 = true;
+					cable2_idx = net_i;
+				}
 			}
 		}
 	}
 
-	CHECK(found_cable1);
-	CHECK(found_cable2);
+	SUBCASE("Check both cables were found")
+	{
+		CHECK(found_cable1);
+		CHECK(found_cable2);
+
+		SUBCASE("Check cables have the correct number of input jacks")
+		{
+			CHECK(player.num_int_cable_ins[cable1_idx] == 1);
+			CHECK(player.num_int_cable_ins[cable2_idx] == 2);
+		}
+	}
 }
 
-TEST_CASE("Output jack mapping to an virtual input jack that has a valid cable")
+TEST_CASE("It's OK to have a Panel output jack mapping to an virtual input jack that has a valid cable")
 {
 	auto *ph = reinterpret_cast<PatchHeader *>(unittest_outmap_overlapping_cable_mmpatch);
 
@@ -164,6 +155,7 @@ TEST_CASE("Output jack mapping to an virtual input jack that has a valid cable")
 		SUBCASE("Unmapped jack is connected to 0,0")
 		{
 			CHECK(player.get_panel_output_connection(3) == Jack{0, 0});
+			CHECK(player.get_panel_output_connection(4) == Jack{0, 0});
 		}
 	}
 }
@@ -211,59 +203,49 @@ TEST_CASE("Simple input jack mapping")
 	}
 }
 
-/*
-TEST_CASE("Panel jack connections")
+#include "patches/unittest_inmapping_overlapping.hh"
+TEST_CASE("Input jack is patched and mapped to a panel jack -- for now we ignore the mapping")
 {
+	auto *ph = reinterpret_cast<PatchHeader *>(unittest_inmapping_overlapping_mmpatch);
+	REQUIRE(ph->num_mapped_ins == 1);
+	REQUIRE(ph->num_int_cables == 1);
+
 	MetaModule::PatchPlayer player;
+	player.load_patch_from_header(ph);
 	player.calc_panel_jack_connections();
 
-	for (int i = 0; i < Panel::NumInJacks; i++) {
-		// printf("in_conns[%d] = {%d, %d}\n", i, player.in_conns[i].module_id, player.in_conns[i].jack_id);
-		if (i != 1) {
-			CHECK(player.get_panel_input_connection(i).module_id == 0);
-			CHECK(player.get_panel_input_connection(i).jack_id == 0);
-		} else {
-			CHECK(player.get_panel_input_connection(i).module_id == 5);
-			CHECK(player.get_panel_input_connection(i).jack_id == 2);
-		}
-	}
+	SUBCASE("No input mappings are present")
+	{
+		CHECK(player.get_panel_input_connection(0) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(1) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(2) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(3) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(4) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(5) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(6) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(7) == Jack{0, 0});
+		CHECK(player.get_panel_input_connection(8) == Jack{0, 0});
 
-	for (int i = 0; i < Panel::NumOutJacks; i++) {
-		// printf("out_conns[%d] = {%d, %d}\n", i, player.out_conns[i].module_id, player.out_conns[i].jack_id);
-		if (i != 1) {
-			CHECK(player.get_panel_output_connection(i).module_id == 0);
-			CHECK(player.get_panel_output_connection(i).jack_id == 0);
-		} else {
-			CHECK(player.get_panel_output_connection(i).module_id == 3);
-			CHECK(player.get_panel_output_connection(i).jack_id == 6);
+		SUBCASE("Internal cable is still present")
+		{
+			CHECK(player.int_cables[0].out == Jack{2, 0});
+			CHECK(player.num_int_cable_ins[0] == 1);
+			CHECK(player.int_cables[0].ins[0] == Jack{1, 0});
 		}
 	}
 }
 
+#include "patches/unittest_dup_mod_index.hh"
 TEST_CASE("Dup module index")
 {
-	SUBCASE("Dual LFOs, one panel")
+	SUBCASE("If there's more than one of a module type, get the correct 'dup index' for each")
 	{
-		const Patch exampleDualLFO = {
-			.modules_used =
-				{
-					"PANEL_8",
-					"LFOSINE",
-					"LFOSINE",
-				},
-			.num_modules = 3,
-		};
-		MetaModule::PatchPlayer player;
-		player.calc_multiple_module_indicies(exampleDualLFO);
-		CHECK(player.get_multiple_module_index(0) == 0); // PANEL_8
-		CHECK(player.get_multiple_module_index(1) == 1); // LFOSINE 1
-		CHECK(player.get_multiple_module_index(2) == 2); // LFOSINE 2
-	}
+		auto *ph = reinterpret_cast<PatchHeader *>(unittest_dup_mod_index_mmpatch);
+		REQUIRE(ph->num_modules == 8);
 
-	SUBCASE("If there's more than one of a module type, caches correct 'dup index' for each")
-	{
 		MetaModule::PatchPlayer player;
-		player.calc_multiple_module_indicies(p);
+		player.load_patch_from_header(ph);
+		player.calc_multiple_module_indicies();
 
 		CHECK(player.get_multiple_module_index(0) == 0); // PANEL_8
 		CHECK(player.get_multiple_module_index(1) == 1); // LFOSINE 1
@@ -276,85 +258,15 @@ TEST_CASE("Dup module index")
 
 		SUBCASE("Unloading a patch clears the indices to 0")
 		{
-			player.unload_patch(p);
-			CHECK(player.get_multiple_module_index(0) == 0); // PANEL_8
-			CHECK(player.get_multiple_module_index(1) == 0); // LFOSINE 1
-			CHECK(player.get_multiple_module_index(2) == 0); // KARPLUS 1
-			CHECK(player.get_multiple_module_index(3) == 0); // LFOSINE 2
-			CHECK(player.get_multiple_module_index(4) == 0); // CLOCK
-			CHECK(player.get_multiple_module_index(5) == 0); // LFOSINE 3
-			CHECK(player.get_multiple_module_index(6) == 0); // LFOSINE 4
-			CHECK(player.get_multiple_module_index(7) == 0); // KARPLUS 2
+			player.unload_patch();
+			CHECK(player.get_multiple_module_index(0) == 0);
+			CHECK(player.get_multiple_module_index(1) == 0);
+			CHECK(player.get_multiple_module_index(2) == 0);
+			CHECK(player.get_multiple_module_index(3) == 0);
+			CHECK(player.get_multiple_module_index(4) == 0);
+			CHECK(player.get_multiple_module_index(5) == 0);
+			CHECK(player.get_multiple_module_index(6) == 0);
+			CHECK(player.get_multiple_module_index(7) == 0);
 		}
 	}
 }
-
-TEST_CASE("MappedParam get_mapped_val")
-{
-
-	MappedKnob knob;
-	float mapped_low;
-	float mapped_high;
-	SUBCASE("Normal mappings (partial range)")
-	{
-		mapped_low = 0.3f;
-		mapped_high = 0.8f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.3));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.55));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.8));
-
-		mapped_low = 0.9f;
-		mapped_high = 1.0f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.9));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.95));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(1.0));
-	}
-
-	SUBCASE("Mapped to full range (trivial mapping)")
-	{
-		mapped_low = 0.0f;
-		mapped_high = 1.0f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.0));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.5));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(1.0));
-	}
-
-	SUBCASE("Zero mapping")
-	{
-		mapped_low = 0.0f;
-		mapped_high = 0.0f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.0));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.0));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.0));
-	}
-
-	SUBCASE("Reverse direction mapping (0..1 => 1..0)")
-	{
-		mapped_low = 1.0f;
-		mapped_high = 0.0f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(1.0));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.5));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.0));
-	}
-	SUBCASE("Partial range, reverse direction(0..1 => 1.0..0.5)")
-	{
-		mapped_low = 0.5f;
-		mapped_high = 0.1f;
-		knob.range = mapped_high - mapped_low;
-		knob.offset = mapped_low;
-		CHECK(knob.get_mapped_val(0.0f) == doctest::Approx(0.5));
-		CHECK(knob.get_mapped_val(0.5f) == doctest::Approx(0.3));
-		CHECK(knob.get_mapped_val(1.0f) == doctest::Approx(0.1));
-	}
-}
-*/
