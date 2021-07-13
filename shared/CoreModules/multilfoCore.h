ce0cd9946cf52b02c460a3651ec47b97e6ccef67
Dan Green <danngreen1@gmail.com>
2021-07-13 14:56:10 -0700

    MultiLFOCore optimized (saw/isaw are basically modPhase already)
    Also knob/CV for rate are only calculated if changed

--- shared/CoreModules/multilfoCore.h
+++ shared/CoreModules/multilfoCore.h
@@ -18,16 +18,16 @@ class MultilfoCore : public CoreProcessor {
 		"RateCV", "PhaseCV", "PWCV", "Reset"};
 	static inline const StaticString<LongNameChars> description{"Multi Output LFO"};
 
-	// clang-format off
-	virtual StaticString<NameChars> knob_name(unsigned idx) override { return (idx < NumKnobs) ? KnobNames[idx] : ""; }
-	virtual StaticString<NameChars> injack_name(unsigned idx) override { return (idx < NumInJacks) ? InJackNames[idx] : ""; }
-	virtual StaticString<NameChars> outjack_name(unsigned idx) override { return (idx < NumOutJacks) ? OutJackNames[idx] : ""; }
-	virtual StaticString<LongNameChars> get_description() override { return description; }
-	// clang-format on
 public:
+	MultilfoCore() = default;
+
 	virtual void update(void) override
 	{
-		float finalRate = rateOffset * setPitchMultiple(rateCV);
+		if (rateChanged) {
+			combineKnobCVFreq();
+			rateChanged = false;
+		}
+
 		phaccu += finalRate / sampRate;
 		if (phaccu >= 1.0f)
 			phaccu -= 1.0f;
@@ -36,13 +36,18 @@ public:
 			modPhase -= 1.0f;
 	}
 
-	MultilfoCore() {}
+	void combineKnobCVFreq()
+	{
+		auto knobFreq = expTable.closest(constrain(rawKnobVal, 0.f, 1.f));
+		finalRate = knobFreq * setPitchMultiple(rawCvVal);
+	}
 
 	virtual void set_param(int const param_id, const float val) override
 	{
 		switch (param_id) {
 			case 0:
-				rateOffset = map_value(val, 0.0f, 1.0f, 0.1f, 10.0f);
+				rawKnobVal = val;
+				rateChanged = true;
 				break;
 			case 1:
 				phaseOffset = val;
@@ -54,14 +59,15 @@ public:
 	}
 	virtual void set_samplerate(const float sr) override
 	{
-		// sampRate = sr;
+		sampRate = sr;
 	}
 
 	virtual void set_input(const int input_id, const float val) override
 	{
 		switch (input_id) {
 			case 0:
-				rateCV = val;
+				rawCvVal = val;
+				rateChanged = true;
 				break;
 			case 1: // phase cv
 				phaseCV = val;
@@ -87,10 +93,10 @@ public:
 				output = sinTable.interp(modPhase);
 				break;
 			case 1: // saw
-				output = sawTable.interp(modPhase);
+				output = modPhase * 2.f - 1.f;
 				break;
 			case 2: // inverted saw
-				output = isawTable.interp(modPhase);
+				output = 1.f - modPhase * 2.f;
 				break;
 			case 3: // pulse
 				float finalPw = constrain(pwOffset + pwCV, 0.0f, 1.0f);
@@ -110,18 +116,25 @@ public:
 	}
 	static constexpr char typeID[20] = "MULTILFO";
 	static inline bool s_registered = ModuleFactory::registerModuleType(typeID, description, create);
+	// clang-format off
+	virtual StaticString<NameChars> knob_name(unsigned idx) override { return (idx < NumKnobs) ? KnobNames[idx] : ""; }
+	virtual StaticString<NameChars> injack_name(unsigned idx) override { return (idx < NumInJacks) ? InJackNames[idx] : ""; }
+	virtual StaticString<NameChars> outjack_name(unsigned idx) override { return (idx < NumOutJacks) ? OutJackNames[idx] : ""; }
+	virtual StaticString<LongNameChars> get_description() override { return description; }
+	// clang-format on
 
 private:
+	bool rateChanged = true;
 	float phaccu = 0.f;
+	float finalRate = 0.1f;
 	float sampRate = 48000.f;
 	float lfoRate = 1.0f;
-	float rateOffset = 1.0f;
+	float rawKnobVal = 1.0f;
+	float rawCvVal = 1.0f;
 	float rateCV = 0.f;
 	float phaseCV = 0.f;
 	float pwOffset = 0.5f;
 	float pwCV = 0.f;
-	InterpArray<float, 2> sawTable = {-1.0f, 1.0f};
-	InterpArray<float, 2> isawTable = {1.0f, -1.0f};
 	float modPhase = 0.f;
 	float phaseOffset = 0.f;
 	bool currentReset = 0.f;
