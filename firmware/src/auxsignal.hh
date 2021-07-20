600ffc228bef1f6caae6c2e93877664184642b01
Dan Green <danngreen1@gmail.com>
2021-07-20 11:26:16 -0700

    Removed DAC stream from AuxSignalFrame

--- firmware/src/auxsignal.hh
+++ firmware/src/auxsignal.hh
@@ -4,10 +4,8 @@
 #include <cstdint>
 
 struct AuxSignalFrame {
-	uint32_t dac1;
-	uint32_t dac2;
 	uint32_t clock_out;
 };
 
-using AuxSignalStreamBlock = std::array<AuxSignalFrame, StreamConf::DAC::BlockSize>;
+using AuxSignalStreamBlock = std::array<AuxSignalFrame, StreamConf::Audio::BlockSize>;
 using DoubleAuxSignalStreamBlock = std::array<AuxSignalStreamBlock, 2>;
