416fc8279a3ce49f681d59ae33eb160036ed6648
Dan Green <danngreen1@gmail.com>
2021-07-29 15:10:44 -0700

    Fixed PCM3168 driver (and SaiTdm) for H7 and MP1

--- shared/util/bitfield.hh
+++ shared/util/bitfield.hh
@@ -1,16 +1,29 @@
 #pragma once
+#include <cstdint>
 
-template <typename FieldT>
+template<typename FieldT>
 struct BitFieldBase {
 	//casts an enum class as a bitfield
-	template <typename T>
-	FieldT constexpr bitfield(T const val) {
+	template<typename T>
+	FieldT constexpr bitfield(T const val)
+	{
 		//assert(sizeof(Rt)>=sizeof(T) || val>>sizeof(Rt) == 0);
-		return(static_cast<FieldT>(val));
+		return (static_cast<FieldT>(val));
 	}
 	template<typename... Args>
-	FieldT constexpr bitfield(const Args... args) {
-		return ( ... | bitfield(args));
+	FieldT constexpr bitfield(const Args... args)
+	{
+		return (... | bitfield(args));
 	}
 };
 
+template<typename T>
+uint8_t constexpr bitfield8(T const val)
+{
+	return (static_cast<uint8_t>(val));
+}
+template<typename... Args>
+uint8_t constexpr bitfield8(const Args... args)
+{
+	return (... | bitfield8(args));
+}
