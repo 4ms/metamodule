7049977fb3ded5018dea7d98c0b447ce6f46b2c3
Dan Green <danngreen1@gmail.com>
2021-08-18 10:52:20 -0700

    vcv: Moved hub knob context menu stuff to separate file

--- vcv/src/hub.cpp
+++ vcv/src/hub.cpp
@@ -2,6 +2,7 @@
 #include "CommModule.h"
 #include "CommWidget.h"
 #include "CoreModules/moduleTypes.h"
+#include "hub_knob_menu.hh"
 #include "localPath.h"
 #include "patch_writer.hh"
 #include "plugin.hpp"
@@ -281,6 +282,11 @@ private:
 	}
 };
 
+class HubKnob : public RoundBlackKnob {
+public:
+	void onButton(const event::Button &e) override;
+};
+
 struct MetaModuleHubWidget;
 
 class HubKnobLabel : public LabeledButton {
@@ -290,11 +296,6 @@ public:
 
 private:
 	MetaModuleHubWidget &_hub;
-};
-
-class HubKnob : public RoundBlackKnob {
-public:
-	void onButton(const event::Button &e) override;
 };
 
 struct MetaModuleHubWidget : CommModuleWidget {
@@ -471,129 +472,6 @@ void HubKnobLabel::onDeselect(const event::Deselect &e)
 		//	module->disableLearn(id);
 	}
 }
-
-struct ParamLabel : ui::MenuLabel {
-	ParamWidget *paramWidget;
-	void step() override
-	{
-		text = paramWidget->paramQuantity->getString();
-		MenuLabel::step();
-	}
-};
-
-struct ParamField : ui::TextField {
-	ParamWidget *paramWidget;
-
-	void step() override
-	{
-		// Keep selected
-		APP->event->setSelected(this);
-		TextField::step();
-	}
-
-	void setParamWidget(ParamWidget *paramWidget)
-	{
-		this->paramWidget = paramWidget;
-		if (paramWidget->paramQuantity)
-			text = paramWidget->paramQuantity->getDisplayValueString();
-		selectAll();
-	}
-
-	void onSelectKey(const event::SelectKey &e) override
-	{
-		if (e.action == GLFW_PRESS && (e.key == GLFW_KEY_ENTER || e.key == GLFW_KEY_KP_ENTER)) {
-			float oldValue = paramWidget->paramQuantity->getValue();
-			if (paramWidget->paramQuantity)
-				paramWidget->paramQuantity->setDisplayValueString(text);
-			float newValue = paramWidget->paramQuantity->getValue();
-
-			if (oldValue != newValue) {
-				// Push ParamChange history action
-				history::ParamChange *h = new history::ParamChange;
-				h->moduleId = paramWidget->paramQuantity->module->id;
-				h->paramId = paramWidget->paramQuantity->paramId;
-				h->oldValue = oldValue;
-				h->newValue = newValue;
-				APP->history->push(h);
-			}
-
-			ui::MenuOverlay *overlay = getAncestorOfType<ui::MenuOverlay>();
-			overlay->requestDelete();
-			e.consume(this);
-		}
-
-		if (!e.getTarget())
-			TextField::onSelectKey(e);
-	}
-};
-
-struct MapField : ui::TextField {
-	ParamWidget *paramWidget;
-
-	void step() override
-	{
-		// Keep selected
-		APP->event->setSelected(this);
-		TextField::step();
-	}
-
-	void setParamWidget(ParamWidget *paramWidget)
-	{
-		this->paramWidget = paramWidget;
-		if (paramWidget->paramQuantity)
-			text = paramWidget->paramQuantity->getDisplayValueString();
-		selectAll();
-	}
-
-	void onSelectKey(const event::SelectKey &e) override
-	{
-		if (e.action == GLFW_PRESS && (e.key == GLFW_KEY_ENTER || e.key == GLFW_KEY_KP_ENTER)) {
-			float oldValue = paramWidget->paramQuantity->getValue();
-			if (paramWidget->paramQuantity)
-				paramWidget->paramQuantity->setDisplayValueString(text);
-			float newValue = paramWidget->paramQuantity->getValue();
-
-			if (oldValue != newValue) {
-				// Push ParamChange history action
-				history::ParamChange *h = new history::ParamChange;
-				h->moduleId = paramWidget->paramQuantity->module->id;
-				h->paramId = paramWidget->paramQuantity->paramId;
-				h->oldValue = oldValue;
-				h->newValue = newValue;
-				APP->history->push(h);
-			}
-
-			ui::MenuOverlay *overlay = getAncestorOfType<ui::MenuOverlay>();
-			overlay->requestDelete();
-			e.consume(this);
-		}
-
-		if (!e.getTarget())
-			TextField::onSelectKey(e);
-	}
-};
-
-struct ParamResetItem : ui::MenuItem {
-	ParamWidget *paramWidget;
-	void onAction(const event::Action &e) override
-	{
-		paramWidget->resetAction();
-	}
-};
-
-struct ParamFineItem : ui::MenuItem {};
-
-struct ParamUnmapItem : ui::MenuItem {
-	ParamWidget *paramWidget;
-	void onAction(const event::Action &e) override
-	{
-		engine::ParamHandle *paramHandle =
-			APP->engine->getParamHandle(paramWidget->paramQuantity->module->id, paramWidget->paramQuantity->paramId);
-		if (paramHandle) {
-			APP->engine->updateParamHandle(paramHandle, -1, 0);
-		}
-	}
-};
 
 void HubKnob::onButton(const event::Button &e)
 {
---
+++ vcv/src/hub_knob_menu.hh
@@ -0,0 +1,124 @@
+#include "plugin.hpp"
+
+struct ParamLabel : ui::MenuLabel {
+	ParamWidget *paramWidget;
+	void step() override
+	{
+		text = paramWidget->paramQuantity->getString();
+		MenuLabel::step();
+	}
+};
+
+struct ParamField : ui::TextField {
+	ParamWidget *paramWidget;
+
+	void step() override
+	{
+		// Keep selected
+		APP->event->setSelected(this);
+		TextField::step();
+	}
+
+	void setParamWidget(ParamWidget *paramWidget)
+	{
+		this->paramWidget = paramWidget;
+		if (paramWidget->paramQuantity)
+			text = paramWidget->paramQuantity->getDisplayValueString();
+		selectAll();
+	}
+
+	void onSelectKey(const event::SelectKey &e) override
+	{
+		if (e.action == GLFW_PRESS && (e.key == GLFW_KEY_ENTER || e.key == GLFW_KEY_KP_ENTER)) {
+			float oldValue = paramWidget->paramQuantity->getValue();
+			if (paramWidget->paramQuantity)
+				paramWidget->paramQuantity->setDisplayValueString(text);
+			float newValue = paramWidget->paramQuantity->getValue();
+
+			if (oldValue != newValue) {
+				// Push ParamChange history action
+				history::ParamChange *h = new history::ParamChange;
+				h->moduleId = paramWidget->paramQuantity->module->id;
+				h->paramId = paramWidget->paramQuantity->paramId;
+				h->oldValue = oldValue;
+				h->newValue = newValue;
+				APP->history->push(h);
+			}
+
+			ui::MenuOverlay *overlay = getAncestorOfType<ui::MenuOverlay>();
+			overlay->requestDelete();
+			e.consume(this);
+		}
+
+		if (!e.getTarget())
+			TextField::onSelectKey(e);
+	}
+};
+
+struct MapField : ui::TextField {
+	ParamWidget *paramWidget;
+
+	void step() override
+	{
+		// Keep selected
+		APP->event->setSelected(this);
+		TextField::step();
+	}
+
+	void setParamWidget(ParamWidget *paramWidget)
+	{
+		this->paramWidget = paramWidget;
+		if (paramWidget->paramQuantity)
+			text = paramWidget->paramQuantity->getDisplayValueString();
+		selectAll();
+	}
+
+	void onSelectKey(const event::SelectKey &e) override
+	{
+		if (e.action == GLFW_PRESS && (e.key == GLFW_KEY_ENTER || e.key == GLFW_KEY_KP_ENTER)) {
+			float oldValue = paramWidget->paramQuantity->getValue();
+			if (paramWidget->paramQuantity)
+				paramWidget->paramQuantity->setDisplayValueString(text);
+			float newValue = paramWidget->paramQuantity->getValue();
+
+			if (oldValue != newValue) {
+				// Push ParamChange history action
+				history::ParamChange *h = new history::ParamChange;
+				h->moduleId = paramWidget->paramQuantity->module->id;
+				h->paramId = paramWidget->paramQuantity->paramId;
+				h->oldValue = oldValue;
+				h->newValue = newValue;
+				APP->history->push(h);
+			}
+
+			ui::MenuOverlay *overlay = getAncestorOfType<ui::MenuOverlay>();
+			overlay->requestDelete();
+			e.consume(this);
+		}
+
+		if (!e.getTarget())
+			TextField::onSelectKey(e);
+	}
+};
+
+struct ParamResetItem : ui::MenuItem {
+	ParamWidget *paramWidget;
+	void onAction(const event::Action &e) override
+	{
+		paramWidget->resetAction();
+	}
+};
+
+struct ParamFineItem : ui::MenuItem {};
+
+struct ParamUnmapItem : ui::MenuItem {
+	ParamWidget *paramWidget;
+	void onAction(const event::Action &e) override
+	{
+		engine::ParamHandle *paramHandle =
+			APP->engine->getParamHandle(paramWidget->paramQuantity->module->id, paramWidget->paramQuantity->paramId);
+		if (paramHandle) {
+			APP->engine->updateParamHandle(paramHandle, -1, 0);
+		}
+	}
+};
