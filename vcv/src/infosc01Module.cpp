99468b579bb91853b48419340b61876b5d572069
Dan Green <danngreen1@gmail.com>
2021-08-19 16:40:41 -0700

    Quick knob/jack placement

--- vcv/src/infosc01Module.cpp
+++ vcv/src/infosc01Module.cpp
@@ -1,12 +1,26 @@
 #include "CommModule.h"
 #include "CommWidget.h"
+// #include "infosc01Core.h"
 #include "util/math.hh"
 
 struct Infosc01Module : public CommModule {
 
-	enum ParamIds { NUM_PARAMS };
-	enum InputIds { NUM_INPUTS };
-	enum OutputIds { NUM_OUTPUTS };
+	enum ParamIds { PitchKnob, TrackKnob, RangeKnob, FMKnob, AbsKnob, PhaseKnob, ModshapeKnob, WaveKnob, NUM_PARAMS };
+	enum InputIds {
+		PitchInJack,
+		RangeInJack,
+		FMInJack,
+		ModInJack,
+		AbsInJack,
+		LinInJack,
+		PhaseInJack,
+		PMInJack,
+		ScanInJack,
+		AddInJack,
+		ResetInJack,
+		NUM_INPUTS
+	};
+	enum OutputIds { WaveOutJack, NUM_OUTPUTS };
 	enum LightIds { NUM_LIGHTS };
 
 	Infosc01Module()
@@ -29,7 +43,23 @@ struct Infosc01Widget : CommModuleWidget {
 		setModule(module);
 		mainModule = module;
 
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/4hpinfosc01.svg")));
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/12hptemplate.svg")));
+
+		for (int i = 0; i < Infosc01Module::NUM_PARAMS /*Infosc01Core::NumKnobs*/; i++) {
+			float x = i % 4;
+			float y = i >> 2;
+			addLabeledKnob("A" /*Infosc01Core::KnobNames[i].cstr()*/, i, {x, y});
+		}
+
+		for (int i = 0; i < Infosc01Module::NUM_INPUTS; i++) {
+			float x = i % 4;
+			float y = i >> 2;
+			addLabeledInput("IN", i, {x, y});
+		}
+
+		addLabeledOutput("Out", Infosc01Module::WaveOutJack, {0, 0});
+
+		addModuleTitle("Infinite Osc");
 	}
 };
 
