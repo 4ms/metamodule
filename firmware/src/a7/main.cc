a312bc9f5cb8a0d2fd8a71e1c08165a6a2887e16
Dan Green <danngreen1@gmail.com>
2021-08-22 21:59:32 -0700

    A7 main loop LED blinker uses HAL_Delay

--- firmware/src/a7/main.cc
+++ firmware/src/a7/main.cc
@@ -70,14 +70,14 @@ void main()
 	audio.start();
 
 	while (true) {
-		// HAL_Delay(500);
-		for (uint32_t i = 0; i < 100000000UL; i++)
-			__NOP();
+		HAL_Delay(500);
+		// for (uint32_t i = 0; i < 100000000UL; i++)
+		// 	__NOP();
 		Debug::red_LED1::high();
 
-		// HAL_Delay(500);
-		for (uint32_t i = 0; i < 100000000UL; i++)
-			__NOP();
+		HAL_Delay(500);
+		// for (uint32_t i = 0; i < 100000000UL; i++)
+		// 	__NOP();
 		Debug::red_LED1::low();
 	}
 }
