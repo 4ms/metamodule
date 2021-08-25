fc045b398f9ec38e82b2435b41fefca3bda13cde
Dan Green <danngreen1@gmail.com>
2021-08-24 23:44:36 -0700

    python makeModule adds screws

--- vcv/py/wrapperTemplate.cpp
+++ vcv/py/wrapperTemplate.cpp
@@ -4,23 +4,15 @@
 
 struct TemplateModule : CommModule {
 
-	enum ParamIds {
-		NUM_PARAMS
-	};
-	enum InputIds {
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+	enum ParamIds { NUM_PARAMS };
+	enum InputIds { NUM_INPUTS };
+	enum OutputIds { NUM_OUTPUTS };
+	enum LightIds { NUM_LIGHTS };
 
 	TemplateModule()
 	{
 		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        core = ModuleFactory::create(SLUG);
+		core = ModuleFactory::create(SLUG);
 		selfID.typeID = SLUG;
 	}
 };
@@ -38,6 +30,10 @@ struct TemplateWidget : CommModuleWidget {
 		mainModule = module;
 
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/4hptemplate.svg")));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 	}
 };
 
