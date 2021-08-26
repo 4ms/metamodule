d611f89e143977f736232f508b074a88e6ed12c4
Dan Green <danngreen1@gmail.com>
2021-08-25 19:20:59 -0700

    Fix missing defs for mini build

--- shared/CoreModules/panel_mini_defs.hh
+++ shared/CoreModules/panel_mini_defs.hh
@@ -12,4 +12,12 @@ struct PanelDef {
 	static constexpr uint32_t NumMetaCV = 1;
 	static constexpr uint32_t NumRgbButton = 2;
 	static constexpr uint32_t NumMetaRgbButton = 0;
+
+	static constexpr int NumKnobs = NumPot;
+	static constexpr int NumOutJacks = NumAudioIn + NumCVIn + NumGateIn;
+	static constexpr int NumInJacks = NumAudioOut + NumDACOut + NumGateOut;
+	// User-facing out jacks, like "Audio Out 3", is an input jack as seen by other virtual modules
+	static constexpr int NumUserFacingOutJacks = NumInJacks;
+	// User-facing in jacks, like "Audio In 1", is an output jack as seen by other virtual modules
+	static constexpr int NumUserFacingInJacks = NumOutJacks;
 };
