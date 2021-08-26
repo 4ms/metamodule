8332d52285de4f1692cc114d690dccd173a91306
Dan Green <danngreen1@gmail.com>
2021-08-25 22:47:39 -0700

    Fixed Hub Medium scaling (AI uses 72dpi, VCV uses 75dpi)

--- vcv/src/hub-medium.cpp
+++ vcv/src/hub-medium.cpp
@@ -29,6 +29,11 @@ struct HubMediumWidget : CommModuleWidget {
 	Label *valueLabel;
 	Label *recLabel;
 
+	Vec fixDPI(Vec v)
+	{
+		return v.mult(75.f / 72.f);
+	}
+
 	HubMediumWidget(HubMedium *module)
 	{
 		setModule(module);
@@ -42,38 +47,38 @@ struct HubMediumWidget : CommModuleWidget {
 
 		addParam(createParamCentered<BefacoPush>((Vec(22.31, 43.16)), module, 0));
 
-		addSmallLabeledKnobPx("", 1, {64.57, 53.24});
-		addSmallLabeledKnobPx("", 2, {27.94, 88.42});
-		addSmallLabeledKnobPx("", 3, {343.34, 92.99});
-		addSmallLabeledKnobPx("", 4, {295.26, 117.58});
-		addSmallLabeledKnobPx("", 5, {64.57, 120.56});
-		addLabeledKnobPx("", 6, {34.23, 166.3});
-		addLabeledKnobPx("", 7, {341.35, 166.41});
-		addLabeledKnobPx("", 8, {94.21, 193.61});
-		addLabeledKnobPx("", 9, {280.72, 193.68});
-		addLabeledKnobPx("", 10, {155.15, 213.81});
-		addLabeledKnobPx("", 11, {220.04, 214.26});
-		addSmallLabeledKnobPx("", 12, {44.14, 226.16});
+		addSmallLabeledKnobPx("", 1, fixDPI({64.57, 53.24}));
+		addSmallLabeledKnobPx("", 2, fixDPI({27.94, 88.42}));
+		addSmallLabeledKnobPx("", 3, fixDPI({343.34, 92.99}));
+		addSmallLabeledKnobPx("", 4, fixDPI({295.26, 117.58}));
+		addSmallLabeledKnobPx("", 5, fixDPI({64.57, 120.56}));
+		addLabeledKnobPx("", 6, fixDPI({34.23, 166.3}));
+		addLabeledKnobPx("", 7, fixDPI({341.35, 166.41}));
+		addLabeledKnobPx("", 8, fixDPI({94.21, 193.61}));
+		addLabeledKnobPx("", 9, fixDPI({280.72, 193.68}));
+		addLabeledKnobPx("", 10, fixDPI({155.15, 213.81}));
+		addLabeledKnobPx("", 11, fixDPI({220.04, 214.26}));
+		addSmallLabeledKnobPx("", 12, fixDPI({44.14, 226.16}));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.9, 274.7)), module, 0));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(66.42, 274.7)), module, 1));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(109.61, 282.75)), module, 2));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(153.13, 282.75)), module, 3));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(36.34, 324.15)), module, 4));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(79.86, 324.15)), module, 5));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(122.6, 324.18)), module, 6));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(166.12, 324.18)), module, 7));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({22.9, 274.7}), module, 0));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({66.42, 274.7}), module, 1));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({109.61, 282.75}), module, 2));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({153.13, 282.75}), module, 3));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({36.34, 324.15}), module, 4));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({79.86, 324.15}), module, 5));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({122.6, 324.18}), module, 6));
+		addInput(createInputCentered<PJ301MPort>(fixDPI({166.12, 324.18}), module, 7));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(352.74, 219.83)), module, 0));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(316.22, 238.83)), module, 1));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(308.9, 282.15)), module, 2));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(352.42, 282.15)), module, 3));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(222.16, 282.79)), module, 4));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(265.68, 282.79)), module, 5));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(209.28, 324.18)), module, 6));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(252.8, 324.18)), module, 7));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(295.21, 324.2)), module, 8));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(338.73, 324.2)), module, 9));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({352.74, 219.83}), module, 0));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({316.22, 238.83}), module, 1));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({308.9, 282.15}), module, 2));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({352.42, 282.15}), module, 3));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({222.16, 282.79}), module, 4));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({265.68, 282.79}), module, 5));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({209.28, 324.18}), module, 6));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({252.8, 324.18}), module, 7));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({295.21, 324.2}), module, 8));
+		addOutput(createOutputCentered<PJ301MPort>(fixDPI({338.73, 324.2}), module, 9));
 	}
 };
 
