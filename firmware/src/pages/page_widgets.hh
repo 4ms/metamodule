80b931fcf8ab1bc82b9e3a6acdf1ec69824120e1
Dan Green <danngreen1@gmail.com>
2021-08-31 16:04:29 -0700

    Fix page widgets showing CV values (not hard-set number of CV jacks)

--- firmware/src/pages/page_widgets.hh
+++ firmware/src/pages/page_widgets.hh
@@ -83,10 +83,10 @@ struct PageWidgets {
 		int y = y_pos;
 		const int box_height = 15;
 		const int box_width = 30;
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < PanelDef::NumCVIn + PanelDef::NumKnobs; i++) {
 			screen.setCursor((i & 0b111) * box_width + 3, y);
-			if (i >= 8)
-				screen.print((int16_t)(params.cvjacks[i - 8] * 100));
+			if (i >= PanelDef::NumKnobs)
+				screen.print((int16_t)(params.cvjacks[i - PanelDef::NumKnobs] * 100));
 			else
 				screen.print((int16_t)(params.knobs[i] * 100));
 			if (i == 7)
