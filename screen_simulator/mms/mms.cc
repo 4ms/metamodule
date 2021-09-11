c0d21810fdad763d9fc08fd18291e413c49b7326
Dan Green <danngreen1@gmail.com>
2021-09-11 15:57:27 -0700

    Simulator: rust app sets metaparams directly, and Simulator mimicks UI

--- screen_simulator/mms/mms.cc
+++ screen_simulator/mms/mms.cc
@@ -38,47 +38,56 @@ struct Simulator {
 
 	void refresh()
 	{
+		handle_rotary();
 		pages.display_current_page();
 	}
+
+	void handle_rotary()
+	{
+		auto rotary_pushed_turned = metaparams.rotary_pushed.use_motion();
+		if (rotary_pushed_turned < 0)
+			pages.prev_page();
+		if (rotary_pushed_turned > 0)
+			pages.next_page();
+	}
 };
+
 static Simulator sim;
 
 extern "C" void rotary_fwd()
 {
-	sim.pages.next_page();
+	sim.metaparams.rotary.motion++;
+	sim.metaparams.rotary.abs_pos++;
+	// sim.pages.next_page();
 }
 
 extern "C" void rotary_back()
 {
-	sim.pages.prev_page();
+	sim.metaparams.rotary.motion--;
+	sim.metaparams.rotary.abs_pos--;
+	// sim.pages.prev_page();
 }
 
 extern "C" void rotary_push_fwd()
 {
-	uint32_t cur_patch_index = sim.patch_list.cur_patch_index();
-	uint32_t new_patch_index = (cur_patch_index == 0) ? (sim.patch_list.NumPatches - 1) : cur_patch_index - 1;
-	sim.patch_player.unload_patch();
-	sim.patch_list.set_cur_patch_index(new_patch_index);
-	sim.patch_player.load_patch(sim.patch_list.cur_patch());
+	sim.metaparams.rotary_pushed.motion++;
+	sim.metaparams.rotary_pushed.abs_pos++;
 }
 
 extern "C" void rotary_push_back()
 {
-	uint32_t cur_patch_index = sim.patch_list.cur_patch_index();
-	uint32_t new_patch_index = cur_patch_index == (sim.patch_list.NumPatches - 1) ? 0 : cur_patch_index + 1;
-	sim.patch_player.unload_patch();
-	sim.patch_list.set_cur_patch_index(new_patch_index);
-	sim.patch_player.load_patch(sim.patch_list.cur_patch());
+	sim.metaparams.rotary_pushed.motion++;
+	sim.metaparams.rotary_pushed.abs_pos++;
 }
 
 extern "C" void rotary_press()
 {
-	//
+	sim.metaparams.rotary_button.register_falling_edge();
 }
 
 extern "C" void rotary_release()
 {
-	//
+	sim.metaparams.rotary_button.register_rising_edge();
 }
 
 extern "C" void jump_to_page(unsigned page_num)
@@ -105,3 +114,12 @@ size_t get_heap_size()
 {
 	return 0;
 }
+
+// void change_patch(uint32_t new_patch_index)
+// {
+// 	uint32_t cur_patch_index = sim.patch_list.cur_patch_index();
+// 	// uint32_t new_patch_index = (cur_patch_index == 0) ? (sim.patch_list.NumPatches - 1) : cur_patch_index - 1;
+// 	sim.patch_player.unload_patch();
+// 	sim.patch_list.set_cur_patch_index(new_patch_index);
+// 	sim.patch_player.load_patch(sim.patch_list.cur_patch());
+// }
