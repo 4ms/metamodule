9d61e6b514d2d2db809d1def791aa8eacfb0f11c
Dan Green <danngreen1@gmail.com>
2021-09-16 06:55:22 +0200

    Name of simulator window

--- screen_simulator/src/main.rs
+++ screen_simulator/src/main.rs
@@ -43,7 +43,7 @@ fn main() {
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    let mut window = Window::new("Test window", WIDTH, HEIGHT, WindowOptions::default())
+    let mut window = Window::new("MetaModule", WIDTH, HEIGHT, WindowOptions::default())
         .unwrap_or_else(|e| {
             panic!("{}", e);
         });
