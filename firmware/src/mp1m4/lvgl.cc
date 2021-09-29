a03f3b7372e1441c9a08a44dabfa7c5980fe99be
Dan Green <danngreen1@gmail.com>
2021-09-28 22:25:08 -0700

    Starting init for lvgl buffers

--- firmware/src/mp1m4/lvgl.cc
+++ firmware/src/mp1m4/lvgl.cc
@@ -1,10 +1,14 @@
+#include "lvgl/lvgl.h"
 
+template<size_t HOR_RES>
 class LVGLDriver {
+	lv_disp_draw_buf_t disp_buf;
+	lv_color_t buf_1[HOR_RES * 10];
+	//lv_color_t buf_2[MY_DISP_HOR_RES * 10];
 
+	/*Initialize `disp_buf` with the buffer(s). With only one buffer use NULL instead buf_2 */
 	LVGLDriver()
 	{
-		static lv_disp_buf_t disp_buf;
-		static lv_color_t buf[LV_HOR_RES_MAX * LV_VER_RES_MAX / 10]; /*Declare a buffer for 1/10 screen size*/
-		lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * LV_VER_RES_MAX / 10); /*Initialize the display buffer*/
+		lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, HOR_RES * 10);
 	}
 };
