6a666c2595fe9c331d60e4849acc6a3702543ed1
Dan Green <danngreen1@gmail.com>
2021-10-08 11:30:03 -0700

    Re-ordered arc to knob mapping, and GUI freezes after loading page

--- firmware/src/pages/arc_test_jq.hh
+++ firmware/src/pages/arc_test_jq.hh
@@ -48,12 +48,14 @@ struct ArcTestJQPage : PageBase {
 
 	void update()
 	{
-		lv_arc_set_value(ui->Main_Example_arc_1, params.knobs[0] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_2, params.knobs[1] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_3, params.knobs[2] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_4, params.knobs[3] * 100.f);
+		// 2 3 6
+		// 1 5 4
+		lv_arc_set_value(ui->Main_Example_arc_2, params.knobs[0] * 100.f);
+		lv_arc_set_value(ui->Main_Example_arc_3, params.knobs[1] * 100.f);
+		lv_arc_set_value(ui->Main_Example_arc_6, params.knobs[2] * 100.f);
+		lv_arc_set_value(ui->Main_Example_arc_1, params.knobs[3] * 100.f);
 		lv_arc_set_value(ui->Main_Example_arc_5, params.knobs[4] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_6, params.knobs[5] * 100.f);
+		lv_arc_set_value(ui->Main_Example_arc_4, params.knobs[5] * 100.f);
 	}
 };
 } // namespace MetaModule
