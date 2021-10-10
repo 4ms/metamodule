98d6b0fad7cb085adddfb3080835f8390d33301f
Dan Green <danngreen1@gmail.com>
2021-10-10 14:50:55 -0700

    don't do anything special for arc sliders

--- firmware/src/pages/arc_test_jq.hh
+++ firmware/src/pages/arc_test_jq.hh
@@ -16,24 +16,24 @@ struct ArcTestJQPage : PageBase {
 
 	void init() override
 	{
-		lv_arc_set_adjustable(ui->Main_Example_arc_1, true);
-		lv_arc_set_adjustable(ui->Main_Example_arc_2, true);
-		lv_arc_set_adjustable(ui->Main_Example_arc_3, true);
-		lv_arc_set_adjustable(ui->Main_Example_arc_4, true);
-		lv_arc_set_adjustable(ui->Main_Example_arc_5, true);
-		lv_arc_set_adjustable(ui->Main_Example_arc_6, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_1, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_2, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_3, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_4, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_5, true);
+		// lv_arc_set_adjustable(ui->Main_Example_arc_6, true);
 	}
 
 	void update() override
 	{
 		// 2 3 6
 		// 1 5 4
-		lv_arc_set_value(ui->Main_Example_arc_2, params.knobs[0] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_3, params.knobs[1] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_6, params.knobs[2] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_1, params.knobs[3] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_5, params.knobs[4] * 100.f);
-		lv_arc_set_value(ui->Main_Example_arc_4, params.knobs[5] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_2, params.knobs[0] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_3, params.knobs[1] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_6, params.knobs[2] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_1, params.knobs[3] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_5, params.knobs[4] * 100.f);
+		// lv_arc_set_value(ui->Main_Example_arc_4, params.knobs[5] * 100.f);
 	}
 };
 } // namespace MetaModule
