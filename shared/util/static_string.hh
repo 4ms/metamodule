7ed196cb985cc6e76bd499468b4e5453c55bf2c8
Dan Green <danngreen1@gmail.com>
2021-10-31 21:37:41 -0700

    util/static_string has constexpr constructors, can take a string_view

--- shared/util/static_string.hh
+++ shared/util/static_string.hh
@@ -1,20 +1,30 @@
 #pragma once
 #include <cstddef>
-//#include <string.h>
 #include <cstring>
+#include <string>
 
+// A null-terminated string with fixed compile-time capacity
 template<size_t CAPACITY>
 struct StaticString {
 	char data[CAPACITY + 1];
 
 	static constexpr size_t capacity = CAPACITY;
-	StaticString()
-	{
+
+	constexpr StaticString() {
 		data[0] = '\0';
 	}
 
-	StaticString(const char *s)
-	{
+	constexpr StaticString(std::string_view s) {
+		size_t i = 0;
+		for (auto c : s) {
+			data[i++] = c;
+			if (i >= CAPACITY)
+				break;
+		}
+		data[i] = '\0';
+	}
+
+	constexpr StaticString(const char *s) {
 		size_t i = 0;
 		while (*s && i < CAPACITY) {
 			data[i] = *s;
@@ -24,24 +34,24 @@ struct StaticString {
 		data[i] = '\0';
 	}
 
-	const char *cstr() const
-	{
+	const char *cstr() const {
+		return data;
+	}
+
+	const char *c_str() const {
 		return data;
 	}
 
 	template<size_t CAP2>
-	bool operator==(const StaticString<CAP2> &rhs) const
-	{
+	bool operator==(const StaticString<CAP2> &rhs) const {
 		return (strcmp(data, rhs.data) == 0);
 	}
 
-	bool is_equal(const char *rhs) const
-	{
+	bool is_equal(const char *rhs) const {
 		return (strcmp(data, rhs) == 0);
 	}
 
-	operator const char *() const
-	{
+	operator const char *() const {
 		return data;
 	}
 };
