b2642c227cea7a4e165c03047b1d63013f06c4bf
Dan Green <danngreen1@gmail.com>
2021-11-04 16:06:26 -0700

    WIP defs template and enosc example

--- shared/CoreModules/defs/defs_template.hh
+++ shared/CoreModules/defs/defs_template.hh
@@ -1,6 +1,6 @@
+#include "util/static_string.hh"
 #include <array>
 #include <cstddef>
-#include <static_string.hh>
 #include <string>
 
 struct KnobDef {
@@ -11,12 +11,13 @@ struct KnobDef {
 	uint32_t y;
 
 	float default_val;
+	enum { Small, Medium, Large } knob_type;
 
 	// TODO: should we use these (they're from VCV, I think)
-	float display_base;
-	float display_mult;
-	float display_offset;
-	std::string units;
+	// float display_base;
+	// float display_mult;
+	// float display_offset;
+	// std::string units;
 
 	std::string_view short_name;
 	std::string_view long_name;
@@ -25,25 +26,26 @@ struct KnobDef {
 
 struct ModuleDefsBase {
 
-	// const lv_img_dsc_t *img_src;
-	// TODO: don't depend on LVGL, instead we need another way to address this asset.
-	// We could have a lookup function like:
-	// lv_img_dsc_t* get_module_image_by_index(uint32_t module_index);
-	// This would only be needed by the same UI code that uses LVGL already
-	// static constexpr uint32_t compiled_image_index = -1;
-	// static constexpr StaticString<31> slug{empt};
 	static constexpr std::string_view slug{""};
 
 	// Image
 	static constexpr uint32_t width_hp = 0;
+
+	static constexpr std::string_view svg_filename{""};
+
+	// Knobs
+	static constexpr uint32_t NumKnobs = 0;
+	static constexpr std::array<KnobDef, NumKnobs> Knobs{};
+
 	constexpr uint32_t get_width_px() {
 		// 240px = 5.059" so 1HP = 0.2" = 9.488px
 		return static_cast<uint32_t>((float)width_hp * 9.488f);
 	}
 
-	static constexpr std::string_view svg_filename = "";
+	static constexpr uint32_t mm2um(float mm) {
+		return static_cast<uint32_t>(1000.f * mm);
+	}
 
-	// Knobs
-	static constexpr uint32_t NumKnobs = 0;
-	std::array<KnobDef, NumKnobs> Knobs;
+	// Need a lookup function somewhere LVGL sees, like:
+	// lv_img_dsc_t* get_module_image_from_slug(StaticString<31> slug);
 };
---
+++ vcv/src/modules/enosc.cpp
@@ -0,0 +1,50 @@
+#include "CommModule.h"
+#include "CommWidget.h"
+#include "CoreModules/moduleTypes.h"
+#include "math.hh"
+
+struct EnOsc : CommModule {
+
+	enum ParamIds { FREQ_KNOB, SHAPE_KNOB, LEVEL_KNOB, NUM_PARAMS };
+	enum InputIds { FM_INPUT, RESET_INPUT, NUM_INPUTS };
+	enum OutputIds { LFO_OUTPUT, NUM_OUTPUTS };
+	enum LightIds { REC_LIGHT, NUM_LIGHTS };
+
+	LfoModule()
+	{
+		configComm(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		core = ModuleFactory::create("LFOSINE");
+		selfID.typeID = "LFOSINE";
+
+		outputJacks[LfoModule::LFO_OUTPUT]->scale = [](float f) { return f * 5.0f; };
+	}
+};
+
+struct LfoWidget : CommModuleWidget {
+
+	LfoModule *mainModule;
+
+	Label *valueLabel;
+	Label *recLabel;
+
+	LfoWidget(LfoModule *module)
+	{
+		setModule(static_cast<Module *>(module));
+		mainModule = module;
+
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/adjTest.svg")));
+
+		addLabeledKnob("SPEED", LfoModule::FREQ_KNOB, {0, 0});
+		addLabeledKnob("PHASE", LfoModule::SHAPE_KNOB, {0, 1});
+		addLabeledKnob("LEVEL", LfoModule::LEVEL_KNOB, {0, 2});
+
+		addLabeledInput("FM", LfoModule::FM_INPUT, {0, 2});
+		addLabeledInput("RESET", LfoModule::RESET_INPUT, {0, 1});
+
+		addLabeledOutput("OUT", LfoModule::LFO_OUTPUT, {0, 0});
+
+		addModuleTitle("LFO");
+	}
+};
+
+Model *modelLFO = createModel<LfoModule, LfoWidget>("lfo");
