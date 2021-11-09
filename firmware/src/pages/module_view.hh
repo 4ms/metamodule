ecd3a787c0789b777b1a8d60c8b2d902f73336ee
Dan Green <danngreen1@gmail.com>
2021-11-09 07:33:44 -0800

    roller WIP

--- firmware/src/pages/module_view.hh
+++ firmware/src/pages/module_view.hh
@@ -42,12 +42,14 @@ struct ModuleViewPage : PageBase {
 	lv_obj_t *roller;
 	lv_style_t button_style;
 	lv_style_t roller_style;
+	lv_style_t roller_selection_style;
 
 	void init() override {
+		constexpr auto width_px = static_cast<int>(Info::width_hp * 9.488f);
 		base = lv_obj_create(nullptr, nullptr);
 		module_img = lv_img_create(base, nullptr);
 		lv_obj_set_pos(module_img, 0, 0);
-		lv_obj_set_size(module_img, (Info::width_hp * 9.488f), 240); // TODO: get image width?
+		lv_obj_set_size(module_img, width_px, 240); // TODO: get image width?
 		lv_obj_set_click(module_img, true);
 		// TODO: get_image_from_slug: Info::slug
 		// lv_img_set_src(module_img, &_module_graphic_SMR_small_alpha_246x240);
@@ -67,7 +69,9 @@ struct ModuleViewPage : PageBase {
 		lv_style_set_outline_color(&button_style, LV_STATE_DEFAULT, lv_color_make(0xff, 0xd6, 0x00));
 		lv_style_set_outline_opa(&button_style, LV_STATE_DEFAULT, 0);
 
-		// All buttons
+		// TODO: combine Knobs+InJacks+OutJacks+Switches and sort by X/Y
+		// then, do lv_btn_create, add_style, etc
+		// Draw an element (or copy an img?) at each knob/jack/switch/button/slider position
 		int i = 0;
 		for (const auto kn : Info::Knobs) {
 			button[i] = lv_btn_create(base, nullptr);
@@ -76,8 +80,6 @@ struct ModuleViewPage : PageBase {
 			lv_obj_set_size(button[i], 13, 13);
 			i++;
 		}
-		// Repeat for Jacks, Switches
-		// Then sort by top-left (maybe floor(top, 20px) ?)
 
 		roller = lv_roller_create(base, nullptr);
 		lv_style_reset(&roller_style);
@@ -90,35 +92,30 @@ struct ModuleViewPage : PageBase {
 		lv_style_set_border_width(&roller_style, LV_STATE_DEFAULT, 0);
 		lv_style_set_text_color(&roller_style, LV_STATE_DEFAULT, lv_color_make(0xff, 0xff, 0xff));
 		lv_obj_add_style(roller, LV_ROLLER_PART_BG, &roller_style);
-		//
-		// Write style LV_ROLLER_PART_SELECTED for module_test_small_roller_2
-		// static lv_style_t style_module_test_small_roller_2_selected;
-		// lv_style_reset(&style_module_test_small_roller_2_selected);
 
-		////Write style state: LV_STATE_DEFAULT for style_module_test_small_roller_2_selected
-		// lv_style_set_bg_color(&style_module_test_small_roller_2_selected, LV_STATE_DEFAULT, lv_color_make(0xff, 0xc3,
-		// 0x70)); lv_style_set_bg_grad_color(&style_module_test_small_roller_2_selected, LV_STATE_DEFAULT,
-		// lv_color_make(0xff, 0xc3, 0x70)); lv_style_set_bg_grad_dir(&style_module_test_small_roller_2_selected,
-		// LV_STATE_DEFAULT, LV_GRAD_DIR_VER); lv_style_set_bg_opa(&style_module_test_small_roller_2_selected,
-		// LV_STATE_DEFAULT, 255); lv_style_set_text_color(&style_module_test_small_roller_2_selected, LV_STATE_DEFAULT,
-		// lv_color_make(0x00, 0x00, 0x00)); lv_obj_add_style(ui->module_test_small_roller_2, LV_ROLLER_PART_SELECTED,
-		// &style_module_test_small_roller_2_selected); lv_obj_set_pos(ui->module_test_small_roller_2, 248, 1);
-		// lv_roller_set_options(ui->module_test_small_roller_2,"PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n
-		// PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n PARAM\n
-		// PARAM\n PARAM 1\nPARAM 2\nPARAM 3\nPARAM 4\nPARAM 5\nPARAM 6\nPARAM 7\nPARAM 8\nPARAM
-		// 9\n",LV_ROLLER_MODE_INIFINITE); lv_obj_set_style_local_text_font(ui->module_test_small_roller_2,
-		// LV_ROLLER_PART_BG, LV_STATE_DEFAULT, &lv_font_MuseoSansRounded_500_12);
-		// lv_obj_set_style_local_text_font(ui->module_test_small_roller_2, LV_ROLLER_PART_BG, LV_STATE_FOCUSED,
-		// &lv_font_MuseoSansRounded_700_12); lv_obj_set_style_local_text_font(ui->module_test_small_roller_2,
-		// LV_ROLLER_PART_SELECTED, LV_STATE_DEFAULT, &lv_font_MuseoSansRounded_500_12);
-		// lv_roller_set_visible_row_count(ui->module_test_small_roller_2,7);
+		lv_style_reset(&roller_selection_style);
+		lv_style_set_bg_color(&roller_selection_style, LV_STATE_DEFAULT, lv_color_make(0xff, 0xc3, 0x70));
+		lv_style_set_bg_grad_color(&roller_selection_style, LV_STATE_DEFAULT, lv_color_make(0xff, 0xc3, 0x70));
+		lv_style_set_bg_grad_dir(&roller_selection_style, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+		lv_style_set_bg_opa(&roller_selection_style, LV_STATE_DEFAULT, 255);
+		lv_style_set_text_color(&roller_selection_style, LV_STATE_DEFAULT, lv_color_make(0x00, 0x00, 0x00));
+		lv_obj_add_style(roller, LV_ROLLER_PART_SELECTED, &roller_selection_style);
+
+		lv_obj_set_pos(roller, width_px, 1);
+
+		lv_obj_set_style_local_text_font(roller, LV_ROLLER_PART_BG, LV_STATE_DEFAULT, &lv_font_MuseoSansRounded_700_12);
+		lv_obj_set_style_local_text_font(
+			roller, LV_ROLLER_PART_SELECTED, LV_STATE_DEFAULT, &lv_font_MuseoSansRounded_500_12);
 
 		init_bg(base);
 
 		// Event and group for roller
-		// group = lv_group_create();
-		lv_group_add_obj(group, ui->module_test_small_roller_2);
-		lv_obj_set_event_cb(ui->module_test_small_roller_2, roller_cb);
+		lv_group_add_obj(group, roller);
+		lv_obj_set_event_cb(roller, roller_cb);
+
+		// Add options
+		lv_roller_set_options(roller, "\n", LV_ROLLER_MODE_INFINITE);
+		lv_roller_set_visible_row_count(roller, 7);
 	}
 };
 
