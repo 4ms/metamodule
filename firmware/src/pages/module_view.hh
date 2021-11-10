f53a075a85e522f3de8f2aa755b65712ea6bccb5
Dan Green <danngreen1@gmail.com>
2021-11-10 12:56:18 -0800

    Part of new capitalization commit

--- firmware/src/pages/module_view.hh
+++ firmware/src/pages/module_view.hh
@@ -1,5 +1,5 @@
 #pragma once
-#include "CoreModules/info/enosc_info.hh"
+#include "CoreModules/info/EnOsc_info.hh"
 #include "CoreModules/info/module_info_base.hh"
 #include "pages/base.hh"
 #include "pages/images/image_list.hh"
