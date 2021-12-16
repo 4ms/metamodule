d07705c42758c7c87ff01a35f7034f9920bf3e6c
Dan Green <danngreen1@gmail.com>
2021-12-16 08:59:32 -0800

    Added stub for using std::string without libcpp

--- firmware/system/libcpp_stub.cc
+++ firmware/system/libcpp_stub.cc
@@ -1,25 +1,27 @@
 namespace std
 {
-void __throw_bad_function_call()
-{
+void __throw_bad_function_call() {
 	return;
 	// while (1)
 	// 	;
 }
-void __throw_bad_alloc()
-{
-	while (1)
+void __throw_bad_alloc() {
+	while (true)
 		;
 }
-void __throw_length_error(char const *)
-{
-	while (1)
+void __throw_length_error(char const *) {
+	while (true)
+		;
+}
+
+//Needed for std::string constructor and function to return std::string
+void __throw_logic_error(char const *) {
+	while (true)
 		;
 }
 } // namespace std
 
-extern "C" void __cxa_pure_virtual()
-{
-	while (1)
+extern "C" void __cxa_pure_virtual() {
+	while (true)
 		;
 }
