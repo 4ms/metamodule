fac0b851bf8cab952fa6bfc80c96fb87812ba750
Dan Green <danngreen1@gmail.com>
2021-12-18 18:11:24 -0800

    Reduced to one patch, to avoid crashing until we get all modules converted

--- firmware/src/patchlist.hh
+++ firmware/src/patchlist.hh
@@ -6,44 +6,37 @@ namespace MetaModule
 
 struct PatchList {
 	//Todo: Deduce this directly from size of constructor
-	static constexpr int32_t NumPatches = 43;
+	static constexpr int32_t NumPatches = 1;
 
 	PatchList();
 
-	ModuleTypeSlug &get_patch_name(uint32_t patch_id)
-	{
+	ModuleTypeSlug &get_patch_name(uint32_t patch_id) {
 		if (patch_id >= NumPatches)
 			patch_id = 0;
 		return _patch_headers[patch_id]->patch_name;
 	}
 
-	PatchHeader *load_patch_header(void *base_addr)
-	{
+	PatchHeader *load_patch_header(void *base_addr) {
 		return reinterpret_cast<PatchHeader *>(base_addr);
 	}
 
-	PatchHeader *cur_patch()
-	{
+	PatchHeader *cur_patch() {
 		return _patch_headers[_cur_patch_index];
 	}
 
-	void set_cur_patch_index(int cur_idx)
-	{
+	void set_cur_patch_index(int cur_idx) {
 		_cur_patch_index = cur_idx;
 	}
 
-	int cur_patch_index()
-	{
+	int cur_patch_index() {
 		return _cur_patch_index;
 	}
 
-	uint32_t next_patch_index()
-	{
+	uint32_t next_patch_index() {
 		return _cur_patch_index >= (NumPatches - 1) ? 0 : _cur_patch_index + 1;
 	}
 
-	uint32_t prev_patch_index()
-	{
+	uint32_t prev_patch_index() {
 		return _cur_patch_index == 0 ? (NumPatches - 1) : _cur_patch_index - 1;
 	}
 
