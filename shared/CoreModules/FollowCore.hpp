290639f36e7752bd40d15f03c226cbb150fe1fe6
Dan Green <danngreen1@gmail.com>
2021-12-18 22:17:59 -0800

    Converted FollowCore

--- shared/CoreModules/FollowCore.hpp
+++ shared/CoreModules/FollowCore.hpp
@@ -3,6 +3,10 @@
 #include "CoreModules/info/Follow_info.hh"
 #include "CoreModules/moduleFactory.hh"
 
+#include "processors/tools/expDecay.h"
+#include "processors/tools/windowComparator.h"
+#include "util/math.hh"
+
 class FollowCore : public CoreProcessor {
 	using Info = FollowInfo;
 	using ThisCore = FollowCore;
@@ -10,17 +14,54 @@ class FollowCore : public CoreProcessor {
 public:
 	FollowCore() = default;
 
-	void update() override {
+	void update(void) override {
+		float rectSignal = signalInput;
+		if (rectSignal < 0)
+			rectSignal *= -1.0f;
+		envOutput = slew.update(rectSignal);
+		wc.update(envOutput);
+		gateOutput = wc.get_output();
 	}
 
 	void set_param(int param_id, float val) override {
+		switch (param_id) {
+			case Info::KnobThresh: //threshold
+			{
+				float topThresh;
+				float bottomThresh;
+				constexpr float errorAmount = 0.1f;
+				topThresh = val + errorAmount;
+				if (topThresh > 1.0f)
+					topThresh = 1.0f;
+				bottomThresh = val - errorAmount;
+				if (bottomThresh < 0)
+					bottomThresh = 0;
+				wc.set_highThreshold(topThresh);
+				wc.set_lowThreshhold(bottomThresh);
+			} break;
+			case Info::KnobRise: //rise
+				slew.attackTime = MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 2000.f);
+				break;
+			case Info::KnobFall: //fall
+				slew.decayTime = MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 2000.0f);
+				break;
+		}
 	}
 
 	void set_input(int input_id, float val) override {
+		if (input_id == Info::InputInput)
+			signalInput = val;
 	}
 
 	float get_output(int output_id) const override {
-		return 0.f;
+		switch (output_id) {
+			case Info::OutputEnv:
+				return envOutput;
+			case Info::OutputGate:
+				return gateOutput;
+			default:
+				return 0.f;
+		}
 	}
 
 	void set_samplerate(float sr) override {
@@ -37,4 +78,9 @@ public:
 	// clang-format on
 
 private:
+	float signalInput = 0;
+	float envOutput = 0;
+	float gateOutput = 0;
+	WindowComparator wc;
+	ExpDecay slew;
 };
