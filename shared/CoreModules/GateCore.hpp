7544c063fc4cc92779a298f61a6ad88bc45c7131
Dan Green <danngreen1@gmail.com>
2021-12-19 14:44:46 -0800

    Converted GateCore

--- shared/CoreModules/GateCore.hpp
+++ shared/CoreModules/GateCore.hpp
@@ -3,6 +3,10 @@
 #include "CoreModules/info/Gate_info.hh"
 #include "CoreModules/moduleFactory.hh"
 
+#include "processors/tools/delayLine.h"
+#include "processors/tools/windowComparator.h"
+#include "util/math.hh"
+
 class GateCore : public CoreProcessor {
 	using Info = GateInfo;
 	using ThisCore = GateCore;
@@ -11,19 +15,60 @@ public:
 	GateCore() = default;
 
 	void update() override {
+		lastGate = currentGate;
+		currentGate = wc.get_output();
+		if (currentGate && !lastGate) {
+			finalDelay =
+				MathTools::map_value(MathTools::constrain(delayCV + delayTime, 0.0f, 1.0f), 0.0f, 1.0f, 0.0f, 1000.0f) /
+				1000.0f * sampleRate;
+			finalLength = MathTools::map_value(
+							  MathTools::constrain(lengthCV + gateLength, 0.0f, 1.0f), 0.0f, 1.0f, 1.0f, 1000.0f) /
+						  1000.0f * sampleRate;
+
+			del.set_delay_samples(finalDelay);
+
+			sinceGate = 0;
+		}
+
+		wc.update(del.update(signalInput));
+
+		gateOutput = (sinceGate < finalLength);
+		sinceGate++;
 	}
 
 	void set_param(int param_id, float val) override {
+		switch (param_id) {
+			case Info::KnobLength:
+				gateLength = val;
+				break;
+			case Info::KnobDelay:
+				delayTime = val;
+				break;
+		}
 	}
 
 	void set_input(int input_id, float val) override {
+		switch (input_id) {
+			case Info::InputInput:
+				signalInput = val;
+				break;
+			case Info::InputLength_Cv:
+				lengthCV = val;
+				break;
+			case Info::InputDelay_Cv:
+				delayCV = val;
+				break;
+		}
 	}
 
 	float get_output(int output_id) const override {
+		if (output_id == Info::OutputOut)
+			return gateOutput ? 1.f : 0.f;
 		return 0.f;
 	}
 
 	void set_samplerate(float sr) override {
+		sampleRate = sr;
 	}
 
 	float get_led_brightness(int led_id) const override {
@@ -37,4 +82,25 @@ public:
 	// clang-format on
 
 private:
+	float sampleRate = 48000;
+	bool lastGate = false;
+	bool currentGate = false;
+	bool gateOutput = false;
+
+	unsigned long sinceGate = 0;
+
+	float gateLength = 10;
+
+	float delayTime = 0;
+
+	float signalInput = 0;
+
+	float lengthCV = 0;
+	float delayCV = 0;
+
+	float finalLength = 10;
+	float finalDelay = 0;
+
+	DelayLine<96000> del;
+	WindowComparator wc;
 };
