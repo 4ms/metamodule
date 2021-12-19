23a28c7b710bbdecee769a5be9cf74d3c5028561
Dan Green <danngreen1@gmail.com>
2021-12-18 18:48:31 -0800

    converted complexEG. Detune WIP

--- shared/CoreModules/DetuneCore.hpp
+++ shared/CoreModules/DetuneCore.hpp
@@ -3,30 +3,120 @@
 #include "CoreModules/info/Detune_info.hh"
 #include "CoreModules/moduleFactory.hh"
 
+#include "processors/pitchShift.h"
+#include "util/math.hh"
+
+class InterpRandomGenerator {
+public:
+	float frequency = 1;
+
+	float update() {
+		phaccu += frequency / sampleRate;
+		if (phaccu >= 1.0f) {
+			phaccu -= 1.0f;
+			lastValue = currentValue;
+			currentValue = MathTools::randomNumber(-1.0f, 1.0f);
+		}
+
+		outputValue = MathTools::interpolate(lastValue, currentValue, phaccu);
+		return (outputValue);
+	}
+
+	void set_samplerate(float sr) {
+		sampleRate = sr;
+	}
+
+private:
+	float sampleRate = 48000.f;
+	float currentValue = 0;
+	float lastValue = 0;
+	float phaccu = 0;
+	float outputValue = 0;
+};
+
 class DetuneCore : public CoreProcessor {
 	using Info = DetuneInfo;
 	using ThisCore = DetuneCore;
 
 public:
-	DetuneCore() = default;
+	DetuneCore() {
+		p.mix = 1.0f;
+		p.windowSize = 240;
+	}
 
 	void update() override {
+		float addWow = 0;
+		float addFlutter = 0;
+		float finalWow = 0;
+		float finalFlutter = 0;
+		if (detuneCvConnected == false) {
+			finalWow = wowDepth;
+			finalFlutter = flutterDepth;
+		} else {
+			finalWow = MathTools::constrain(wowDepth + cvAmount, 0.0f, 1.0f);
+			finalFlutter = MathTools::constrain(flutterDepth + cvAmount, 0.0f, 1.0f);
+		}
+		addWow = wowGen.update() * (finalWow * finalWow);
+		addFlutter = flutterGen.update() * (finalFlutter * finalFlutter);
+		p.shiftAmount = addWow + addFlutter;
+		signalOutput = p.update(signalInput);
 	}
 
-	void set_param(int param_id, float val) override {
+	void set_param(const int param_id, const float val) override {
+		switch (param_id) {
+			case 0:
+				wowGen.frequency = MathTools::map_value(val, 0.0f, 1.0f, 0.1f, 5.0f);
+				break;
+			case 1:
+				wowDepth = val;
+				break;
+			case 2:
+				flutterGen.frequency = MathTools::map_value(val, 0.0f, 1.0f, 5.0f, 30.0f);
+				break;
+			case 3:
+				flutterDepth = val;
+				break;
+		}
 	}
 
-	void set_input(int input_id, float val) override {
+	void set_samplerate(const float sr) override {
+		p.setSampleRate(sr);
+		flutterGen.set_samplerate(sr);
+		wowGen.set_samplerate(sr);
 	}
 
-	float get_output(int output_id) const override {
-		return 0.f;
+	void set_input(const int input_id, const float val) override {
+		switch (input_id) {
+			case 0:
+				signalInput = val;
+				break;
+			case 1:
+				cvAmount = val;
+				break;
+		}
 	}
 
-	void set_samplerate(float sr) override {
+	float get_output(const int output_id) const override {
+		float output = 0;
+		switch (output_id) {
+			case 0:
+				output = signalOutput;
+				break;
+		}
+		return output;
 	}
 
-	float get_led_brightness(int led_id) const override {
+	void mark_input_unpatched(const int input_id) override {
+		if (input_id == 1)
+			detuneCvConnected = false;
+	}
+
+	void mark_input_patched(const int input_id) override {
+		if (input_id == 1)
+			detuneCvConnected = true;
+	}
+
+	float get_led_brightness(const int led_id) const override {
 		return 0.f;
 	}
 
@@ -37,4 +127,16 @@ public:
 	// clang-format on
 
 private:
+	PitchShift<9600> p;
+	float flutterDepth = 0;
+	float wowDepth = 0;
+
+	float signalInput = 0;
+	float signalOutput = 0;
+
+	bool detuneCvConnected = false;
+	float cvAmount = 0;
+
+	InterpRandomGenerator flutterGen;
+	InterpRandomGenerator wowGen;
 };
