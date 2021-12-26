3adf53aeca8ec80a2617cef32d8523e282971567
Dan Green <danngreen1@gmail.com>
2021-12-25 20:13:03 -0600

    StaticString has data() getter and string_view cast

--- shared/util/static_string.hh
+++ shared/util/static_string.hh
@@ -7,60 +7,68 @@
 // A null-terminated string with fixed compile-time capacity
 template<size_t CAPACITY>
 struct StaticString {
-	char data[CAPACITY + 1];
+	char _data[CAPACITY + 1];
 
 	static constexpr size_t capacity = CAPACITY;
 
 	constexpr StaticString() {
-		data[0] = '\0';
+		_data[0] = '\0';
 	}
 
 	constexpr StaticString(std::string_view s) {
 		size_t i = 0;
 		for (auto c : s) {
-			data[i++] = c;
+			_data[i++] = c;
 			if (i >= CAPACITY)
 				break;
 		}
-		data[i] = '\0';
+		_data[i] = '\0';
 	}
 
 	constexpr StaticString(const char *s) {
 		size_t i = 0;
 		while (*s && i < CAPACITY) {
-			data[i] = *s;
+			_data[i] = *s;
 			i++;
 			s++;
 		}
-		data[i] = '\0';
+		_data[i] = '\0';
 	}
 
 	const char *cstr() const {
-		return data;
+		return _data;
 	}
 
 	const char *c_str() const {
-		return data;
+		return _data;
+	}
+
+	const char *data() const {
+		return _data;
 	}
 
 	size_t length() const {
-		return std::string_view{data}.length();
+		return std::string_view{_data}.length();
 	}
 
 	template<size_t CAP2>
 	bool operator==(const StaticString<CAP2> &rhs) const {
-		return std::string_view{data} == std::string_view{rhs};
+		return std::string_view{_data} == std::string_view{rhs};
 	}
 
 	bool is_equal(const char *rhs) const {
-		return std::string_view{data} == std::string_view{rhs};
+		return std::string_view{_data} == std::string_view{rhs};
 	}
 
 	operator const char *() const {
-		return data;
+		return _data;
 	}
 
 	operator std::string() const {
-		return data;
+		return _data;
+	}
+
+	operator std::string_view() const {
+		return _data;
 	}
 };
