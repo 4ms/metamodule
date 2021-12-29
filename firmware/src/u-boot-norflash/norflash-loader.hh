5f8566010a03a4a83750296eae088e8a6fd8a169
Dan Green <danngreen1@gmail.com>
2021-12-28 17:26:20 -0800

    Flash Loader erases blocks before writing

--- firmware/src/u-boot-norflash/norflash-loader.hh
+++ firmware/src/u-boot-norflash/norflash-loader.hh
@@ -4,21 +4,87 @@
 #include "qspi_flash_driver.hh"
 #include "u-boot-img.h"
 #include "u-boot-spl-stm32.h"
+#include <array>
+#include <memory>
 
 struct NorFlashLoader {
-	mdrivlib::QSpiFlash flash;
+	using QSpiFlash = mdrivlib::QSpiFlash;
+	QSpiFlash flash;
 
 	NorFlashLoader()
 		: flash{qspi_flash_conf} {
 
+		printf("Erasing Flash\n\r");
+
 		Debug::blue_LED1::high();
 		Debug::green_LED1::high();
 
-		//Red on
-		Debug::red_LED1::low();
-		bool ok = flash.Erase(mdrivlib::QSpiFlash::ENTIRE_CHIP, 0x00, mdrivlib::QSpiFlash::EXECUTE_FOREGROUND);
-		if (!ok) {
+		bool ok;
+
+		//Erase 64kblocks for FSBL1 + 2: Red on. Error: flash green
+		for (int i = 0; i < 8; i++) {
+			Debug::red_LED1::low();
+			printf("Erasing Block#%d @ 0x%x\n\r", i, i * QSPI_64KBLOCK_SIZE);
+			ok = flash.Erase(QSpiFlash::BLOCK_64K, i * QSPI_64KBLOCK_SIZE, QSpiFlash::EXECUTE_FOREGROUND);
+			if (!ok) {
+				while (true) {
+					Debug::green_LED1::low();
+					HAL_Delay(250);
+					Debug::green_LED1::high();
+					HAL_Delay(250);
+				}
+			}
 			Debug::red_LED1::high();
+		}
+
+		// Write FSBL1: blue on. Error: flash red
+		Debug::blue_LED1::low();
+		printf("Writing %d bytes to 0x%x\n\r", u_boot_spl_stm32_len, 0x00);
+		ok = flash.Write(u_boot_spl_stm32, 0x00, u_boot_spl_stm32_len); // ~100k
+		if (!ok) {
+			while (true) {
+				Debug::red_LED1::low();
+				HAL_Delay(250);
+				Debug::red_LED1::high();
+				HAL_Delay(250);
+			}
+		}
+		Debug::blue_LED1::high();
+
+		//Verify
+		{
+			auto *data = new uint8_t[101977];
+			//uint8_t data[u_boot_spl_stm32_len];
+			printf("Reading %d bytes to 0x%x\n\r", u_boot_spl_stm32_len, 0x00);
+			ok = flash.Read(data, 0, u_boot_spl_stm32_len, mdrivlib::QSpiFlash::EXECUTE_FOREGROUND);
+			if (!ok) {
+				while (true) {
+					Debug::blue_LED1::low();
+					Debug::blue_LED1::high();
+				}
+			}
+			for (int i = 0; i < u_boot_spl_stm32_len; i++) {
+				if (data[i] != u_boot_spl_stm32[i]) {
+					while (true) {
+						Debug::blue_LED1::low();
+						Debug::red_LED1::low();
+						Debug::green_LED1::low();
+						HAL_Delay(250);
+						Debug::blue_LED1::high();
+						Debug::red_LED1::high();
+						Debug::green_LED1::high();
+						HAL_Delay(250);
+					}
+				}
+			}
+			delete[] data;
+		}
+
+		//Write FSBL2 @ 256k, blue + red on. Error: flash green
+		Debug::red_LED1::low();
+		Debug::blue_LED1::low();
+		ok = flash.Write(u_boot_spl_stm32, 256 * 1024, u_boot_spl_stm32_len);
+		if (!ok) {
 			while (true) {
 				Debug::green_LED1::low();
 				HAL_Delay(250);
@@ -27,34 +93,24 @@ struct NorFlashLoader {
 			}
 		}
 		Debug::red_LED1::high();
-
-		// Wrtie FSBL1: blue on
-		Debug::blue_LED1::low();
-		ok = flash.Write(u_boot_spl_stm32, 0x00, u_boot_spl_stm32_len); //101961 bytes
-		if (!ok) {
-			while (true) {
-				Debug::red_LED1::low();
-				HAL_Delay(250);
-				Debug::red_LED1::high();
-				HAL_Delay(250);
-			}
-		}
 		Debug::blue_LED1::high();
 
-		//Write FSBL2 @ 256k, blue + red on
-		Debug::red_LED1::low();
-		Debug::blue_LED1::low();
-		ok = flash.Write(u_boot_spl_stm32, 256 * 1024, u_boot_spl_stm32_len); //101961 bytes
-		if (!ok) {
-			while (true) {
-				Debug::red_LED1::low();
-				HAL_Delay(250);
-				Debug::red_LED1::high();
-				HAL_Delay(250);
+		//Erase 64kblocks for SSBL: Red on. Error: flash green
+		const int num_blocks = (u_boot_img_len / QSPI_64KBLOCK_SIZE) + 1;
+		for (int i = 8; i < (8 + num_blocks); i++) {
+			Debug::red_LED1::low();
+			printf("Erasing Block#%d @ 0x%x\n\r", i, i * QSPI_64KBLOCK_SIZE);
+			ok = flash.Erase(QSpiFlash::BLOCK_64K, i * QSPI_64KBLOCK_SIZE, QSpiFlash::EXECUTE_FOREGROUND);
+			if (!ok) {
+				while (true) {
+					Debug::green_LED1::low();
+					HAL_Delay(250);
+					Debug::green_LED1::high();
+					HAL_Delay(250);
+				}
 			}
+			Debug::red_LED1::high();
 		}
-		Debug::red_LED1::high();
-		Debug::blue_LED1::high();
 
 		//Write SSBL @ 512k (2M length), green on
 		Debug::green_LED1::low();
