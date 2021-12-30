1f89ada333dac492e05a782a7fee56939ac3cb54
Dan Green <danngreen1@gmail.com>
2021-12-30 10:59:28 -0800

    Building u-boot sets DEVICE_TREE to metamodule-p6

--- firmware/makefile_mp1a7.mk
+++ firmware/makefile_mp1a7.mk
@@ -265,7 +265,7 @@ include makefile_common.mk
 
 arch = $(shell uname -m)
 ifeq ($(arch),arm64)
-ubootbuildcmd = arch -x86_64 zsh -c "make O=../build CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig && make -j16 O=../build CROSS_COMPILE=arm-none-eabi- all"
+ubootbuildcmd = arch -x86_64 zsh -c "make O=../build CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig && make -j16 O=../build DEVICE_TREE=stm32mp157c-metamodule-p6 CROSS_COMPILE=arm-none-eabi- all"
 else
 ubootbuildcmd = make O=../build CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig && make -j16 O=../build CROSS_COMPILE=arm-none-eabi- all
 endif
