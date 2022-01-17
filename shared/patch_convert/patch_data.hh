aafc72e9a0640dd836d16eca779626adb70a47b1
Dan Green <danngreen1@gmail.com>
2022-01-17 11:17:29 -0800

    comments

--- shared/patch_convert/patch_data.hh
+++ shared/patch_convert/patch_data.hh
@@ -1,12 +1,13 @@
+#pragma once
 #include "patch/patch.hh"
 #include <vector>
 
-//TODO: move this to patch.hh?
+//72B = 6 vec * 12B ea (data, sz, cap)
 struct PatchData {
-	std::vector<ModuleTypeSlug> module_slugs;
-	std::vector<InternalCable> int_cables;
-	std::vector<MappedInputJack> mapped_ins;
-	std::vector<MappedOutputJack> mapped_outs;
-	std::vector<StaticParam> static_knobs;
-	std::vector<MappedKnob> mapped_knobs;
+	std::vector<ModuleTypeSlug> module_slugs;  //32B
+	std::vector<InternalCable> int_cables;	   //16B
+	std::vector<MappedInputJack> mapped_ins;   //16B
+	std::vector<MappedOutputJack> mapped_outs; //8B
+	std::vector<StaticParam> static_knobs;	   //8B
+	std::vector<MappedKnob> mapped_knobs;	   //16B
 };
