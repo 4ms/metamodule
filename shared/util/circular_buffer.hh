ff44f3b3391c8483d084bb7a4ba149d8bbdfedb3
Dan Green <danngreen1@gmail.com>
2022-01-18 16:03:20 -0800

    refactored circular buffer a bit

--- shared/util/circular_buffer.hh
+++ shared/util/circular_buffer.hh
@@ -1,27 +1,26 @@
 #pragma once
+#include "math.hh"
 #include <array>
-#include <cstdio>
-#include <memory>
 
 template<class T, size_t max_size_>
 class CircularBuffer {
 public:
+	// static_assert(MathTools::is_power_of_2(max_size_), "CircularBuffer size must be a power of 2");
+	// static constexpr size_t SIZE_MASK = max_size_ - 1;
+
 	CircularBuffer(){};
 
-	void put(T item)
-	{
+	void put(T item) {
 		buf_[head_] = item;
 
-		if (full_) {
-			tail_ = (tail_ + 1) % max_size_;
-		}
+		if (full_)
+			inc_tail();
 
-		head_ = (head_ + 1) % max_size_;
+		inc_head();
 		full_ = head_ == tail_;
 	}
 
-	T get()
-	{
+	T get() {
 		if (empty()) {
 			return T();
 		}
@@ -29,54 +28,46 @@ public:
 		// Read data and advance the tail (we now have a free space)
 		auto val = buf_[tail_];
 		full_ = false;
-		tail_ = (tail_ + 1) % max_size_;
+		inc_tail();
 
 		return val;
 	}
 
-	void set_head(size_t head)
-	{
+	void set_head(size_t head) {
 		head_ = head;
 	}
 
-	void remove_first()
-	{
+	void remove_first() {
 		if (!empty()) {
 			full_ = false;
-			tail_ = (tail_ + 1) % max_size_;
+			inc_tail();
 		}
 	}
 
 	// Return a reference to the first element
-	T &first()
-	{
+	T &first() {
 		return buf_[tail_];
 	}
 
-	void reset()
-	{
+	void reset() {
 		head_ = tail_;
 		full_ = false;
 	}
 
-	bool empty() const
-	{
+	bool empty() const {
 		// if head and tail are equal, we are empty
 		return (!full_ && (head_ == tail_));
 	}
 
-	bool full() const
-	{
+	bool full() const {
 		return full_;
 	}
 
-	size_t capacity() const
-	{
+	size_t capacity() const {
 		return max_size_;
 	}
 
-	size_t size() const
-	{
+	size_t size() const {
 		size_t size = max_size_;
 
 		if (!full_) {
@@ -89,10 +80,31 @@ public:
 		return size;
 	}
 
+	void inc_head() {
+		//head_ = (head_ + 1) & SIZE_MASK;
+		head_ = (head_ + 1) % max_size_;
+	}
+
+	void inc_tail() {
+		//tail_ = (tail_ + 1) & SIZE_MASK;
+		tail_ = (tail_ + 1) % max_size_;
+	}
+
+	size_t get_head_idx() {
+		return head_;
+	}
+
+	size_t get_tail_idx() {
+		return tail_;
+	}
+
+	void fill_buffer(std::array<T, max_size_> &src) {
+		memcpy(buf_.data(), src.data(), max_size_);
+	}
+
 private:
 	std::array<T, max_size_> buf_{};
 	size_t head_ = 0;
 	size_t tail_ = 0;
 	bool full_ = 0;
 };
-
