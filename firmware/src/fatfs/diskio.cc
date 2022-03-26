f52a4cbb3f52c87dfb37c7915850a7397188d24e
Dan Green <danngreen1@gmail.com>
2022-03-26 16:02:32 -0700

    fatfs diskio.cc changed to use generic DiskOps

--- firmware/src/fatfs/diskio.cc
+++ firmware/src/fatfs/diskio.cc
@@ -1,166 +1,76 @@
 #include "diskio.h" /* Declarations of disk functions */
+#include "disk_ops.hh"
 #include "ff.h"
 #include "norfs.hh"
 #include <cstring>
 
-// default values
-uint32_t DriveRamDisk = 0;
-uint32_t DriveSDCard = 1;
-uint32_t DriveUSB = 2;
+constexpr size_t MaxNumDisks = 3;
 
-static NorFlashFS *norfs = nullptr;
-void register_norfs(NorFlashFS *nfs, uint8_t drive_num) {
-	norfs = nfs;
-	DriveRamDisk = drive_num;
+namespace
+{
+std::array<DiskOps *, MaxNumDisks> _diskops{nullptr, nullptr, nullptr};
 }
 
-/*-----------------------------------------------------------------------*/
-/* Get Drive Status                                                      */
-/*-----------------------------------------------------------------------*/
+// Register a set of disk operations with the FatFS filesystem.
+// Returns false if failed: disk_id is already taken or out of range.
+bool fatfs_register_disk(DiskOps *ops, uint8_t disk_id) {
+	if (disk_id >= MaxNumDisks)
+		return false;
+	if (_diskops[disk_id] != nullptr)
+		return false;
+	if (!ops)
+		return false;
+
+	_diskops[disk_id] = ops;
+	return true;
+}
 
 extern "C" DSTATUS disk_status(BYTE pdrv) {
-	if (pdrv == DriveRamDisk) {
-		return 0;
-	}
-
-	else if (pdrv == DriveSDCard)
-	{
-		return 0;
-	}
-
-	else if (pdrv == DriveUSB)
-	{
-		return 0;
-	}
-
-	else
+	if (pdrv >= MaxNumDisks || !_diskops[pdrv])
 		return STA_NOINIT;
+
+	return _diskops[pdrv]->get_status();
 }
 
-/*-----------------------------------------------------------------------*/
-/* Inidialize a Drive                                                    */
-/*-----------------------------------------------------------------------*/
-
-DSTATUS disk_initialize(BYTE pdrv) {
-	if (pdrv == DriveRamDisk) {
-		return 0;
-	}
-
-	else if (pdrv == DriveSDCard)
-	{
-		return 0;
-	}
-
-	else if (pdrv == DriveUSB)
-	{
-		return 0;
-	}
-
-	else
+extern "C" DSTATUS disk_initialize(BYTE pdrv) {
+	if (pdrv >= MaxNumDisks || !_diskops[pdrv])
 		return STA_NOINIT;
+
+	return _diskops[pdrv]->initialize();
 }
 
-/*-----------------------------------------------------------------------*/
-/* Read Sector(s)                                                        */
-/*-----------------------------------------------------------------------*/
-
-DRESULT disk_read(BYTE pdrv,	/* Physical drive number to identify the drive */
-				  BYTE *buff,	/* Data buffer to store read data */
-				  LBA_t sector, /* Start sector in LBA */
-				  UINT count	/* Number of sectors to read */
+extern "C" DRESULT disk_read(BYTE pdrv,	   /* Physical drive number to identify the drive */
+							 BYTE *buff,   /* Data buffer to store read data */
+							 LBA_t sector, /* Start sector in LBA */
+							 UINT count	   /* Number of sectors to read */
 ) {
-	if (pdrv == DriveRamDisk) {
-		if (!norfs)
-			return RES_NOTRDY;
-		norfs->read_sectors(buff, sector, count);
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveSDCard)
-	{
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveUSB)
-	{
-		return RES_OK;
-	}
-
-	else
+	if (pdrv >= MaxNumDisks || !_diskops[pdrv])
 		return RES_PARERR;
-}
 
-/*-----------------------------------------------------------------------*/
-/* Write Sector(s)                                                       */
-/*-----------------------------------------------------------------------*/
+	return _diskops[pdrv]->read(buff, sector, count);
+}
 
 #if FF_FS_READONLY == 0
 
-DRESULT disk_write(BYTE pdrv,		 /* Physical drive nmuber to identify the drive */
-				   const BYTE *buff, /* Data to be written */
-				   LBA_t sector,	 /* Start sector in LBA */
-				   UINT count		 /* Number of sectors to write */
+extern "C" DRESULT disk_write(BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+							  const BYTE *buff, /* Data to be written */
+							  LBA_t sector,		/* Start sector in LBA */
+							  UINT count		/* Number of sectors to write */
 ) {
-	if (pdrv == DriveRamDisk) {
-		if (!norfs)
-			return RES_NOTRDY;
-		norfs->write_sectors(buff, sector, count);
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveSDCard)
-	{
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveUSB)
-	{
-		return RES_OK;
-	}
-
-	else
+	if (pdrv >= MaxNumDisks || !_diskops[pdrv])
 		return RES_PARERR;
+
+	return _diskops[pdrv]->write(buff, sector, count);
 }
 
 #endif
 
-/*-----------------------------------------------------------------------*/
-/* Miscellaneous Functions                                               */
-/*-----------------------------------------------------------------------*/
-
-DRESULT disk_ioctl(BYTE pdrv, /* Physical drive nmuber (0..) */
-				   BYTE cmd,  /* Control code */
-				   void *buff /* Buffer to send/receive control data */
+extern "C" DRESULT disk_ioctl(BYTE pdrv, /* Physical drive nmuber (0..) */
+							  BYTE cmd,	 /* Control code */
+							  void *buff /* Buffer to send/receive control data */
 ) {
-	if (pdrv == DriveRamDisk) {
-		switch (cmd) {
-			case GET_SECTOR_SIZE: // Get R/W sector size (WORD)
-				*(WORD *)buff = norfs->RamDiskBlockSize;
-				break;
-			case GET_BLOCK_SIZE: // Get erase block size in unit of sector (DWORD)
-				*(DWORD *)buff = 8;
-				break;
-			case GET_SECTOR_COUNT:
-				*(DWORD *)buff = norfs->RamDiskSizeBytes / norfs->RamDiskBlockSize;
-				break;
-			case CTRL_SYNC:
-				break;
-			case CTRL_TRIM:
-				break;
-		}
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveSDCard)
-	{
-		return RES_OK;
-	}
-
-	else if (pdrv == DriveUSB)
-	{
-		return RES_OK;
-	}
-
-	else
+	if (pdrv >= MaxNumDisks || !_diskops[pdrv])
 		return RES_PARERR;
+
+	return _diskops[pdrv]->ioctl(cmd, (uint8_t *)buff);
 }
