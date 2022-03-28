4d185009857f0d8492240adede3a5e6e81e838ef
Dan Green <danngreen1@gmail.com>
2022-03-28 13:24:38 -0700

    PatchFileIO uses foreach file and doesn't need ff anymore

--- firmware/src/patchfileio.cc
+++ firmware/src/patchfileio.cc
@@ -1,10 +1,6 @@
 #include "patchfileio.hh"
 #include "patchlist.hh"
-
-//TODO: make a DirIterator class in FileIO, so we don't need to include ff.h here
-//template<typename Action>
-//void for_each_file_regex(Disk disk, std::string_view regex, Action action);
-#include "ff.h"
+#include "printf.h"
 
 namespace MetaModule::PatchFileIO
 {
@@ -37,38 +33,31 @@ bool factory_reset(Disk disk) {
 }
 
 void load_patches_from_disk(Disk disk, PatchList &patch_list) {
-	//TODO: compare stack buffer vs dynamic buffer
 	constexpr size_t MaxFileSize = 32768;
+	//TODO: compare stack buffer vs dynamic buffer
 	char buf[MaxFileSize];
-	uint32_t filesize;
-	DIR dj;
-	FILINFO fileinfo;
 
 	patch_list.set_status(PatchList::Status::Loading);
 	patch_list.clear_all_patches();
 
-	auto res = f_findfirst(&dj, &fileinfo, "", "*.yml");
-	while (res == FR_OK && fileinfo.fname[0]) {
-		// Skip dot files
-		if (fileinfo.fname[0] == '.')
-			continue;
-		printf("Found patch file: %s, Reading... ", fileinfo.fname);
-		filesize = FileIO::read_file(fileinfo.fname, buf, MaxFileSize);
+	FileIO::for_each_file_regex(disk, "*.yml", [&](const char *fname) {
+		if (fname[0] == '.')
+			return;
+
+		printf("Found patch file: %s, Reading... ", fname);
+		uint32_t filesize = FileIO::read_file(fname, buf, MaxFileSize);
 
 		if (filesize == MaxFileSize) {
 			printf("File exceeds %zu bytes, too big. Skipping\r\n", MaxFileSize);
-			continue;
+			return;
 		}
 		if (!filesize) {
 			printf("File cannot be read. Skipping\r\n");
-			continue;
+			return;
 		}
 		printf("Read %d bytes.. parsing... ", filesize);
 		patch_list.add_patch_from_yaml({buf, filesize});
-
-		res = f_findnext(&dj, &fileinfo);
-	}
-	// NumPatches = _patch_data.size();
+	});
 
 	patch_list.set_status(PatchList::Status::Ready);
 }
