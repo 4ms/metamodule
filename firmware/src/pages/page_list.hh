26d5124f7f245ac7a0027e1b693205b3d65e5f1e
Dan Green <danngreen1@gmail.com>
2022-04-16 16:55:43 -0700

    PageList: handles page history and requesting new/last pages

--- firmware/src/pages/page_list.hh
+++ firmware/src/pages/page_list.hh
@@ -1,2 +1,65 @@
 #pragma once
-enum class PageId { PatchSel, PatchView, Module };
+#include <optional>
+#include <vector>
+
+namespace MetaModule
+{
+
+//forward declare
+struct PageBase;
+
+class PageList {
+	static inline PageBase *_requested_page = nullptr;
+	static inline bool _new_page_requested = false;
+	static inline std::vector<PageBase *> _history;
+
+public:
+	// Requests that we jump to the given page
+	// Another client should use get_new_page() to get this page and jump to it
+	static void request_new_page(PageBase &page) {
+		_history.push_back(_requested_page);
+		_requested_page = &page;
+		_new_page_requested = true;
+	}
+
+	// Requests to jump to the last page in history
+	// Returns false if history is empty, otherwise true
+	// Another client should use get_requested_page() to get this page and jump to it
+	static bool request_last_page() {
+		auto lastpage = PageList::_get_last_page().value_or(nullptr);
+		if (!lastpage)
+			return false;
+
+		_requested_page = lastpage;
+		_new_page_requested = true;
+		return true;
+	}
+
+	// Returns the requested page, or else nullopt if no new page is requested
+	static std::optional<PageBase *> get_requested_page() {
+		if (_new_page_requested) {
+			_new_page_requested = false;
+			return _requested_page;
+		} else
+			return std::nullopt;
+	}
+
+	// Clears history and resets internal state
+	static void clear_history() {
+		_history.clear();
+		_requested_page = nullptr;
+		_new_page_requested = false;
+	}
+
+private:
+	// Returns the last page in the history, or else nullopt if history is empty
+	static std::optional<PageBase *> _get_last_page() {
+		if (_history.empty())
+			return std::nullopt;
+
+		auto back = _history.back();
+		_history.pop_back();
+		return back;
+	}
+};
+} // namespace MetaModule
---
+++ firmware/tests/page_list_tests.cc
@@ -0,0 +1,84 @@
+#include "doctest.h"
+
+//
+
+#include "pages/page_list.hh"
+
+using namespace MetaModule;
+
+namespace MetaModule
+{
+class PageBase {
+public:
+	int x;
+};
+} // namespace MetaModule
+
+TEST_CASE("typical use case") {
+	PageList::clear_history();
+
+	PageBase pagea{1};
+	PageBase pageb{2};
+	PageBase pagec{3};
+	PageBase paged{4};
+
+	// printf("a = %p\n", &pagea);
+	// printf("b = %p\n", &pageb);
+	// printf("c = %p\n", &pagec);
+	// printf("d = %p\n", &paged);
+
+	// printf("start at page a\n");
+	PageList::request_new_page(pagea);
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagea);
+
+	// printf("page a gets a ui event, requests page b\n");
+	PageList::request_new_page(pageb);
+	//pageManager gets the page and jumps to it
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pageb);
+
+	// printf("page b gets a ui event to go back (to pagea)\n");
+	CHECK(PageList::request_last_page());
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagea);
+
+	// printf("page a gets a ui event to go to page c\n");
+	PageList::request_new_page(pagec);
+	//pageManager gets the page and jumps to it
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagec);
+
+	// printf("page c gets a ui event to go to page d\n");
+	PageList::request_new_page(paged);
+	//pageManager gets the page and jumps to it
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &paged);
+
+	// printf("page d gets a ui event to go back multiple times\n");
+	// d -> c -> a -> stop
+	CHECK(PageList::request_last_page());
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagec);
+	CHECK(PageList::request_last_page());
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagea);
+
+	CHECK_FALSE(PageList::request_last_page());
+	CHECK(PageList::get_requested_page().value_or(nullptr) == nullptr);
+}
+
+TEST_CASE("request_new_page() without get_requested() will still add to the history") {
+	PageList::clear_history();
+
+	PageBase pagea{1};
+	PageBase pageb{2};
+
+	// printf("a = %p\n", &pagea);
+	// printf("b = %p\n", &pageb);
+	// printf("c = %p\n", &pagec);
+	// printf("d = %p\n", &paged);
+
+	PageList::request_new_page(pagea);
+	//Oops! didn't actually jump to page a, since we didn't ever call get_requested_page()
+	PageList::request_new_page(pageb);
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pageb);
+	CHECK(PageList::request_last_page());
+
+	//Here we see page a is still in the history
+	//Bug? Should it be fixed? Doesn't matter?
+	CHECK(PageList::get_requested_page().value_or(nullptr) == &pagea);
+}
