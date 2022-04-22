cfa62f98adb61b9ef5cf0b57a427651fa8096e4a
Dan Green <danngreen1@gmail.com>
2022-04-22 12:04:33 -0700

    Added another default patch

--- firmware/src/patches_default.hh
+++ firmware/src/patches_default.hh
@@ -1,13 +1,20 @@
+#include "AllReal4ms.hh"
 #include "CoreModules/moduleFactory.hh"
 #include "Djembe2.hh"
 #include "Djembe4.hh"
 
 struct DefaultPatches {
+	static uint32_t num_patches() {
+		return 3;
+	}
+
 	static ModuleTypeSlug get_filename(uint32_t id) {
 		if (id == 0)
 			return "djembe2.yml";
 		if (id == 1)
 			return "djembe4.yml";
+		if (id == 2)
+			return "allread4ms.yml";
 		return "";
 	}
 
@@ -16,6 +23,8 @@ struct DefaultPatches {
 			return Djembe2_yml_len;
 		if (id == 1)
 			return Djembe4_yml_len;
+		if (id == 2)
+			return AllReal4ms_yml_len;
 		return 0;
 	}
 
@@ -24,6 +33,8 @@ struct DefaultPatches {
 			return Djembe2_yml;
 		if (id == 1)
 			return Djembe4_yml;
+		if (id == 2)
+			return AllReal4ms_yml;
 		return nullptr;
 	}
 
@@ -32,6 +43,8 @@ struct DefaultPatches {
 			return Djembe2_yml;
 		if (id == 1)
 			return Djembe4_yml;
+		if (id == 2)
+			return AllReal4ms_yml;
 		return {};
 	}
 };
