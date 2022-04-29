ef503736981b4affd786b3dd4fca327e19c116e7
Dan Green <danngreen1@gmail.com>
2022-04-29 13:50:30 -0700

    update vcv patchwriter tests to new staticstring

--- vcv/tests/patch_writer_tests.cpp
+++ vcv/tests/patch_writer_tests.cpp
@@ -43,11 +43,11 @@ TEST_CASE("ids are re-arranged properly")
 
 	SUBCASE("modules_used starts with PANEL, but otherwise is in order")
 	{
-		CHECK(pw.get_data().module_slugs[0].cstr()[0] == 'P');
-		CHECK(pw.get_data().module_slugs[1].cstr()[0] == 'A');
-		CHECK(pw.get_data().module_slugs[2].cstr()[0] == 'B');
-		CHECK(pw.get_data().module_slugs[3].cstr()[0] == 'D');
-		CHECK(pw.get_data().module_slugs[4].cstr()[0] == 'E');
+		CHECK(pw.get_data().module_slugs[0].c_str()[0] == 'P');
+		CHECK(pw.get_data().module_slugs[1].c_str()[0] == 'A');
+		CHECK(pw.get_data().module_slugs[2].c_str()[0] == 'B');
+		CHECK(pw.get_data().module_slugs[3].c_str()[0] == 'D');
+		CHECK(pw.get_data().module_slugs[4].c_str()[0] == 'E');
 	}
 
 	SUBCASE("Static Knob List is in order")
