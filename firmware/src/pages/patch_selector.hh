5c76fbc8f058ad6e1519b1d94c3063e51e85fee7
Dan Green <danngreen1@gmail.com>
2022-05-09 14:38:09 -0700

    Default to 5th page, so patchlist is centered

--- firmware/src/pages/patch_selector.hh
+++ firmware/src/pages/patch_selector.hh
@@ -73,7 +73,8 @@ struct PatchSelectorPage : PageBase {
 
 		lv_roller_set_options(roller, patchnames.c_str(), LV_ROLLER_MODE_NORMAL);
 		// lv_roller_set_visible_row_count(roller, 9);
-		lv_roller_set_selected(roller, 0, LV_ANIM_OFF);
+		unsigned default_sel = std::min(patchnames.size(), 5U);
+		lv_roller_set_selected(roller, default_sel, LV_ANIM_OFF);
 		printf("Preselecting %d from %p\n", lv_roller_get_selected(roller), roller);
 	}
 
