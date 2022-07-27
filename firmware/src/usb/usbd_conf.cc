2c88a2c93fa6ecd91d1685d36f931f093c18aafa
Dan Green <danngreen1@gmail.com>
2022-07-26 22:57:36 -0700

    usbd_conf.cc formatting

--- firmware/src/usb/usbd_conf.cc
+++ firmware/src/usb/usbd_conf.cc
@@ -18,9 +18,10 @@
 
 /* Includes ------------------------------------------------------------------ */
 #include "drivers/stm32xx.h"
+#include "usb_drive_device.hh"
 #include "usbd_core.h"
 #include "usbd_msc.h"
-#include "usb_drive_device.hh" 
+
 
 PCD_HandleTypeDef hpcd;
 
@@ -77,7 +78,7 @@ void HAL_PCD_MspDeInit(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_SetupStage((USBD_HandleTypeDef*)hpcd->pData, (uint8_t *)hpcd->Setup);
+	USBD_LL_SetupStage((USBD_HandleTypeDef *)hpcd->pData, (uint8_t *)hpcd->Setup);
 }
 
 /**
@@ -87,7 +88,7 @@ void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_DataOutStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
-	USBD_LL_DataOutStage((USBD_HandleTypeDef*)hpcd->pData, epnum, hpcd->OUT_ep[epnum].xfer_buff);
+	USBD_LL_DataOutStage((USBD_HandleTypeDef *)hpcd->pData, epnum, hpcd->OUT_ep[epnum].xfer_buff);
 }
 
 /**
@@ -97,7 +98,7 @@ void HAL_PCD_DataOutStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
  * @retval None
  */
 void HAL_PCD_DataInStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
-	USBD_LL_DataInStage((USBD_HandleTypeDef*)hpcd->pData, epnum, hpcd->IN_ep[epnum].xfer_buff);
+	USBD_LL_DataInStage((USBD_HandleTypeDef *)hpcd->pData, epnum, hpcd->IN_ep[epnum].xfer_buff);
 }
 
 /**
@@ -106,7 +107,7 @@ void HAL_PCD_DataInStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
  * @retval None
  */
 void HAL_PCD_SOFCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_SOF((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_SOF((USBD_HandleTypeDef *)hpcd->pData);
 }
 
 /**
@@ -133,9 +134,9 @@ void HAL_PCD_ResetCallback(PCD_HandleTypeDef *hpcd) {
 	}
 
 	/* Reset Device */
-	USBD_LL_Reset((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_Reset((USBD_HandleTypeDef *)hpcd->pData);
 
-	USBD_LL_SetSpeed((USBD_HandleTypeDef*)hpcd->pData, speed);
+	USBD_LL_SetSpeed((USBD_HandleTypeDef *)hpcd->pData, speed);
 }
 
 /**
@@ -144,7 +145,7 @@ void HAL_PCD_ResetCallback(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_SuspendCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_Suspend((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_Suspend((USBD_HandleTypeDef *)hpcd->pData);
 }
 
 /**
@@ -153,7 +154,7 @@ void HAL_PCD_SuspendCallback(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_ResumeCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_Resume((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_Resume((USBD_HandleTypeDef *)hpcd->pData);
 }
 
 /**
@@ -163,7 +164,7 @@ void HAL_PCD_ResumeCallback(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_ISOOUTIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
-	USBD_LL_IsoOUTIncomplete((USBD_HandleTypeDef*)hpcd->pData, epnum);
+	USBD_LL_IsoOUTIncomplete((USBD_HandleTypeDef *)hpcd->pData, epnum);
 }
 
 /**
@@ -173,7 +174,7 @@ void HAL_PCD_ISOOUTIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
  * @retval None
  */
 void HAL_PCD_ISOINIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
-	USBD_LL_IsoINIncomplete((USBD_HandleTypeDef*)hpcd->pData, epnum);
+	USBD_LL_IsoINIncomplete((USBD_HandleTypeDef *)hpcd->pData, epnum);
 }
 
 /**
@@ -182,7 +183,7 @@ void HAL_PCD_ISOINIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum) {
  * @retval None
  */
 void HAL_PCD_ConnectCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_DevConnected((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_DevConnected((USBD_HandleTypeDef *)hpcd->pData);
 	// fops->Init is called already...?
 	// uint32_t num_lun = USBD_MSC_fops.GetMaxLun();
 	// while (num_lun >= 0) {
@@ -197,7 +198,7 @@ void HAL_PCD_ConnectCallback(PCD_HandleTypeDef *hpcd) {
  * @retval None
  */
 void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef *hpcd) {
-	USBD_LL_DevDisconnected((USBD_HandleTypeDef*)hpcd->pData);
+	USBD_LL_DevDisconnected((USBD_HandleTypeDef *)hpcd->pData);
 	int8_t num_lun = UsbDriveDevice::ops.GetMaxLun();
 	while (num_lun >= 0) {
 		UsbDriveDevice::ops.Eject(num_lun);
