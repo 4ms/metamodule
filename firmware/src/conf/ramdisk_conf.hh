ae2a75443ea4ab734c1a25f5f78d8af4772cbd27
Dan Green <danngreen1@gmail.com>
2022-08-23 16:40:59 -0700

    RamDisk => 4MB

--- firmware/src/conf/ramdisk_conf.hh
+++ firmware/src/conf/ramdisk_conf.hh
@@ -1,5 +1,5 @@
 #pragma once
 #include <cstdint>
 
-static constexpr uint32_t RamDiskSizeBytes = 1 * 1024 * 1024;
+static constexpr uint32_t RamDiskSizeBytes = 4 * 1024 * 1024;
 static constexpr uint32_t RamDiskBlockSize = 512;
