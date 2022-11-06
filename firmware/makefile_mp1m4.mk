9621228738d0b79eba089ecf718d621d28a7046e
Dan Green <danngreen1@gmail.com>
2022-11-06 14:48:42 -0800

    Makefiles: add usb files to m4, cleanup a7 and m4

--- firmware/makefile_mp1m4.mk
+++ firmware/makefile_mp1m4.mk
@@ -28,6 +28,9 @@ DEVICEDIR = $(DEVICEBASE)/stm32mp157c
 TARGETDEVICEDIR = $(DRIVERLIB)/target/stm32mp1
 TARGETDEVICEDIR_CM4 = $(DRIVERLIB)/target/stm32mp1_cm4
 
+usb_src := src/usb
+usbhost_libdir = $(LIBDIR)/stm32-usb-host-lib
+usbdev_libdir = $(LIBDIR)/stm32-usb-dev-lib
 STARTUP = $(TARGETDEVICEDIR_CM4)/boot/startup_stm32mp157cxx_cm4.s
 SYSTEM = $(DEVICEBASE)/stm32mp157c/templates/system_stm32mp1xx.c
 
@@ -37,45 +40,56 @@ include makefile_opts.mk
 
 ASM_SOURCES  = $(STARTUP)
 
-SOURCES  += $(SYSTEM)
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_adc.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_adc_ex.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_i2c.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_i2c_ex.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_rcc.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_rcc_ex.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_ll_tim.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_mdma.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_dma.c
-# SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_qspi.c
-SOURCES  += $(HALDIR)/src/stm32mp1xx_hal_cortex.c
-SOURCES  += $(target_src)/controls.cc
-SOURCES  += $(target_chip_src)/main-m4.cc
-SOURCES  += system/libc_stub.c
-SOURCES  += system/libcpp_stub.cc
-SOURCES  += system/new.cc
-SOURCES  += $(DRIVERLIB)/drivers/hal_handlers.cc
-SOURCES  += $(DRIVERLIB)/drivers/i2c.cc
-SOURCES  += $(TARGETDEVICEDIR_CM4)/drivers/interrupt_handler.cc
-SOURCES  += $(DRIVERLIB)/drivers/pin.cc
-# SOURCES  += $(DRIVERLIB)/drivers/qspi_flash_driver.cc
-SOURCES  += $(DRIVERLIB)/drivers/rotary.cc
-SOURCES  += $(DRIVERLIB)/drivers/tim.cc
-SOURCES  += $(DRIVERLIB)/drivers/timekeeper.cc
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/core/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/draw/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/layouts/flex/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/layouts/grid/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/others/snapshot/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/themes/*/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/extra/widgets/*/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/font/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/gpu/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/hal/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/misc/*.c)
-# SOURCES  += $(wildcard $(LIBDIR)/lvgl/lvgl/src/widgets/*.c)
+SOURCES += $(SYSTEM)
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_adc.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_adc_ex.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_i2c.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_i2c_ex.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_ll_rcc.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_rcc.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_rcc_ex.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_ll_tim.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_mdma.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_dma.c
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_cortex.c
+SOURCES += $(target_src)/controls.cc
+SOURCES += $(target_chip_src)/main-m4.cc
+SOURCES += system/libc_stub.c
+SOURCES += system/libcpp_stub.cc
+SOURCES += system/new.cc
+SOURCES += $(DRIVERLIB)/drivers/hal_handlers.cc
+SOURCES += $(DRIVERLIB)/drivers/i2c.cc
+SOURCES += $(TARGETDEVICEDIR_CM4)/drivers/interrupt_handler.cc
+SOURCES += $(DRIVERLIB)/drivers/pin.cc
+SOURCES += $(DRIVERLIB)/drivers/rotary.cc
+SOURCES += $(DRIVERLIB)/drivers/tim.cc
+SOURCES += $(DRIVERLIB)/drivers/timekeeper.cc
+
+# USB:
+SOURCES += $(HALDIR)/src/stm32mp1xx_ll_usb.c
+
+# USB Host:
+SOURCES += $(HALDIR)/src/stm32mp1xx_hal_hcd.c
+SOURCES += $(usb_src)/usbh_conf.c
+SOURCES += $(usb_src)/usbh_MIDI.c
+SOURCES += $(usb_src)/midi_host.cc
+SOURCES += $(usbhost_libdir)/Class/HUB/Src/usbh_hub.c
+SOURCES += $(usbhost_libdir)/Core/Src/usbh_core.c
+SOURCES += $(usbhost_libdir)/Core/Src/usbh_ctlreq.c
+SOURCES += $(usbhost_libdir)/Core/Src/usbh_ioreq.c
+SOURCES += $(usbhost_libdir)/Core/Src/usbh_pipes.c
+
+# USB Device:
+# SOURCES += $(HALDIR)/src/stm32mp1xx_hal_pcd.c
+# SOURCES += $(HALDIR)/src/stm32mp1xx_hal_pcd_ex.c
+# SOURCES += $(usbdev_libdir)/Class/MSC/Src/usbd_msc.c
+# SOURCES += $(usbdev_libdir)/Class/MSC/Src/usbd_msc_bot.c
+# SOURCES += $(usbdev_libdir)/Class/MSC/Src/usbd_msc_data.c
+# SOURCES += $(usbdev_libdir)/Class/MSC/Src/usbd_msc_scsi.c
+# SOURCES += $(usbdev_libdir)/Core/Src/usbd_core.c
+# SOURCES += $(usbdev_libdir)/Core/Src/usbd_ctlreq.c
+# SOURCES += $(usbdev_libdir)/Core/Src/usbd_ioreq.c
 
 ifeq "$(target_board)" "mini"
 SOURCES  += $(DRIVERLIB)/drivers/pca9685_led_driver.cc
@@ -103,7 +117,12 @@ INCLUDES = -I$(DEVICEDIR)/include \
 			-I$(SHARED)/CoreModules \
 			-I$(SHARED)/cpputil \
 			-I$(SHARED)/patch \
-			# -I$(LIBDIR)/lvgl \
+			-I$(usb_src) \
+			-I$(usbhost_libdir)/Core/Inc \
+			
+			# -I$(usbdev_libdir)/Class/MSC/Inc \
+			# -I$(usbdev_libdir)/Core/Inc \ 
+			# -I$(usbdev_libdir)/Class/HUB/Inc \
 
 MCU = -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mthumb -mlittle-endian -mfloat-abi=hard
 
