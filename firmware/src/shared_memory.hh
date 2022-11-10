30cb3ac3437e140dafac0135e6f139668a30718c
Dan Green <danngreen1@gmail.com>
2022-11-09 20:46:38 -0800

    Idea for alt method of SharedMemory

--- firmware/src/shared_memory.hh
+++ firmware/src/shared_memory.hh
@@ -25,8 +25,31 @@ struct SharedMemory {
 
 	enum : uint32_t {
 		ParamsPtrLocation = 0,
-		ScreenBufLocation,
+		RamDiskLocation,
 		AuxSignalBlockLocation,
 		PatchPlayerLocation,
 	};
+
+	//////////////// TODO: Test this alternative method:
+	// enum class Location : uint32_t {
+	// 	ParamsPtr = 0,
+	// 	RamDisk,
+	// 	AuxSignalBlock,
+	// 	PatchPlayer,
+	// };
+
+	// static inline __attribute__((section(".sharedmemindex"))) uint32_t shared_memory_addrs[4];
+
+	// template<Location Loc, typename T>
+	// static void register_address_of(T *object) {
+	// 	auto index = static_cast<uint32_t>(Loc);
+	// 	shared_memory_addrs[index] = reinterpret_cast<uint32_t>(object);
+	// 	mdrivlib::SystemCache::clean_dcache_by_addr(&(shared_memory_addrs[index]));
+	// }
+
+	// template<typename T>
+	// static T get_shared_obj_ptr(Location loc) {
+	// 	auto index = static_cast<uint32_t>(loc);
+	// 	return reinterpret_cast<T>(shared_memory_addrs[index]);
+	// }
 };
