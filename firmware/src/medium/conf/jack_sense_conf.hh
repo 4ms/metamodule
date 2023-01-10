6df7f595c67d0e9a53b5670b6fe49d4753c0cd08
Dan Green <danngreen1@gmail.com>
2023-01-09 17:27:00 -0800

    Fix jacks out of order for pin_sense

--- firmware/src/medium/conf/jack_sense_conf.hh
+++ firmware/src/medium/conf/jack_sense_conf.hh
@@ -1,3 +1,3 @@
 #pragma once
 
-static inline const unsigned jacksense_pin_order[18] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+static inline const unsigned jacksense_pin_order[18] = {14, 11, 15, 13, 10, 8, 12, 9, 4, 5, 6, 1, 2, 7, 3, 0};
