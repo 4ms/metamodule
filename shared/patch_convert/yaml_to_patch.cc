254b2e1d1763bfc3ffee9bce6b891a67f8176882
Dan Green <danngreen1@gmail.com>
2023-01-30 16:27:38 -0800

    rm unused yaml to patch code

--- shared/patch_convert/yaml_to_patch.cc
+++ shared/patch_convert/yaml_to_patch.cc
@@ -34,31 +34,5 @@ bool yaml_raw_to_patch(std::span<char> yaml, PatchData &pd) {
 }
 
 bool yaml_string_to_patch(std::string yaml, PatchData &pd) {
-	//TODO: why can't we do:
-	//return yaml_raw_to_patch(yaml.data(), yaml.size_bytes(), pd);
-	RymlInit::init_once();
-
-	ryml::Tree tree = ryml::parse_in_place(ryml::substr(yaml.data(), yaml.size()));
-
-	if (tree.num_children(0) == 0)
-		return false;
-
-	ryml::NodeRef data_root = tree[0];
-	if (data_root.key() != "PatchData")
-		return false;
-
-	data_root["patch_name"] >> pd.patch_name;
-
-	if (data_root.has_child("description"))
-		data_root["description"] >> pd.description;
-
-	data_root["module_slugs"] >> pd.module_slugs;
-	data_root["int_cables"] >> pd.int_cables;
-	data_root["mapped_ins"] >> pd.mapped_ins;
-
-	data_root["mapped_outs"] >> pd.mapped_outs;
-	data_root["static_knobs"] >> pd.static_knobs;
-	data_root["mapped_knobs"] >> pd.mapped_knobs;
-
-	return true;
+	return yaml_raw_to_patch(yaml.data(), yaml.size(), pd);
 }
