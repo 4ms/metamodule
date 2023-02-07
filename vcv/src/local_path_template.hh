0ed9ad0fc3871db8ebaa8bea5a81fc826deb22af
Dan Green <danngreen1@gmail.com>
2023-02-07 12:26:48 -0800

    remove unused debugFile name

--- vcv/src/local_path_template.hh
+++ vcv/src/local_path_template.hh
@@ -1,5 +1,4 @@
 #pragma once
 #include <string>
-const std::string debugFile = "./debug_log.txt";
 const std::string examplePatchDir = "/Users/dann/4ms/stm32/meta-module/shared/patch/";
 const std::string testPatchDir = "/Users/dann/4ms/stm32/meta-module/shared/patch/integration_tests/";
