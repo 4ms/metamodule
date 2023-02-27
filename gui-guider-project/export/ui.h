f167e30e87a8172442dbc9f0f0cb926692ac9528
Dan Green <danngreen1@gmail.com>
2023-02-26 17:37:22 -0800

    layout idea in SLS

---
+++ gui-guider-project/export/ui.h
@@ -0,0 +1,32 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.2.1
+// LVGL VERSION: 8.3.4
+// PROJECT: MM
+
+#ifndef _MM_UI_H
+#define _MM_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    #include "lvgl.h"
+
+extern lv_obj_t *ui_Screen1;
+void ui_event_media( lv_event_t * e);
+extern lv_obj_t *ui_media;
+extern lv_obj_t *ui_Label2;
+extern lv_obj_t *ui_patchlist;
+
+
+
+
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
