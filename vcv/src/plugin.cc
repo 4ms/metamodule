83d4f75c280d0f2fdef2b4d5bf44d1700f293e0e
Lennart <Lennart@binarylabs.dev>
2023-03-27 16:20:54 +0200

    fix conflicting declarations

--- vcv/src/plugin.cc
+++ vcv/src/plugin.cc
@@ -8,190 +8,190 @@ std::unique_ptr<CentralData> centralData;
 // include and define models below here
 
 #include "CoreModules/info/SHEV_info.hh"
-auto modelSHEV = createModelFromInfo<SHEVInfo>();
+Model* modelSHEV = createModelFromInfo<SHEVInfo>();
 
 #include "CoreModules/info/ENVVCA_info.hh"
-auto modelENVVCA = createModelFromInfo<ENVVCAInfo>();
+Model* modelENVVCA = createModelFromInfo<ENVVCAInfo>();
 
 #include "CoreModules/info/DEV_info.hh"
-auto modelDEV = createModelFromInfo<DEVInfo>();
+Model* modelDEV = createModelFromInfo<DEVInfo>();
 
 #include "CoreModules/info/Freeverb_info.hh"
-auto modelFreeverb = createModelFromInfo<FreeverbInfo>();
+Model* modelFreeverb = createModelFromInfo<FreeverbInfo>();
 
 #include "CoreModules/info/STS_info.hh"
-auto modelSTS = createModelFromInfo<STSInfo>();
+Model* modelSTS = createModelFromInfo<STSInfo>();
 
 #include "CoreModules/info/VCAM_info.hh"
-auto modelVCAM = createModelFromInfo<VCAMInfo>();
+Model* modelVCAM = createModelFromInfo<VCAMInfo>();
 
 #include "CoreModules/info/Tapo_info.hh"
-auto modelTapo = createModelFromInfo<TapoInfo>();
+Model* modelTapo = createModelFromInfo<TapoInfo>();
 
 #include "CoreModules/info/Shift_info.hh"
-auto modelShift = createModelFromInfo<ShiftInfo>();
+Model* modelShift = createModelFromInfo<ShiftInfo>();
 
 #include "CoreModules/info/SISM_info.hh"
-auto modelSISM = createModelFromInfo<SISMInfo>();
+Model* modelSISM = createModelFromInfo<SISMInfo>();
 
 #include "CoreModules/info/SH_info.hh"
-auto modelSH = createModelFromInfo<SHInfo>();
+Model* modelSH = createModelFromInfo<SHInfo>();
 
 #include "CoreModules/info/SCM_info.hh"
-auto modelSCM = createModelFromInfo<SCMInfo>();
+Model* modelSCM = createModelFromInfo<SCMInfo>();
 
 #include "CoreModules/info/RCD_info.hh"
-auto modelRCD = createModelFromInfo<RCDInfo>();
+Model* modelRCD = createModelFromInfo<RCDInfo>();
 
 #include "CoreModules/info/QPLFO_info.hh"
-auto modelQPLFO = createModelFromInfo<QPLFOInfo>();
+Model* modelQPLFO = createModelFromInfo<QPLFOInfo>();
 
 #include "CoreModules/info/QCD_info.hh"
-auto modelQCD = createModelFromInfo<QCDInfo>();
+Model* modelQCD = createModelFromInfo<QCDInfo>();
 
 #include "CoreModules/info/PI_info.hh"
-auto modelPI = createModelFromInfo<PIInfo>();
+Model* modelPI = createModelFromInfo<PIInfo>();
 
 #include "CoreModules/info/PEG_info.hh"
-auto modelPEG = createModelFromInfo<PEGInfo>();
+Model* modelPEG = createModelFromInfo<PEGInfo>();
 
 #include "CoreModules/info/Mixer_info.hh"
-auto modelMixer = createModelFromInfo<MixerInfo>();
+Model* modelMixer = createModelFromInfo<MixerInfo>();
 
 #include "CoreModules/info/LPG_info.hh"
-auto modelLPG = createModelFromInfo<LPGInfo>();
+Model* modelLPG = createModelFromInfo<LPGInfo>();
 
 #include "CoreModules/info/LIO_info.hh"
-auto modelLIO = createModelFromInfo<LIOInfo>();
+Model* modelLIO = createModelFromInfo<LIOInfo>();
 
 #include "CoreModules/info/L4_info.hh"
-auto modelL4 = createModelFromInfo<L4Info>();
+Model* modelL4 = createModelFromInfo<L4Info>();
 
 #include "CoreModules/info/Gate8_info.hh"
-auto modelGate8 = createModelFromInfo<Gate8Info>();
+Model* modelGate8 = createModelFromInfo<Gate8Info>();
 
 #include "CoreModules/info/Gate32_info.hh"
-auto modelGate32 = createModelFromInfo<Gate32Info>();
+Model* modelGate32 = createModelFromInfo<Gate32Info>();
 
 #include "CoreModules/info/GRev_info.hh"
-auto modelGRev = createModelFromInfo<GRevInfo>();
+Model* modelGRev = createModelFromInfo<GRevInfo>();
 
 #include "CoreModules/info/Fade_info.hh"
-auto modelFade = createModelFromInfo<FadeInfo>();
+Model* modelFade = createModelFromInfo<FadeInfo>();
 
 #include "CoreModules/info/DLD_info.hh"
-auto modelDLD = createModelFromInfo<DLDInfo>();
+Model* modelDLD = createModelFromInfo<DLDInfo>();
 
 #include "CoreModules/info/Comp_info.hh"
-auto modelComp = createModelFromInfo<CompInfo>();
+Model* modelComp = createModelFromInfo<CompInfo>();
 
 #include "CoreModules/info/CLKM_info.hh"
-auto modelCLKM = createModelFromInfo<CLKMInfo>();
+Model* modelCLKM = createModelFromInfo<CLKMInfo>();
 
 #include "CoreModules/info/CLKD_info.hh"
-auto modelCLKD = createModelFromInfo<CLKDInfo>();
+Model* modelCLKD = createModelFromInfo<CLKDInfo>();
 
 #include "CoreModules/info/ADEnv_info.hh"
-auto modelADEnv = createModelFromInfo<ADEnvInfo>();
+Model* modelADEnv = createModelFromInfo<ADEnvInfo>();
 
 #include "CoreModules/info/Sequant_info.hh"
-auto modelSequant = createModelFromInfo<SequantInfo>();
+Model* modelSequant = createModelFromInfo<SequantInfo>();
 
 #include "CoreModules/info/Verb_info.hh"
-auto modelVerb = createModelFromInfo<VerbInfo>();
+Model* modelVerb = createModelFromInfo<VerbInfo>();
 
 #include "CoreModules/info/Switch41_info.hh"
-auto modelSwitch41 = createModelFromInfo<Switch41Info>();
+Model* modelSwitch41 = createModelFromInfo<Switch41Info>();
 
 #include "CoreModules/info/Switch14_info.hh"
-auto modelSwitch14 = createModelFromInfo<Switch14Info>();
+Model* modelSwitch14 = createModelFromInfo<Switch14Info>();
 
 #include "CoreModules/info/Seq8_info.hh"
-auto modelSeq8 = createModelFromInfo<Seq8Info>();
+Model* modelSeq8 = createModelFromInfo<Seq8Info>();
 
 #include "CoreModules/info/Seq4_info.hh"
-auto modelSeq4 = createModelFromInfo<Seq4Info>();
+Model* modelSeq4 = createModelFromInfo<Seq4Info>();
 
 #include "CoreModules/info/Quant_info.hh"
-auto modelQuant = createModelFromInfo<QuantInfo>();
+Model* modelQuant = createModelFromInfo<QuantInfo>();
 
 #include "CoreModules/info/Prob8_info.hh"
-auto modelProb8 = createModelFromInfo<Prob8Info>();
+Model* modelProb8 = createModelFromInfo<Prob8Info>();
 
 #include "CoreModules/info/Octave_info.hh"
-auto modelOctave = createModelFromInfo<OctaveInfo>();
+Model* modelOctave = createModelFromInfo<OctaveInfo>();
 
 #include "CoreModules/info/MNMX_info.hh"
-auto modelMNMX = createModelFromInfo<MNMXInfo>();
+Model* modelMNMX = createModelFromInfo<MNMXInfo>();
 
 #include "CoreModules/info/LPF_info.hh"
-auto modelLPF = createModelFromInfo<LPFInfo>();
+Model* modelLPF = createModelFromInfo<LPFInfo>();
 
 #include "CoreModules/info/StMix_info.hh"
-auto modelStMix = createModelFromInfo<StMixInfo>();
+Model* modelStMix = createModelFromInfo<StMixInfo>();
 
 #include "CoreModules/info/Source_info.hh"
-auto modelSource = createModelFromInfo<SourceInfo>();
+Model* modelSource = createModelFromInfo<SourceInfo>();
 
 #include "CoreModules/info/Slew_info.hh"
-auto modelSlew = createModelFromInfo<SlewInfo>();
+Model* modelSlew = createModelFromInfo<SlewInfo>();
 
 #include "CoreModules/info/PitchShift_info.hh"
-auto modelPitchShift = createModelFromInfo<PitchShiftInfo>();
+Model* modelPitchShift = createModelFromInfo<PitchShiftInfo>();
 
 #include "CoreModules/info/Pan_info.hh"
-auto modelPan = createModelFromInfo<PanInfo>();
+Model* modelPan = createModelFromInfo<PanInfo>();
 
 #include "CoreModules/info/Noise_info.hh"
-auto modelNoise = createModelFromInfo<NoiseInfo>();
+Model* modelNoise = createModelFromInfo<NoiseInfo>();
 
 #include "CoreModules/info/MultiLFO_info.hh"
-auto modelMultiLFO = createModelFromInfo<MultiLFOInfo>();
+Model* modelMultiLFO = createModelFromInfo<MultiLFOInfo>();
 
 #include "CoreModules/info/KPLS_info.hh"
-auto modelKPLS = createModelFromInfo<KPLSInfo>();
+Model* modelKPLS = createModelFromInfo<KPLSInfo>();
 
 #include "CoreModules/info/InfOsc_info.hh"
-auto modelInfOsc = createModelFromInfo<InfOscInfo>();
+Model* modelInfOsc = createModelFromInfo<InfOscInfo>();
 
 #include "CoreModules/info/HPF_info.hh"
-auto modelHPF = createModelFromInfo<HPFInfo>();
+Model* modelHPF = createModelFromInfo<HPFInfo>();
 
 #include "CoreModules/info/Gate_info.hh"
-auto modelGate = createModelFromInfo<GateInfo>();
+Model* modelGate = createModelFromInfo<GateInfo>();
 
 #include "CoreModules/info/Follow_info.hh"
-auto modelFollow = createModelFromInfo<FollowInfo>();
+Model* modelFollow = createModelFromInfo<FollowInfo>();
 
 #include "CoreModules/info/FM_info.hh"
-auto modelFM = createModelFromInfo<FMInfo>();
+Model* modelFM = createModelFromInfo<FMInfo>();
 
 #include "CoreModules/info/Drum_info.hh"
-auto modelDrum = createModelFromInfo<DrumInfo>();
+Model* modelDrum = createModelFromInfo<DrumInfo>();
 
 #include "CoreModules/info/Djembe_info.hh"
-auto modelDjembe = createModelFromInfo<DjembeInfo>();
+Model* modelDjembe = createModelFromInfo<DjembeInfo>();
 
 #include "CoreModules/info/Detune_info.hh"
-auto modelDetune = createModelFromInfo<DetuneInfo>();
+Model* modelDetune = createModelFromInfo<DetuneInfo>();
 
 #include "CoreModules/info/ComplexEG_info.hh"
-auto modelComplexEG = createModelFromInfo<ComplexEGInfo>();
+Model* modelComplexEG = createModelFromInfo<ComplexEGInfo>();
 
 #include "CoreModules/info/BPF_info.hh"
-auto modelBPF = createModelFromInfo<BPFInfo>();
+Model* modelBPF = createModelFromInfo<BPFInfo>();
 
 #include "CoreModules/info/Atvert2_info.hh"
-auto modelAtvert2 = createModelFromInfo<Atvert2Info>();
+Model* modelAtvert2 = createModelFromInfo<Atvert2Info>();
 
 #include "CoreModules/info/SMR_info.hh"
-auto modelSMR = createModelFromInfo<SMRInfo>();
+Model* modelSMR = createModelFromInfo<SMRInfo>();
 
 #include "CoreModules/modules/enosc/altparam_EnOsc_info.hh"
-auto modelEnOsc = createModelFromInfo<APEnOscInfo>();
+Model* modelEnOsc = createModelFromInfo<APEnOscInfo>();
 
 #include "CoreModules/info/BuffMult_info.hh"
-auto modelBuffMult = createModelFromInfo<BuffMultInfo>();
+Model* modelBuffMult = createModelFromInfo<BuffMultInfo>();
 ///////////////////////////////////////////////////////
 
 void init(Plugin *p)
