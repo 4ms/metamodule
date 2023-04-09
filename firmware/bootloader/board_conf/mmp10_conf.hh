118a975cd360e429bdb6024544984b07879b50a6
Dan Green <danngreen1@gmail.com>
2023-04-09 13:56:27 -0700

    update mp1-boot: check GPIO to determine boot path

--- firmware/bootloader/board_conf/mmp10_conf.hh
+++ firmware/bootloader/board_conf/mmp10_conf.hh
@@ -3,7 +3,7 @@
 #include "drivers/i2c_conf.hh"
 #include "drivers/leds.hh"
 
-namespace MMp10
+namespace Board
 {
 using RedLED = Led<GPIO::E, PinNum::_2, LedActive::High>;
 using GreenLED = Led<GPIO::D, PinNum::_3, LedActive::High>;
@@ -12,6 +12,10 @@ using GreenLED2 = Led<GPIO::E, PinNum::_1, LedActive::High>;
 
 using OrangeLED = RedLED2;
 using BlueLED = Led<GPIO::A, PinNum::_14, LedActive::Low>;
+
+//EXPANDER: right-most pins shorted
+constexpr bool UseBootSelect = true;
+constexpr PinConf BootSelectPin{GPIO::G, PinNum::_11};
 
 constexpr uint32_t ConsoleUART = USART6_BASE;
 constexpr PinConf UartRX{GPIO::C, PinNum::_7, PinAF::AF_7};
@@ -34,4 +38,4 @@ constexpr uint32_t HSE_Clock_Hz = 24000000;
 constexpr uint32_t MPU_MHz = 800;
 constexpr auto ClockType = SystemClocks::HSEClockSource::Resonator;
 
-} // namespace MMp10
+} // namespace Board
