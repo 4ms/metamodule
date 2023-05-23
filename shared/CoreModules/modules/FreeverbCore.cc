35dc7759b2eeffa5ca4e9f8bfb60387991ba3fa4
Dan Green <danngreen1@gmail.com>
2023-05-23 11:28:32 -0700

    Add DC-block to FreeVerb

--- shared/CoreModules/modules/FreeverbCore.cc
+++ shared/CoreModules/modules/FreeverbCore.cc
@@ -3,6 +3,7 @@
 #include "CoreModules/moduleFactory.hh"
 #include "processors/allpass.h"
 #include "processors/comb.h"
+#include "processors/tools/dcBlock.h"
 #include "util/math.hh"
 
 class FreeverbCore : public CoreProcessor {
@@ -38,7 +39,7 @@ public:
 			wetSignal = apFilter[i].process(wetSignal);
 		}
 
-		signalOut = MathTools::interpolate(signalIn, wetSignal, mix);
+		signalOut = dcblock.update(MathTools::interpolate3(signalIn, wetSignal, mix));
 	}
 
 	void set_param(int param_id, float val) override {
@@ -116,6 +117,7 @@ private:
 
 	Comb<6000> combFilter[numComb];
 	AllPass<6000> apFilter[numAll];
+	DCBlock dcblock;
 
 	float mix = 0;
 };
