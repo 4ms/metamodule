2966b30e3732cfb1ee4ee2da8bf23b194d029603
Dan Green <danngreen1@gmail.com>
2023-06-01 11:06:35 -0700

    Use inheritance of Elements to handle groups of types

--- vcv/src/elements/vcv_module_creator.hh
+++ vcv/src/elements/vcv_module_creator.hh
@@ -11,70 +11,68 @@ struct VCVModuleParamCreator {
 		: module{module} {
 	}
 
-	void count_element(BaseElement element){};
-
-	void count_element(InJackElement element) {
+	void count_element(BaseElement){};
+	void count_element(JackInput) {
 		num_inputs++;
 	};
-	void count_element(OutJackElement element) {
+	void count_element(JackOutput) {
 		num_outputs++;
 	};
-	void count_element(KnobElement element) {
+	void count_element(Pot) {
 		num_params++;
 	};
-	void count_element(LightElement element) {
+	void count_element(Switch) {
+		num_params++;
+	};
+	void count_element(Light) {
 		num_lights++;
 	};
-
-	void count_element(MomentaryButton element) {
+	void count_element(MediumLight<RedGreenBlueLight>) {
+		num_lights += 3;
+	};
+	void count_element(MomentaryButtonRGB element) {
 		num_lights += 3;
 		num_params++;
 	}
-	void count_element(LatchingButton element) {
+	void count_element(LatchingButtonMonoLight element) {
 		num_params++;
 		num_lights++;
 	}
-	void count_element(Toggle2pos element) {
-		num_params++;
-	};
-	void count_element(Toggle3pos element) {
-		num_params++;
-	};
 	void count_element(LEDEncoder element) {
 		num_lights += 3;
 		num_params++;
 	};
 
-	void config_element(BaseElement element){};
-	void config_element(InJackElement element) {
-		std::visit([this](auto &el) { module->configInput(el.idx, el.short_name.data()); }, element);
+	void config_element(BaseElement){};
+	void config_element(JackInput el) {
+		module->configInput(el.idx, el.short_name.data());
 	};
-	void config_element(OutJackElement element) {
-		std::visit([this](auto &el) { module->configOutput(el.idx, el.short_name.data()); }, element);
+	void config_element(JackOutput el) {
+		module->configOutput(el.idx, el.short_name.data());
 	};
-	void config_element(KnobElement element) {
-		std::visit([this](auto &el) { module->configParam(el.idx, 0.f, 1.f, el.default_val, el.short_name.data()); },
-				   element);
+	void config_element(Pot el) {
+		module->configParam(el.idx, 0.f, 1.f, el.default_val, el.short_name.data());
 	};
-	void config_element(LightElement element) {
-		std::visit([this](auto &el) { module->configLight(el.idx, el.short_name.data()); }, element);
+	void config_element(Light el) {
+		module->configLight(el.idx, el.short_name.data());
 	};
-
-	void config_element(MomentaryButton element) {
+	void config_element(MomentaryButtonRGB element) {
 		module->configParam(element.idx, 0.f, 1.f, element.default_val, element.short_name.data());
+		//TODO: LED?
 	}
-	void config_element(LatchingButton element) {
+	void config_element(LatchingButtonMonoLight element) {
 		module->configParam(element.idx, 0.f, 1.f, element.default_val, element.short_name.data());
+		//TODO: LED?
 	}
-	void config_element(Toggle2pos element) {
+	void config_element(Switch element) {
 		module->configParam(element.idx, 0.f, 1.f, 0.f, element.short_name.data());
 	};
 	void config_element(Toggle3pos element) {
 		module->configParam(element.idx, 0.f, 2.f, 0.f, element.short_name.data());
-		// scaleFactor = 1.f/2.f;
 	};
 	void config_element(LEDEncoder element) {
 		module->configParam(element.idx, -INFINITY, INFINITY, 0.0f, element.short_name.data());
+		// TODO: LED?
 	};
 
 	unsigned num_params = 0;
