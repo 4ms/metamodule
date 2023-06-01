90994855ef2c603f426db466f21a449e313a1063
Dan Green <danngreen1@gmail.com>
2023-05-31 21:56:13 -0700

    Temp: Iterate just Knobs in generic module

--- vcv/src/generic_module_new.hh
+++ vcv/src/generic_module_new.hh
@@ -1,6 +1,6 @@
 #pragma once
-#include "CoreModules/module_info_base.hh"
 #include "CoreModules/moduleFactory.hh"
+#include "CoreModules/module_info_base.hh"
 #include "comm/comm_module.hh"
 #include "comm/widget_creator.hh"
 #include "components.h"
@@ -8,23 +8,23 @@
 #include "util/base_concepts.hh"
 
 using namespace rack;
+using namespace MetaModule;
 
-template<Derived<ModuleInfoBase> Defs>
+template<Derived<ElementInfoBase> Defs>
 struct GenericModuleNew {
-	static rack::Model *create() {
-		return rack::createModel<Module, Widget>(Defs::slug.data());
-	}
+	static rack::Model *create() { return rack::createModel<Module, Widget>(Defs::slug.data()); }
 
 	struct Module : CommModule {
-		Module() {
+		Module()
+		{
 			// create processing core
 			core = ModuleFactory::create(Defs::slug);
 			selfID.slug = Defs::slug;
 
 			MetaModule::VCVModuleParamCreator creator{this};
 
-			//Count the elements of each type
-			for (auto &element : Defs::elements) {
+			// Count the elements of each type
+			for (auto &element : Defs::Knobs) {
 				std::visit([&creator](auto el) { creator.count_element(el); }, element);
 			}
 
@@ -32,7 +32,7 @@ struct GenericModuleNew {
 			configComm(creator.num_params, creator.num_inputs, creator.num_outputs, creator.num_lights);
 
 			// Configure elements with VCV
-			for (auto &element : Defs::elements) {
+			for (auto &element : Defs::Knobs) {
 				std::visit([&creator](auto el) { creator.config_element(el); }, element);
 			}
 
@@ -44,30 +44,31 @@ struct GenericModuleNew {
 		}
 	};
 
-	struct Widget : rack::app::ModuleWidget
-	{
+	struct Widget : rack::app::ModuleWidget {
 		CommModule *mainModule;
 
 		Widget(CommModule *module)
-			: mainModule{module} {
+			: mainModule{module}
+		{
 			// link this widget to given module
 			setModule(static_cast<Module *>(module));
 
 			// use svg file as panel
 			setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, Defs::svg_filename.data())));
-			
+
 			// draw screws
 			addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, 0)));
 			addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 			if (box.size.x > RACK_GRID_WIDTH * 7) // >7HP = 2 screws
 			{
 				addChild(createWidget<ScrewBlack>(rack::math::Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-				addChild(createWidget<ScrewBlack>(rack::math::Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+				addChild(createWidget<ScrewBlack>(
+					rack::math::Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 			}
 
 			// create widgets from all elements
 			MetaModule::VCVWidgetCreator creator{this, module};
-			for (auto &element : Defs::elements) {
+			for (auto &element : Defs::Knobs) {
 				std::visit([&creator](auto el) { creator.createWidget(el); }, element);
 			}
 
@@ -186,7 +187,8 @@ struct GenericModuleNew {
 			AltParamQty(CommModule &module, const AltParamDef &alt)
 				: _alt{alt}
 				, _module{module}
-				, _val{alt.default_val} {
+				, _val{alt.default_val}
+			{
 				for (auto &ap : _module.altParams) {
 					if (ap.id == _alt.id) {
 						_val = ap.val;
@@ -195,7 +197,8 @@ struct GenericModuleNew {
 				}
 			}
 
-			void setValue(float value) override {
+			void setValue(float value) override
+			{
 				float prev_val = _val;
 				_val = std::clamp(value, _alt.min_val, _alt.max_val);
 				if (_alt.control_type == AltParamDef::Range::Integer)
@@ -213,47 +216,36 @@ struct GenericModuleNew {
 				}
 			}
 
-			std::string getDisplayValueString() override {
+			std::string getDisplayValueString() override
+			{
 				if (_module.core)
 					return std::string{_module.core->get_alt_param_value(_alt.id, _val)};
 				return std::to_string(_val);
 			}
 
-			float getValue() override {
-				return _val;
-			}
-			float getMinValue() override {
-				return _alt.min_val;
-			}
-			float getMaxValue() override {
-				return _alt.max_val;
-			}
-			float getDefaultValue() override {
-				return _alt.default_val;
-			}
+			float getValue() override { return _val; }
+			float getMinValue() override { return _alt.min_val; }
+			float getMaxValue() override { return _alt.max_val; }
+			float getDefaultValue() override { return _alt.default_val; }
 		};
 
 		struct AltParamSlider : rack::ui::Slider {
-			AltParamSlider(CommModule &module, const AltParamDef &alt) {
-				quantity = new AltParamQty{module, alt};
-			}
-			~AltParamSlider() {
-				delete quantity;
-			}
+			AltParamSlider(CommModule &module, const AltParamDef &alt) { quantity = new AltParamQty{module, alt}; }
+			~AltParamSlider() { delete quantity; }
 		};
 
-		void appendContextMenu(rack::ui::Menu *menu) override {
-			menu->addChild(new rack::ui::MenuEntry);
-			for (auto &alt : Defs::AltParams) {
-				auto *item = new rack::ui::MenuItem;
-				item->text = std::string{alt.short_name};
-				menu->addChild(item);
+		// void appendContextMenu(rack::ui::Menu *menu) override
+		// {
+		// 	menu->addChild(new rack::ui::MenuEntry);
+		// 	for (auto &alt : Defs::AltParams) {
+		// 		auto *item = new rack::ui::MenuItem;
+		// 		item->text = std::string{alt.short_name};
+		// 		menu->addChild(item);
 
-				auto slider = new AltParamSlider{*mainModule, alt};
-				slider->box.size.x = 100;
-				menu->addChild(slider);
-			}
-		}
+		// 		auto slider = new AltParamSlider{*mainModule, alt};
+		// 		slider->box.size.x = 100;
+		// 		menu->addChild(slider);
+		// 	}
+		// }
 	};
 };
-
