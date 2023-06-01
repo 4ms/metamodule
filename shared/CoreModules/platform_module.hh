96e9a819c7a9a589b8fc931c0618cc9dbaa4a4ef
Dan Green <danngreen1@gmail.com>
2023-06-01 11:10:15 -0700

    PlatformModule auto-registers in VCV

--- shared/CoreModules/platform_module.hh
+++ shared/CoreModules/platform_module.hh
@@ -1,8 +1,17 @@
 #ifdef VCVRACK
 
+#include "moduleFactory.hh"
 #include "rack.hpp"
-template<typename T, typename U>
-struct PlatformModule : rack::Module {};
+
+template<typename Info, typename Core>
+struct PlatformModule : rack::Module {
+	// Auto-register in ModuleFactory
+	static std::unique_ptr<CoreProcessor> create() {
+		return std::make_unique<Core>();
+	}
+	static inline bool s_registered =
+		ModuleFactory::registerModuleType(Info::slug, create, MetaModule::ElementInfoView::makeView<Info>());
+};
 
 using rack::simd::float_4;
 
