edd13973443b7f3df77c8f4df18dd20b559dd0e8
Dan Green <danngreen1@gmail.com>
2023-05-31 21:55:40 -0700

    Add case for BaseElement in VCVModuleParamCreator

--- vcv/src/comm/widget_creator.hh
+++ vcv/src/comm/widget_creator.hh
@@ -8,11 +8,11 @@ namespace MetaModule
 struct VCVWidgetCreator {
 	VCVWidgetCreator(rack::ModuleWidget *module_widget, rack::Module *module)
 		: module{module}
-		, module_widget{module_widget} {
-	}
+		, module_widget{module_widget}
+	{}
 
 	template<typename T>
-	void createWidget(T element){};
+	void createWidget(const T element){};
 
 private:
 	[[maybe_unused]] rack::Module *module;
@@ -22,69 +22,72 @@ private:
 struct VCVModuleParamCreator {
 
 	VCVModuleParamCreator(rack::Module *module)
-		: module{module} {
-	}
+		: module{module}
+	{}
 
-	void count_element(InJackElement element) {
-		num_inputs++;
-	};
-	void count_element(OutJackElement element) {
-		num_outputs++;
-	};
-	void count_element(KnobElement element) {
-		num_params++;
-	};
-	void count_element(LightElement element) {
-		num_lights++;
-	};
+	void count_element(BaseElement element){};
 
-	void count_element(MomentaryButton element) {
+	void count_element(InJackElement element) { num_inputs++; };
+	void count_element(OutJackElement element) { num_outputs++; };
+	void count_element(KnobElement element) { num_params++; };
+	void count_element(LightElement element) { num_lights++; };
+
+	void count_element(MomentaryButton element)
+	{
 		num_lights += 3;
 		num_params++;
 	}
-	void count_element(LatchingButton element) {
+	void count_element(LatchingButton element)
+	{
 		num_params++;
 		num_lights++;
 	}
-	void count_element(Toggle2pos element) {
-		num_params++;
-	};
-	void count_element(Toggle3pos element) {
-		num_params++;
-	};
-	void count_element(LEDEncoder element) {
+	void count_element(Toggle2pos element) { num_params++; };
+	void count_element(Toggle3pos element) { num_params++; };
+	void count_element(LEDEncoder element)
+	{
 		num_lights += 3;
 		num_params++;
 	};
 
-	void config_element(InJackElement element) {
+	void config_element(BaseElement element){};
+	void config_element(InJackElement element)
+	{
 		std::visit([this](auto &el) { module->configInput(el.idx, el.short_name.data()); }, element);
 	};
-	void config_element(OutJackElement element) {
+	void config_element(OutJackElement element)
+	{
 		std::visit([this](auto &el) { module->configOutput(el.idx, el.short_name.data()); }, element);
 	};
-	void config_element(KnobElement element) {
+	void config_element(KnobElement element)
+	{
 		std::visit([this](auto &el) { module->configParam(el.idx, 0.f, 1.f, el.default_val, el.short_name.data()); },
 				   element);
 	};
-	void config_element(LightElement element) {
+	void config_element(LightElement element)
+	{
 		std::visit([this](auto &el) { module->configLight(el.idx, el.short_name.data()); }, element);
 	};
 
-	void config_element(MomentaryButton element) {
+	void config_element(MomentaryButton element)
+	{
 		module->configParam(element.idx, 0.f, 1.f, element.default_val, element.short_name.data());
 	}
-	void config_element(LatchingButton element) {
+	void config_element(LatchingButton element)
+	{
 		module->configParam(element.idx, 0.f, 1.f, element.default_val, element.short_name.data());
 	}
-	void config_element(Toggle2pos element) {
+	void config_element(Toggle2pos element)
+	{
 		module->configParam(element.idx, 0.f, 1.f, 0.f, element.short_name.data());
 	};
-	void config_element(Toggle3pos element) {
+	void config_element(Toggle3pos element)
+	{
 		module->configParam(element.idx, 0.f, 2.f, 0.f, element.short_name.data());
-		//scaleFactor = 1.f/2.f;
+		// scaleFactor = 1.f/2.f;
 	};
-	void config_element(LEDEncoder element) {
+	void config_element(LEDEncoder element)
+	{
 		module->configParam(element.idx, -INFINITY, INFINITY, 0.0f, element.short_name.data());
 	};
 
