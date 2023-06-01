b43567c39d33bf9606a2fc97dc8852b59b8d8ee8
Dan Green <danngreen1@gmail.com>
2023-06-01 11:07:39 -0700

    DualAtenuverter_info: combine elements into one Elements array. Fix SVG path

--- shared/CoreModules/info/DualAtenuverter_info.hh
+++ shared/CoreModules/info/DualAtenuverter_info.hh
@@ -24,30 +24,22 @@ struct DualAtenuverterInfo : ElementInfoBase {
 	static constexpr std::string_view slug{"DualAtenuverter"};
 	static constexpr std::string_view description{"Befaco Dual Attenuvertor"};
 	static constexpr uint32_t width_hp = 4;
-	static constexpr std::string_view svg_filename{"res/modules/befaco/DualAtenuverter.svg"};
+	static constexpr std::string_view svg_filename{"res/modules/Befaco/DualAtenuverter.svg"};
 
 	// Regex to convert addParam(...) or addInput/Output() to *Elements:
 	// s/add\w\+(create\w\+<\(.*\)>(Vec(\(\d\+\), \(\d\+\)), .*::\(.*\)_\(\w\+\).*/\t\t\1{\2, \3, "\4", "", \4_\5},/
 	// Then manually add the names from Module::configParam()
 	// and manually split into arrays for each type of element
-	static constexpr std::array<KnobElement, 4> Knobs{{
+
+	static constexpr std::array<Element, 10> Elements{{
 		Davies1900hWhiteKnob{20, 33, "Ch 1 gain", "", ATEN1_PARAM},
 		Davies1900hRedKnob{20, 91, "Ch 1 offset", "", OFFSET1_PARAM},
 		Davies1900hWhiteKnob{20, 201, "Ch 2 gain", "", ATEN2_PARAM},
 		Davies1900hRedKnob{20, 260, "Ch 2 offset", "", OFFSET2_PARAM},
-	}};
-
-	static constexpr std::array<InJackElement, 2> InJacks{{
 		BefacoInputPort{7, 152, "IN1", "", IN1_INPUT},
 		BefacoInputPort{7, 319, "IN2", "", IN2_INPUT},
-	}};
-
-	static constexpr std::array<OutJackElement, 2> OutJacks{{
 		BefacoOutputPort{43, 152, "OUT1", "", OUT1_OUTPUT},
 		BefacoOutputPort{43, 319, "OUT2", "", OUT2_OUTPUT},
-	}};
-
-	static constexpr std::array<LightElement, 2> Leds{{
 		MediumLight<RedGreenBlueLight>{33, 143, "OUT1", "", OUT1_LIGHT},
 		MediumLight<RedGreenBlueLight>{33, 311, "OUT2", "", OUT2_LIGHT},
 	}};
