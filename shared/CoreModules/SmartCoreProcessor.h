aba7696b5e4f57b9432c8ed43c039e1686ae21cf
Dan Green <danngreen1@gmail.com>
2023-06-01 09:32:39 -0700

    SmartCoreProcessor checks all accesses

--- shared/CoreModules/SmartCoreProcessor.h
+++ shared/CoreModules/SmartCoreProcessor.h
@@ -3,93 +3,84 @@
 #include <array>
 #include <optional>
 
-
-template <typename INFO>
-class SmartCoreProcessor : public CoreProcessor
-{
+template<typename INFO>
+class SmartCoreProcessor : public CoreProcessor {
 public:
-	SmartCoreProcessor()
-	{
-	}	
-
-protected:
-	void setOutput(int i, float val)
-	{
-		outputValues[i] = val;
-	}
-
-	void setLED(int i, float val)
-	{
-		ledValues[i] = val;
-	}
-
-	std::optional<float> getInput(int i)
-	{
-		auto result = inputValues[i];
-		inputValues[i].reset();
-		return result;
-	}
-
-	float getKnob(int i)
-	{
-		return knobValues[i];
-	}
-
-	float getSwitch(int i)
-	{
-		return switchValues[i];
+	SmartCoreProcessor() {
 	}
 
 protected:
-
-	float get_output(int output_id) const override
-	{
-		return outputValues[output_id] / 5.0f;
+	void setOutput(int i, float val) {
+		if (i < (int)outputValues.size())
+			outputValues[i] = val;
 	}
 
-	void set_input(int input_id, float val) override
-	{
-		inputValues[input_id] = val * 5.0f;
+	void setLED(int i, float val) {
+		if (i < (int)ledValues.size())
+			ledValues[i] = val;
 	}
 
-	void set_param(int param_id, float val) override
-	{
-		// Here we are assuming how params are merged in a different place
-		if (param_id < knobValues.size()) 
-		{
-			
-			knobValues[param_id] = val;
+	std::optional<float> getInput(int i) {
+		if (i < (int)inputValues.size()) {
+			auto result = inputValues[i];
+			inputValues[i].reset();
+			return result;
 		}
-		else
-		{
+		return {};
+	}
+
+	float getKnob(int i) {
+		if (i < (int)knobValues.size())
+			return knobValues[i];
+		return 0.f;
+	}
+
+	float getSwitch(int i) {
+		if (i < (int)switchValues.size())
+			return switchValues[i];
+		return 0.f;
+	}
+
+protected:
+	float get_output(int output_id) const override {
+		if (output_id < (int)outputValues.size())
+			return outputValues[output_id] / 5.0f;
+		return 0.f;
+	}
+
+	void set_input(int input_id, float val) override {
+		if (input_id < (int)inputValues.size())
+			inputValues[input_id] = val * 5.0f;
+	}
+
+	void set_param(int param_id, float val) override {
+		// Here we are assuming how params are merged in a different place
+		if (param_id < (int)knobValues.size()) {
+
+			knobValues[param_id] = val;
+		} else {
 			param_id -= knobValues.size();
-			if (param_id < switchValues.size())
-			{
+			if (param_id < (int)switchValues.size()) {
 				switchValues[param_id] = val;
 			}
 		}
 	}
 
-	float get_led_brightness(int led_id) const override
-	{
+	float get_led_brightness(int led_id) const override {
 		// ignores switches with light for now
-		if (led_id < ledValues.size())
-		{
-			return ledValues[led_id];	
-		}
-		else
-		{
+		if (led_id < (int)ledValues.size()) {
+			return ledValues[led_id];
+		} else {
 			return 0.0f;
 		}
 	}
 
 private:
-
-	std::array<float,INFO::Knobs.size()> knobValues;
-	std::array<std::optional<float>,INFO::InJacks.size()> inputValues;
-	std::array<float,INFO::OutJacks.size()> outputValues;
-	std::array<float,INFO::Switches.size()> switchValues;
+	std::array<float, INFO::Knobs.size()> knobValues;
+	std::array<std::optional<float>, INFO::InJacks.size()> inputValues;
+	std::array<float, INFO::OutJacks.size()> outputValues;
+	std::array<float, INFO::Switches.size()> switchValues;
 
 	// ignores switches with light for now
-	std::array<float,INFO::Leds.size()> ledValues;	
+	std::array<float, INFO::Leds.size()> ledValues;
 };
