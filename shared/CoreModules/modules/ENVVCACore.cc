b195cd08651121bbb8b029706d9b527037014c48
Dan Green <danngreen1@gmail.com>
2023-05-31 21:53:19 -0700

    Temp: Remove ModuleFactory registration in ENVVCACore

--- shared/CoreModules/modules/ENVVCACore.cc
+++ shared/CoreModules/modules/ENVVCACore.cc
@@ -214,8 +214,8 @@ public:
 
 	// Boilerplate to auto-register in ModuleFactory
 	// clang-format off
-	static std::unique_ptr<CoreProcessor> create() { return std::make_unique<ThisCore>(); }
-	static inline bool s_registered = ModuleFactory::registerModuleType(Info::slug, create, ModuleInfoView::makeView<Info>());
+	// static std::unique_ptr<CoreProcessor> create() { return std::make_unique<ThisCore>(); }
+	// static inline bool s_registered = ModuleFactory::registerModuleType(Info::slug, create, ModuleInfoView::makeView<Info>());
 	// clang-format on
 
 private:
