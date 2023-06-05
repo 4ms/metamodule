0d3ce91f6ddec91be0029c27628561a125f752ae
Dan Green <danngreen1@gmail.com>
2023-06-04 17:32:08 -0700

    Fix center/top-left coordinate confusion

--- firmware/src/pages/element_helpers.hh
+++ firmware/src/pages/element_helpers.hh
@@ -143,39 +143,50 @@ struct ElementDrawer {
 	lv_obj_t *canvas;
 
 	lv_obj_t *draw_element(const BaseElement &, const lv_img_dsc_t *) {
+		pr_dbg("draw_element(BaseElement), not found\n");
 		return nullptr;
 	}
 
 	// Pot -- create lvgl image object so it can be animated later
 	lv_obj_t *draw_element(const Pot &el, const lv_img_dsc_t *img) {
-		if (!img)
+		if (!img) {
+			pr_dbg("draw_element(Pot), image not found\n");
 			return nullptr;
+		}
 		uint32_t width = img->header.w;
 		uint32_t height = img->header.h;
-		uint32_t left = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height) - width / 2.f);
-		uint32_t top = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height) - height / 2.f);
+		// if x,y are top-left corner:
+		uint32_t left = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height));
+		uint32_t top = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height));
+		// if x,y are center:
+		// uint32_t left = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height) - width / 2.f);
+		// uint32_t top = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height) - height / 2.f);
 
 		lv_obj_t *obj = lv_img_create(canvas);
 		lv_img_set_src(obj, img);
 		lv_obj_set_pos(obj, left, top);
 		lv_img_set_pivot(obj, width / 2, height / 2);
 		lv_obj_add_style(obj, &Gui::mapped_knob_style, LV_PART_MAIN);
-
+		pr_dbg("Draw pot at %d, %d (w:%d h:%d) module_height=%d\n", left, top, width, height, module_height);
 		return obj;
 	}
 
 	// Jack -- draw knob on canvas
 	lv_obj_t *draw_element(const JackElement &el, const lv_img_dsc_t *img) {
-		if (!img)
+		if (!img) {
+			pr_dbg("draw_element(JackElement), image not found\n");
 			return nullptr;
+		}
 		uint32_t width = img->header.w;
 		uint32_t height = img->header.h;
-		uint32_t left = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height) - width / 2.f);
-		uint32_t top = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height) - height / 2.f);
+		uint32_t left = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height));
+		uint32_t top = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height));
 
 		lv_draw_img_dsc_t draw_img_dsc;
 		lv_draw_img_dsc_init(&draw_img_dsc);
 		lv_canvas_draw_img(canvas, left, top, img, &draw_img_dsc);
+
+		pr_dbg("Draw jack at %d, %d (w:%d h:%d) module_height=%d\n", left, top, width, height, module_height);
 
 		return nullptr;
 	}
@@ -200,8 +211,8 @@ struct MapRingDrawer {
 		float scale = module_height / 240.f;
 		float radius = img->header.w * 0.5f + ring_offset * scale;
 
-		uint16_t c_x = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height));
-		uint16_t c_y = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height));
+		uint16_t c_x = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height) + img->header.w / 2.f);
+		uint16_t c_y = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height) + img->header.h / 2.f);
 
 		ring.color = Gui::knob_palette[panel_knob_id % 6];
 		// TODO: how to draw MIDI maps?
@@ -236,8 +247,8 @@ struct MapRingDrawer {
 	}
 
 	void draw_jack_ring(const JackElement &el, const lv_img_dsc_t *img, uint32_t panel_jack_id) {
-		uint16_t c_x = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height));
-		uint16_t c_y = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height));
+		uint16_t c_x = std::round(ElementInfoBase::mm_to_px(el.x_mm, module_height) + img->header.w / 2.f);
+		uint16_t c_y = std::round(ElementInfoBase::mm_to_px(el.y_mm, module_height) + img->header.h / 2.f);
 
 		Gui::mapped_jack_small_arcdsc.color = Gui::palette_main[panel_jack_id % 8];
 		uint16_t radius = (img->header.w + 6) / 2;
@@ -285,6 +296,14 @@ struct MappedElement {
 		if (!element_img)
 			return;
 		if (auto mapped_jack = patch.find_mapped_injack(Jack{(uint16_t)module_idx, (uint16_t)el.idx})) {
+			MapRingDrawer{module_height, canvas}.draw_jack_ring(el, element_img, mapped_jack->panel_jack_id);
+		}
+	}
+
+	void operator()(const JackOutput &el) {
+		if (!element_img)
+			return;
+		if (auto mapped_jack = patch.find_mapped_outjack(Jack{(uint16_t)module_idx, (uint16_t)el.idx})) {
 			MapRingDrawer{module_height, canvas}.draw_jack_ring(el, element_img, mapped_jack->panel_jack_id);
 		}
 	}
