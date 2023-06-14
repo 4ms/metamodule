cc01c7106d787e38b64c95217a1cac0cee7ad438
Dan Green <danngreen1@gmail.com>
2023-06-13 19:22:09 -0700

    Cleanup DualAtenuverter info and add explicit comments

--- shared/CoreModules/info/DualAtenuverter_info.hh
+++ shared/CoreModules/info/DualAtenuverter_info.hh
@@ -2,8 +2,8 @@
 #include "CoreModules/elements.hh"
 #include <array>
 
-// TODO: get these out of global namespace
 // Enums copied from VCV Module:
+// They're not strictly necessary but they make the regex easier and improve readability
 enum ParamIds { ATEN1_PARAM, OFFSET1_PARAM, ATEN2_PARAM, OFFSET2_PARAM, NUM_PARAMS };
 enum InputIds { IN1_INPUT, IN2_INPUT, NUM_INPUTS };
 enum OutputIds { OUT1_OUTPUT, OUT2_OUTPUT, NUM_OUTPUTS };
@@ -13,17 +13,25 @@ namespace MetaModule
 {
 
 struct DualAtenuverterInfo : ElementInfoBase {
-	// Module info: written by hand
+
+	// Slug must match VCV's slug
 	static constexpr std::string_view slug{"DualAtenuverter"};
+
+	// Description shows up in MetaModule, can be anything
 	static constexpr std::string_view description{"Befaco Dual Attenuvertor"};
+
+	// Width is used by MetaModule
 	static constexpr uint32_t width_hp = 4;
+
+	// This is only used with the ElementInfo-to-VCV wrapper (vcv/generic_module_new.hh)
 	static constexpr std::string_view svg_filename{"res/modules/Befaco/DualAtenuverter.svg"};
 
+	// Array of all module elements:
+	//
 	// vim regex to convert addParam(...) or addInput/Output() to Element:
+	// s/add\w\+(create\w\+<\(.*\)>(Vec(\(\d\+\), \(\d\+\)), .*::\(.*\)_\(\w\+\).*/\t\t\1{to_mm(\2), to_mm(\3), "\4", "", \4_\5},/
 	//
-	// s/add\w\+(create\w\+<\(.*\)>(Vec(\(\d\+\), \(\d\+\)), .*::\(.*\)_\(\w\+\).*/\t\t\1{\2, \3, "\4", "", \4_\5},/
-	//
-	// Then manually add the human names, min/max/default values from Module::configParam()
+	// Then manually edit the text names and add the min/max/default values from Module::configParam()
 
 	static constexpr std::array<Element, 10> Elements{{
 		Davies1900hWhiteKnob{to_mm(20), to_mm(33), "Ch 1 gain", "", ATEN1_PARAM, -1.0, 1.0, 0.0},
@@ -37,11 +45,6 @@ struct DualAtenuverterInfo : ElementInfoBase {
 		MediumLight<RedGreenBlueLight>{to_mm(33), to_mm(143), "OUT1", "", OUT1_LIGHT},
 		MediumLight<RedGreenBlueLight>{to_mm(33), to_mm(311), "OUT2", "", OUT2_LIGHT},
 	}};
-
-	// static constexpr std::span<const Element> Knobs{&Elements[0], 4};
-	static constexpr std::span<const Element> InJacks{&Elements[4], 2};
-	static constexpr std::span<const Element> OutJacks{&Elements[6], 2};
-	static constexpr std::span<const Element> Switches{};
-	static constexpr std::span<const Element> Lights{&Elements[8], 2};
 };
+
 } // namespace MetaModule
