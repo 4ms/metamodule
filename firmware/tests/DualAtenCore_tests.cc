451fc882e4cf9c2c1fb063e6cd40d12b646c1e49
Dan Green <danngreen1@gmail.com>
2023-06-14 21:21:54 -0700

    Fix tests for ElementCounter

--- firmware/tests/DualAtenCore_tests.cc
+++ firmware/tests/DualAtenCore_tests.cc
@@ -1,19 +1,25 @@
-#include "CoreModules/Befaco/DualAtenuverterCore.hh"
+#include "CoreModules/element_counter.hh"
+#include "info/DualAtenuverter_info.hh"
 
-// must define this to avoid linker error missing vtable for this class
-void DualAtenuverterCore::process(const ProcessArgs &args) {
-}
+namespace
+{
+using Info = MetaModule::DualAtenuverterInfo;
+constexpr static typename ElementCount<Info>::Counts counts = ElementCount<Info>::count();
+static_assert(counts.num_params == 4);
+static_assert(counts.num_inputs == 2);
+static_assert(counts.num_outputs == 2);
+static_assert(counts.num_lights == 6); //why 6?
 
-static_assert(DualAtenuverterCore::params.size() == 4);
-static_assert(DualAtenuverterCore::inputs.size() == 2);
-static_assert(DualAtenuverterCore::outputs.size() == 2);
-static_assert(DualAtenuverterCore::lights.size() == 2);
-static_assert(DualAtenuverterCore::param_scales.size() == 4);
-static_assert(DualAtenuverterCore::param_scales[0].range == 2.f);
-static_assert(DualAtenuverterCore::param_scales[0].offset == -1.f);
-static_assert(DualAtenuverterCore::param_scales[1].range == 20.f);
-static_assert(DualAtenuverterCore::param_scales[1].offset == -10.f);
-static_assert(DualAtenuverterCore::param_scales[2].range == 2.f);
-static_assert(DualAtenuverterCore::param_scales[2].offset == -1.f);
-static_assert(DualAtenuverterCore::param_scales[3].range == 20.f);
-static_assert(DualAtenuverterCore::param_scales[3].offset == -10.f);
+using ParamScale = typename ElementCount<Info>::ParamScale;
+constexpr static std::array<ParamScale, counts.num_params> param_scales = ElementCount<Info>::param_scales();
+
+static_assert(param_scales.size() == 4);
+static_assert(param_scales[0].range == 2.f);
+static_assert(param_scales[0].offset == -1.f);
+static_assert(param_scales[1].range == 20.f);
+static_assert(param_scales[1].offset == -10.f);
+static_assert(param_scales[2].range == 2.f);
+static_assert(param_scales[2].offset == -1.f);
+static_assert(param_scales[3].range == 20.f);
+static_assert(param_scales[3].offset == -10.f);
+} // namespace
