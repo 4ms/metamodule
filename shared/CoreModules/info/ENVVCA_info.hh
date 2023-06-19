3998fb06d1ef630d364c9e713cd8f6d75a96c1c8
Dan Green <danngreen1@gmail.com>
2023-06-19 09:49:16 -0700

    Less verbose names in ENVVCA_info

--- shared/CoreModules/info/ENVVCA_info.hh
+++ shared/CoreModules/info/ENVVCA_info.hh
@@ -45,18 +45,9 @@ struct ENVVCAInfo : ElementInfoBase {
 	};
 
 	static constexpr std::array<Element, 19> Elements{{
-		Slider25mmVert{
-			to_mm<72>(23.185f), to_mm<72>(108.81f), "Rise Slider", "Rise Slider", KnobRise_Slider, 0.f, 1.f, 0.25f},
-		Slider25mmVert{
-			to_mm<72>(57.325f), to_mm<72>(109.02f), "Fall Slider", "Fall Slider", KnobFall_Slider, 0.f, 1.f, 0.25f},
-		Slider25mmVert{to_mm<72>(91.505f),
-					   to_mm<72>(108.81f),
-					   "Env Level Slider",
-					   "Env Level Slider",
-					   KnobEnv_Level_Slider,
-					   0.f,
-					   1.f,
-					   1.f},
+		Slider25mmVert{to_mm<72>(23.185f), to_mm<72>(108.81f), "Rise Slider", "", KnobRise_Slider, 0.f, 1.f, 0.25f},
+		Slider25mmVert{to_mm<72>(57.325f), to_mm<72>(109.02f), "Fall Slider", "", KnobFall_Slider, 0.f, 1.f, 0.25f},
+		Slider25mmVert{to_mm<72>(91.505f), to_mm<72>(108.81f), "Env Level", "", KnobEnv_Level_Slider, 0.f, 1.f, 1.f},
 		Knob9mm{to_mm<72>(21.69f), to_mm<72>(178.25f), "Rise CV", "Rise CV", KnobRise_Cv, 0.f, 1.f, 0.f},
 		Knob9mm{to_mm<72>(92.85f), to_mm<72>(178.25f), "Fall CV", "Fall CV", KnobFall_Cv, 0.f, 1.f, 0.f},
 
