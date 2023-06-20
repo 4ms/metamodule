b2d798be719684b70ace04be1f98cd597252d340
Dan Green <danngreen1@gmail.com>
2023-06-19 17:03:57 -0700

    generate minblep table on PC, store as a table
    TODO: generate it constexpr, will need constexpr version of std::hypot() and more..

--- firmware/src/VCV-adaptor/dsp/minblep.hpp
+++ firmware/src/VCV-adaptor/dsp/minblep.hpp
@@ -1,10 +1,8 @@
 #pragma once
-// #include <dsp/common.hpp>
+#include "VCV-adaptor/dsp/minblep_16_32.h"
 #include "VCV-adaptor/math.hpp"
 
-namespace rack
-{
-namespace dsp
+namespace rack::dsp
 {
 
 /** Computes the minimum-phase bandlimited step (MinBLEP)
@@ -13,7 +11,8 @@ o: oversample factor
 output: must be length `2 * z * o`.
 https://www.cs.cmu.edu/~eli/papers/icmc01-hardsync.pdf
 */
-void minBlepImpulse(int z, int o, float *output);
+
+// void minBlepImpulse(int z, int o, float *output);
 
 template<int Z, int O, typename T = float>
 struct MinBlepGenerator {
@@ -45,5 +44,32 @@ struct MinBlepGenerator {
 	}
 };
 
-} // namespace dsp
-} // namespace rack
+template<>
+struct MinBlepGenerator<16, 32, float> {
+	static constexpr int Z = 16;
+	static constexpr int O = 32;
+	using T = float;
+
+	T buf[2 * Z] = {};
+	int pos = 0;
+
+	/** Places a discontinuity with magnitude `x` at -1 < p <= 0 relative to the current frame */
+	void insertDiscontinuity(float p, T x) {
+		if (!(-1 < p && p <= 0))
+			return;
+		for (int j = 0; j < 2 * Z; j++) {
+			float minBlepIndex = ((float)j - p) * O;
+			int index = (pos + j) % (2 * Z);
+			buf[index] += x * (-1.f + math::interpolateLinear(MinBlep_16_32.data(), minBlepIndex));
+		}
+	}
+
+	T process() {
+		T v = buf[pos];
+		buf[pos] = T(0);
+		pos = (pos + 1) % (2 * Z);
+		return v;
+	}
+};
+
+} // namespace rack::dsp
