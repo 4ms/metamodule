1e4b39c401f44806f50bd330f098782991c1c740
Dan Green <danngreen1@gmail.com>
2023-07-03 11:49:06 -0700

    Cleanup:
    Use existing `element_index` function
    
    Add missing includes
    
    Remove debug message in case of bad call to getState
    
    Set Toggle threshold to 0.5/1.5 (allows pots to be mapped to switches)
    
    apply clang-format

--- shared/CoreModules/SmartCoreProcessor.h
+++ shared/CoreModules/SmartCoreProcessor.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "CoreModules/coreProcessor.h"
 #include "CoreModules/elements/element_counter.hh"
-#include "CoreModules/elements/param_scales.hh"
 #include "CoreModules/elements/element_state_conversion.hh"
+#include "CoreModules/elements/param_scales.hh"
 #include <array>
 #include <optional>
 
@@ -46,19 +46,18 @@ protected:
 		return paramValues[idx.param_idx];
 	}
 
-	template <typename INFO::Elem EL>
-	auto getState() 
-	{
+	template<typename INFO::Elem EL>
+	auto getState() {
 		// get back the typed element from the list of elements
-		constexpr auto elementID = static_cast<size_t>(EL);
-		constexpr auto& elementRef = INFO::Elements[elementID];
+		constexpr auto elementID = element_index(EL);
+		constexpr auto &elementRef = INFO::Elements[elementID];
 
 		// read raw value
 		auto rawValue = getParam(EL);
 
 		// construct element of same type as the element the enum points to
 		constexpr auto variantIndex = elementRef.index();
-		std::variant_alternative_t<variantIndex,MetaModule::Element> DummyElement;
+		std::variant_alternative_t<variantIndex, MetaModule::Element> DummyElement;
 
 		// call conversion function for that type of element
 		auto result = MetaModule::StateConversion::convertState(DummyElement, rawValue);
