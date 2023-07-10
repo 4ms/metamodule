521fae0ad63665f117c5aa52b1afaec3197c62bf
Dan Green <danngreen1@gmail.com>
2023-07-10 10:27:44 -0700

    Add Befaco slide pot images

--- firmware/src/pages/elements/befaco_images.hh
+++ firmware/src/pages/elements/befaco_images.hh
@@ -16,6 +16,10 @@ LV_IMG_DECLARE(BefacoBigKnob_240);
 LV_IMG_DECLARE(BefacoBigKnob_120);
 LV_IMG_DECLARE(BefacoTinyKnobWhite_240);
 LV_IMG_DECLARE(BefacoTinyKnobWhite_120);
+LV_IMG_DECLARE(BefacoSlidePotSmall_240);
+LV_IMG_DECLARE(BefacoSlidePotSmall_120);
+LV_IMG_DECLARE(BefacoSlidePotHandleSmall_240);
+LV_IMG_DECLARE(BefacoSlidePotHandleSmall_120);
 
 namespace MetaModule::ElementImageImpl
 {
@@ -46,6 +50,14 @@ inline const lv_img_dsc_t *get_img(const BefacoOutputPort &, uint32_t scale_px) 
 
 inline const lv_img_dsc_t *get_img(const MediumLight<RedGreenBlueLight> &, uint32_t scale_px) {
 	return scale_px == 240 ? &MediumLight_240 : &MediumLight_120;
+}
+
+inline const lv_img_dsc_t *get_img(const BefacoSlidePot &, uint32_t scale_px) {
+	return scale_px == 240 ? &BefacoSlidePotSmall_240 : &BefacoSlidePotSmall_120;
+}
+
+inline const lv_img_dsc_t *get_img_handle(const BefacoSlidePot &, uint32_t scale_px) {
+	return scale_px == 240 ? &BefacoSlidePotHandleSmall_240 : &BefacoSlidePotHandleSmall_120;
 }
 
 } // namespace MetaModule::ElementImageImpl
