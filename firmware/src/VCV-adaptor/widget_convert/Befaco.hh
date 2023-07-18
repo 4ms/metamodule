bf0cd6069f1faf1174786e08fbdf807c7196228e
Dan Green <danngreen1@gmail.com>
2023-07-17 17:11:30 -0700

    TEST: possible work-around for widgets defined in plugin file

--- firmware/src/VCV-adaptor/widget_convert/Befaco.hh
+++ firmware/src/VCV-adaptor/widget_convert/Befaco.hh
@@ -3,7 +3,25 @@
 #include "VCV-adaptor/widget_convert/base.hh"
 #include "VCV-adaptor/widgets.hh"
 
+// #include "VCV-adaptor/window.hpp"
+// #include "VCV-adaptor/asset.hpp"
+// using namespace rack::window;
+// using namespace rack::math;
+// using namespace rack::engine;
+// extern rack::plugin::Plugin* pluginInstance;
+// #include "vcv-ports/Befaco/src/widgets.hpp"
+
 namespace rack
 {
-//
-}
+
+// template<>
+// struct ElementConvert<BefacoTinyKnobLightGrey> {
+// 	using ElementType = MetaModule::BefacoTinyKnobLightGrey;
+// };
+
+// template<>
+// struct ElementConvert<BefacoPush> {
+// 	using ElementType = MetaModule::BefacoTinyKnobLightGrey;
+// };
+
+} // namespace rack
