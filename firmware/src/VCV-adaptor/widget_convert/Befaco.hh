6d1132cbbbac50d91c6e5679566c6c0fd19586ee
Dan Green <danngreen1@gmail.com>
2023-07-17 20:50:12 -0700

    ElementConvert for Befaco widgets

--- firmware/src/VCV-adaptor/widget_convert/Befaco.hh
+++ firmware/src/VCV-adaptor/widget_convert/Befaco.hh
@@ -3,25 +3,133 @@
 #include "VCV-adaptor/widget_convert/base.hh"
 #include "VCV-adaptor/widgets.hh"
 
-// #include "VCV-adaptor/window.hpp"
-// #include "VCV-adaptor/asset.hpp"
-// using namespace rack::window;
-// using namespace rack::math;
-// using namespace rack::engine;
-// extern rack::plugin::Plugin* pluginInstance;
-// #include "vcv-ports/Befaco/src/widgets.hpp"
+// Forward declare widgets from Befaco/src/plugin.hpp
+struct Knurlie;
+struct BefacoTinyKnobWhite;
+struct BefacoTinyKnobRed;
+struct BefacoTinyKnobDarkGrey;
+struct BefacoTinyKnobLightGrey;
+struct BefacoTinyKnobBlack;
+struct Davies1900hLargeGreyKnob;
+struct Davies1900hLightGreyKnob;
+struct Davies1900hDarkGreyKnob;
+struct BananutRed;
+struct BananutBlack;
+struct CKSSNarrow;
+struct Crossfader;
+struct BefacoSwitchHorizontal;
+struct CKSSHoriz2;
+struct CKSSVert7;
+struct CKSSHoriz4;
+struct CKSSNarrow3;
+struct Davies1900hLargeLightGreyKnob;
+struct BefacoSlidePotSmall;
 
 namespace rack
 {
 
-// template<>
-// struct ElementConvert<BefacoTinyKnobLightGrey> {
-// 	using ElementType = MetaModule::BefacoTinyKnobLightGrey;
-// };
+template<>
+struct ElementConvert<Knurlie> {
+	using ElementType = MetaModule::NullElement;
+};
 
-// template<>
-// struct ElementConvert<BefacoPush> {
-// 	using ElementType = MetaModule::BefacoTinyKnobLightGrey;
-// };
+template<>
+struct ElementConvert<BefacoTinyKnobWhite> {
+	using ElementType = MetaModule::BefacoTinyKnob;
+};
+
+template<>
+struct ElementConvert<BefacoTinyKnobRed> {
+	using ElementType = MetaModule::BefacoTinyKnob;
+};
+
+template<>
+struct ElementConvert<BefacoTinyKnobDarkGrey> {
+	using ElementType = MetaModule::BefacoTinyKnob;
+};
+
+template<>
+struct ElementConvert<BefacoTinyKnobLightGrey> {
+	using ElementType = MetaModule::BefacoTinyKnob;
+};
+
+template<>
+struct ElementConvert<BefacoTinyKnobBlack> {
+	using ElementType = MetaModule::BefacoTinyKnob;
+};
+
+template<>
+struct ElementConvert<Davies1900hLargeGreyKnob> {
+	using ElementType = MetaModule::Davies1900hLargeGreyKnob;
+};
+
+template<>
+struct ElementConvert<Davies1900hLightGreyKnob> {
+	using ElementType = MetaModule::Davies1900hLightGreyKnob;
+};
+
+template<>
+struct ElementConvert<Davies1900hDarkGreyKnob> {
+	using ElementType = MetaModule::Davies1900hDarkGreyKnob;
+};
+
+// using Davies1900hDarkBlackAlt = Davies1900hBlackKnob;
+
+template<>
+struct ElementConvert<BananutRed> {
+	using ElementType = MetaModule::BefacoOutputPort;
+};
+// using BefacoOutputPort = BananutRed;
+
+template<>
+struct ElementConvert<BananutBlack> {
+	using ElementType = MetaModule::BefacoInputPort;
+};
+// using BefacoInputPort = BananutBlack;
+
+template<>
+struct ElementConvert<CKSSNarrow> {
+	using ElementType = MetaModule::BefacoSlidePotSmall;
+};
+
+template<>
+struct ElementConvert<Crossfader> {
+	using ElementType = MetaModule::Slider25mmHoriz;
+};
+
+template<>
+struct ElementConvert<BefacoSwitchHorizontal> {
+	using ElementType = MetaModule::BefacoSwitchHorizontal;
+};
+
+template<>
+struct ElementConvert<CKSSHoriz2> {
+	using ElementType = MetaModule::NullElement;
+};
+
+template<>
+struct ElementConvert<CKSSVert7> {
+	using ElementType = MetaModule::NullElement;
+};
+
+template<>
+struct ElementConvert<CKSSHoriz4> {
+	using ElementType = MetaModule::NullElement;
+};
+
+template<>
+struct ElementConvert<CKSSNarrow3> {
+	using ElementType = MetaModule::NullElement;
+};
+
+template<>
+struct ElementConvert<Davies1900hLargeLightGreyKnob> {
+	using ElementType = MetaModule::Davies1900hLargeLightGreyKnob;
+};
+
+template<>
+struct ElementConvert<BefacoSlidePotSmall> {
+	using ElementType = MetaModule::BefacoSlidePotSmall;
+};
 
 } // namespace rack
