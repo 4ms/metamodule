7b005e5d78f0af2cadec6331cc6bcb53dc8b23e1
Dan Green <danngreen1@gmail.com>
2023-07-18 17:04:57 -0700

    Fix BraidsInfo

--- shared/CoreModules/AudibleInstruments/info/Braids_info.hh
+++ shared/CoreModules/AudibleInstruments/info/Braids_info.hh
@@ -15,20 +15,20 @@ struct BraidsInfo : ModuleInfoBase {
     using enum Coords;
 
 	static constexpr std::array<Element, 14> Elements{{
-		Rogan2PSWhite{to_mm(19), to_mm(138), Center, "Fine", "Fine Freq"},
-		Rogan2PSWhite{to_mm(97), to_mm(138), Center, "Coarse", "Coarse Freq"},
-		Rogan2PSWhite{to_mm(176), to_mm(138), Center, "FM", "" },
-		Rogan2PSGreen{to_mm(19), to_mm(217), Center, "Timbre", ""},
-		Rogan2PSGreen{to_mm(97), to_mm(217), Center, "Modulation", ""},
-		Rogan2PSRed{to_mm(176), to_mm(217), Center, "Color", ""},
-		Rogan2SGray{to_mm(176), to_mm(59), Center, "Model", ""},
-		PJ301MPortIn{to_mm(10), to_mm(316), Center, "Trigger", ""},
-		PJ301MPortIn{to_mm(47), to_mm(316), Center, "Pitch", "Pitch (1V/oct)"},
-		PJ301MPortIn{to_mm(84), to_mm(316), Center, "FM", ""},
-		PJ301MPortIn{to_mm(122), to_mm(316), Center, "Timbre", ""},
-		PJ301MPortIn{to_mm(160), to_mm(316), Center, "Color", ""},
-		PJ301MPortOut{to_mm(205), to_mm(316), Center, "Out", ""},
-		BraidsDisplay148x56{to_mm(14), to_mm(53), Center, "", ""},
+		Rogan2PSWhite{to_mm(19), to_mm(138), TopLeft, "Fine", "Fine Freq"},
+		Rogan2PSWhite{to_mm(97), to_mm(138), TopLeft, "Coarse", "Coarse Freq"},
+		Rogan2PSWhite{to_mm(176), to_mm(138), TopLeft, "FM", "" },
+		Rogan2PSGreen{to_mm(19), to_mm(217), TopLeft, "Timbre", ""},
+		Rogan2PSGreen{to_mm(97), to_mm(217), TopLeft, "Modulation", ""},
+		Rogan2PSRed{to_mm(176), to_mm(217), TopLeft, "Color", ""},
+		Rogan2SGray{to_mm(176), to_mm(59), TopLeft, "Model", ""},
+		PJ301MPortIn{to_mm(10), to_mm(316), TopLeft, "Trigger", ""},
+		PJ301MPortIn{to_mm(47), to_mm(316), TopLeft, "Pitch", "Pitch (1V/oct)"},
+		PJ301MPortIn{to_mm(84), to_mm(316), TopLeft, "FM", ""},
+		PJ301MPortIn{to_mm(122), to_mm(316), TopLeft, "Timbre", ""},
+		PJ301MPortIn{to_mm(160), to_mm(316), TopLeft, "Color", ""},
+		PJ301MPortOut{to_mm(205), to_mm(316), TopLeft, "Out", ""},
+		BraidsDisplay148x56{to_mm(14), to_mm(53), TopLeft, "", ""},
 	}};
 };
 } // namespace MetaModule
