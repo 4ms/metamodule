a604e67254df303d41ea514bba90f82eda8d8d63
Dan Green <danngreen1@gmail.com>
2023-07-19 08:00:39 -0700

    Organize component SVGs (WIP)

--- firmware/makefile_component_images.mk
+++ firmware/makefile_component_images.mk
@@ -13,11 +13,11 @@ $(comp_png_c): %.c : %.png
 # brands = Rack Befaco AudibleInstruments 
 
 # rack_dir ?= $(RACK_DIR)/../Rack/res/ComponentLibrary
-# befaco_dir ?= $(RACK_DIR)/../Befaco/res/components
+befaco_dir ?= ../graphics/Befaco/res/components
 # audible_dir ?= $(RACK_DIR)/../AudibleInstruments/res/components
 
 # rack_comp_svgs := BefacoBigKnob BefacoPush_0 BefacoSlidePot BefacoSwitch_1 CKSSThreeHorizontal_1 CKSS_1
 
-# befaco_comp_svgs := BananutBlack BananutRed BefacoButton_0 BefacoSlidePotSmall BefacoSwitchHoriz_1 Davies1900hBlack Davies1900hDarkGrey
-# befaco_comp_svgs += Davies1900hLargeGrey Davies1900hLargeLightGrey Davies1900hLightGrey SwitchNarrowHoriz_1 SwitchNarrow_1
+befaco_comp_svgs := BananutBlack BananutRed BefacoButton_0 BefacoSlidePotSmall BefacoSwitchHoriz_1 Davies1900hBlack Davies1900hDarkGrey
+befaco_comp_svgs += Davies1900hLargeGrey Davies1900hLargeLightGrey Davies1900hLightGrey SwitchNarrowHoriz_1 SwitchNarrow_1
 
