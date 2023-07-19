01ab7eba941c3cc49849c9a4b476fc43c2acc143
Dan Green <danngreen1@gmail.com>
2023-07-18 17:05:30 -0700

    WIP: component image makefile

---
+++ firmware/makefile_component_images.mk
@@ -0,0 +1,12 @@
+brands = Rack Befaco AudibleInstruments 
+#TODO:
+#foreach brand...
+
+rack_dir ?= $(RACK_DIR)/../Rack/res/ComponentLibrary
+befaco_dir ?= $(RACK_DIR)/../Befaco/res/components
+audible_dir ?= $(RACK_DIR)/../AudibleInstruments/res/components
+
+rack_comp_svgs := BefacoBigKnob BefacoPush_0 BefacoSlidePot BefacoSwitch_1 CKSSThreeHorizontal_1 CKSS_1
+
+befaco_comp_svgs := BananutBlack BananutRed BefacoButton_0 BefacoSlidePotSmall BefacoSwitchHoriz_1 Davies1900hBlack Davies1900hDarkGrey
+befaco_comp_svgs += Davies1900hLargeGrey Davies1900hLargeLightGrey Davies1900hLightGrey SwitchNarrowHoriz_1 SwitchNarrow_1
