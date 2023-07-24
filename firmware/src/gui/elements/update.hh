c7fb6913ba3263bacf6a5ddcae5aef0e095aa8f8
Dan Green <danngreen1@gmail.com>
2023-07-23 20:53:32 -0700

    Simple map ring animation when param updated

--- firmware/src/gui/elements/update.hh
+++ firmware/src/gui/elements/update.hh
@@ -43,8 +43,21 @@ update_element(const Knob &element, const Params &params, const PatchData &patch
 
 		if (std::abs(angle - cur_angle) > 10) {
 			lv_img_set_angle(gui_el.obj, angle);
-			if (gui_el.map_ring)
-				lv_obj_set_style_outline_opa(gui_el.map_ring, LV_OPA_50, LV_STATE_DEFAULT);
+			if (gui_el.map_ring) {
+				lv_anim_t a;
+				lv_anim_init(&a);
+				lv_anim_set_exec_cb(
+					&a, (lv_anim_exec_xcb_t)[](void *var, int32_t val) {
+						lv_obj_set_style_outline_opa((lv_obj_t *)var, val, LV_STATE_DEFAULT);
+					});
+				lv_anim_set_var(&a, gui_el.map_ring);
+				lv_anim_set_time(&a, 500);
+				lv_anim_set_values(&a, LV_OPA_50, LV_OPA_0);
+				lv_anim_set_delay(&a, 200);
+				lv_anim_set_path_cb(&a, lv_anim_path_ease_in);
+				lv_anim_set_early_apply(&a, true);
+				lv_anim_start(&a);
+			}
 		}
 	}
 }
