5b77b7f909a3714f086ce5338b4fc5c85c23d45f
Dan Green <danngreen1@gmail.com>
2023-08-08 20:52:30 -0700

    sim: Can pass a path to set the path to patch files

--- simulator/src/main.cc
+++ simulator/src/main.cc
@@ -2,16 +2,30 @@
 #include "lv_port_indev.h"
 #include "lvgl.h"
 #include "ui.hh"
+#include <iostream>
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	// Set zoom to startup with a larger window (zoomed in)
-	int zoom = 1;
+	int zoom = 2;
+
+	// First argument is path to dir with patch files
+	std::string_view patch_path{""};
+
+	if (argc > 1) {
+		patch_path = argv[1];
+	}
+
+	if (patch_path.length() == 0) {
+		std::cout << "No path to patch dir specified, using '../shared/patch/'"
+				  << "\n";
+		patch_path = "../shared/patch/";
+	}
 
 	lv_init();
 	lv_port_disp_init(320, 240, zoom);
 
 	MetaModule::PatchPlayer patch_player;
-	MetaModule::PatchStorageProxy patch_storage{"../shared/patch/"};
+	MetaModule::PatchStorageProxy patch_storage{patch_path};
 	MetaModule::PatchPlayLoader patch_playloader{patch_storage, patch_player};
 	MetaModule::PatchModQueue patch_mod_queue;
 	MetaModule::Ui ui{patch_playloader, patch_storage, patch_mod_queue};
