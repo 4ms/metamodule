e0a1e9ff0a2c3050ec500041a4a9bbbde0102a10
Lennart <Lennart@binarylabs.dev>
2023-08-09 14:30:44 +0200

    envvca: adapt voltage to frequency mapping to match ranges from manual

--- shared/CoreModules/4ms/core/ENVVCACore.cc
+++ shared/CoreModules/4ms/core/ENVVCACore.cc
@@ -24,29 +24,41 @@ struct VoltageToFreqTableRange
 };
 constinit auto VoltageToFrequencyTable = Mapping::LookupTable_t<50>::generate<VoltageToFreqTableRange>([](auto voltage)
 {
+	// voltage offset to not calculate with negative values during fitting
+	// once the table is created, we can lookup negative voltages values just fine
+	constexpr double VoltageOffset = 0.1;
+
     // two points in the V->f curve
-    constexpr double V_1 = 0.4;
-    constexpr double f_1 = 0.009;
-    constexpr double V_2 = 0.06;
-    constexpr double f_2 = 1000.0;
+	// taken from voltages from the model at specific switch positions and expected frequencies from the manual
+    constexpr double V_1 = 0.31527 + VoltageOffset;
+    constexpr double f_1 = 1.0 / (5 * 60) / 2;
+    constexpr double V_2 = -0.055331 + VoltageOffset;
+    constexpr double f_2 = 800 * 2;
 
-    // std::pow is not required to be constexpr by the standard
-    // so this might not work in clang
-    constexpr double ArgScalingFactor = 10.0;
-    constexpr double arg = gcem::log2(f_1 / f_2) / (V_1 - V_2);
-    constexpr double b = gcem::pow(2.0f, arg / ArgScalingFactor);
-    constexpr double a = f_1 / gcem::pow(gcem::pow(2.0, arg), V_1);
+	// helper constant to limit ranges of intermediate values and improve precision
+	constexpr double ArgScalingFactor = 100.0;
 
-	auto coreFunc = [a,b,ArgScalingFactor](auto voltage) -> float
+    // This does not work in clang because math functions are not constexpr
+    // It doesn't work with gcem either because of numeric problems
+    // constexpr double arg = std::log2(f_1 / f_2) / (V_1 - V_2);
+    // constexpr double b = std::pow(2.0f, arg / ArgScalingFactor);
+    // constexpr double a = f_1 / std::pow(std::pow(2.0, arg), V_1);
+	
+    // So we just hardcode the factors that GCC produces for now
+    constexpr double a = 8.4172569220933146e+3;
+    constexpr double b = 6.8957132479261685e-1;
+
+	auto coreFunc = [](auto voltage) -> float
 	{
 		return float(gcem::pow(b, double(voltage) * ArgScalingFactor) * a);
 	};
 
+	// make sure the fitting is correct
 	static_assert(std::abs(coreFunc(V_1)- f_1)/f_1 < 1e-2f);
 	static_assert(std::abs(coreFunc(V_2)- f_2)/f_2 < 1e-2f);
 
     // interpolate
-    auto frequency = coreFunc(voltage);
+    auto frequency = coreFunc(voltage + VoltageOffset);
 
     return frequency;
 });
@@ -74,7 +86,9 @@ public:
 			frequency = std::clamp(frequency, 1.0f/(60 * 3), 20e3f);
 
 			// convert to period length
-			return 1.0f / frequency;
+			auto time = 1.0f / frequency;
+
+			return time;
 		};
 
 		osc.setRiseTimeInS(VoltageToTime(riseCV));
