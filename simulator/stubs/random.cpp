ec50f42cad227883e44a9d5c44fcd73fed5b788f
Dan Green <danngreen1@gmail.com>
2023-08-16 11:30:22 -0700

    Add random.cpp/hpp to VCV Adaptor
    Implement random seed differently for simulator and firmware
    
    firmware: VCV Adaptor random::init() uses hardware RNG

---
+++ simulator/stubs/random.cpp
@@ -0,0 +1,29 @@
+#include "VCV_adaptor/random.hpp"
+#include <time.h>
+
+namespace rack::random
+{
+
+static Xoroshiro128Plus rng;
+
+void init() {
+	// Don't reset state if already seeded
+	if (rng.isSeeded())
+		return;
+
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+
+	rng.seed(ts.tv_sec, ts.tv_nsec);
+
+	// Shift state a few times due to low seed entropy
+	for (int i = 0; i < 4; i++) {
+		rng();
+	}
+}
+
+Xoroshiro128Plus &local() {
+	return rng;
+}
+
+} // namespace rack::random
