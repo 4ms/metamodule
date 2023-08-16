a0d1d9ada0903734a48f8ac364d1361b2033d1f5
Dan Green <danngreen1@gmail.com>
2023-08-15 21:49:56 -0700

    Cleanup modules.mk

--- firmware/vcv_ports/glue/hetrickcv/modules.mk
+++ firmware/vcv_ports/glue/hetrickcv/modules.mk
@@ -5,91 +5,74 @@ hetrickcv_modules += BinaryGate
 hetrickcv_modules += BinaryNoise
 hetrickcv_modules += Bitshift
 hetrickcv_modules += Boolean3
-
-# Slugs:
 hetrickcv_modules += Chaos1Op
 hetrickcv_modules += Chaos2Op
 hetrickcv_modules += Chaos3Op
-# Faceplate svg names originally started with a number, 
-# But struct names manually changed to match module name:
-# hetrickcv_modules += 1OpChaos
-# hetrickcv_modules += 2OpChaos
-# hetrickcv_modules += 3OpChaos
-
 hetrickcv_modules += ChaoticAttractors
 hetrickcv_modules += ClockedNoise
 hetrickcv_modules += Comparator
 hetrickcv_modules += Contrast
-hetrickcv_modules += Crackle
-hetrickcv_modules += DToA
-hetrickcv_modules += DataCompander
-hetrickcv_modules += Delta
-hetrickcv_modules += Dust
-hetrickcv_modules += Exponent
-hetrickcv_modules += FBSineChaos
-hetrickcv_modules += FlipFlop
-hetrickcv_modules += FlipPan
-hetrickcv_modules += GateDelay
-hetrickcv_modules += GateJunction
 
-# Slug:
+########### above this line compiles ############3
+
+# hetrickcv_modules += Crackle
+# hetrickcv_modules += DToA
+# hetrickcv_modules += DataCompander
+# hetrickcv_modules += Delta
+# hetrickcv_modules += Dust
+# hetrickcv_modules += Exponent
+# hetrickcv_modules += FBSineChaos
+# hetrickcv_modules += FlipFlop
+# hetrickcv_modules += FlipPan
+# hetrickcv_modules += GateDelay
+# hetrickcv_modules += GateJunction
 # hetrickcv_modules += GateJunctionExp
-# Faceplate:
-hetrickcv_modules += GateJunctionExpanded
-
-hetrickcv_modules += Gingerbread
-
-# Slug:
+# hetrickcv_modules += Gingerbread
 # hetrickcv_modules += LogicCombine
-# Faceplate:
-hetrickcv_modules += LogicCombiner
+# hetrickcv_modules += MidSide
+# hetrickcv_modules += MinMax
+# hetrickcv_modules += PhaseDrivenSequencer
+# hetrickcv_modules += PhaseDrivenSequencer32
+# hetrickcv_modules += PhasorAnalyzer
+# hetrickcv_modules += PhasorBurstGen
+# hetrickcv_modules += PhasorDivMult
+# hetrickcv_modules += PhasorEuclidean
+# hetrickcv_modules += PhasorGates
+# hetrickcv_modules += PhasorGates32
+# hetrickcv_modules += PhasorGates64
+# hetrickcv_modules += PhasorGen
+# hetrickcv_modules += PhasorGeometry
+# hetrickcv_modules += PhasorHumanizer
+# hetrickcv_modules += PhasorOctature
+# hetrickcv_modules += PhasorQuadrature
+# hetrickcv_modules += PhasorRandom
+# hetrickcv_modules += PhasorRanger
+# hetrickcv_modules += PhasorReset
+# hetrickcv_modules += PhasorRhythmGroup
+# hetrickcv_modules += PhasorShape
+# hetrickcv_modules += PhasorShift
+# hetrickcv_modules += PhasorStutter
+# hetrickcv_modules += PhasorSubstepShape
+# hetrickcv_modules += PhasorSwing
+# hetrickcv_modules += PhasorTimetable
+# hetrickcv_modules += PhasorToClock
+# hetrickcv_modules += PhasorToLFO
+# hetrickcv_modules += PhasorToWaveforms
+# hetrickcv_modules += XYToPolar
+# hetrickcv_modules += Probability
+# hetrickcv_modules += RandomGates
+# hetrickcv_modules += Rotator
+# hetrickcv_modules += Rungler
+# hetrickcv_modules += Scanner
+# hetrickcv_modules += TwoToFour
+# hetrickcv_modules += VectorMix
+# hetrickcv_modules += Waveshape
 
-hetrickcv_modules += MidSide
-hetrickcv_modules += MinMax
-hetrickcv_modules += PhaseDrivenSequencer
-hetrickcv_modules += PhaseDrivenSequencer32
-hetrickcv_modules += PhasorAnalyzer
-hetrickcv_modules += PhasorBurstGen
-hetrickcv_modules += PhasorDivMult
-hetrickcv_modules += PhasorEuclidean
-hetrickcv_modules += PhasorGates
-hetrickcv_modules += PhasorGates32
-hetrickcv_modules += PhasorGates64
-hetrickcv_modules += PhasorGen
-hetrickcv_modules += PhasorGeometry
-hetrickcv_modules += PhasorHumanizer
-hetrickcv_modules += PhasorOctature
-hetrickcv_modules += PhasorQuadrature
-hetrickcv_modules += PhasorRandom
-hetrickcv_modules += PhasorRanger
-hetrickcv_modules += PhasorReset
-hetrickcv_modules += PhasorRhythmGroup
-hetrickcv_modules += PhasorShape
-hetrickcv_modules += PhasorShift
-hetrickcv_modules += PhasorStutter
-hetrickcv_modules += PhasorSubstepShape
-hetrickcv_modules += PhasorSwing
-hetrickcv_modules += PhasorTimetable
-hetrickcv_modules += PhasorToClock
-hetrickcv_modules += PhasorToLFO
-hetrickcv_modules += PhasorToWaveforms
-
-# Slug:
-# hetrickcv_modules += Polar
-# Faceplate:
-hetrickcv_modules += XYToPolar
-
-hetrickcv_modules += Probability
-hetrickcv_modules += RandomGates
-hetrickcv_modules += Rotator
-hetrickcv_modules += Rungler
-hetrickcv_modules += Scanner
-
-# Slug:
-hetrickcv_modules += TwoToFour
-# Faceplate original was 2To4.svg, 
-# but struct name changed to be TwoToFour
-# hetrickcv_modules += 2To4
-
-hetrickcv_modules += VectorMix
-hetrickcv_modules += Waveshape
+# Modules with module *.cpp names different from faceplate *.svg name:
+# Module is GateJunctionExp, faceplate is GateJunctionExpanded
+# Module is Polar, faceplate is XYToPolar
+# Module is LogicCombine, faceplate is LogicCombiner
+# Module is TwoToFour, faceplate is 2To4 >>> LVGL image struct name changed to match faceplate
+# Module is Chaos1Op, faceplate is 1OpChaos >>> LVGL image struct name changed to match faceplate
+# Module is Chaos2Op, faceplate is 2OpChaos >>> LVGL image struct name changed to match faceplate
+# Module is Chaos3Op, faceplate is 3OpChaos >>> LVGL image struct name changed to match faceplate
