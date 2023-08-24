0a8ff10fc0f1c9346425de73e8a7c1311e4b5239
Dan Green <danngreen1@gmail.com>
2023-08-24 10:05:40 -0700

    CMake prints error if arm-none-eabi-gcc is not found on PATH

--- firmware/cmake/arm-none-eabi-gcc.cmake
+++ firmware/cmake/arm-none-eabi-gcc.cmake
@@ -3,7 +3,8 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 
 if(MINGW
    OR CYGWIN
-   OR WIN32)
+   OR WIN32
+)
   set(UTIL_SEARCH_CMD where)
 elseif(UNIX OR APPLE)
   set(UTIL_SEARCH_CMD which)
@@ -14,7 +15,15 @@ set(TOOLCHAIN_PREFIX arm-none-eabi-)
 execute_process(
   COMMAND ${UTIL_SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
   OUTPUT_VARIABLE BINUTILS_PATH
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if("${BINUTILS_PATH}" STREQUAL "")
+  message(
+    FATAL_ERROR
+      "arm-none-eabi-gcc is not found on your PATH. Please install it and add the bin/ dir where arm-none-eabi-gcc is located to your PATH"
+  )
+endif()
 
 get_filename_component(ARM_TOOLCHAIN_DIR ${BINUTILS_PATH} DIRECTORY)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
@@ -24,10 +33,12 @@ set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY
     ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy
-    CACHE INTERNAL "objcopy tool")
+    CACHE INTERNAL "objcopy tool"
+)
 set(CMAKE_SIZE_UTIL
     ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size
-    CACHE INTERNAL "size tool")
+    CACHE INTERNAL "size tool"
+)
 
 set(CMAKE_FIND_ROOT_PATH ${BINUTILS_PATH})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
