cc13320754f344695c0f3f24eda78afa817371b9
Dan Green <danngreen1@gmail.com>
2023-08-29 06:38:15 -0700

    Hide/Show lv helper

---
+++ firmware/src/gui/helpers/lv_helpers.hh
@@ -0,0 +1,20 @@
+#pragma once
+#include "lvgl.h"
+
+namespace MetaModule
+{
+
+void lv_hide(lv_obj_t *obj) {
+	lv_obj_add_flag(obj, LV_OBJ_FLAG_HIDDEN);
+}
+void lv_show(lv_obj_t *obj) {
+	lv_obj_clear_flag(obj, LV_OBJ_FLAG_HIDDEN);
+}
+void lv_show(lv_obj_t *obj, bool visible) {
+	if (visible)
+		lv_show(obj);
+	else
+		lv_hide(obj);
+}
+
+} // namespace MetaModule
