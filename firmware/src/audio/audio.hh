976ab80a9e5dac01790796fbc529cde4507a7386
Dan Green <danngreen1@gmail.com>
2023-08-31 11:17:09 -0700

    Remove audio DAC code (2/2)

--- firmware/src/audio/audio.hh
+++ firmware/src/audio/audio.hh
@@ -77,7 +77,6 @@ private:
 	bool ext_audio_connected = false;
 
 	AudioConf::SampleT get_audio_output(int output_id);
-	uint32_t get_dac_output(int output_id);
 	void set_input(int input_id, AudioConf::SampleT in);
 	bool check_patch_change(int motion);
 	void send_zeros_to_patch();
