7c1120ed4945ae72caf7396e4c7072fab737a7af
Dan Green <danngreen1@gmail.com>
2023-09-18 10:21:56 -0700

    Cleanup FATFS mounting

--- firmware/src/fs/fatfs/fat_file_io.hh
+++ firmware/src/fs/fatfs/fat_file_io.hh
@@ -1,7 +1,7 @@
 #pragma once
 #include "disk_ops.hh"
 #include "fs/volumes.hh"
-#include "printf.h"
+#include "pr_dbg.hh"
 #include <cstdint>
 #include <span>
 #include <string_view>
@@ -9,6 +9,7 @@
 // defined in fatfs/diskio.cc:
 bool fatfs_register_disk(DiskOps *ops, uint8_t disk_id);
 
+// required by fatfs:
 PARTITION VolToPart[FF_VOLUMES] = {
 	{0, 0}, /* "0:" ==> Auto detect partition on USB */
 	{1, 0}, /* "1:" ==> Auto detect partition on SdCard */
@@ -38,24 +39,38 @@ public:
 		, _fatvol{char(char(_vol) + '0'), ':', '\0'}
 		, _volname{'F', 'A', 'T', 'F', 'S', ':', char(char(_vol) + '0'), '\0'} {
 		if (!fatfs_register_disk(diskops, static_cast<unsigned>(_vol))) {
-			printf_("Failed to register FAT FS Disk %d\n", _vol);
+			pr_err("Failed to register FAT FS Disk %d\n", _vol);
 		}
 	}
 
 	bool mount_disk() {
-		// Try mounting previously mounted partition, then auto-detect, then each partition 1 - 8.
-		for (uint8_t part_num = 0; part_num <= 9; part_num++) {
-			uint8_t err;
+		// Try mounting previously mounted partition,
+		// if that fails, try auto-detect, then each partition 1 - 8.
 
-			if (err = f_mount(&fs, _fatvol, 1); err == FR_OK)
+		uint8_t err = 0;
+
+		// Don't try auto-scan twice in a row
+		uint8_t first_part_num = (VolToPart[(unsigned)_vol].pt == 0) ? 1 : 0;
+
+		for (uint8_t part_num = first_part_num; part_num <= 9; part_num++) {
+
+			if (err = f_mount(&fs, _fatvol, 1); err == FR_OK) {
+				pr_trace("Mounted partition %d on volume %d\n", VolToPart[(unsigned)_vol].pt, _vol);
 				return true;
+			}
 
-			printf_("Could not mount volume %d, partition %d. err:%d\n", _vol, VolToPart[(unsigned)_vol].pt, err);
+			pr_trace("Tried and failed to mount vol %d, part %d. err:%d\n", _vol, VolToPart[(unsigned)_vol].pt, err);
 
-			// Try 1 - 8
 			VolToPart[(unsigned)_vol].pt = part_num;
 		}
 
+		if (VolToPart[(unsigned)_vol].pt == 9) {
+			VolToPart[(unsigned)_vol].pt = 0;
+		}
+
+		pr_err("Could not mount volume %d, tried auto scanning and manually loading partitions 1-8. last err: %d\n",
+			   _vol,
+			   err);
 		return false;
 	}
 
@@ -87,10 +102,10 @@ public:
 
 		res = f_mount(&fs, _fatvol, 1);
 		if (res != FR_OK) {
-			printf_("Disk not formatted, err %d\n", res);
+			pr_err("Disk not formatted, err %d\n", res);
 			return false;
 		}
-		printf_("Disk formatted\n");
+		pr_trace("Disk formatted\n");
 		return true;
 	}
 
@@ -123,7 +138,7 @@ public:
 		FILINFO fno;
 
 		if (!get_fat_filinfo(filename, fno)) {
-			printf_("Could not read file %s\n", filename.data());
+			pr_err("Could not read file %s\n", filename.data());
 			return;
 		}
 
@@ -131,7 +146,7 @@ public:
 		fno.ftime = timestamp & 0xFFFF;
 
 		if (f_utime(filename.data(), &fno) != FR_OK)
-			printf_("Could not update timestamp of %s\n", filename.data());
+			pr_err("Could not update timestamp of %s\n", filename.data());
 	}
 
 	uint32_t get_file_timestamp(std::string_view filename) {
--- firmware/src/usb/msc_host.hh
+++ firmware/src/usb/msc_host.hh
@@ -1,17 +1,16 @@
 #pragma once
 #include "drivers/interrupt.hh"
 #include "drivers/interrupt_control.hh"
-#include "usb_host_helper.hh"
-
 #include "fat_file_io.hh"
 #include "fs/volumes.hh"
 #include "msc_fatfs_ops.hh"
+#include "usb_host_helper.hh"
 #include "usbh_core.h"
 #include "usbh_msc.h"
+#include <string_view>
 
 class MSCHost {
 	USBH_HandleTypeDef &usbhost;
-	bool _is_connected = false;
 	MSCOps msc_ops{usbhost};
 	FatFileIO msc;
 
@@ -22,32 +21,21 @@ public:
 	}
 
 	bool init() {
-		printf_("Registered MSC with USB Host\n");
+		pr_trace("Registered MSC with USB Host\n");
 		USBH_RegisterClass(&usbhost, USBH_MSC_CLASS);
 
 		return true;
 	}
 
-	// bool is_connected() {
-	// 	return _is_connected;
-	// }
-
 	void connect() {
 		if (msc.mount_disk()) {
-			printf_("Mounted MSC drive with FATFS\n");
-			msc.foreach_file_with_ext(".yml", [](std::string_view fname, uint32_t timestamp, uint32_t fsize) {
-				if (fname[0] == '\0' || fname[0] == '.')
-					return;
-				printf_("%.255s\t\t0x%x\t\t%d B\n", fname.data(), timestamp, fsize);
-			});
-			// _is_connected = true;
+			pr_trace("Mounted USB drive\n");
 		} else
-			printf_("Failed to mount MSC drive\n");
+			pr_warn("Did not mount a USB drive\n");
 	}
 
 	void disconnect() {
 		msc.unmount_disk();
-		// _is_connected = false;
 	}
 
 	FatFileIO &get_fileio() {
