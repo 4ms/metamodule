66436ed520c1f4502fabcbff589965dc11ddc7c4
Dan Green <danngreen1@gmail.com>
2023-09-19 15:34:31 -0700

    Implement __errno() to be compatible with gcc errors

--- firmware/system/libc_stub.c
+++ firmware/system/libc_stub.c
@@ -1,15 +1,32 @@
-void _init(void) {}
-void _fini(void) {}
-int __errno;
+void _init(void) {
+}
+void _fini(void) {
+}
 void *__dso_handle = (void *)&__dso_handle;
 
-void _exit(int x){ while(1) ;}
-void _kill(int x){}
-int _getpid(){ return -1;}
-void _write(char x){}
-void _close(){}
-void _fstat(){}
-void _isatty(){}
-void _lseek(){}
-void _read(){}
+void _exit(int x) {
+	while (1)
+		;
+}
+void _kill(int x) {
+}
+int _getpid() {
+	return -1;
+}
+void _write(char x) {
+}
+void _close() {
+}
+void _fstat() {
+}
+void _isatty() {
+}
+void _lseek() {
+}
+void _read() {
+}
 
+int *__errno() {
+	static int errno = 0;
+	return &errno;
+}
