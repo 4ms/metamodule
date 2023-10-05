c52fcf9c3bd0141d1c261bdf49f9c965d77e85ed
Dan Green <danngreen1@gmail.com>
2023-10-05 11:37:42 -0700

    remove unused file

--- firmware/src/gui/images/4ms/modules/ADEnv_register.cc
+++
@@ -1,6 +0,0 @@
-#include "CoreModules/moduleFactory.hh"
-#include "lvgl.h"
-LV_IMG_DECLARE(ADEnv_artwork_240);
-
-static const bool ADEnv_faceplate_register =
-	ModuleFactory::registerModuleFaceplate("ADEnv", (void *)(&ADEnv_artwork_240));
