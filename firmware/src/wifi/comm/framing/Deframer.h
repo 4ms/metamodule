d7acb85722a82fcdaee0318a375662f81ae36e35
Lennart <Lennart@binarylabs.dev>
2023-10-13 13:52:25 +0200

    Framing: remove dependency on std::function

--- firmware/src/wifi/comm/framing/Deframer.h
+++ firmware/src/wifi/comm/framing/Deframer.h
@@ -4,36 +4,31 @@
 
 #include "Configuration.h"
 
-#include <functional>
 #include <span>
 
 namespace Framing
 {
 
+template <typename IMPL>
 class Deframer
 {
 public:
-	using FUNC = std::function<void(std::span<uint8_t>)>;
+	Deframer(const Configuration_t&, IMPL*);
 
-	Deframer(const Configuration_t&, const FUNC);
-	virtual ~Deframer() = default;
-
-	void parse(uint8_t);
+	template <typename FUNC>
+	void parse(uint8_t, const FUNC&& func);
 	void reset();
-
-private:
-	virtual void doReset() = 0;
-	virtual void doAdd(uint8_t) = 0;
-	virtual void doComplete() = 0;
-
-protected:
-	FUNC func;
 
 private:
 	const Configuration_t Configuration;
 	enum ParsingState_t {WAIT_HEADER, IN_MESSAGE, ESCAPE};
 	ParsingState_t state;
+
+private:
+	IMPL* impl;
 };
 
 }
+
+#include "Deframer.hpp"
 
--- firmware/src/wifi/comm/framing/DynamicDeframer.h
+++ firmware/src/wifi/comm/framing/DynamicDeframer.h
@@ -9,19 +9,36 @@
 namespace Framing
 {
 
-class DynamicDeframer : public Deframer
+class DynamicDeframer : public Deframer<DynamicDeframer>
 {
 public:
-	DynamicDeframer(const Configuration_t& config_, const FUNC func_) : Deframer(config_, func_) {};
+	DynamicDeframer(const Configuration_t& config_) : Deframer<DynamicDeframer>(config_, this) {};
 	~DynamicDeframer() = default;
 
 private:
-	virtual void doReset() override;
-	virtual void doAdd(uint8_t) override;
-	virtual void doComplete() override;
+	template <typename FUNC>
+	void doReset(const FUNC&&)
+	{
+		receiveBuffer.clear();
+	}
+
+	template <typename FUNC>
+	void doAdd(uint8_t data, const FUNC&& func)
+	{
+		receiveBuffer.push_back(data);
+	}
+
+	template <typename FUNC>
+	void doComplete(const FUNC&& func)
+	{
+		func({receiveBuffer.data(), receiveBuffer.size()});
+	}
 
 private:
 	std::vector<uint8_t> receiveBuffer;
+
+private:
+	friend Deframer<DynamicDeframer>;
 };
 
 }
