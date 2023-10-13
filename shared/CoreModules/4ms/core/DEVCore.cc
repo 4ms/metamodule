b4b4e9da5f0163f869db56e3bcf623a8f8b9a54b
Robert Kunz | Binary Labs <robert@binarylabs.dev>
2023-10-13 16:38:16 +0200

    DEV: working with both channels

--- shared/CoreModules/4ms/core/DEVCore.cc
+++ shared/CoreModules/4ms/core/DEVCore.cc
@@ -66,171 +66,299 @@ class DEVCore : public SmartCoreProcessor<DEVInfo> {
 	using ThisCore = DEVCore;
 	using enum Info::Elem;
 
-public:
-	DEVCore() 
-		: triggerDetectorA(1.0f, 2.0f), triggerDetectorB(1.0f, 2.0f) {
+private:
+	template <class Mapping>
+	class Channel
+	{
+	private:
+		SSI2162 vca;
+		TriangleOscillator osc;
 
+		FlipFlop triggerDetector;
+		EdgeDetector triggerEdgeDetector;
+
+	private:
+		float cycleLED;
+		float riseCV;
+		float fallCV;
+		float rScaleLEDs = 0.f;
+		float fScaleLEDs = 0.f;
+		float envOut;
+
+	private:
+		float timeStepInS = 1.f / 48000.f;
+
+	private:
+		SmartCoreProcessor<DEVInfo>* parent;
+
+	public:
+		Channel(SmartCoreProcessor<DEVInfo>* parent_)
+			: triggerDetector(1.0f, 2.0f), parent(parent_){
+		}
+
+		void update(bool cycleTriggerIn, auto input) {
+			auto [riseCV, fallCV] = getRiseAndFallCV();
+
+			// Convert voltage to time without dealing with details of transistor core
+			auto VoltageToTime = [](float voltage) -> float {
+				auto frequency = VoltageToFrequencyTableDEV.lookup(voltage);
+
+				// limit to valid frequency range
+				frequency = std::clamp(frequency, 1.0f / (60 * 3), 20e3f);
+
+				// convert to period length
+				auto time = 1.0f / frequency;
+
+				return time;
+			};
+
+			osc.setRiseTimeInS(VoltageToTime(riseCV));
+			osc.setFallTimeInS(VoltageToTime(fallCV));
+
+			runOscillator(cycleTriggerIn);
+
+			displayEnvelope(osc.getOutput(), osc.getSlopeState());
+
+			if(auto vcaCV = parent->getInput<Mapping::VcaCvIn>(); vcaCV) {
+				runAudioPath(*vcaCV, input);
+			} else {
+				runAudioPath(osc.getOutput(), input);
+			}		
+			
+		}
+
+		void runAudioPath(float triangleWave, auto input) {
+			triangleWave = InvertingAmpWithBias(triangleWave, 100e3f, 100e3f, 1.94f);
+
+			constexpr float VCAInputImpendance = 5e3f;
+			triangleWave = triangleWave * VoltageDivider(VCAInputImpendance, 2.7e3f);
+
+			// This value influences the maximum gain a lot
+			// Tweaked manually to achieve approximately max 0dB
+			constexpr float SchottkyForwardVoltage = 0.22f;
+			constexpr float MaxGainInV = 5.0f + SchottkyForwardVoltage;
+			constexpr float MinGainInV = VoltageDivider(47e3f, 1e6f) * 5.0f - SchottkyForwardVoltage;
+
+			triangleWave = std::clamp(triangleWave, MinGainInV, MaxGainInV);
+
+			vca.setScaling(triangleWave);
+
+			if (input) {
+				auto output = vca.process(*input);
+				parent->setOutput<Mapping::AudioOut>(output);
+			} else {
+				parent->setOutput<Mapping::AudioOut>(0.f);
+			}
+		}
+
+		void displayEnvelope(float val, TriangleOscillator::SlopeState_t slopeState) {
+			parent->setLED<Mapping::RiseSlider>(slopeState == TriangleOscillator::SlopeState_t::RISING ? val / 8.f : 0);
+			parent->setLED<Mapping::FallSlider>(slopeState == TriangleOscillator::SlopeState_t::FALLING ? val / 8.f : 0);
+
+			envOut = val / VoltageDivider(100e3f, 100e3f);
+			envOut *= parent->getState<Mapping::LevelKnob>() * 2.0f - 1.0f;
+			envOut += parent->getState<Mapping::OffsetKnob>() * 20.0f - 10.0f;
+			parent->setOutput<Mapping::EnvOut>(envOut);
+			// setLED<Mapping::EnvLedLight>(val / 8.f);
+
+		}
+
+		auto getEnvOut() {
+			return envOut;
+		}
+
+		void runOscillator(bool cycleTriggerIn) {
+			bool isCycling =
+				(parent->getState<Mapping::CycleButton>() == LatchingButton::State_t::DOWN) ^ cycleTriggerIn;
+
+			osc.setCycling(isCycling);
+			if (cycleLED != isCycling) {
+				cycleLED = isCycling;
+				parent->setLED<Mapping::CycleButton>(cycleLED);
+			}
+
+			if (auto inputFollowValue = parent->getInput<Mapping::FollowIn>(); inputFollowValue) {
+				osc.setTargetVoltage(*inputFollowValue);
+			}
+
+			if (auto triggerInputValue = parent->getInput<Mapping::TrigIn>(); triggerInputValue) {
+				if (triggerEdgeDetector(triggerDetector(*triggerInputValue))) {
+					osc.doRetrigger();
+				}
+			}
+
+			osc.proceed(timeStepInS);
+		}
+
+		std::pair<float, float> getRiseAndFallCV() {
+			auto ProcessCVOffset = [](auto slider, Toggle3pos::State_t range) -> float {
+				// Slider plus resistor in parallel to tweak curve
+				const float SliderImpedance = 100e3f;
+				auto offset = 5.0f * VoltageDivider(slider * SliderImpedance + 17.4e3f,
+													0 + ParallelCircuit(100e3f, (1.0f - slider) * SliderImpedance));
+
+				// Select one of three bias voltages
+				auto BiasFromRange = [](auto range) -> float {
+					if (range == Toggle3pos::State_t::UP) {
+						return -12.0f * VoltageDivider(1e3f, 10e3f);
+					} else if (range == Toggle3pos::State_t::DOWN) {
+						return 12.0f * VoltageDivider(1e3f, 8.2e3f);
+					} else {
+						// middle position, and fail-safe default
+						return 0.0f;
+					}
+				};
+
+				auto bias = BiasFromRange(range);
+
+				return InvertingAmpWithBias(offset, 100e3f, 100e3f, bias);
+			};
+
+			if (auto timeCVValue = parent->getInput<Mapping::TimeCvIn>(); timeCVValue) {
+				// scale down cv input
+				const auto scaledTimeCV = *timeCVValue * -100e3f / 137e3f;
+
+				// apply attenuverter knobs
+				rScaleLEDs = InvertingAmpWithBias(scaledTimeCV, 100e3f, 100e3f, parent->getState<Mapping::RiseKnob>() * scaledTimeCV);
+				fScaleLEDs = InvertingAmpWithBias(scaledTimeCV, 100e3f, 100e3f, parent->getState<Mapping::FallKnob>() * scaledTimeCV);
+			}
+
+			// sum with static value from fader + range switch
+			auto riseRange = parent->getState<Mapping::SlowMedFastRiseSwitch>();
+			auto fallRange = parent->getState<Mapping::SlowMedFastFallSwitch>();
+			riseCV = -rScaleLEDs - ProcessCVOffset(parent->getState<Mapping::RiseSlider>(), riseRange);
+			fallCV = -fScaleLEDs - ProcessCVOffset(parent->getState<Mapping::FallSlider>(), fallRange);
+
+			// TODO: LEDs only need to be updated ~60Hz instead of 48kHz
+			parent->setLED<Mapping::RiseLedLight>(BipolarColor_t{rScaleLEDs / 10.f});
+			parent->setLED<Mapping::FallLedLight>(BipolarColor_t{fScaleLEDs / 10.f});
+
+			// TODO: low pass filter
+
+			// apply rise time limit and scale down
+			constexpr float DiodeDropInV = 1.0f;
+			const float ClippingVoltage = 5.0f * VoltageDivider(100e3f, 2e3f) + DiodeDropInV;
+			riseCV = riseCV * VoltageDivider(2.2e3f + 33e3f, 16.9e3f);
+			riseCV = std::min(riseCV, ClippingVoltage);
+			riseCV = riseCV * VoltageDivider(2.2e3f, 33e3f);
+
+			// scale down falling CV without additional limiting
+			fallCV = fallCV * VoltageDivider(2.2e3f, 10e3f + 40.2e3f);
+
+			return {riseCV, fallCV};
+		}
+
+		TriangleOscillator::SlopeState_t getOscillatorSlopeState()
+		{
+			return osc.getSlopeState();
+		}
+
+		void set_samplerate(float sr) {
+			timeStepInS = 1.0f / sr;
+		}
+
+	};
+
+private:
+	struct MappingA
+	{
+		const static Info::Elem AudioIn = AudioAIn;
+		const static Info::Elem AudioOut = AudioAOut;
+		const static Info::Elem RiseSlider = RiseASlider;
+		const static Info::Elem FallSlider = FallASlider;
+		const static Info::Elem EnvOut = EnvAOut;
+		const static Info::Elem EnvLedLight = EnvALedLight;
+		const static Info::Elem LevelKnob = LevelAKnob;
+		const static Info::Elem OffsetKnob = OffsetAKnob;
+		const static Info::Elem CycleButton = CycleAButton;
+		const static Info::Elem CycleTrig = CycleTrig_In;
+		const static Info::Elem FollowIn = FollowAIn;
+		const static Info::Elem TrigIn = TrigAIn;
+		const static Info::Elem RiseKnob = RiseAKnob;
+		const static Info::Elem FallKnob = FallAKnob;
+		const static Info::Elem TimeCvIn = TimeCvAIn;
+		const static Info::Elem SlowMedFastRiseSwitch = SlowMedFastRiseASwitch;
+		const static Info::Elem SlowMedFastFallSwitch = SlowMedFastFallASwitch;
+		const static Info::Elem RiseLedLight = RiseLedALight;
+		const static Info::Elem FallLedLight = FallLedALight;
+		const static Info::Elem VcaCvIn = VcaCvAIn;
+	};
+
+	struct MappingB
+	{
+		const static Info::Elem AudioIn = AudioBIn;
+		const static Info::Elem AudioOut = AudioBOut;
+		const static Info::Elem RiseSlider = RiseBSlider;
+		const static Info::Elem FallSlider = FallBSlider;
+		const static Info::Elem EnvOut = EnvBOut;
+		const static Info::Elem EnvLedLight = EnvBLedLight;
+		const static Info::Elem LevelKnob = LevelBKnob;
+		const static Info::Elem OffsetKnob = OffsetBKnob;
+		const static Info::Elem CycleButton = CycleBButton;
+		const static Info::Elem CycleTrig = CycleTrig_In;
+		const static Info::Elem FollowIn = FollowBIn;
+		const static Info::Elem TrigIn = TrigBIn;
+		const static Info::Elem RiseKnob = RiseBKnob;
+		const static Info::Elem FallKnob = FallBKnob;
+		const static Info::Elem TimeCvIn = TimeCvBIn;
+		const static Info::Elem SlowMedFastRiseSwitch = SlowMedFastRiseBSwitch;
+		const static Info::Elem SlowMedFastFallSwitch = SlowMedFastFallBSwitch;
+		const static Info::Elem RiseLedLight = RiseLedBLight;
+		const static Info::Elem FallLedLight = FallLedBLight;
+		const static Info::Elem VcaCvIn = VcaCvBIn;
+	};
+
+	Channel<MappingA> channelA;
+	Channel<MappingB> channelB;
+
+friend Channel<MappingA>;
+friend Channel<MappingB>;
+
+public:
+	DEVCore()
+		: channelA(this), channelB(this){
+		
 	}
 
 	void update() override {
-		auto [riseCV, fallCV] = getRiseAndFallCV();
+		bool cycleTriggerIn = CVToBool(getInput<CycleTrig_In>().value_or(0.0f));
 
-		// Convert voltage to time without dealing with details of transistor core
-		auto VoltageToTime = [](float voltage) -> float {
-			auto frequency = VoltageToFrequencyTableDEV.lookup(voltage);
-
-			// limit to valid frequency range
-			frequency = std::clamp(frequency, 1.0f / (60 * 3), 20e3f);
-
-			// convert to period length
-			auto time = 1.0f / frequency;
-
-			return time;
-		};
-
-		oscA.setRiseTimeInS(VoltageToTime(riseCV));
-		oscA.setFallTimeInS(VoltageToTime(fallCV));
-
-		runOscillator();
-
-		displayOscillatorState(oscA.getSlopeState());
-
-		displayEnvelope(oscA.getOutput(), oscA.getSlopeState());
-		runAudioPath(oscA.getOutput());
-	}
-
-	void runAudioPath(float triangleWave) {
-		triangleWave = InvertingAmpWithBias(triangleWave, 100e3f, 100e3f, 1.94f);
-
-		constexpr float VCAInputImpendance = 5e3f;
-		triangleWave = triangleWave * VoltageDivider(VCAInputImpendance, 2.7e3f);
-
-		// This value influences the maximum gain a lot
-		// Tweaked manually to achieve approximately max 0dB
-		constexpr float SchottkyForwardVoltage = 0.22f;
-		constexpr float MaxGainInV = 5.0f + SchottkyForwardVoltage;
-		constexpr float MinGainInV = VoltageDivider(47e3f, 1e6f) * 5.0f - SchottkyForwardVoltage;
-
-		triangleWave = std::clamp(triangleWave, MinGainInV, MaxGainInV);
-
-		vcaA.setScaling(triangleWave);
-
-		// Ignoring input impedance and inverting 400kHz lowpass
-
-		if (auto input = getInput<AudioAIn>(); input) {
-			auto output = vcaA.process(*input);
-			setOutput<AudioAOut>(output);
+		auto inputA = getInput<MappingA::AudioIn>();
+		channelA.update(cycleTriggerIn, inputA);
+		
+		if(auto inputB = getInput<MappingB::AudioIn>(); inputB) {
+			channelB.update(cycleTriggerIn, inputB);
 		} else {
-			setOutput<AudioAOut>(0.f);
+			channelB.update(cycleTriggerIn, inputA);
 		}
+		
+		displayOscillatorState();
 
-		// Ignoring output impedance and inverting 400kHz lowpass
+		setOutput<OrOut>(std::max(channelA.getEnvOut(), channelB.getEnvOut()));
 	}
 
-	void displayEnvelope(float val, TriangleOscillator::SlopeState_t slopeState) {
-		setLED<RiseASlider>(slopeState == TriangleOscillator::SlopeState_t::RISING ? val / 8.f : 0);
-		setLED<FallASlider>(slopeState == TriangleOscillator::SlopeState_t::FALLING ? val / 8.f : 0);
-
-		val = val / VoltageDivider(100e3f, 100e3f);
-		val *= getState<LevelAKnob>() * 2.0f - 1.0f;
-		val += getState<OffsetAKnob>() * 20.0f - 10.0f;
-		setOutput<EnvAOut>(val);
-		setLED<EnvALedLight>(val / 8.f);
-	}
-
-	void displayOscillatorState(TriangleOscillator::SlopeState_t slopeState) {
-		if (slopeState == TriangleOscillator::SlopeState_t::FALLING) {
+	void displayOscillatorState()
+	{
+		if(auto slopeStateA = channelA.getOscillatorSlopeState(); slopeStateA == TriangleOscillator::SlopeState_t::FALLING) {
 			setOutput<EorAOut>(8.f);
 			// setLED<EorLedLight>(true);
 		} else {
 			setOutput<EorAOut>(0);
 			// setLED<EorLedLight>(false);
 		}
-	}
 
-	void runOscillator() {
-		bool isCycling =
-			(getState<CycleAButton>() == LatchingButton::State_t::DOWN) ^ CVToBool(getInput<CycleTrig_In>().value_or(0.0f));
-
-		oscA.setCycling(isCycling);
-		if (cycleLEDA != isCycling) {
-			cycleLEDA = isCycling;
-			setLED<CycleAButton>(cycleLEDA);
+		if(auto slopeStateB = channelB.getOscillatorSlopeState(); slopeStateB == TriangleOscillator::SlopeState_t::RISING) {
+			setOutput<EofBOut>(8.f);
+			// setLED<EofLedLight>(true);
+		} else {
+			setOutput<EofBOut>(0);
+			// setLED<EofLedLight>(false);
 		}
-
-		if (auto inputFollowValue = getInput<FollowAIn>(); inputFollowValue) {
-			oscA.setTargetVoltage(*inputFollowValue);
-		}
-
-		if (auto triggerInputValue = getInput<TrigAIn>(); triggerInputValue) {
-			if (triggerEdgeDetectorA(triggerDetectorA(*triggerInputValue))) {
-				oscA.doRetrigger();
-			}
-		}
-
-		oscA.proceed(timeStepInS);
-	}
-
-	std::pair<float, float> getRiseAndFallCV() {
-		auto ProcessCVOffset = [](auto slider, Toggle3pos::State_t range) -> float {
-			// Slider plus resistor in parallel to tweak curve
-			const float SliderImpedance = 100e3f;
-			auto offset = 5.0f * VoltageDivider(slider * SliderImpedance + 17.4e3f,
-												0 + ParallelCircuit(100e3f, (1.0f - slider) * SliderImpedance));
-
-			// Select one of three bias voltages
-			auto BiasFromRange = [](auto range) -> float {
-				if (range == Toggle3pos::State_t::UP) {
-					return -12.0f * VoltageDivider(1e3f, 10e3f);
-				} else if (range == Toggle3pos::State_t::DOWN) {
-					return 12.0f * VoltageDivider(1e3f, 8.2e3f);
-				} else {
-					// middle position, and fail-safe default
-					return 0.0f;
-				}
-			};
-
-			auto bias = BiasFromRange(range);
-
-			return InvertingAmpWithBias(offset, 100e3f, 100e3f, bias);
-		};
-
-		if (auto timeCVValue = getInput<TimeCvAIn>(); timeCVValue) {
-			// scale down cv input
-			const auto scaledTimeCV = *timeCVValue * -100e3f / 137e3f;
-
-			// apply attenuverter knobs
-			rScaleLEDsA = InvertingAmpWithBias(scaledTimeCV, 100e3f, 100e3f, getState<RiseAKnob>() * scaledTimeCV);
-			fScaleLEDsA = InvertingAmpWithBias(scaledTimeCV, 100e3f, 100e3f, getState<FallBKnob>() * scaledTimeCV);
-		}
-
-		// sum with static value from fader + range switch
-		auto riseRange = getState<SlowMedFastRiseASwitch>();
-		auto fallRange = getState<SlowMedFastFallASwitch>();
-		riseCVA = -rScaleLEDsA - ProcessCVOffset(getState<RiseASlider>(), riseRange);
-		fallCVA = -fScaleLEDsA - ProcessCVOffset(getState<FallASlider>(), fallRange);
-
-		// TODO: LEDs only need to be updated ~60Hz instead of 48kHz
-		setLED<RiseLedALight>(BipolarColor_t{rScaleLEDsA / 10.f});
-		setLED<FallLedALight>(BipolarColor_t{fScaleLEDsA / 10.f});
-
-		// TODO: low pass filter
-
-		// apply rise time limit and scale down
-		constexpr float DiodeDropInV = 1.0f;
-		const float ClippingVoltage = 5.0f * VoltageDivider(100e3f, 2e3f) + DiodeDropInV;
-		riseCVA = riseCVA * VoltageDivider(2.2e3f + 33e3f, 16.9e3f);
-		riseCVA = std::min(riseCVA, ClippingVoltage);
-		riseCVA = riseCVA * VoltageDivider(2.2e3f, 33e3f);
-
-		// scale down falling CV without additional limiting
-		fallCVA = fallCVA * VoltageDivider(2.2e3f, 10e3f + 40.2e3f);
-
-		return {riseCVA, fallCVA};
 	}
 
 	void set_samplerate(float sr) override {
-		timeStepInS = 1.0f / sr;
+		channelA.set_samplerate(sr);
+		channelB.set_samplerate(sr);
 	}
 
 	// Boilerplate to auto-register in ModuleFactory
@@ -240,33 +368,8 @@ public:
 	// clang-format on
 
 private:
-	// temporary results that are buffered
-	float cycleLEDA;
-	float cycleLEDB;
-	float riseCVA;
-	float riseCVB;
-	float fallCVA;
-	float fallCVB;
-	float rScaleLEDsA = 0.f;
-	float fScaleLEDsA = 0.f;
-	float rScaleLEDsB = 0.f;
-	float fScaleLEDsB = 0.f;
+	
 
-	FlipFlop triggerDetectorA;
-	EdgeDetector triggerEdgeDetectorA;
-
-	FlipFlop triggerDetectorB;
-	EdgeDetector triggerEdgeDetectorB;
-
-private:
-	SSI2162 vcaA;
-	TriangleOscillator oscA;
-
-	SSI2162 vcaB;
-	TriangleOscillator oscB;
-
-private:
-	float timeStepInS = 1.f / 48000.f;
 };
 
 } // namespace MetaModule
