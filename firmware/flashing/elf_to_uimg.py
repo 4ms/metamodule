1927583095b4735bbb3d81bf727f828c2ee1380c
Lennart <Lennart@binarylabs.dev>
2023-11-21 10:46:55 +0100

    respect LMA in elf to uimg script

--- firmware/flashing/elf_to_uimg.py
+++ firmware/flashing/elf_to_uimg.py
@@ -31,41 +31,50 @@ def get_images_for_elf(filename, *, destination, loader):
 
             contained_sections = [section for section in sections if segment.section_in_segment(section)]
 
-            region_address = segment["p_vaddr"]
-            region_size = segment["p_memsz"]
-            write_size = segment["p_filesz"]
+            # This is the location where everything will end up just before main()
+            vma = segment["p_vaddr"]
+            vma_size = segment["p_memsz"]
 
-            if write_size > 0:
-                logger.info("Include segment 0x{:08x} - 0x{:08x} (load size 0x{:08x})".format(region_address, region_address+region_size, write_size))
+            # This is the location of the initialization data the for the region above
+            # Can be smaller than above if this segments contains sections that are not initialized
+            lma = segment["p_paddr"]
+            lma_size = segment["p_filesz"]
+
+            # Check if there is any initialization data to load at all
+            if lma_size > 0:
+                logger.info("Include segment (VMA: 0x{:08x}-0x{:08x} LMA: 0x{:08x}-0x{:08x})".format(vma, vma+vma_size, lma, lma+lma_size))
 
                 for section in contained_sections:
                     logger.debug("Contains {} at 0x{:08x} - 0x{:08x} ".format(section.name, section.header.sh_addr, section.header.sh_addr + section.header.sh_size))
                 
                 payload = segment.data()
-                name = "{}_0x{:08x}".format(destination, region_address)
+                name = "{}_0x{:08x}".format(destination, vma)
 
+                # The core that does the loading has different memory mapping than the core that executes
+                # Remap relevant sections from what is given in the elf file to where the loader core has that mapped
                 if destination != loader:
                     dest_col = 0 if destination == CoreType.A7 else 1
                     loader_col = 0 if loader == CoreType.A7 else 1
                     for mapping in memory_mappings:
-                        if region_address >= mapping[dest_col] and region_address <= mapping[dest_col] + mapping[2]:
-                            new_region_address = region_address - mapping[dest_col] + mapping[loader_col]
-                            logging.debug("Remap 0x{:08x} -> 0x{:08x}".format(region_address, new_region_address))
-                            region_address = new_region_address
+                        if lma >= mapping[dest_col] and lma <= mapping[dest_col] + mapping[2]:
+                            new_lma = lma - mapping[dest_col] + mapping[loader_col]
+                            logger.debug("Remap 0x{:08x} -> 0x{:08x}".format(lma, new_lma))
+                            lma = new_lma
 
-                print("Creating {} at 0x{:08x} with size {}".format(name, region_address, write_size))
-                header = create_uimg_header(payload, loadaddr=region_address, entryaddr=0, name=name)
+                print("Creating {} at 0x{:08x} with size {}".format(name, lma, lma_size))
+                header = create_uimg_header(payload, loadaddr=lma, entryaddr=0, name=name)
 
+                # Just concatenate generated images
                 output = output + header + payload
                 
             else:
-                logger.debug("Skipping empty segment 0x{:08x} - 0x{:08x} (Sections: {})".format(region_address, region_address+region_size, ', '.join([s.name for s in contained_sections])))
+                logger.debug("Skipping empty segment (VMA: 0x{:08x}-0x{:08x} Sections: {})".format(vma, vma+vma_size, ', '.join([s.name for s in contained_sections])))
 
         return output
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Creat concatenated uimg from elf files")
+    parser = argparse.ArgumentParser("Create concatenated uimg from elf files")
     parser.add_argument("--a7", dest="a7_file", help="Input a7 elf file")
     parser.add_argument("--m4", dest="m4_file", help="Input m4 elf file")
     parser.add_argument("out_file", help="Output image file")
