dae7d655d88eb46d5a300db8170c9d7fa5106afe
Lennart <Lennart@binarylabs.dev>
2023-11-28 15:09:35 +0100

    wifi: calculate hashes for wifi images in cmake

--- firmware/src/wifi/wifi_interface.cc
+++ firmware/src/wifi/wifi_interface.cc
@@ -96,11 +96,11 @@ void WifiInterface::checkForUpdate()
 
         const uint32_t FirmwareStartAddress = 0x0;
         std::span<const uint8_t> Firmware ((uint8_t*)&_binary_firmware_bin_start, std::size_t((uint8_t*)&_binary_firmware_bin_end - (uint8_t*)&_binary_firmware_bin_start));
-        const auto FirmwareChecksum = "f70d403246cb27a84eb0c428a49245ad";
+        const auto FirmwareChecksum = firmware_MD5;
 
         const uint32_t FilesystemStartAddress = 0x200000;
         std::span<const uint8_t> Filesystem ((uint8_t*)&_binary_littlefs_img_start, std::size_t((uint8_t*)&_binary_littlefs_img_end - (uint8_t*)&_binary_littlefs_img_start));
-        const auto FileystemChecksum = "f70d403246cb27a84eb0c428a49245ad";
+        const auto FileystemChecksum = littlefs_MD5;
 
         printf("Firmware image at   %p-%p\n", &_binary_firmware_bin_start, &_binary_firmware_bin_end);
         printf("Filesystem image at %p-%p\n", &_binary_littlefs_img_start, &_binary_littlefs_img_end);
