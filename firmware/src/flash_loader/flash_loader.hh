66ad9f77008043755a950f473b0208fb1094ccc0
Dan Green <danngreen1@gmail.com>
2023-12-10 22:07:02 -0800

    Separate FlashLoader and BootloaderUpdater

---
+++ firmware/src/flash_loader/flash_loader.hh
@@ -0,0 +1,19 @@
+#pragma once
+#include "drivers/qspi_flash_driver.hh"
+#include <cstdint>
+#include <span>
+
+namespace MetaModule
+{
+
+class FlashLoader {
+public:
+	FlashLoader();
+	bool check_flash_chip();
+	bool write_sectors(uint32_t base_addr, std::span<uint8_t> buffer);
+
+private:
+	mdrivlib::QSpiFlash flash;
+};
+
+} // namespace MetaModule
