2acb6f44c26651eff858a2f6c2581374b19d9b35
Lennart <Lennart@binarylabs.dev>
2024-01-04 14:15:08 +0100

    wifi: replace complete image with separate bootloader image

--- firmware/src/wifi/flasher/wifi_update.cc
+++ firmware/src/wifi/flasher/wifi_update.cc
@@ -3,18 +3,24 @@
 
 #include <pr_dbg.hh>
 
-extern unsigned int _binary_firmware_bin_start;
-extern unsigned int _binary_firmware_bin_end;
-extern unsigned int _binary_littlefs_img_start;
-extern unsigned int _binary_littlefs_img_end;
+extern unsigned int _binary_bootloader_bin_start;
+extern unsigned int _binary_bootloader_bin_end;
+extern unsigned int _binary_application_bin_start;
+extern unsigned int _binary_application_bin_end;
+extern unsigned int _binary_filesystem_img_start;
+extern unsigned int _binary_filesystem_img_end;
 
-const uint32_t FirmwareStartAddress = 0x0;
-std::span<const uint8_t> Firmware ((uint8_t*)&_binary_firmware_bin_start, std::size_t((uint8_t*)&_binary_firmware_bin_end - (uint8_t*)&_binary_firmware_bin_start));
-const auto FirmwareChecksum = firmware_MD5;
+const uint32_t BootloaderStartAddress = 0x0;
+std::span<const uint8_t> Bootloader ((uint8_t*)&_binary_bootloader_bin_start, std::size_t((uint8_t*)&_binary_bootloader_bin_end - (uint8_t*)&_binary_bootloader_bin_start));
+const auto BootloaderChecksum = bootloader_MD5;
+
+const uint32_t ApplicationStartAddress = 0x10000;
+std::span<const uint8_t> Application ((uint8_t*)&_binary_application_bin_start, std::size_t((uint8_t*)&_binary_application_bin_end - (uint8_t*)&_binary_application_bin_start));
+const auto ApplicationChecksum = application_MD5;
 
 const uint32_t FilesystemStartAddress = 0x200000;
-std::span<const uint8_t> Filesystem ((uint8_t*)&_binary_littlefs_img_start, std::size_t((uint8_t*)&_binary_littlefs_img_end - (uint8_t*)&_binary_littlefs_img_start));
-const auto FileystemChecksum = littlefs_MD5;
+std::span<const uint8_t> Filesystem ((uint8_t*)&_binary_filesystem_img_start, std::size_t((uint8_t*)&_binary_filesystem_img_end - (uint8_t*)&_binary_filesystem_img_start));
+const auto FileystemChecksum = filesystem_MD5;
 
 
 namespace MetaModule
@@ -39,21 +45,28 @@ void WifiUpdate::run()
 
 void WifiUpdate::checkForUpdate()
 {
-    pr_dbg("Update firmware partition\n");
+    pr_dbg("Update bootloader partition\n");
 
-    auto result = Flasher::conditional_flash(FirmwareStartAddress, Firmware, FirmwareChecksum);
+    auto result = Flasher::conditional_flash(BootloaderStartAddress, Bootloader, BootloaderChecksum);
 
     if (result == ESP_LOADER_SUCCESS)
     {
-        pr_dbg("Update filesystem partition\n");
+        pr_dbg("Update application partition\n");
 
-        result = Flasher::conditional_flash(FilesystemStartAddress, Filesystem, FileystemChecksum);
+        auto result = Flasher::conditional_flash(ApplicationStartAddress, Application, ApplicationChecksum);
 
         if (result == ESP_LOADER_SUCCESS)
         {
-            pr_dbg("Reboot wifi bridge\n");
-            Flasher::reboot();
-            return;
+            pr_dbg("Update filesystem partition\n");
+
+            result = Flasher::conditional_flash(FilesystemStartAddress, Filesystem, FileystemChecksum);
+
+            if (result == ESP_LOADER_SUCCESS)
+            {
+                pr_dbg("Reboot wifi bridge\n");
+                Flasher::reboot();
+                return;
+            }
         }
     }
 
