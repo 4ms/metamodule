f2293140dd686eafdf2201ecb37060a538d836e8
Dan Green <danngreen1@gmail.com>
2024-01-11 16:56:32 -0800

    WIP

--- firmware/src/dynload/elf_reloc.hh
+++ firmware/src/dynload/elf_reloc.hh
@@ -31,9 +31,30 @@ public:
 		return ELF32_R_TYPE(raw_reloc->r_info);
 	}
 
-	uint32_t offset() {
+	// Address (relative to start of elf file) to write the symbol's address to
+	uint32_t reloc_offset() {
 		return raw_reloc->r_offset;
+	}
+
+	// "Value" of symbol, used to determine the actual address of the loaded symbol
+	uint32_t symbol_value() {
+		return symbol.offset();
 	}
 };
 
+class Relocater {
+	uint32_t base_address;
+
+public:
+	Relocater(uint32_t elf_load_address)
+		: base_address{elf_load_address} {
+	}
+
+	void write_reloc(ElfReloc &reloc) {
+		if (reloc.symbol_type() == R_ARM_GLOB_DAT) {
+			auto reloc_address = reinterpret_cast<uint32_t *>(reloc.reloc_offset() + base_address);
+			*reloc_address = reloc.symbol_value() + base_address;
+		}
+	}
+};
 } // namespace ElfFile
