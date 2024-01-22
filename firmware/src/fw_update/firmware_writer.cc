6bc17eaca94f2af39c759e823dc5ec842d6b089f
Lennart <Lennart@binarylabs.dev>
2024-01-22 13:32:03 +0100

    firmware_writer: extract wifi flashing functionality

--- firmware/src/fw_update/firmware_writer.cc
+++ firmware/src/fw_update/firmware_writer.cc
@@ -2,10 +2,8 @@
  
 #include <wifi/flasher/flasher.h>
 
-
 namespace MetaModule
 {
-
 
 FirmwareWriter::FirmwareWriter(FatFileIO &sdcard_fileio, FatFileIO &usb_fileio)
 	: sdcard_{sdcard_fileio}
@@ -20,28 +18,7 @@ void FirmwareWriter::handle_message(IntercoreStorageMessage &message)
 
 		pr_dbg("-> Compare with checksum %s at 0x%08x\n", message.checksum.c_str(), message.address);
 
-		auto result = Flasher::init(230400);
-
-		if (result == ESP_LOADER_SUCCESS)
-		{
-			result = Flasher::verify(message.address, message.length, {message.checksum.data(), 32});
-			if (result == ESP_LOADER_ERROR_INVALID_MD5)
-			{
-				pending_send_message.message_type = ChecksumMismatch;
-			}
-			else if (result == ESP_LOADER_SUCCESS)
-			{
-				pr_dbg("-> Checksum matches\n");
-				pending_send_message.message_type = ChecksumMatch;
-			}				
-		}
-		else
-		{
-			pr_err("Cannot connect to wifi bootloader\n");
-			pending_send_message.message_type = ChecksumFailed;
-		}
-
-		Flasher::deinit();
+		pending_send_message = compareChecksumWifi(message.address, message.length, {message.checksum.data()});
 	}
 	else if (message.message_type == StartFlashing)
 	{
@@ -55,73 +32,7 @@ void FirmwareWriter::handle_message(IntercoreStorageMessage &message)
 
 		if (fileio != nullptr)
 		{
-			auto result = Flasher::init(230400);
-
-			if (result == ESP_LOADER_SUCCESS)
-			{
-				uint32_t& bytesWritten = *message.bytes_processed;
-				const uint32_t bytesTotal = message.length;
-
-				std::array<uint8_t, 1024> BatchBuffer;
-
-				result = Flasher::flash_start(message.address, message.length, BatchBuffer.size());
-
-				if (result == ESP_LOADER_SUCCESS)
-				{
-					bool error_during_writes = false;
-
-					while (bytesWritten < bytesTotal)
-					{
-						auto to_read = std::min<std::size_t>(bytesTotal - bytesWritten, BatchBuffer.size());
-
-						auto thisReadBuffer = std::span<char>((char*)BatchBuffer.data(), to_read);
-						auto bytesRead = fileio->read_file(message.filename, thisReadBuffer, bytesWritten);
-
-						if (bytesRead == to_read)
-						{
-							result = Flasher::flash_process(std::span<uint8_t>((uint8_t*)thisReadBuffer.data(), thisReadBuffer.size()));
-							if (result != ESP_LOADER_SUCCESS)
-							{
-								error_during_writes = true;
-								break;
-							}
-							else
-							{
-								bytesWritten += to_read;
-							}
-						}
-						else
-						{
-							pr_err("Cannot read from update file in range %u - %u\n", bytesWritten, bytesWritten+to_read);
-							error_during_writes = true;
-							break;
-						}
-					}
-
-					if (not error_during_writes)
-					{
-						pr_dbg("-> Flashing completed\n");
-						pending_send_message.message_type = FlashingOk;
-					}
-					else
-					{
-						pr_dbg("-> Flashing failed\n");
-						pending_send_message.message_type = FlashingFailed;
-					}
-				}
-				else
-				{
-					pr_err("Cannot start flashing wifi\n");
-					pending_send_message.message_type = FlashingFailed;
-				}
-			}
-			else
-			{
-				pr_err("Cannot connect to wifi bootloader\n");
-				pending_send_message.message_type = FlashingFailed;
-			}
-
-			Flasher::deinit();
+			pending_send_message = flashWifi(fileio, message.filename, message.address, message.length, *message.bytes_processed);
 		}
 		else
 		{
@@ -130,6 +41,108 @@ void FirmwareWriter::handle_message(IntercoreStorageMessage &message)
 		}
 	}
 
+}
+
+IntercoreStorageMessage FirmwareWriter::compareChecksumWifi(uint32_t address, uint32_t length, Checksum_t checksum)
+{
+	IntercoreStorageMessage returnValue;
+
+	auto result = Flasher::init(230400);
+
+	if (result == ESP_LOADER_SUCCESS)
+	{
+		result = Flasher::verify(address, length, checksum);
+		if (result == ESP_LOADER_ERROR_INVALID_MD5)
+		{
+			returnValue = {.message_type = ChecksumMismatch};
+		}
+		else if (result == ESP_LOADER_SUCCESS)
+		{
+			pr_dbg("-> Checksum matches\n");
+			returnValue = {.message_type = ChecksumMatch};
+		}				
+	}
+	else
+	{
+		pr_err("Cannot connect to wifi bootloader\n");
+		returnValue = {.message_type = ChecksumFailed};
+	}
+
+	Flasher::deinit();
+
+	return returnValue;
+}
+
+IntercoreStorageMessage FirmwareWriter::flashWifi(FatFileIO* fileio, std::string_view filename, uint32_t address, const uint32_t length, uint32_t& bytesWritten)
+{
+	IntercoreStorageMessage returnValue;
+
+	auto result = Flasher::init(230400);
+
+	if (result == ESP_LOADER_SUCCESS)
+	{
+		std::array<uint8_t, 1024> BatchBuffer;
+
+		result = Flasher::flash_start(address, length, BatchBuffer.size());
+
+		if (result == ESP_LOADER_SUCCESS)
+		{
+			bool error_during_writes = false;
+
+			while (bytesWritten < length)
+			{
+				auto to_read = std::min<std::size_t>(length - bytesWritten, BatchBuffer.size());
+
+				auto thisReadBuffer = std::span<char>((char*)BatchBuffer.data(), to_read);
+				auto bytesRead = fileio->read_file(filename, thisReadBuffer, bytesWritten);
+
+				if (bytesRead == to_read)
+				{
+					result = Flasher::flash_process(std::span<uint8_t>((uint8_t*)thisReadBuffer.data(), thisReadBuffer.size()));
+					if (result != ESP_LOADER_SUCCESS)
+					{
+						error_during_writes = true;
+						break;
+					}
+					else
+					{
+						bytesWritten += to_read;
+					}
+				}
+				else
+				{
+					pr_err("Cannot read from update file in range %u - %u\n", bytesWritten, bytesWritten+to_read);
+					error_during_writes = true;
+					break;
+				}
+			}
+
+			if (not error_during_writes)
+			{
+				pr_dbg("-> Flashing completed\n");
+				returnValue = {.message_type = FlashingOk};
+			}
+			else
+			{
+				pr_dbg("-> Flashing failed\n");
+				returnValue = {.message_type = FlashingFailed};
+			}
+		}
+		else
+		{
+			pr_err("Cannot start flashing wifi\n");
+			returnValue = {.message_type = FlashingFailed};
+		}
+	}
+	else
+	{
+		pr_err("Cannot connect to wifi bootloader\n");
+		returnValue = {.message_type = FlashingFailed};
+	}
+
+	Flasher::deinit();
+
+	return returnValue;
 }
 
 void FirmwareWriter::send_pending_message(InterCoreComm &comm) {
--- firmware/src/fw_update/firmware_writer.hh
+++ firmware/src/fw_update/firmware_writer.hh
@@ -10,12 +10,18 @@ namespace MetaModule
 struct FirmwareWriter {
 	using InterCoreComm = mdrivlib::InterCoreComm<mdrivlib::ICCCoreType::Responder, IntercoreStorageMessage>;
 	using enum IntercoreStorageMessage::MessageType;
+	using Checksum_t = std::string_view;
 
+public:
 	FirmwareWriter(FatFileIO &sdcard_fileio, FatFileIO &usb_fileio);
 
 	void handle_message(IntercoreStorageMessage &message);
 
 	void send_pending_message(InterCoreComm &comm);
+
+private:
+	IntercoreStorageMessage compareChecksumWifi(uint32_t, uint32_t, Checksum_t);
+	IntercoreStorageMessage flashWifi(FatFileIO* fileio, std::string_view filename, uint32_t address, const uint32_t length, uint32_t& bytesWritten);
 
 private:
 	FatFileIO &sdcard_;
