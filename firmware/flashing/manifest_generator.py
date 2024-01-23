09fbe8ae45e2cde85d62984b11d78cffecd25ac5
Lennart <Lennart@binarylabs.dev>
2024-01-23 11:12:38 +0100

    manifest_generator: fix wrong binary selected

--- firmware/flashing/manifest_generator.py
+++ firmware/flashing/manifest_generator.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         process_file(destination_dir, args.app_file, "app", name="Main App", address=0x80000),
         process_file(destination_dir, args.wifi_bl_file, "wifi", name="Wifi Bootloader", address=0x0),
         process_file(destination_dir, args.wifi_app_file, "wifi", name="Wifi Application", address=0x10000),
-        process_file(destination_dir, args.wifi_app_file, "wifi", name="Wifi Filesystem", address=0x20000),
+        process_file(destination_dir, args.wifi_fs_file, "wifi", name="Wifi Filesystem", address=0x20000),
     ]
 
     with open(args.out_file, "w+") as out_file:
