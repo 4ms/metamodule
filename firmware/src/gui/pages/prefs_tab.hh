33c507faf390b11cbea5acb2644167b42330362c
Dan Green <danngreen1@gmail.com>
2024-02-21 16:55:38 -0800

    Button to load plugins via UI (WIP)

--- firmware/src/gui/pages/prefs_tab.hh
+++ firmware/src/gui/pages/prefs_tab.hh
@@ -12,8 +12,35 @@ namespace MetaModule
 struct PrefsTab {
 
 	PrefsTab(FileStorageProxy &patch_storage)
-		: file_storage{patch_storage} {
+		: file_storage{patch_storage}
+		, plugin_button(lv_btn_create(ui_SystemMenuPrefs))
+		, plugin_button_label(lv_label_create(plugin_button)) {
 		lv_obj_add_event_cb(ui_ResetFactoryPatchesButton, resetbut_cb, LV_EVENT_CLICKED, this);
+		lv_obj_add_event_cb(plugin_button_label, load_plugin_but_cb, LV_EVENT_CLICKED, this);
+
+		////// set up UI: TODO do this in SLS
+		lv_obj_set_width(plugin_button, LV_SIZE_CONTENT);
+		lv_obj_set_height(plugin_button, LV_SIZE_CONTENT);
+		lv_obj_set_align(plugin_button, LV_ALIGN_CENTER);
+		lv_obj_add_flag(plugin_button, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
+		lv_obj_clear_flag(plugin_button,
+						  LV_OBJ_FLAG_PRESS_LOCK | LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_SNAPPABLE |
+							  LV_OBJ_FLAG_SCROLLABLE | LV_OBJ_FLAG_SCROLL_ELASTIC | LV_OBJ_FLAG_SCROLL_MOMENTUM |
+							  LV_OBJ_FLAG_SCROLL_CHAIN);
+		lv_obj_set_style_bg_color(plugin_button, lv_color_hex(0x444444), LV_PART_MAIN | LV_STATE_DEFAULT);
+		lv_obj_set_style_bg_opa(plugin_button, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+		lv_obj_set_style_text_font(plugin_button, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
+		lv_obj_set_style_outline_color(plugin_button, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_FOCUSED);
+		lv_obj_set_style_outline_opa(plugin_button, 255, LV_PART_MAIN | LV_STATE_FOCUSED);
+		lv_obj_set_style_outline_width(plugin_button, 2, LV_PART_MAIN | LV_STATE_FOCUSED);
+		lv_obj_set_style_outline_pad(plugin_button, 3, LV_PART_MAIN | LV_STATE_FOCUSED);
+		lv_obj_set_style_outline_color(plugin_button, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_FOCUS_KEY);
+		lv_obj_set_style_outline_opa(plugin_button, 255, LV_PART_MAIN | LV_STATE_FOCUS_KEY);
+
+		lv_obj_set_width(plugin_button_label, LV_SIZE_CONTENT);
+		lv_obj_set_height(plugin_button_label, LV_SIZE_CONTENT);
+		lv_obj_set_align(plugin_button_label, LV_ALIGN_CENTER);
+		lv_label_set_text(plugin_button_label, "Load Plugins");
 	}
 
 	void prepare_focus(lv_group_t *group) {
@@ -52,9 +79,19 @@ private:
 			"Delete");
 	}
 
+	static void load_plugin_but_cb(lv_event_t *event) {
+		if (!event || !event->user_data)
+			return;
+
+		auto page = static_cast<PrefsTab *>(event->user_data);
+	}
+
 	lv_group_t *group = nullptr;
 	FileStorageProxy &file_storage;
 	ConfirmPopup confirm_popup;
+
+	lv_obj_t *plugin_button;
+	lv_obj_t *plugin_button_label;
 };
 
 } // namespace MetaModule
