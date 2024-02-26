acabfa250e684285e69e641eb5567963c7ef358c
Dan Green <danngreen1@gmail.com>
2024-02-26 12:05:52 -0800

    VCV: load default value for AltParamChoice

--- vcv/src/widgets/base_modules_implemenation.hh
+++ vcv/src/widgets/base_modules_implemenation.hh
@@ -43,7 +43,7 @@ inline void do_config_element(AltParamContinuous el, const Indices &indices, con
 }
 
 inline void do_config_element(AltParamChoice el, const Indices &indices, const ModuleContext_t &context) {
-	context.module->configParam(indices.param_idx, 1, el.num_pos, 0, el.short_name.data());
+	context.module->configParam(indices.param_idx, 1, el.num_pos, el.DefaultValue, el.short_name.data());
 }
 
 } // namespace MetaModule::VCVImplementation::Module
