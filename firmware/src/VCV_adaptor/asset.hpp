8c22f4dbabe519c8c6f2e42bceb051afcead71d5
Dan Green <danngreen1@gmail.com>
2024-03-04 17:04:44 -0800

    Temporarily strip path from internal plugin assets

--- firmware/src/VCV_adaptor/asset.hpp
+++ firmware/src/VCV_adaptor/asset.hpp
@@ -11,15 +11,26 @@ inline void svg_to_png(std::string &path) {
 	}
 }
 
+inline void strip_paths(std::string &path) {
+//Hack: remove strip_paths after we are loading internal plugin assets to RamDisk
+#ifdef METAMODULE_INTERNAL_PLUGIN
+	auto lastslash = path.find_last_of('/');
+	if (lastslash != std::string::npos)
+		path = path.substr(lastslash + 1);
+#endif
+}
+
 inline std::string system(std::string_view filename = "") {
 	auto path = std::string("sys/") + std::string(filename);
 	svg_to_png(path);
+	strip_paths(path);
 	return path;
 }
 
 inline std::string user(std::string_view filename = "") {
 	auto path = std::string("usr/") + std::string(filename);
 	svg_to_png(path);
+	strip_paths(path);
 	return path;
 }
 
@@ -32,6 +43,8 @@ inline std::string plugin(plugin::Plugin *plugin, std::string_view filename = ""
 
 	path += filename;
 	svg_to_png(path);
+	strip_paths(path);
+
 	return path;
 }
 
