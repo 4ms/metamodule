0536dd3425c38fbc440c3e5dd747127bd0971bba
Dan Green <danngreen1@gmail.com>
2024-03-07 16:49:34 -0800

    Remove cmake registration of image files

--- firmware/src/VCV_adaptor/asset.hpp
+++ firmware/src/VCV_adaptor/asset.hpp
@@ -12,18 +12,25 @@ inline void svg_to_png(std::string &path) {
 }
 
 inline void strip_paths(std::string &path) {
-//Hack: remove strip_paths after we are loading internal plugin assets to RamDisk
-#ifdef METAMODULE_INTERNAL_PLUGIN
-	auto lastslash = path.find_last_of('/');
-	if (lastslash != std::string::npos)
-		path = path.substr(lastslash + 1);
-#else
-#endif
+	//Hack: remove strip_paths after we are loading internal plugin assets to RamDisk
+	// #ifdef METAMODULE_INTERNAL_PLUGIN
+	// 	auto lastslash = path.find_last_of('/');
+	// 	if (lastslash != std::string::npos)
+	// 		path = path.substr(lastslash + 1);
+	// #else
+	// #endif
 }
 
 inline std::string system(std::string_view filename = "") {
 	auto path = std::string("sys/") + std::string(filename);
 	svg_to_png(path);
+
+	const std::string_view prefix{"sys/res/ComponentLibrary/"};
+	if (path.starts_with(prefix)) {
+		path = path.substr(prefix.length());
+		path = std::string("racklib/") + path;
+	}
+
 	strip_paths(path);
 	return path;
 }
