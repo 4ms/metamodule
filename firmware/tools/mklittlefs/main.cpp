be6a1ef9ff56ad74deca3b1e0ed8184331b83bd2
Dan Green <danngreen1@gmail.com>
2024-03-08 16:44:14 -0800

    remove unused option from mklittlefs

--- firmware/tools/mklittlefs/main.cpp
+++ firmware/tools/mklittlefs/main.cpp
@@ -39,7 +39,6 @@ static std::vector<uint8_t> s_flashmem;
 static std::string s_dirName;
 static std::string s_imageName;
 static uint32_t s_imageSize;
-static uint32_t s_pageSize;
 static uint32_t s_blockSize;
 static std::string s_fromFile;
 
@@ -96,12 +95,12 @@ void setLfsConfig()
   s_cfg.erase = lfs_flash_erase;
   s_cfg.sync  = lfs_flash_sync;
 
-  s_cfg.read_size = 64;
-  s_cfg.prog_size = 64;
+  s_cfg.read_size = 16;
+  s_cfg.prog_size = 16;
   s_cfg.block_size =  s_blockSize;
   s_cfg.block_count = s_flashmem.size() / s_blockSize;
-  s_cfg.block_cycles = 16; // TODO - need better explanation
-  s_cfg.cache_size = 64;
+  s_cfg.block_cycles = 500; // TODO - need better explanation
+  s_cfg.cache_size = 1024;
   s_cfg.lookahead_size = 64;
   s_cfg.read_buffer = nullptr;
   s_cfg.prog_buffer = nullptr;
@@ -814,14 +813,12 @@ void processArgs(int argc, const char** argv) {
     TCLAP::SwitchArg listArg( "l", "list", "list files in littlefs image", false);
     TCLAP::UnlabeledValueArg<std::string> outNameArg( "image_file", "littlefs image file", true, "", "image_file"  );
     TCLAP::ValueArg<int> imageSizeArg( "s", "size", "fs image size, in bytes", false, 0, "number" );
-    TCLAP::ValueArg<int> pageSizeArg( "p", "page", "fs page size, in bytes", false, 256, "number" );
     TCLAP::ValueArg<int> blockSizeArg( "b", "block", "fs block size, in bytes", false, 4096, "number" );
     TCLAP::SwitchArg addAllFilesArg( "a", "all-files", "when creating an image, include files which are normally ignored; currently only applies to '.DS_Store' files and '.git' directories", false);
     TCLAP::ValueArg<int> debugArg( "d", "debug", "Debug level. 0 means no debug output.", false, 0, "0-5" );
     TCLAP::ValueArg<std::string> fromFileArg( "T", "from-file", "when creating an image, include paths in from_file instead of scanning pack_dir", false, "", "from_file");
 
     cmd.add( imageSizeArg );
-    cmd.add( pageSizeArg );
     cmd.add( blockSizeArg );
     cmd.add( addAllFilesArg );
     cmd.add( debugArg );
@@ -849,7 +846,6 @@ void processArgs(int argc, const char** argv) {
 
     s_imageName = outNameArg.getValue();
     s_imageSize = imageSizeArg.getValue();
-    s_pageSize  = pageSizeArg.getValue();
     s_blockSize = blockSizeArg.getValue();
     s_addAllFiles = addAllFilesArg.isSet();
 }
