a7d94164aed86621718fc64d256f0edc2d16dc84
Robert Kunz | Binary Labs <robert@binarylabs.dev>
2024-03-12 15:13:18 +0100

    SISM: switch to SmartCore, add scale and shift

--- shared/CoreModules/4ms/core/SISMCore.cc
+++ shared/CoreModules/4ms/core/SISMCore.cc
@@ -1,35 +1,57 @@
-#include "CoreModules/CoreProcessor.hh"
+#include "CoreModules/SmartCoreProcessor.hh"
 #include "CoreModules/moduleFactory.hh"
 #include "info/SISM_info.hh"
 
 namespace MetaModule
 {
 
-class SISMCore : public CoreProcessor {
+class SISMCore : public SmartCoreProcessor<SISMInfo> {
 	using Info = SISMInfo;
 	using ThisCore = SISMCore;
+	using enum Info::Elem;
 
 public:
-	SISMCore() = default;
+	SISMCore()
+		: outputValue{0.f,0.f,0.f,0.f}, inputValue{0.f,0.f,0.f,0.f} {
+	}
 
 	void update() override {
-	}
+		if(auto channel1InputValue = getInput<In1In>(); channel1InputValue) {
+			inputValue[0] = *channel1InputValue;
+		} else {
+			inputValue[0] = 0.f;
+		}
 
-	void set_param(int param_id, float val) override {
-	}
+		if(auto channel2InputValue = getInput<In2In>(); channel2InputValue) {
+			inputValue[1] = *channel2InputValue;
+		} else {
+			inputValue[1] = inputValue[0];
+		}
 
-	void set_input(int input_id, float val) override {
-	}
+		if(auto channel3InputValue = getInput<In3In>(); channel3InputValue) {
+			inputValue[2] = *channel3InputValue;
+		} else {
+			inputValue[2] = 0.f;
+		}
 
-	float get_output(int output_id) const override {
-		return 0.f;
+		if(auto channel4InputValue = getInput<In4In>(); channel4InputValue) {
+			inputValue[3] = *channel4InputValue;
+		} else {
+			inputValue[3] = inputValue[2];
+		}
+
+		outputValue[0] = process(inputValue[0], getState<Scale1Knob>(), getState<Shift1Knob>());
+		outputValue[1] = process(inputValue[1], getState<Scale2Knob>(), getState<Shift2Knob>());
+		outputValue[2] = process(inputValue[2], getState<Scale3Knob>(), getState<Shift3Knob>());
+		outputValue[3] = process(inputValue[3], getState<Scale4Knob>(), getState<Shift4Knob>());
+
+		setOutput<Out1Out>(outputValue[0]);
+		setOutput<Out2Out>(outputValue[1]);
+		setOutput<Out3Out>(outputValue[2]);
+		setOutput<Out4Out>(outputValue[3]);
 	}
 
 	void set_samplerate(float sr) override {
-	}
-
-	float get_led_brightness(int led_id) const override {
-		return 0.f;
 	}
 
 	// Boilerplate to auto-register in ModuleFactory
@@ -39,6 +61,18 @@ public:
 	// clang-format on
 
 private:
+	static constexpr float maximumShiftInV = 9.5f;
+	std::array<float, 4> outputValue;
+	std::array<float, 4> inputValue;
+
+private:
+	float process(float input, float scaleAmount, float shiftAmount) {
+		return input * attenueverte(scaleAmount) + attenueverte(shiftAmount) * maximumShiftInV;
+	}
+
+	float attenueverte(float input) {
+		return 2.0f * input - 1.0f;
+	}
 };
 
 } // namespace MetaModule
