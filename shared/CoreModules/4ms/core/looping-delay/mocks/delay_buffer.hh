137d1739e340cf9b30c7bf80568bae6ecec21c24
Lennart <Lennart@binarylabs.dev>
2024-04-02 13:54:54 +0200

    dld source: add mock for non-static delay buffer

---
+++ shared/CoreModules/4ms/core/looping-delay/mocks/delay_buffer.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <span>
+
+#include "conf.hh"
+
+namespace LDKit
+{
+
+struct DelayBuffer {
+	static constexpr uint32_t size = Brain::MemorySizeBytes / sizeof(int16_t);
+	using array = std::array<int16_t, size>;
+	using span = std::span<int16_t, size>;
+
+    DelayBuffer()
+        : sp_{buf_.data(), buf_.size()}
+    {}
+
+	span& get() {
+		return sp_;
+	}
+
+private:
+    array buf_;
+    span sp_;
+};
+
+} // namespace LDKit
