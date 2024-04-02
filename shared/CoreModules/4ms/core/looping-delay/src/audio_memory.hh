fe2a0ce98ae40fc78b712babc3496b3cff3c859d
Lennart <Lennart@binarylabs.dev>
2024-04-02 14:14:55 +0200

    dld source: make audio_memory hardware independent (empty mock)

--- shared/CoreModules/4ms/core/looping-delay/src/audio_memory.hh
+++ shared/CoreModules/4ms/core/looping-delay/src/audio_memory.hh
@@ -1,18 +1,12 @@
 #pragma once
-#include "conf/board_conf.hh"
-#include "loop_util.hh"
 #include <cstdint>
-#include <cstring>
 
 namespace LDKit
 {
 
 struct Memory {
 	static void clear() {
-		std::memset(reinterpret_cast<void *>(Brain::MemoryStartAddr), 0, Brain::MemorySizeBytes);
-		// On F427: Takes 700ms to clear the channel buffer in 32-bit chunks, roughly 83ns per write
-		// for (uint32_t i = Brain::MemoryStartAddr; i < Brain::MemoryEndAddr; i += 4)
-		// 	*reinterpret_cast<uint32_t *>(i) = 0x00000000U;
+		// TODO: implement
 	}
 };
 } // namespace LDKit
