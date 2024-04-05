cd8e944bd0cf592495525e93c48cf4bd5630af7f
Lennart <Lennart@binarylabs.dev>
2024-04-05 16:01:13 +0200

    dld source: partly implement pin mocks

--- shared/CoreModules/4ms/core/looping-delay/mocks/mocks.hh
+++ shared/CoreModules/4ms/core/looping-delay/mocks/mocks.hh
@@ -3,12 +3,48 @@
 namespace LDKit::Mocks
 {
 
-struct MockedTrigger
+struct InputPin
 {
-	void update() {}
-	bool is_pressed() { return false;}
-	bool is_just_released()  { return false;}
-    bool is_just_pressed()  { return false;}
+    void update()
+    {
+    }
+	
+    bool just_went_high()
+    {
+        if (value and not compareValue)
+		{
+			compareValue = true;
+			return true;
+		}
+		return false;
+    }
+
+	bool just_went_low()
+    {
+        if (not value and compareValue)
+		{
+			compareValue = false;
+			return true;
+		}
+		return false;
+    }
+
+	void sideload_set(bool val)
+	{
+		value = val;
+	}
+
+protected:
+	bool value;
+	bool compareValue;
+};
+
+
+struct MockedTrigger : public InputPin
+{
+	bool is_pressed() { return value; }
+	bool is_just_released()  { return just_went_low(); }
+    bool is_just_pressed()  { return just_went_high(); }
 };
 
 struct MockedButton : public MockedTrigger
@@ -39,27 +75,26 @@ struct OutputPin
 
 	void low()
 	{
+		value = false;
 	}
 
 	void high()
 	{
+		value = true;
 	}
+
+	bool sideload_get()
+	{
+		return value;
+	}
+
+private:
+	bool value;
 };
 
 struct MockedLED : OutputPin
 {
 };
 
-struct InputPin
-{
-    void update()
-    {
-
-    }
-    bool just_went_high()
-    {
-        return false;
-    }
-};
 
 }
