9648ada63117a5ca59798514d65c5a5d0c40d219
Dan Green <danngreen1@gmail.com>
2024-04-06 21:26:26 -0700

    Build all AI modules

--- firmware/plugins/AI/plugin.cpp
+++ firmware/plugins/AI/plugin.cpp
@@ -6,12 +6,12 @@ void init(rack::Plugin *p) {
 	pluginInstance = p;
 
 	p->addModel(modelBraids);
-	// p->addModel(modelPlaits);
+	p->addModel(modelPlaits);
 	p->addModel(modelElements);
-	// p->addModel(modelTides);
+	p->addModel(modelTides);
 	p->addModel(modelTides2);
-	// p->addModel(modelClouds);
-	// p->addModel(modelWarps);
+	p->addModel(modelClouds);
+	p->addModel(modelWarps);
 	p->addModel(modelRings);
 	p->addModel(modelLinks);
 	p->addModel(modelKinks);
@@ -19,10 +19,10 @@ void init(rack::Plugin *p) {
 	p->addModel(modelBranches);
 	p->addModel(modelBlinds);
 	p->addModel(modelVeils);
-	// p->addModel(modelFrames);
+	p->addModel(modelFrames);
 	p->addModel(modelMarbles);
-	// p->addModel(modelStages);
+	p->addModel(modelStages);
 	p->addModel(modelRipples);
 	p->addModel(modelShelves);
-	// p->addModel(modelStreams);
+	p->addModel(modelStreams);
 }
