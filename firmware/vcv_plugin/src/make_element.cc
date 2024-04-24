3e6060ab8a4eef67e99dc3e2f6514d17b2072b3a
Dan Green <danngreen1@gmail.com>
2024-04-23 21:36:18 -0700

    make_element: Take code from interface

--- firmware/vcv_plugin/src/make_element.cc
+++ firmware/vcv_plugin/src/make_element.cc
@@ -33,16 +33,16 @@ Element make_element(rack::app::Knob const *widget, BaseElement b) {
 Element make_element(rack::app::SvgKnob *widget, BaseElement b) {
 	// SvgKnobs have a base SVG, and sometimes have a bg svg.
 	// If there is a bg svg, then use its name.
-	if (widget->fb->_bg && widget->fb->_bg->svg_filename.length()) {
+	if (widget->fb->_bg && widget->fb->_bg->svg && widget->fb->_bg->svg->filename.length()) {
 		if (widget->box.size == rack::math::Vec{})
 			widget->box.size = widget->fb->_bg->box.size;
-		return Knob{b, widget->fb->_bg->svg_filename};
+		return Knob{b, widget->fb->_bg->svg->filename};
 
-	} else if (widget->sw->svg_filename.size()) {
-		return Knob{b, widget->sw->svg_filename};
+	} else if (widget->sw->svg->filename.size()) {
+		return Knob{b, widget->sw->svg->filename};
 
 	} else {
-		return Knob{b, widget->svg_filename};
+		return Knob{b, widget->svg->filename};
 	}
 }
 
@@ -54,18 +54,18 @@ Element make_element(rack::app::SliderKnob const *widget, BaseElement b) {
 
 Element make_element_slideswitch(rack::app::SvgSlider const *widget, BaseElement b) {
 	//Note: num_pos and labels are filled in later
-	if (widget->background->svg_filename.length()) {
-		return SlideSwitch{{b, widget->background->svg_filename}, 2, widget->handle->svg_filename};
+	if (widget->background->svg->filename.length()) {
+		return SlideSwitch{{b, widget->background->svg->filename}, 2, widget->handle->svg->filename};
 	} else {
-		return SlideSwitch{{b, widget->svg_filename}, 2, widget->handle->svg_filename};
+		return SlideSwitch{{b, widget->svg->filename}, 2, widget->handle->svg->filename};
 	}
 }
 
 Element make_element(rack::app::SvgSlider const *widget, BaseElement b) {
-	if (widget->background->svg_filename.length()) {
-		return Slider{{b, widget->background->svg_filename}, widget->handle->svg_filename};
+	if (widget->background->svg->filename.length()) {
+		return Slider{{b, widget->background->svg->filename}, widget->handle->svg->filename};
 	} else {
-		return Slider{{b, widget->svg_filename}, widget->handle->svg_filename};
+		return Slider{{b, widget->svg->filename}, widget->handle->svg->filename};
 	}
 }
 
@@ -76,9 +76,9 @@ Element make_element(rack::componentlibrary::Rogan const *widget, BaseElement b)
 	// The SvgKnob::sw rotates
 	// The SvgWidget::svg is apparently not used?
 	if (widget->sw)
-		return Knob{b, widget->sw->svg_filename};
+		return Knob{b, widget->sw->svg->filename};
 	else
-		return Knob{b, widget->svg_filename};
+		return Knob{b, widget->svg->filename};
 }
 
 //
@@ -87,16 +87,17 @@ Element make_element(rack::componentlibrary::Rogan const *widget, BaseElement b)
 
 Element make_element(rack::app::SvgSwitch const *widget, BaseElement b) {
 	if (widget->frames.size() == 3) {
-		return FlipSwitch{{b}, 3, {widget->frames[0], widget->frames[1], widget->frames[2]}};
+		return FlipSwitch{
+			{b}, 3, {widget->frames[0]->filename, widget->frames[1]->filename, widget->frames[2]->filename}};
 
 	} else if (widget->frames.size() == 2) {
 		if (widget->momentary)
-			return MomentaryButton{{b, widget->frames[0]}, widget->frames[1]};
+			return MomentaryButton{{b, widget->frames[0]->filename}, widget->frames[1]->filename};
 		else
-			return FlipSwitch{{b}, 2, {widget->frames[0], widget->frames[1]}};
+			return FlipSwitch{{b}, 2, {widget->frames[0]->filename, widget->frames[1]->filename}};
 
 	} else if (widget->frames.size() == 1) {
-		return MomentaryButton{{b, widget->frames[0]}};
+		return MomentaryButton{{b, widget->frames[0]->filename}};
 
 	} else {
 		pr_warn("make_element(): Unknown SvgSwitch, frames size is not 1, 2 or 3\n");
@@ -121,6 +122,27 @@ Element make_momentary_mono(std::string_view image, NVGcolor c, BaseElement cons
 
 Element make_latching_mono(std::string_view image, NVGcolor c, BaseElement const &el) {
 	return LatchingButton{{{el, image}}, RGB565{c.r, c.g, c.b}};
+}
+
+Element
+make_button_light(rack::app::MultiLightWidget const *light, rack::app::SvgSwitch const *widget, BaseElement const &el) {
+	if (light->getNumColors() == 1) {
+		auto c = light->baseColors[0];
+		if (widget->momentary)
+			return make_momentary_mono(widget->frames[0]->filename, c, el);
+		else
+			return make_latching_mono(widget->frames[0]->filename, c, el);
+	}
+
+	if (light->getNumColors() == 3) {
+		if (widget->momentary)
+			return make_momentary_rgb(widget->frames[0]->filename, el);
+		else
+			return make_latching_rgb(widget->frames[0]->filename, el);
+	}
+
+	// printf("make_element(): Unknown VCVLightBezel\n");
+	return NullElement{};
 }
 
 //
@@ -176,6 +198,22 @@ Element make_element(rack::app::MultiLightWidget const *widget, BaseElement el) 
 	}
 }
 
+Element
+make_multi_led_element(std::string_view image, rack::app::MultiLightWidget const *widget, BaseElement const &el) {
+	if (widget->getNumColors() == 1) {
+		return make_mono_led_element(image, widget, el);
+	}
+	if (widget->getNumColors() == 2) {
+		return make_dual_led_element(image, widget, el);
+	}
+	if (widget->getNumColors() == 3) {
+		return make_rgb_led_element(image, widget, el);
+	}
+
+	pr_warn("SVG Light widget not handled (%d colors)\n", widget->getNumColors());
+	return NullElement{};
+}
+
 //
 // Not supported
 //
@@ -185,9 +223,9 @@ Element make_element(rack::app::SvgScrew const *widget, BaseElement) {
 }
 
 Element make_element(rack::app::ParamWidget const *widget, BaseElement b) {
-	if (widget->svg_filename.size()) {
+	if (widget->svg && widget->svg->filename.size()) {
 		pr_dbg("Unknown ParamWidget, using image as a ParamElement\n");
-		return ParamElement{b, widget->svg_filename};
+		return ParamElement{b, widget->svg->filename};
 
 	} else {
 		pr_dbg("ParamWidget without an SVG, using a blank ParamElement\n");
@@ -198,15 +236,15 @@ Element make_element(rack::app::ParamWidget const *widget, BaseElement b) {
 }
 
 Element make_element(rack::widget::SvgWidget const *widget, BaseElement b) {
-	if (widget->svg_filename.size()) {
+	if (widget->svg->filename.size()) {
 		pr_dbg("Unknown SvgWidget, using image as a ImageElement\n");
-		b.width_mm = to_mm(widget->box.size.x);
-		b.height_mm = to_mm(widget->box.size.y);
-		return ImageElement{b, widget->svg_filename};
+		return ImageElement{b, widget->svg->filename};
 
 	} else {
 		pr_warn("Unknown SvgWidget\n");
-		return NullElement{};
+		b.width_mm = to_mm(widget->box.size.x);
+		b.height_mm = to_mm(widget->box.size.y);
+		return NullElement{b};
 	}
 }
 } // namespace MetaModule
