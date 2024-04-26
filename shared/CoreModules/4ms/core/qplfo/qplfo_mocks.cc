5bf2e68fb8d48aa2e576af20750640f1cdcebc9e
Lennart <Lennart@binarylabs.dev>
2024-04-26 11:18:46 +0200

    qplfo source: convert to class (empty mocks)

---
+++ shared/CoreModules/4ms/core/qplfo/qplfo_mocks.cc
@@ -0,0 +1,33 @@
+#include "qplfo.hh"
+
+namespace MetaModule::QPLFO
+{
+
+void Module::TAPLED_ON(uint8_t x)
+{
+
+}
+void Module::TAPLED_OFF(uint8_t x)
+{
+
+}
+void Module::setDACChannel(uint8_t, int16_t)
+{
+
+}
+uint32_t Module::TAPBUT(uint8_t x)
+{
+    return 0;
+}
+uint32_t Module::PING(uint8_t x)
+{
+    return 0;
+}
+uint32_t Module::RESETJACK(uint8_t x)
+{
+    return 0;
+}
+
+
+
+}
