f2168627f950c41ec068e74b3dca8175fb317857
Dan Green <danngreen1@gmail.com>
2024-05-04 14:27:24 -0700

    Remove comments

--- shared/CoreModules/4ms/core/SCMCore.cc
+++ shared/CoreModules/4ms/core/SCMCore.cc
@@ -312,31 +312,6 @@ private:
 	std::array<int32_t, 8> slipamt;
 	std::array<int32_t, 8> slip;
 
-	// Alt structure: TODO
-	// struct ClockOut {
-	// 	bool is_shuffling;
-	// 	uint32_t base_mult;
-	// 	uint32_t d;		  //mult
-	// 	uint32_t tmr;	  //phase
-	// 	uint32_t per;	  //maxphase
-	// 	uint32_t pw;	  //pw
-	// 	uint32_t p;		  //pulse_ctr
-	// 	uint32_t s;		  //?
-	// 	uint32_t dd;	  //mult_prefast
-	// 	uint32_t slip;	  //?
-	// 	uint32_t slipamt; //?
-	// };
-
-	// std::array<ClockOut, 8> jacks{{
-	// 	{.is_shuffling = false, .base_mult = 1},
-	// 	{.is_shuffling = false, .base_mult = 2},
-	// 	{.is_shuffling = true, .base_mult = 3},
-	// 	{.is_shuffling = true, .base_mult = 4},
-	// 	{.is_shuffling = true, .base_mult = 5},
-	// 	{.is_shuffling = true, .base_mult = 6},
-	// 	{.is_shuffling = true, .base_mult = 8},
-	// 	{.is_shuffling = false, .base_mult = 8},
-	// }};
 };
 
 } // namespace MetaModule
