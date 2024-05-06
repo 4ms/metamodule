ebe9d01907b39fc031e58785ec18318734a2b63c
Robert Kunz | Binary Labs <robert@binarylabs.dev>
2024-05-06 11:00:21 +0200

    sideload digio drivers

--- shared/CoreModules/4ms/core/MPEGCore.cc
+++ shared/CoreModules/4ms/core/MPEGCore.cc
@@ -4,6 +4,8 @@
 
 #include "peg/main.hh"
 
+#include "helpers/FlipFlop.h"
+
 namespace MetaModule
 {
 
@@ -13,16 +15,32 @@ class MPEGCore : public SmartCoreProcessor<MPEGInfo> {
 	using enum Info::Elem;
 
 public:
-	MPEGCore()
+	MPEGCore() :
+		pingIn(1.f,2.f),
+		cycleIn(1.f,2.f),
+		triggerIn(1.f,2.f)
 	{
 
 	};
 
 	void update() override {
+		sideloadDrivers();
 		peg.update();
 	}
 
 	void set_samplerate(float sr) override {
+	}
+
+private:
+	void sideloadDrivers()
+	{
+		peg.digio.PingBut.sideload_set(getState<PingButton>() == MomentaryButton::State_t::PRESSED);
+		peg.digio.CycleBut.sideload_set(getState<CycleButton>() == LatchingButton::State_t::DOWN);
+
+		peg.digio.PingJack.sideload_set(pingIn(getInput<PingTrigIn>().value_or(0.f)));
+		peg.digio.CycleJack.sideload_set(cycleIn(getInput<CycleTrigIn>().value_or(0.f)));
+		peg.digio.TrigJack.sideload_set(triggerIn(getInput<TriggerIn>().value_or(0.f)));
+
 	}
 
 private:
@@ -33,6 +51,11 @@ private:
 	static std::unique_ptr<CoreProcessor> create() { return std::make_unique<ThisCore>(); }
 	static inline bool s_registered = ModuleFactory::registerModuleType(Info::slug, create, ModuleInfoView::makeView<Info>());
 	// clang-format on
+
+private:
+	FlipFlop pingIn;
+	FlipFlop cycleIn;
+	FlipFlop triggerIn;
 };
 
 } // namespace MetaModule
