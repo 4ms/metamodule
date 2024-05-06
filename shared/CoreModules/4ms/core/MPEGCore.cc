b27dd35102af8a96d09e7bacff2e9b92102bb0ef
Lennart <Lennart@binarylabs.dev>
2024-05-06 14:19:24 +0200

    MPEGCore: cleanup driver sideloading

--- shared/CoreModules/4ms/core/MPEGCore.cc
+++ shared/CoreModules/4ms/core/MPEGCore.cc
@@ -49,12 +49,24 @@ public:
 private:
 	void sideloadDrivers()
 	{
-		peg.adc_dma_buffer[0] = uint16_t(std::clamp(getInput<ShapeCvIn>().value_or(0.f) / -10.f + 0.5f, 0.f, 1.f) * 4095.f);
-		peg.adc_dma_buffer[1] = uint16_t(std::clamp(getInput<DivIn>().value_or(0.f) / -10.f + 0.5f, 0.f, 1.f) * 4095.f);
-		peg.adc_dma_buffer[2] = uint16_t(getState<ScaleKnob>() * 4095.f);
-		peg.adc_dma_buffer[3] = uint16_t(getState<OffsetKnob>() * 4095.f);
-		peg.adc_dma_buffer[4] = uint16_t(getState<ShapeKnob>() * 4095.f);
-		peg.adc_dma_buffer[5] = uint16_t(getState<Div_MultKnob>() * 4095.f);
+		auto MapOutputFunc = [](auto val) -> uint16_t
+		{
+			auto result = val / CVInputFullScaleInV + 0.5f;
+			return uint16_t(std::clamp(result, 0.f, 1.f) * 4095.f);
+		};
+
+		peg.adc_dma_buffer[CV_SHAPE]   = MapOutputFunc(getInput<ShapeCvIn>().value_or(0.f));
+		peg.adc_dma_buffer[CV_DIVMULT] = MapOutputFunc(getInput<DivIn>().value_or(0.f));
+
+		auto MapKnobFunc = [](auto val) -> uint16_t
+		{
+			return uint16_t(val * 4095.f);
+		};
+
+		peg.adc_dma_buffer[POT_SCALE]   = MapKnobFunc(getState<ScaleKnob>());
+		peg.adc_dma_buffer[POT_OFFSET]  = MapKnobFunc(getState<OffsetKnob>());
+		peg.adc_dma_buffer[POT_SHAPE]   = MapKnobFunc(getState<ShapeKnob>());
+		peg.adc_dma_buffer[POT_DIVMULT] = MapKnobFunc(getState<Div_MultKnob>());
 
 		peg.digio.PingBut.sideload_set(getState<PingButton>() == MomentaryButton::State_t::PRESSED);
 		peg.digio.CycleBut.sideload_set(getState<CycleButton>() == MomentaryButton::State_t::PRESSED);
@@ -63,32 +75,45 @@ private:
 		peg.digio.CycleJack.sideload_set(cycleIn(getInput<CycleTrigIn>().value_or(0.f)));
 		peg.digio.TrigJack.sideload_set(triggerIn(getInput<TriggerIn>().value_or(0.f)));
 
-		setOutput<EofOut>(peg.digio.EOJack.sideload_get() ? 5.0f : 0.f);
+		setOutput<EofOut>(peg.digio.EOJack.sideload_get() ? TriggerOutputInV : 0.f);
 
-		setOutput<EnvOut>(-20.f/4095.f * float(peg.dac_vals[0]) + 10.f);
-		setOutput<_5VEnvOut>( float(peg.dac_vals[1]) / 4095.f * 5.f);
+		auto MapDACFunc = [](auto val) -> float
+		{
+			return float(val) / 4095.f;
+		};
 
-		auto pwmToFloat = [](uint16_t pwm_val){return float(pwm_val)/float(4095);};
+		setOutput<EnvOut>(MapDACFunc(peg.dac_vals[0]) * EnvelopeOutFullScaleInV + EnvelopeOutOffsetInV);
+		setOutput<_5VEnvOut>(MapDACFunc(peg.dac_vals[1]) * 5.f);
 
-		setLED<EnvOutLight>(std::array<float,3>{pwmToFloat(peg.pwm_vals[PWM_ENVA_R]),pwmToFloat(peg.pwm_vals[PWM_ENVA_G]),pwmToFloat(peg.pwm_vals[PWM_ENVA_B])});
-		setLED<_5VEnvLight>(std::array<float,3>{pwmToFloat(peg.pwm_vals[PWM_ENVB_R]),pwmToFloat(peg.pwm_vals[PWM_ENVB_G]),pwmToFloat(peg.pwm_vals[PWM_ENVB_B])});
-		setLED<CycleButton>(std::array<float,3>{pwmToFloat(peg.pwm_vals[PWM_CYCLEBUT_R]),pwmToFloat(peg.pwm_vals[PWM_CYCLEBUT_G]),pwmToFloat(peg.pwm_vals[PWM_CYCLEBUT_B])});
-		setLED<PingButton>(std::array<float,3>{pwmToFloat(peg.pwm_vals[PWM_PINGBUT_R]),pwmToFloat(peg.pwm_vals[PWM_PINGBUT_G]),pwmToFloat(peg.pwm_vals[PWM_PINGBUT_B])});
-		setLED<EofLight>(pwmToFloat(peg.pwm_vals[PWM_EOF_LED]));
+		auto PWMToFloatFunc = [](uint16_t pwm_val) -> float
+		{
+			return float(pwm_val) / float(4095);
+		};
+
+		setLED<EnvOutLight>(std::array<float,3>{PWMToFloatFunc(peg.pwm_vals[PWM_ENVA_R]),PWMToFloatFunc(peg.pwm_vals[PWM_ENVA_G]),PWMToFloatFunc(peg.pwm_vals[PWM_ENVA_B])});
+		setLED<_5VEnvLight>(std::array<float,3>{PWMToFloatFunc(peg.pwm_vals[PWM_ENVB_R]),PWMToFloatFunc(peg.pwm_vals[PWM_ENVB_G]),PWMToFloatFunc(peg.pwm_vals[PWM_ENVB_B])});
+		setLED<CycleButton>(std::array<float,3>{PWMToFloatFunc(peg.pwm_vals[PWM_CYCLEBUT_R]),PWMToFloatFunc(peg.pwm_vals[PWM_CYCLEBUT_G]),PWMToFloatFunc(peg.pwm_vals[PWM_CYCLEBUT_B])});
+		setLED<PingButton>(std::array<float,3>{PWMToFloatFunc(peg.pwm_vals[PWM_PINGBUT_R]),PWMToFloatFunc(peg.pwm_vals[PWM_PINGBUT_G]),PWMToFloatFunc(peg.pwm_vals[PWM_PINGBUT_B])});
+		setLED<EofLight>(PWMToFloatFunc(peg.pwm_vals[PWM_EOF_LED]));
 	}
 
 	void sideloadSystemSettings()
 	{
-		peg.settings.limit_skew = getState<SkewLimitAltParam>();
+		peg.settings.limit_skew          = getState<SkewLimitAltParam>();
 		//TODO: this might need scaling
-		peg.settings.free_running_ping = getState<FreeNRunningPingAltParam>();
-		peg.settings.trigout_is_trig = getState<EofJackTypeAltParam>();
-		peg.settings.trigin_function = TrigInFunctions(getState<TrigJackModeAltParam>());
-		peg.settings.trigout_function = TrigOutFunctions(getState<EofJackModeAltParam>());
-
+		peg.settings.free_running_ping   = getState<FreeNRunningPingAltParam>();
+		peg.settings.trigout_is_trig     = getState<EofJackTypeAltParam>();
+		peg.settings.trigin_function     = TrigInFunctions(getState<TrigJackModeAltParam>());
+		peg.settings.trigout_function    = TrigOutFunctions(getState<EofJackModeAltParam>());
 		peg.settings.cycle_jack_behavior = CycleJackBehaviors(getState<CycleJackModeAltParam>());
-		peg.settings.shift_value = int32_t(getState<ShiftAltParam>() * 4095.f - 2048.f);
+		peg.settings.shift_value         = int32_t(getState<ShiftAltParam>() * 4095.f - 2048.f);
 	}
+
+private:
+	static constexpr float CVInputFullScaleInV     = -10.0f;
+	static constexpr float TriggerOutputInV        = 5.0f;
+	static constexpr float EnvelopeOutFullScaleInV = -20.0f;
+	static constexpr float EnvelopeOutOffsetInV    = 10.0f;
 
 private:
 	PEG::MiniPEG peg;
