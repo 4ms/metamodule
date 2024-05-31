241e8ac3be101c7ba22012e21ced7a774bfe7699
Dan Green <danngreen1@gmail.com>
2024-05-30 22:36:36 -0700

    Add FIXME comment (see issue #272)

--- shared/CoreModules/4ms/4ms_element_state_conversions.hh
+++ shared/CoreModules/4ms/4ms_element_state_conversions.hh
@@ -7,6 +7,8 @@
 namespace MetaModule::StateConversion
 {
 
+// FIXME: These are never called because convertState() is only called for direct members of the Element variant, not derived types
+
 template<typename T>
 constexpr Toggle2pos::State_t convertState(const T &, float val) requires(std::derived_from<T, Toggle2pos>)
 {
