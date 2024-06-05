984db663b36b19199dd2d16716f8bbbe55ff14df
Dan Green <danngreen1@gmail.com>
2024-06-05 10:49:11 -0700

    SvgButton knows its size

--- firmware/vcv_plugin/src/app/SvgButton.cc
+++ firmware/vcv_plugin/src/app/SvgButton.cc
@@ -8,7 +8,8 @@ void SvgButton::addFrame(std::shared_ptr<window::Svg> svg) {
 	if (svg->filename.length()) {
 		frames.push_back(svg);
 		if (frames.size() == 1) {
-			sw->setSvg(svg);	
+			sw->setSvg(svg);
+			box.size = get_svg_size(svg->filename);
 		}
 	}
 }
