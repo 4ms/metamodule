09524a30ab141dd806bec22f22b0d70a78116465
Dan Green <danngreen1@gmail.com>
2024-06-05 17:01:27 -0700

    PatchStorage: Can save/duplicate a patch

--- firmware/src/patch_file/file_storage_proxy.hh
+++ firmware/src/patch_file/file_storage_proxy.hh
@@ -198,6 +198,17 @@ public:
 		view_patch_loc_.vol = Volume::RamDisk;
 	}
 
+	void duplicate_view_patch(std::string_view filepath, Volume vol) {
+		view_patch_loc_.filename.copy(filepath);
+		view_patch_loc_.vol = vol;
+
+		auto patch = open_patches_.overwrite(view_patch_loc_, {});
+		*patch = *view_patch_; //copy
+		view_patch_ = patch;
+
+		write_patch();
+	}
+
 	void update_view_patch_module_states(std::vector<ModuleInitState> const &states) {
 		view_patch_->module_states = states; //copy
 	}
