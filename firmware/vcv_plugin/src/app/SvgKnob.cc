ea78efad133edc63a065aa77b0c55913e2ddbbf5
Dan Green <danngreen1@gmail.com>
2024-06-06 22:12:05 -0700

    Remove _bg.png workaround for SvgKnobs

--- firmware/vcv_plugin/src/app/SvgKnob.cc
+++ firmware/vcv_plugin/src/app/SvgKnob.cc
@@ -7,6 +7,8 @@ namespace rack::app
 
 void SvgKnob::setSvg(std::shared_ptr<window::Svg> svg) {
 	sw->setSvg(svg);
+	tw->box.size = sw->box.size;
+	fb->box.size = sw->box.size;
 	box.size = sw->box.size;
 	pr_dbg("SvgKnob::setSvg(%s) set box.size to %f x %f\n", svg->filename.c_str(), box.size.x, box.size.y);
 }
--- firmware/vcv_plugin/src/widget/FramebufferWidget.cc
+++ firmware/vcv_plugin/src/widget/FramebufferWidget.cc
@@ -4,14 +4,9 @@ namespace rack::widget
 {
 
 void FramebufferWidget::addChildBelow(SvgWidget *child, Widget *sibling) {
-	//we take ownership
-	_bg = child;
-	printf("FramebufferWidget::_bg = %p\n", _bg);
+	Widget::addChildBelow(child, sibling);
 }
 
-FramebufferWidget::~FramebufferWidget() {
-	if (_bg)
-		delete _bg;
-}
+FramebufferWidget::~FramebufferWidget() = default;
 
 } // namespace rack::widget
