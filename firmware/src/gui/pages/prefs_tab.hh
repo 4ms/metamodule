1586d5c4892227086a2f883d6024e2f4127f1801
Dan Green <danngreen1@gmail.com>
2024-06-13 15:00:37 -0700

    Update system menu tabs to new gui

--- firmware/src/gui/pages/prefs_tab.hh
+++ firmware/src/gui/pages/prefs_tab.hh
@@ -2,7 +2,6 @@
 #include "dynload/plugin_loader.hh"
 #include "gui/helpers/lv_helpers.hh"
 #include "gui/pages/base.hh"
-#include "gui/pages/confirm_popup.hh"
 #include "gui/pages/page_list.hh"
 #include "gui/slsexport/meta5/ui.h"
 #include "gui/styles.hh"
@@ -12,25 +11,14 @@ namespace MetaModule
 
 struct PrefsTab {
 
-	PrefsTab(FileStorageProxy &patch_storage)
-		: file_storage{patch_storage} {
-		lv_obj_add_event_cb(ui_ResetFactoryPatchesButton, resetbut_cb, LV_EVENT_CLICKED, this);
-	}
+	PrefsTab() = default;
 
 	void prepare_focus(lv_group_t *group) {
 		this->group = group;
-		lv_group_remove_obj(ui_ResetFactoryPatchesButton);
-		lv_group_add_obj(group, ui_ResetFactoryPatchesButton);
-		lv_group_focus_obj(ui_ResetFactoryPatchesButton);
-		confirm_popup.init(ui_SystemMenu, group);
 	}
 
 	// Returns true if this pages uses the back event
 	bool consume_back_event() {
-		if (confirm_popup.is_visible()) {
-			confirm_popup.hide();
-			return true;
-		}
 		return false;
 	}
 
@@ -38,25 +26,7 @@ struct PrefsTab {
 	}
 
 private:
-	static void resetbut_cb(lv_event_t *event) {
-		if (!event || !event->user_data)
-			return;
-
-		auto page = static_cast<PrefsTab *>(event->user_data);
-		page->confirm_popup.show(
-			[page](bool ok) {
-				if (ok) {
-					page->file_storage.request_reset_factory_patches();
-				}
-			},
-			"Do you really want to PERMENENTLY DELETE all patches stored internally? This will replace them with "
-			"factory default patches.\n",
-			"Delete");
-	}
-
 	lv_group_t *group = nullptr;
-	FileStorageProxy &file_storage;
-	ConfirmPopup confirm_popup;
 };
 
 } // namespace MetaModule
--- firmware/src/gui/pages/system_status_tab.hh
+++ firmware/src/gui/pages/system_status_tab.hh
@@ -2,6 +2,7 @@
 #include "git_version.h"
 #include "gui/helpers/lv_helpers.hh"
 #include "gui/pages/base.hh"
+#include "gui/pages/confirm_popup.hh"
 #include "gui/pages/page_list.hh"
 #include "gui/slsexport/meta5/ui.h"
 #include "gui/styles.hh"
@@ -11,6 +12,11 @@ namespace MetaModule
 
 struct SystemStatusTab {
 
+	SystemStatusTab(FileStorageProxy &patch_storage)
+		: storage{patch_storage} {
+		lv_obj_add_event_cb(ui_ResetFactoryPatchesButton, resetbut_cb, LV_EVENT_CLICKED, this);
+	}
+
 	void prepare_focus(lv_group_t *group) {
 		this->group = group;
 
@@ -19,12 +25,47 @@ struct SystemStatusTab {
 			fw_version.remove_prefix(9);
 
 		lv_label_set_text_fmt(ui_SystemMenuFWversion, "Firmware version: %s", fw_version.data());
+
+		lv_group_remove_obj(ui_SystemCalibrationButton);
+		lv_group_remove_obj(ui_ResetFactoryPatchesButton);
+		lv_group_add_obj(group, ui_SystemCalibrationButton);
+		lv_group_add_obj(group, ui_ResetFactoryPatchesButton);
+
+		lv_group_focus_obj(ui_SystemCalibrationButton);
+		confirm_popup.init(ui_SystemMenu, group);
+	}
+
+	// Returns true if this pages uses the back event
+	bool consume_back_event() {
+		if (confirm_popup.is_visible()) {
+			confirm_popup.hide();
+			return true;
+		}
+		return false;
 	}
 
 	void update() {
 	}
 
 private:
+	static void resetbut_cb(lv_event_t *event) {
+		if (!event || !event->user_data)
+			return;
+
+		auto page = static_cast<SystemStatusTab *>(event->user_data);
+		page->confirm_popup.show(
+			[page](bool ok) {
+				if (ok) {
+					page->storage.request_reset_factory_patches();
+				}
+			},
+			"Do you really want to PERMENENTLY DELETE all patches stored internally? This will replace them with "
+			"factory default patches.\n",
+			"Delete");
+	}
+
 	lv_group_t *group = nullptr;
+	FileStorageProxy &storage;
+	ConfirmPopup confirm_popup;
 };
 } // namespace MetaModule
