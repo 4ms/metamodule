1e21b118d61c018e10357739813eef1c537eb5c1
Dan Green <danngreen1@gmail.com>
2024-06-24 09:29:55 -0700

    Calibration flash layout is a sector (4k)

--- firmware/src/fs/norflash_layout.hh
+++ firmware/src/fs/norflash_layout.hh
@@ -3,12 +3,12 @@
 
 //first 10MB is for bootloaders (512kB) and application (9.5MB)
 //next 4MB is Assets (4MB is reserved for this but for now we keep the image to 2MB for faster flashing)
-//last 2MB is pathches
+//last 2MB is pathces, minus 4k for Calibration data
 static constexpr uint32_t AssetVolFlashOffset = 10 * 1024 * 1024; //0xA00000;
 static constexpr uint32_t AssetVolFlashSize = 2 * 1024 * 1024;
 
 static constexpr uint32_t PatchVolFlashOffset = 14 * 1024 * 1024; //0xE00000;
-static constexpr uint32_t PatchVolFlashSize = (2 * 1024 * 1024) - (2 * 1024);
+static constexpr uint32_t PatchVolFlashSize = (2 * 1024 * 1024) - (4096);
 
 static constexpr uint32_t CalDataFlashOffset = PatchVolFlashOffset + PatchVolFlashSize;
-static constexpr uint32_t CalDataFlashSize = 2 * 1024;
+static constexpr uint32_t CalDataFlashSize = 4096;
