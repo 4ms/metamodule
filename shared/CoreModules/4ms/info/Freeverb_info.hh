fd361d9481353c056aae4a92a8b2f7392045116e
Dan Green <danngreen1@gmail.com>
2024-06-26 16:20:52 -0700

    Update Freeverb artwork: -> smaller, rename knobs

--- shared/CoreModules/4ms/info/Freeverb_info.hh
+++ shared/CoreModules/4ms/info/Freeverb_info.hh
@@ -9,23 +9,23 @@ namespace MetaModule
 struct FreeverbInfo : ModuleInfoBase {
     static constexpr std::string_view slug{"Freeverb"};
     static constexpr std::string_view description{"Reverb Effect"};
-    static constexpr uint32_t width_hp = 10;
+    static constexpr uint32_t width_hp = 8;
     static constexpr std::string_view svg_filename{"res/modules/Freeverb_artwork.svg"};
     static constexpr std::string_view png_filename{"4ms/fp/Freeverb.png"};
 
     using enum Coords;
 
     static constexpr std::array<Element, 10> Elements{{
-		Davies1900hBlackKnob{{to_mm<72>(37.85), to_mm<72>(46.42), Center, "Size", ""}, 0.25f},
-		Davies1900hBlackKnob{{to_mm<72>(106.4), to_mm<72>(46.42), Center, "Time", ""}, 0.25f},
-		Davies1900hBlackKnob{{to_mm<72>(37.85), to_mm<72>(104.62), Center, "Damp", ""}, 0.25f},
-		Davies1900hBlackKnob{{to_mm<72>(106.4), to_mm<72>(104.62), Center, "Mix", ""}, 0.875f},
-		AnalogJackInput4ms{{to_mm<72>(29.94), to_mm<72>(232.1), Center, "Input", ""}},
-		AnalogJackInput4ms{{to_mm<72>(72.13), to_mm<72>(232.1), Center, "Size CV", ""}},
-		AnalogJackInput4ms{{to_mm<72>(115.17), to_mm<72>(232.1), Center, "Time CV", ""}},
-		AnalogJackInput4ms{{to_mm<72>(29.94), to_mm<72>(272.23), Center, "Damp CV", ""}},
-		AnalogJackInput4ms{{to_mm<72>(72.13), to_mm<72>(272.23), Center, "Ratio CV", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(115.17), to_mm<72>(313.84), Center, "Out", ""}},
+		Davies1900hBlackKnob{{to_mm<72>(31.96), to_mm<72>(57.97), Center, "Size", ""}, 0.5f},
+		Davies1900hBlackKnob{{to_mm<72>(83.49), to_mm<72>(57.97), Center, "Time", ""}, 0.5f},
+		Davies1900hBlackKnob{{to_mm<72>(31.96), to_mm<72>(119.21), Center, "Damp", ""}, 0.25f},
+		Davies1900hBlackKnob{{to_mm<72>(83.49), to_mm<72>(119.21), Center, "Mix", ""}, 0.875f},
+		AnalogJackInput4ms{{to_mm<72>(32.13), to_mm<72>(214.66), Center, "Input", ""}},
+		AnalogJackInput4ms{{to_mm<72>(83.32), to_mm<72>(214.66), Center, "Size CV", ""}},
+		AnalogJackInput4ms{{to_mm<72>(32.13), to_mm<72>(263.38), Center, "Time CV", ""}},
+		AnalogJackInput4ms{{to_mm<72>(83.32), to_mm<72>(263.38), Center, "Damp CV", ""}},
+		AnalogJackInput4ms{{to_mm<72>(32.13), to_mm<72>(312.1), Center, "Mix CV", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(83.32), to_mm<72>(312.1), Center, "Out", ""}},
 }};
 
     enum class Elem {
@@ -37,7 +37,7 @@ struct FreeverbInfo : ModuleInfoBase {
         SizeCvIn,
         TimeCvIn,
         DampCvIn,
-        RatioCvIn,
+        MixCvIn,
         Out,
     };
 
@@ -57,7 +57,7 @@ struct FreeverbInfo : ModuleInfoBase {
         InputSize_Cv, 
         InputTime_Cv, 
         InputDamp_Cv, 
-        InputRatio_Cv, 
+        InputMix_Cv, 
         NumInJacks,
     };
     
