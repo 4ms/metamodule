1e3af3b480a971a9d21f7a0728bdc9483126309f
Lennart <Lennart@binarylabs.dev>
2024-07-08 12:51:28 +0200

    wifi: restructure file

--- firmware/src/wifi/comm/wifi_interface.cc
+++ firmware/src/wifi/comm/wifi_interface.cc
@@ -34,6 +34,129 @@ enum ChannelID_t : uint8_t {Broadcast = 0, Management = 1, Connections = 2};
 
 std::optional<IPAddress_t> currentIPAddress;
 
+void handle_management_channel(std::span<uint8_t>);
+void handle_client_channel(uint8_t, std::span<uint8_t>);
+
+///////////////////////////
+
+void sendFrame(uint8_t channel, std::span<uint8_t> payload) {
+	uint16_t payloadLength = payload.size() + 1;
+
+	framer.sendStart(BufferedUSART2::transmit);
+	framer.sendPayload(uint8_t(payloadLength & 0xFF), BufferedUSART2::transmit);
+	framer.sendPayload(uint8_t((payloadLength & 0xFF00) >> 8), BufferedUSART2::transmit);
+	framer.sendPayload(channel, BufferedUSART2::transmit);
+
+	for (auto p : payload) {
+		framer.sendPayload(p, BufferedUSART2::transmit);
+	}
+	framer.sendStop(BufferedUSART2::transmit);
+}
+
+void receiveFrame(std::span<uint8_t> fullFrame) {
+	if (fullFrame.size() >= 2) {
+		uint16_t parsedLength = fullFrame[0] | fullFrame[1] << 8;
+		uint16_t actualLength = fullFrame.size() - 2;
+		if (parsedLength == actualLength) {
+			auto destination = fullFrame[2];
+			auto payload = fullFrame.subspan(3, fullFrame.size() - 3);
+
+			if (destination == ChannelID_t::Management)
+			{
+				handle_management_channel(payload);
+			}
+			else
+			{
+				handle_client_channel(destination, payload);
+			}
+
+		} else {
+			pr_err("Invalid length (parsed %u actual %u)", parsedLength, actualLength);
+		}
+	}
+};
+
+///////////////////////////
+
+void requestIP()
+{
+	// For now, every request on the management channel is responded the IP
+	std::array<uint8_t,3> payload{0xA, 0xB, 0xC};
+	sendFrame(ChannelID_t::Management, std::span(payload));
+}
+
+void send_heartbeat()
+{
+	flatbuffers::FlatBufferBuilder fbb;
+	auto answer = fbb.CreateStruct(Heartbeat());
+	auto message = CreateMessage(fbb, AnyMessage_Heartbeat, answer.Union());
+	fbb.Finish(message);
+
+	sendFrame(ChannelID_t::Broadcast, fbb.GetBufferSpan());
+}
+
+Timestamp_t getTimestamp()
+{
+	return HAL_GetTick();
+}
+
+////////////////////////////////
+
+void init(PatchStorage *storage) {
+	printf("Initializing Wifi\n");
+
+	patchStorage = storage;
+}
+
+void start() {
+	pr_dbg("Wifi: Starting RX\n");
+	BufferedUSART2::init();
+}
+
+void stop() {
+	pr_dbg("Wifi: Stopping RX\n");
+	BufferedUSART2::deinit();
+}
+
+void run() {
+	if (auto val = BufferedUSART2::receive(); val) {
+		deframer.parse(*val, receiveFrame);
+	}
+
+	if (not lastHeartbeatSentTime or (getTimestamp() - *lastHeartbeatSentTime) > HeartbeatInterval)
+	{
+		send_heartbeat();
+		lastHeartbeatSentTime = getTimestamp();
+
+		requestIP();
+	}
+}
+
+std::optional<IPAddress_t> getCurrentIP()
+{
+	return currentIPAddress;
+}
+
+////////////////////////////////
+
+void handle_management_channel(std::span<uint8_t> payload)
+{
+	if (payload.size() == 4)
+	{
+		const IPAddress_t DummyAddress{0,0,0,0};
+
+		if (std::equal(DummyAddress.begin(), DummyAddress.end(), payload.data()))
+		{
+			currentIPAddress.reset();
+		}
+		else
+		{
+			currentIPAddress = DummyAddress;
+			std::copy(payload.begin(), payload.end(), currentIPAddress->data());
+		}
+	}
+}
+
 flatbuffers::Offset<Message> constructPatchesMessage(flatbuffers::FlatBufferBuilder &fbb) {
 	auto CreateVector = [&fbb](auto fileList) {
 		std::vector<flatbuffers::Offset<PatchInfo>> elems(fileList.files.size());
@@ -60,198 +183,90 @@ flatbuffers::Offset<Message> constructPatchesMessage(flatbuffers::FlatBufferBuil
 	return message;
 }
 
-///////////////////////////
+void handle_client_channel(uint8_t destination, std::span<uint8_t> payload) {
 
-void sendFrame(uint8_t channel, std::span<uint8_t> payload) {
-	uint16_t payloadLength = payload.size() + 1;
+	auto sendResponse = [destination](auto payload) {
+		sendFrame(destination, payload);
+	};
 
-	framer.sendStart(BufferedUSART2::transmit);
-	framer.sendPayload(uint8_t(payloadLength & 0xFF), BufferedUSART2::transmit);
-	framer.sendPayload(uint8_t((payloadLength & 0xFF00) >> 8), BufferedUSART2::transmit);
-	framer.sendPayload(channel, BufferedUSART2::transmit);
+	auto sendBroadcast = [](auto payload) {
+		sendFrame(ChannelID_t::Broadcast, payload);
+	};
 
-	for (auto p : payload) {
-		framer.sendPayload(p, BufferedUSART2::transmit);
-	}
-	framer.sendStop(BufferedUSART2::transmit);
-}
+	// Parse message
 
-void receiveFrame(std::span<uint8_t> fullFrame) {
-	if (fullFrame.size() >= 2) {
-		uint16_t parsedLength = fullFrame[0] | fullFrame[1] << 8;
-		uint16_t actualLength = fullFrame.size() - 2;
-		if (parsedLength == actualLength) {
-			auto destination = fullFrame[2];
-			auto payload = fullFrame.subspan(3, fullFrame.size() - 3);
-			handle_received_frame(destination, payload);
-		} else {
-			pr_err("Invalid length (parsed %u actual %u)", parsedLength, actualLength);
-		}
-	}
-};
+	auto message = GetMessage(payload.data());
 
-void sendBroadcast(std::span<uint8_t> payload) {
-	sendFrame(ChannelID_t::Broadcast, payload);
-}
+	if (auto content = message->content(); content) {
+		if (auto patchNameMessage = message->content_as_Patches(); patchNameMessage) {
+			flatbuffers::FlatBufferBuilder fbb;
+			auto message = constructPatchesMessage(fbb);
+			fbb.Finish(message);
 
-void requestIP()
-{
-	// For now, every request on the management channel is responded the IP
-	std::array<uint8_t,3> payload{0xA, 0xB, 0xC};
-	sendFrame(ChannelID_t::Management, std::span(payload));
-}
+			sendResponse(fbb.GetBufferSpan());
+		} else if (auto uploadPatchMessage = message->content_as_UploadPatch(); uploadPatchMessage) {
+			auto destination = uploadPatchMessage->destination();
 
-std::optional<IPAddress_t> getCurrentIP()
-{
-       return currentIPAddress;
-}
+			assert(uploadPatchMessage->content()->is_span_observable);
+			auto receivedPatchData =
+				std::span(uploadPatchMessage->content()->data(), uploadPatchMessage->content()->size());
 
-////////////////////////////////
+			auto filename = flatbuffers::GetStringView(uploadPatchMessage->filename());
 
-void init(PatchStorage *storage) {
-	printf("Initializing Wifi\n");
+			printf("Received Patch of %u bytes for location %u\n", receivedPatchData.size(), destination);
 
-	patchStorage = storage;
-}
+			auto LocationToVolume = [](auto location) -> std::optional<Volume> {
+				switch (location) {
+					case StorageLocation::StorageLocation_USB:
+						return Volume::USB;
+					case StorageLocation::StorageLocation_FLASH:
+						return Volume::NorFlash;
+					case StorageLocation::StorageLocation_SDCARD:
+						return Volume::SDCard;
+					default:
+						return std::nullopt;
+				}
+			};
 
-void start() {
-	pr_dbg("Wifi: Starting RX\n");
-	BufferedUSART2::init();
-}
+			flatbuffers::FlatBufferBuilder fbb;
+			bool filesUpdated = false;
 
-void stop() {
-	pr_dbg("Wifi: Stopping RX\n");
-	BufferedUSART2::deinit();
-}
+			if (auto thisVolume = LocationToVolume(destination); thisVolume) {
+				auto success = patchStorage->write_file(*thisVolume, filename, receivedPatchData);
 
-Timestamp_t getTimestamp()
-{
-	return HAL_GetTick();
-}
+				if (success) {
+					auto result = CreateResult(fbb, true);
+					auto message = CreateMessage(fbb, AnyMessage_Result, result.Union());
+					fbb.Finish(message);
 
-void run() {
-	if (auto val = BufferedUSART2::receive(); val) {
-		deframer.parse(*val, receiveFrame);
-	}
-
-	if (not lastHeartbeatSentTime or (getTimestamp() - *lastHeartbeatSentTime) > HeartbeatInterval)
-	{
-		send_heartbeat();
-		lastHeartbeatSentTime = getTimestamp();
-
-		requestIP();
-	}
-}
-
-void send_heartbeat()
-{
-	flatbuffers::FlatBufferBuilder fbb;
-	auto answer = fbb.CreateStruct(Heartbeat());
-	auto message = CreateMessage(fbb, AnyMessage_Heartbeat, answer.Union());
-	fbb.Finish(message);
-
-	sendBroadcast(fbb.GetBufferSpan());
-}
-
-void handle_received_frame(uint8_t destination, std::span<uint8_t> payload) {
-	if (destination == ChannelID_t::Management)
-	{
-		if (payload.size() == 4)
-		{
-			const IPAddress_t DummyAddress{0,0,0,0};
-
-			if (std::equal(DummyAddress.begin(), DummyAddress.end(), payload.data()))
-			{
-				currentIPAddress.reset();
-			}
-			else
-			{
-				currentIPAddress = DummyAddress;
-				std::copy(payload.begin(), payload.end(), currentIPAddress->data());
-			}
-		}
-	}
-	else
-	{
-		auto sendResponse = [destination](auto payload) {
-			sendFrame(destination, payload);
-		};
-
-		// Parse message
-
-		auto message = GetMessage(payload.data());
-
-		if (auto content = message->content(); content) {
-			if (auto patchNameMessage = message->content_as_Patches(); patchNameMessage) {
-				flatbuffers::FlatBufferBuilder fbb;
-				auto message = constructPatchesMessage(fbb);
-				fbb.Finish(message);
-
-				sendResponse(fbb.GetBufferSpan());
-			} else if (auto uploadPatchMessage = message->content_as_UploadPatch(); uploadPatchMessage) {
-				auto destination = uploadPatchMessage->destination();
-
-				assert(uploadPatchMessage->content()->is_span_observable);
-				auto receivedPatchData =
-					std::span(uploadPatchMessage->content()->data(), uploadPatchMessage->content()->size());
-
-				auto filename = flatbuffers::GetStringView(uploadPatchMessage->filename());
-
-				printf("Received Patch of %u bytes for location %u\n", receivedPatchData.size(), destination);
-
-				auto LocationToVolume = [](auto location) -> std::optional<Volume> {
-					switch (location) {
-						case StorageLocation::StorageLocation_USB:
-							return Volume::USB;
-						case StorageLocation::StorageLocation_FLASH:
-							return Volume::NorFlash;
-						case StorageLocation::StorageLocation_SDCARD:
-							return Volume::SDCard;
-						default:
-							return std::nullopt;
-					}
-				};
-
-				flatbuffers::FlatBufferBuilder fbb;
-				bool filesUpdated = false;
-
-				if (auto thisVolume = LocationToVolume(destination); thisVolume) {
-					auto success = patchStorage->write_file(*thisVolume, filename, receivedPatchData);
-
-					if (success) {
-						auto result = CreateResult(fbb, true);
-						auto message = CreateMessage(fbb, AnyMessage_Result, result.Union());
-						fbb.Finish(message);
-
-						filesUpdated = true;
-					} else {
-						auto description = fbb.CreateString("Saving failed");
-						auto result = CreateResult(fbb, false, description);
-						auto message = CreateMessage(fbb, AnyMessage_Result, result.Union());
-						fbb.Finish(message);
-					}
+					filesUpdated = true;
 				} else {
-					auto description = fbb.CreateString("Invalid volume id");
+					auto description = fbb.CreateString("Saving failed");
 					auto result = CreateResult(fbb, false, description);
 					auto message = CreateMessage(fbb, AnyMessage_Result, result.Union());
 					fbb.Finish(message);
 				}
-
-				sendResponse(fbb.GetBufferSpan());
-
-				if (filesUpdated) {
-					flatbuffers::FlatBufferBuilder fbb;
-					auto message = constructPatchesMessage(fbb);
-					fbb.Finish(message);
-
-					sendBroadcast(fbb.GetBufferSpan());
-				}
 			} else {
-				printf("Other option\n");
+				auto description = fbb.CreateString("Invalid volume id");
+				auto result = CreateResult(fbb, false, description);
+				auto message = CreateMessage(fbb, AnyMessage_Result, result.Union());
+				fbb.Finish(message);
+			}
+
+			sendResponse(fbb.GetBufferSpan());
+
+			if (filesUpdated) {
+				flatbuffers::FlatBufferBuilder fbb;
+				auto message = constructPatchesMessage(fbb);
+				fbb.Finish(message);
+
+				sendBroadcast(fbb.GetBufferSpan());
 			}
 		} else {
-			printf("Invalid message\n");
+			printf("Other option\n");
 		}
+	} else {
+		printf("Invalid message\n");
 	}
 }
 
