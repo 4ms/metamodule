67219ae2b5f7bedf93f59e5c576f5dae17308b28
audiobird <cantrell@madronemusic.com>
2024-07-30 11:21:35 -0700

    Set the panel connection dropdown list to the first unpatched jack

--- firmware/src/gui/pages/module_view_mapping_pane.hh
+++ firmware/src/gui/pages/module_view_mapping_pane.hh
@@ -420,11 +420,15 @@ private:
 		auto page = static_cast<ModuleViewMappingPane *>(event->user_data);
 
 		std::string choices;
+		auto first_unpatched_jack = -1;
 		if (page->this_jack_type == ElementType::Input && !page->this_jack_has_connections) {
 			for (auto i = 0u; i < PanelDef::NumUserFacingInJacks; i++) {
 				choices += get_panel_name<PanelDef>(JackInput{}, i);
 				if (page->patch->find_mapped_injack(i))
 					choices += " (patched)";
+				else if (first_unpatched_jack == -1)
+					first_unpatched_jack = i;
+
 				choices += "\n";
 			}
 		} else {
@@ -432,10 +436,13 @@ private:
 				choices += get_panel_name<PanelDef>(JackOutput{}, i);
 				if (page->patch->find_mapped_outjack(i))
 					choices += " (patched)";
+				else if (first_unpatched_jack == -1)
+					first_unpatched_jack = i;
 				choices += "\n";
 			}
 		}
 		choices.pop_back(); //remove trailing \n
+		first_unpatched_jack = first_unpatched_jack == -1 ? 0 : first_unpatched_jack;
 
 		auto action = [page](unsigned choice) {
 			if (choice == 0 || choice > 8)
@@ -464,7 +471,8 @@ private:
 			page->gui_state.new_cable = std::nullopt;
 		};
 
-		page->panel_cable_popup.show(action, "Which panel jack do you want to connect to?", "Connect", choices.c_str());
+		page->panel_cable_popup.show(
+			action, "Which panel jack do you want to connect to?", "Connect", choices.c_str(), first_unpatched_jack);
 	}
 
 	static void finish_cable_button_cb(lv_event_t *event) {
