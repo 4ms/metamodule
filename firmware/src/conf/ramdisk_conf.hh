6efd197d1d2f234a5db1167df9e198b3c6428233
Dan Green <danngreen1@gmail.com>
2024-08-12 11:01:05 -0700

    Update ram layout:
    - RamDisk 16 => 32MB
    - A7 RAM: stack for each core 1MB => 16MB each
    - A7 RAM: bss+stacks max 128MB => 96MB (~64MB for bss)
    - A7 heap 64MB larger (claimed unused 32MB before DMABUF, and 32MB from bss)

--- firmware/src/conf/ramdisk_conf.hh
+++ firmware/src/conf/ramdisk_conf.hh
@@ -2,7 +2,7 @@
 #include "fs/ramdisk.hh"
 #include <cstdint>
 
-static constexpr uint32_t RamDiskSizeBytes = 16 * 1024 * 1024; //==VIRTDRIVE_SZ is not constexpr
+static constexpr uint32_t RamDiskSizeBytes = 32 * 1024 * 1024; //==VIRTDRIVE_SZ is not constexpr
 static constexpr uint32_t RamDiskBlockSize = 512;
 
 using RamDrive = RamDisk<RamDiskSizeBytes, RamDiskBlockSize>;
