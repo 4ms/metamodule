0656560544c7744878382a1146c32f9ec04de9d0
Dan Green <danngreen1@gmail.com>
2024-08-12 12:05:20 -0700

    Fix volume sidebar not working in Duplicate Patch dialog:
    Partially reverts pull request #322

--- firmware/src/gui/pages/patch_selector_sidebar.hh
+++ firmware/src/gui/pages/patch_selector_sidebar.hh
@@ -59,8 +59,7 @@ struct PatchSelectorSubdirPanel {
 
 			// Add root-level dir on volume
 			lv_obj_set_user_data(vol_item, &root);
-			if (root.files.size())
-				lv_group_add_obj(group, vol_item);
+			lv_group_add_obj(group, vol_item);
 
 			// Add all dirs on volume
 			for (auto &dir : root.dirs)
