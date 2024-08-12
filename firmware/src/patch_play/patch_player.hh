59192cb5731401ae852b4ecf6b0ab15058c16689
Dan Green <danngreen1@gmail.com>
2024-08-12 15:06:22 -0700

    Fix auto-map causing jacks to be marked as patched,
    even if not patched on panel

--- firmware/src/patch_play/patch_player.hh
+++ firmware/src/patch_play/patch_player.hh
@@ -401,14 +401,12 @@ public:
 		pd.add_mapped_injack(panel_jack_id, jack);
 		if (panel_jack_id < in_conns.size())
 			update_or_add(in_conns[panel_jack_id], jack);
-		modules[jack.module_id]->mark_input_patched(jack.jack_id);
 	}
 
 	void add_outjack_mapping(uint16_t panel_jack_id, Jack jack) {
 		pd.add_mapped_outjack(panel_jack_id, jack);
 		if (panel_jack_id < out_conns.size())
 			out_conns[panel_jack_id] = jack;
-		modules[jack.module_id]->mark_output_patched(jack.jack_id);
 	}
 
 	void disconnect_injack(Jack jack) {
