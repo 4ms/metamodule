f8b326de96ec817f8c1fb8f65364439d56c7e6c9
Dan Green <danngreen1@gmail.com>
2024-08-16 14:23:51 -0700

    Remove unnec. const

--- firmware/vcv_plugin/export/src/VCV_module_wrapper.cc
+++ firmware/vcv_plugin/export/src/VCV_module_wrapper.cc
@@ -27,19 +27,19 @@ void VCVModuleWrapper::set_param(int id, float val) {
 		params[id].setValue(val);
 }
 
-void VCVModuleWrapper::set_input(const int input_id, const float val) {
+void VCVModuleWrapper::set_input(int input_id, float val) {
 	if ((size_t)input_id < inputs.size())
 		inputs[input_id].setVoltage(val);
 }
 
-float VCVModuleWrapper::get_output(const int output_id) const {
+float VCVModuleWrapper::get_output(int output_id) const {
 	if ((size_t)output_id < outputs.size())
 		return outputs[output_id].getVoltage();
 	else
 		return 0.f;
 }
 
-float VCVModuleWrapper::get_led_brightness(const int led_id) const {
+float VCVModuleWrapper::get_led_brightness(int led_id) const {
 	if ((size_t)led_id < lights.size()) {
 		auto l = std::clamp(lights[led_id].value, 0.f, 1.f);
 		return l;
@@ -56,7 +56,7 @@ void VCVModuleWrapper::mark_all_inputs_unpatched() {
 	}
 }
 
-void VCVModuleWrapper::mark_input_unpatched(const int input_id) {
+void VCVModuleWrapper::mark_input_unpatched(int input_id) {
 	if ((size_t)input_id < inputs.size()) {
 		inputs[input_id].channels = 0;
 		for (auto &chan_volts : inputs[input_id].voltages) {
@@ -65,7 +65,7 @@ void VCVModuleWrapper::mark_input_unpatched(const int input_id) {
 	}
 }
 
-void VCVModuleWrapper::mark_input_patched(const int input_id) {
+void VCVModuleWrapper::mark_input_patched(int input_id) {
 	if ((size_t)input_id < inputs.size())
 		inputs[input_id].channels = 1;
 }
@@ -75,12 +75,12 @@ void VCVModuleWrapper::mark_all_outputs_unpatched() {
 		out.channels = 0;
 }
 
-void VCVModuleWrapper::mark_output_unpatched(const int output_id) {
+void VCVModuleWrapper::mark_output_unpatched(int output_id) {
 	if ((size_t)output_id < outputs.size())
 		outputs[output_id].channels = 0;
 }
 
-void VCVModuleWrapper::mark_output_patched(const int output_id) {
+void VCVModuleWrapper::mark_output_patched(int output_id) {
 	if ((size_t)output_id < outputs.size())
 		outputs[output_id].channels = 1;
 }
