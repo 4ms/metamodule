72bc56ab5f53051c572b832f936275c4a58cab0f
Dan Green <danngreen1@gmail.com>
2024-08-20 11:49:02 -0700

    More clear text when making a cable

--- firmware/src/gui/slsexport/meta5/screens/ui_MappingMenu.c
+++ firmware/src/gui/slsexport/meta5/screens/ui_MappingMenu.c
@@ -141,7 +141,7 @@ lv_obj_set_width( ui_ModuleViewCableCreateLabel, lv_pct(100));
 lv_obj_set_height( ui_ModuleViewCableCreateLabel, LV_SIZE_CONTENT);   /// 1
 lv_obj_set_align( ui_ModuleViewCableCreateLabel, LV_ALIGN_LEFT_MID );
 lv_label_set_long_mode(ui_ModuleViewCableCreateLabel,LV_LABEL_LONG_CLIP);
-lv_label_set_text(ui_ModuleViewCableCreateLabel,"Select a cable:");
+lv_label_set_text(ui_ModuleViewCableCreateLabel,"Creating a cable");
 lv_obj_add_flag( ui_ModuleViewCableCreateLabel, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK );   /// Flags
 lv_obj_clear_flag( ui_ModuleViewCableCreateLabel, LV_OBJ_FLAG_PRESS_LOCK | LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_SNAPPABLE | LV_OBJ_FLAG_SCROLLABLE | LV_OBJ_FLAG_SCROLL_ELASTIC | LV_OBJ_FLAG_SCROLL_MOMENTUM | LV_OBJ_FLAG_SCROLL_CHAIN );    /// Flags
 lv_obj_set_scrollbar_mode(ui_ModuleViewCableCreateLabel, LV_SCROLLBAR_MODE_OFF);
