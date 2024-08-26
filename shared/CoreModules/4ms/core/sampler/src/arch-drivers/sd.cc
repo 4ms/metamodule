5bbf820c6730cc3ddf1b73f876c6477277ad3916
Dan Green <danngreen1@gmail.com>
2024-08-26 11:58:14 -0700

    Move STS files to CoreModules

--- shared/CoreModules/4ms/core/sampler/src/arch-drivers/sd.cc
+++
@@ -1,8 +0,0 @@
-#include "drivers/stm32xx.h"
-
-volatile bool sd_rx = false;
-volatile bool sd_tx = false;
-
-extern "C" void HAL_SD_RxCpltCallback(SD_HandleTypeDef *hsd) { sd_rx = true; }
-extern "C" void HAL_SD_TxCpltCallback(SD_HandleTypeDef *hsd) { sd_tx = true; }
-
--- shared/CoreModules/4ms/core/sampler/src/sample_header.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sample_type.hh"
-#include "sdcard.hh"
-#include <cstdint>
-
-namespace SamplerKit
-{
-uint32_t load_sample_header(Sample *s_sample, FIL *sample_file, Sdcard &sd);
-}
