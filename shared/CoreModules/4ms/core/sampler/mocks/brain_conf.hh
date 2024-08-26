4372ab36ac32c1367728fe1171478eecfd1e862f
Dan Green <danngreen1@gmail.com>
2024-08-26 11:50:34 -0700

    STS conf: pots/cv change threshold removed

--- shared/CoreModules/4ms/core/sampler/mocks/brain_conf.hh
+++ shared/CoreModules/4ms/core/sampler/mocks/brain_conf.hh
@@ -3,7 +3,9 @@
 namespace SamplerKit::Brain
 {
 
-static constexpr int MinPotChange = 1;
-static constexpr int MinCVChange = 1;
+inline constexpr int MinPotChange = 0;
+inline constexpr int MinCVChange = 0;
+inline constexpr uint32_t MemorySizeBytes = 8 * 1024 * 1024;
+inline constexpr float AudioGain = 1.f;
 
 } // namespace SamplerKit::Brain
