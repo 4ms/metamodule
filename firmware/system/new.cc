cc0e01144adf33066ae3eebc06d175c1a6175ee1
Dan Green <danngreen1@gmail.com>
2024-09-23 21:37:15 -0700

    Split new.cc and sbrk.cc (easier to test)

--- firmware/system/new.cc
+++ firmware/system/new.cc
@@ -55,34 +55,3 @@ void operator delete[](void *p, std::nothrow_t) noexcept
 {
 	operator delete(p);
 }
-
-static char *heap_end = nullptr;
-// Defined by the linker
-extern char _sheap;
-
-size_t get_heap_size()
-{
-	return reinterpret_cast<size_t>(heap_end) - reinterpret_cast<size_t>(&_sheap);
-}
-
-extern "C" size_t _sbrk(int incr)
-{
-	// Defined by the linker
-	extern char _eheap;
-
-	char *prev_heap_end;
-
-	if (heap_end == nullptr) {
-		heap_end = &_sheap;
-	}
-	prev_heap_end = heap_end;
-
-	if (heap_end + incr > &_eheap) {
-		// OOM!!!
-		while (true)
-			;
-	}
-
-	heap_end += incr;
-	return (size_t)prev_heap_end;
-}
