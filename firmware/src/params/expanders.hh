6a4c3cf3b3ab1e9088500e8338734cc3991684ca
Dan Green <danngreen1@gmail.com>
2024-10-09 13:44:40 -0700

    Add Button expander status to Expanders

--- firmware/src/params/expanders.hh
+++ firmware/src/params/expanders.hh
@@ -6,9 +6,11 @@ namespace MetaModule::Expanders
 struct ConnectedState {
 	// bool wifi_connected{}
 	bool ext_audio_connected{};
+	unsigned num_button_connected = 0;
 };
 
 void ext_audio_found(bool found);
+void button_exp_found(unsigned num);
 ConnectedState get_connected();
 
 } // namespace MetaModule::Expanders
