28697c10c5b1fc2dee74830dd410b10bdb993dbc
Dan Green <danngreen1@gmail.com>
2024-10-14 17:01:52 -0700

    Default value for Elements matches VCV

--- firmware/vcv_ports/glue/AudibleInstruments/info/Elements_info.hh
+++ firmware/vcv_ports/glue/AudibleInstruments/info/Elements_info.hh
@@ -6,18 +6,18 @@
 namespace MetaModule
 {
 struct ElementsInfo : ModuleInfoBase {
-    static constexpr std::string_view slug{"Elements"};
-    static constexpr std::string_view description{"Pecussion Synthesizer"};
-    static constexpr uint32_t width_hp = 34;
-    static constexpr std::string_view svg_filename{"res/modules/Elements_artwork.svg"};
-    static constexpr std::string_view png_filename{"4ms/fp/Elements.png"};
+	static constexpr std::string_view slug{"Elements"};
+	static constexpr std::string_view description{"Pecussion Synthesizer"};
+	static constexpr uint32_t width_hp = 34;
+	static constexpr std::string_view svg_filename{"res/modules/Elements_artwork.svg"};
+	static constexpr std::string_view png_filename{"4ms/fp/Elements.png"};
 
-    using enum Coords;
+	using enum Coords;
 
-    static constexpr std::array<Element, 48> Elements{{
-		WhiteMediumKnob{{to_mm<72>(47.58), to_mm<72>(60.35), Center, "Contour", ""}, 0.5f},
-		WhiteMediumKnob{{to_mm<72>(114.99), to_mm<72>(60.35), Center, "Bow", ""}, 0.5f},
-		RedMediumKnob{{to_mm<72>(182.51), to_mm<72>(60.35), Center, "Blow", ""}, 0.5f},
+	static constexpr std::array<Element, 48> Elements{{
+		WhiteMediumKnob{{to_mm<72>(47.58), to_mm<72>(60.35), Center, "Contour", ""}, 1.0f},
+		WhiteMediumKnob{{to_mm<72>(114.99), to_mm<72>(60.35), Center, "Bow", ""}, 0.0f},
+		RedMediumKnob{{to_mm<72>(182.51), to_mm<72>(60.35), Center, "Blow", ""}, 0.0f},
 		RedMediumKnob{{to_mm<72>(250.03), to_mm<72>(60.35), Center, "Strike", ""}, 0.5f},
 		WhiteMediumKnob{{to_mm<72>(317.55), to_mm<72>(60.35), Center, "Coarse", ""}, 0.5f},
 		WhiteMediumKnob{{to_mm<72>(385.07), to_mm<72>(60.35), Center, "Fine", ""}, 0.5f},
@@ -31,7 +31,7 @@ struct ElementsInfo : ModuleInfoBase {
 		BlueMediumKnob{{to_mm<72>(250.03), to_mm<72>(214.27), Center, "Strike Timbre", ""}, 0.5f},
 		WhiteMediumKnob{{to_mm<72>(317.55), to_mm<72>(214.27), Center, "Damping", ""}, 0.5f},
 		WhiteMediumKnob{{to_mm<72>(385.07), to_mm<72>(214.27), Center, "Position", ""}, 0.5f},
-		WhiteMediumKnob{{to_mm<72>(452.59), to_mm<72>(214.27), Center, "Space", ""}, 0.5f},
+		WhiteMediumKnob{{to_mm<72>(452.59), to_mm<72>(214.27), Center, "Space", ""}, 0.0f},
 		Trimpot{{to_mm<72>(108.81), to_mm<72>(270.97), Center, "Bow Timbre Atten", ""}, 0.5f},
 		Trimpot{{to_mm<72>(145.66), to_mm<72>(270.97), Center, "Flow Atten", ""}, 0.5f},
 		Trimpot{{to_mm<72>(182.51), to_mm<72>(270.97), Center, "Blow Timbre Atten", ""}, 0.5f},
@@ -63,128 +63,127 @@ struct ElementsInfo : ModuleInfoBase {
 		AnalogJackOutput4ms{{to_mm<72>(64.67), to_mm<72>(315.07), Center, "Out R", ""}},
 		GreenLight{{to_mm<72>(182.51), to_mm<72>(163.87), Center, "ControlExciter LED", ""}},
 		RedLight{{to_mm<72>(385.07), to_mm<72>(163.87), Center, "Resonator LED", ""}},
-}};
+	}};
 
-    enum class Elem {
-        ContourKnob,
-        BowKnob,
-        BlowKnob,
-        StrikeKnob,
-        CoarseKnob,
-        FineKnob,
-        FmKnob,
-        FlowKnob,
-        MalletKnob,
-        GeometryKnob,
-        BrightnessKnob,
-        BowTimbreKnob,
-        BlowTimbreKnob,
-        StrikeTimbreKnob,
-        DampingKnob,
-        PositionKnob,
-        SpaceKnob,
-        BowTimbreAttenKnob,
-        FlowAttenKnob,
-        BlowTimbreAttenKnob,
-        MalletAttenKnob,
-        StrikeTimbreAttenKnob,
-        DampingAttenKnob,
-        GeometryAttenKnob,
-        PositionAttenKnob,
-        BrightnessAttenKnob,
-        SpaceAttenKnob,
-        PlayButton,
-        V_OctIn,
-        FmIn,
-        GateIn,
-        StrengthIn,
-        ExtInBlowIn,
-        ExtInStrikeIn,
-        BowTimbreCvIn,
-        FlowCvIn,
-        BlowTimbreCvIn,
-        MalletCvIn,
-        StrikeTimbreCvIn,
-        DampingCvIn,
-        GeometryCvIn,
-        PositionCvIn,
-        BrightnessCvIn,
-        SpaceCvIn,
-        OutLOut,
-        OutROut,
-        ControlexciterLedLight,
-        ResonatorLedLight,
-    };
+	enum class Elem {
+		ContourKnob,
+		BowKnob,
+		BlowKnob,
+		StrikeKnob,
+		CoarseKnob,
+		FineKnob,
+		FmKnob,
+		FlowKnob,
+		MalletKnob,
+		GeometryKnob,
+		BrightnessKnob,
+		BowTimbreKnob,
+		BlowTimbreKnob,
+		StrikeTimbreKnob,
+		DampingKnob,
+		PositionKnob,
+		SpaceKnob,
+		BowTimbreAttenKnob,
+		FlowAttenKnob,
+		BlowTimbreAttenKnob,
+		MalletAttenKnob,
+		StrikeTimbreAttenKnob,
+		DampingAttenKnob,
+		GeometryAttenKnob,
+		PositionAttenKnob,
+		BrightnessAttenKnob,
+		SpaceAttenKnob,
+		PlayButton,
+		V_OctIn,
+		FmIn,
+		GateIn,
+		StrengthIn,
+		ExtInBlowIn,
+		ExtInStrikeIn,
+		BowTimbreCvIn,
+		FlowCvIn,
+		BlowTimbreCvIn,
+		MalletCvIn,
+		StrikeTimbreCvIn,
+		DampingCvIn,
+		GeometryCvIn,
+		PositionCvIn,
+		BrightnessCvIn,
+		SpaceCvIn,
+		OutLOut,
+		OutROut,
+		ControlexciterLedLight,
+		ResonatorLedLight,
+	};
 
-    // Legacy naming (safe to remove once all legacy 4ms CoreModules are converted)
-    
-    enum {
-        KnobContour, 
-        KnobBow, 
-        KnobBlow, 
-        KnobStrike, 
-        KnobCoarse, 
-        KnobFine, 
-        KnobFm, 
-        KnobFlow, 
-        KnobMallet, 
-        KnobGeometry, 
-        KnobBrightness, 
-        KnobBow_Timbre, 
-        KnobBlow_Timbre, 
-        KnobStrike_Timbre, 
-        KnobDamping, 
-        KnobPosition, 
-        KnobSpace, 
-        KnobBow_Timbre_Atten, 
-        KnobFlow_Atten, 
-        KnobBlow_Timbre_Atten, 
-        KnobMallet_Atten, 
-        KnobStrike_Timbre_Atten, 
-        KnobDamping_Atten, 
-        KnobGeometry_Atten, 
-        KnobPosition_Atten, 
-        KnobBrightness_Atten, 
-        KnobSpace_Atten, 
-        NumKnobs,
-    };
-    
-    enum {
-        SwitchPlay, 
-        NumSwitches,
-    };
-    
-    enum {
-        InputV_Oct, 
-        InputFm, 
-        InputGate, 
-        InputStrength, 
-        InputExt_In_Blow, 
-        InputExt_In_Strike, 
-        InputBow_Timbre_Cv, 
-        InputFlow_Cv, 
-        InputBlow_Timbre_Cv, 
-        InputMallet_Cv, 
-        InputStrike_Timbre_Cv, 
-        InputDamping_Cv, 
-        InputGeometry_Cv, 
-        InputPosition_Cv, 
-        InputBrightness_Cv, 
-        InputSpace_Cv, 
-        NumInJacks,
-    };
-    
-    enum {
-        OutputOut_L, 
-        OutputOut_R, 
-        NumOutJacks,
-    };
-    
-    enum {
-        LedControlexciter_Led, 
-        LedResonator_Led, 
-        NumDiscreteLeds,
-    };
-    
+	// Legacy naming (safe to remove once all legacy 4ms CoreModules are converted)
+
+	enum {
+		KnobContour,
+		KnobBow,
+		KnobBlow,
+		KnobStrike,
+		KnobCoarse,
+		KnobFine,
+		KnobFm,
+		KnobFlow,
+		KnobMallet,
+		KnobGeometry,
+		KnobBrightness,
+		KnobBow_Timbre,
+		KnobBlow_Timbre,
+		KnobStrike_Timbre,
+		KnobDamping,
+		KnobPosition,
+		KnobSpace,
+		KnobBow_Timbre_Atten,
+		KnobFlow_Atten,
+		KnobBlow_Timbre_Atten,
+		KnobMallet_Atten,
+		KnobStrike_Timbre_Atten,
+		KnobDamping_Atten,
+		KnobGeometry_Atten,
+		KnobPosition_Atten,
+		KnobBrightness_Atten,
+		KnobSpace_Atten,
+		NumKnobs,
+	};
+
+	enum {
+		SwitchPlay,
+		NumSwitches,
+	};
+
+	enum {
+		InputV_Oct,
+		InputFm,
+		InputGate,
+		InputStrength,
+		InputExt_In_Blow,
+		InputExt_In_Strike,
+		InputBow_Timbre_Cv,
+		InputFlow_Cv,
+		InputBlow_Timbre_Cv,
+		InputMallet_Cv,
+		InputStrike_Timbre_Cv,
+		InputDamping_Cv,
+		InputGeometry_Cv,
+		InputPosition_Cv,
+		InputBrightness_Cv,
+		InputSpace_Cv,
+		NumInJacks,
+	};
+
+	enum {
+		OutputOut_L,
+		OutputOut_R,
+		NumOutJacks,
+	};
+
+	enum {
+		LedControlexciter_Led,
+		LedResonator_Led,
+		NumDiscreteLeds,
+	};
 };
 } // namespace MetaModule
