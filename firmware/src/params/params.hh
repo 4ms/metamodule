d1ed0a52da37eeb4a21e2262031a7f9381eb42ce
Dan Green <danngreen1@gmail.com>
2024-10-28 13:13:49 -0700

    Remove Params::cvjacks

--- firmware/src/params/params.hh
+++ firmware/src/params/params.hh
@@ -10,21 +10,18 @@
 namespace MetaModule
 {
 struct Params {
-	std::array<float, PanelDef::NumCVIn> cvjacks{};
 	std::array<bool, PanelDef::NumGateIn> gate_ins{};
 	std::array<Toggler, PanelDef::NumRgbButton> buttons{};
 	std::array<float, PanelDef::NumPot> knobs{};
 
-	Midi::Event midi_event{};
-	MidiMessage raw_msg{};
+	Midi::Event midi_event{}; //8B
+	MidiMessage raw_msg{};	  //3B
 
 	Params() {
 		clear();
 	}
 
 	void clear() {
-		for (float &cvjack : cvjacks)
-			cvjack = 0.f;
 		for (auto &gate_in : gate_ins)
 			gate_in = false;
 		for (auto &button : buttons)
@@ -36,8 +33,6 @@ struct Params {
 	}
 
 	void copy(const Params &that) {
-		for (unsigned i = 0; i < PanelDef::NumCVIn; i++)
-			cvjacks[i] = that.cvjacks[i];
 		for (unsigned i = 0; i < PanelDef::NumGateIn; i++)
 			gate_ins[i] = that.gate_ins[i];
 		for (unsigned i = 0; i < PanelDef::NumRgbButton; i++)
