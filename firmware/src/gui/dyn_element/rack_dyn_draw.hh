858815f48836d7444e401aecbbeab9e1525acbdd
Dan Green <danngreen1@gmail.com>
2024-12-05 15:59:36 -0800

    Rack Dyn Drawer creates the nanovg context

--- firmware/src/gui/dyn_element/rack_dyn_draw.hh
+++ firmware/src/gui/dyn_element/rack_dyn_draw.hh
@@ -1,20 +1,22 @@
 #pragma once
 #include "base_dyn_draw.hh"
 #include <app/ModuleWidget.hpp>
+#include <window/Window.hpp>
 #include <memory>
 
 namespace MetaModule
 {
 
 struct RackDynDraw : BaseDynDraw {
+	rack::app::ModuleWidget::DrawArgs args{};
+
 	RackDynDraw(std::shared_ptr<rack::app::ModuleWidget> module_widget)
 		: module_widget{module_widget} {
+		args.vg = nvgCreatePixelBufferContext(0);
 	}
 
 	void draw() override {
 		if (auto mw = module_widget.lock()) {
-
-			rack::app::ModuleWidget::DrawArgs args{};
 
 			for (auto &w : mw->drawable_widgets) {
 				w->draw(args);
