fc6d03c724a9dbaf39598e49a63d39919f084442
Dan Green <danngreen1@gmail.com>
2024-12-05 14:56:24 -0800

    RackDynDraw calls drawLayer

--- firmware/src/gui/dyn_element/rack_dyn_draw.hh
+++ firmware/src/gui/dyn_element/rack_dyn_draw.hh
@@ -13,9 +13,12 @@ struct RackDynDraw : BaseDynDraw {
 
 	void draw() override {
 		if (auto mw = module_widget.lock()) {
+
 			rack::app::ModuleWidget::DrawArgs args{};
+
 			for (auto &w : mw->drawable_widgets) {
 				w->draw(args);
+				w->drawLayer(args, 1);
 			}
 		}
 	}
