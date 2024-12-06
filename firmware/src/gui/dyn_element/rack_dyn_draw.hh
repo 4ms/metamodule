025e0b5d5142135c35e242a89e4c84d96b6a7778
Dan Green <danngreen1@gmail.com>
2024-12-06 15:21:23 -0800

    Rack dyn drawer offsets children properly

--- firmware/src/gui/dyn_element/rack_dyn_draw.hh
+++ firmware/src/gui/dyn_element/rack_dyn_draw.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include "base_dyn_draw.hh"
-#include "src/core/lv_obj.h"
+#include "lvgl.h"
+#include "pr_dbg.hh"
 #include "vcv_plugin/internal/nanovg_gl.hh"
 #include <app/ModuleWidget.hpp>
 #include <memory>
@@ -9,29 +10,40 @@ namespace MetaModule
 {
 
 struct RackDynDraw : BaseDynDraw {
-	static inline rack::app::ModuleWidget::DrawArgs args{}; // there can be only one of these
 
 	RackDynDraw(std::shared_ptr<rack::app::ModuleWidget> module_widget)
 		: module_widget{module_widget} {
 	}
 
 	void prepare(lv_obj_t *widget_canvas) override {
+		pr_dbg("RackDynDraw::prepare(canvas = %p)\n", widget_canvas);
 		args.vg = nvgCreatePixelBufferContext(widget_canvas);
+		args.fb = nullptr;
 	}
 
 	void draw() override {
 		if (auto mw = module_widget.lock()) {
 
-			for (auto &w : mw->drawable_widgets) {
-				args.clipBox = w->getBox();
+			// args.clipBox = mw->getBox();
 
-				w->draw(args);
-				w->drawLayer(args, 1);
+			for (auto &w : mw->drawable_widgets) {
+				if (!w->isVisible())
+					continue;
+
+				// Don't draw if child is outside clip box
+				// if (!args.clipBox.intersects(w->box))
+				// 	continue;
+
+				mw->drawChild(w, args);
+				mw->drawChild(w, args, 1);
 			}
 		}
 	}
 
+	~RackDynDraw() override = default;
+
 private:
 	std::weak_ptr<rack::app::ModuleWidget> module_widget{};
+	rack::app::ModuleWidget::DrawArgs args{}; // one per module
 };
 } // namespace MetaModule
