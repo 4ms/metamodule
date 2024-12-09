c998db2442a1dec29dc6358574fcb044a94de167
Dan Green <danngreen1@gmail.com>
2024-12-09 10:22:33 -0800

    Cleanup rack drawing. Set clipbox

--- firmware/src/gui/dyn_element/rack_dyn_draw.hh
+++ firmware/src/gui/dyn_element/rack_dyn_draw.hh
@@ -17,47 +17,25 @@ struct RackDynDraw : BaseDynDraw {
 	}
 
 	void prepare(lv_obj_t *widget_canvas) override {
-		pr_dbg("RackDynDraw::prepare(canvas = %p)\n", widget_canvas);
 		args.vg = nvgCreatePixelBufferContext(widget_canvas);
 		args.fb = nullptr;
 		canvas = widget_canvas;
 		lv_obj_refr_size(canvas);
 		width = lv_obj_get_width(canvas);
 		height = lv_obj_get_height(canvas);
-		printf("Dyn canvas %d,%d\n", width, height);
 	}
 
 	void draw() override {
 
-		clear_canvas();
-
 		if (auto mw = module_widget.lock()) {
 
-			// args.clipBox = mw->getBox();
+			clear_canvas();
 
 			for (auto &widget : mw->drawable_widgets) {
 				if (!widget->isVisible())
 					continue;
 
-				// Don't draw if child is outside clip box
-				// if (!args.clipBox.intersects(w->box))
-				// 	continue;
-
-				// auto b = widget->getBox();
-				// auto x = svgpx_to_pngpx(b.pos.x);
-				// auto y = svgpx_to_pngpx(b.pos.y);
-				// auto w = svgpx_to_pngpx(b.size.x);
-				// auto h = svgpx_to_pngpx(b.size.y);
-
-				// auto buf = lv_canvas_get_img(canvas);
-				// const auto cw = buf->header.w;
-				// const uint32_t px_size = 3; //lv_img_cf_get_px_size(buf->header.cf) >> 3;
-
-				// uint32_t px = cw * y * px_size + x * px_size;
-				// for (lv_coord_t i = 0; i < h; i++) {
-				// 	lv_memset_00((void *)&buf->data[px], w * px_size);
-				// 	px += cw * px_size;
-				// }
+				args.clipBox = widget->getBox();
 
 				mw->drawChild(widget, args);
 				mw->drawChild(widget, args, 1);
@@ -75,6 +53,7 @@ private:
 	std::weak_ptr<rack::app::ModuleWidget> module_widget{};
 	rack::app::ModuleWidget::DrawArgs args{}; // one per module
 
+	// Takes ~50us for A 14HP-ish module
 	void clear_canvas() {
 		if (canvas) {
 			auto buf = lv_canvas_get_img(canvas);
@@ -83,8 +62,5 @@ private:
 			}
 		}
 	}
-
-	// void clear_canvas(lv_coord_t x, lv_coord_t y, lv_coord_t w, lv_coord_t h) {
-	// }
 };
 } // namespace MetaModule
