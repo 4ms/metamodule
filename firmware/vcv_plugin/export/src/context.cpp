2566132aa19f11595533d1ab33ca5ed9c319f145
Dan Green <danngreen1@gmail.com>
2024-12-18 16:08:10 -0800

    Support APP->scene->rackScroll->zoomWidget

--- firmware/vcv_plugin/export/src/context.cpp
+++ firmware/vcv_plugin/export/src/context.cpp
@@ -1,3 +1,4 @@
+#include "console/pr_dbg.hh"
 #include "engine/Engine.hpp"
 #include "window/Window.hpp"
 #include <app/Scene.hpp>
@@ -9,6 +10,9 @@ namespace rack
 static app::Scene _scene;
 static window::Window _window;
 static engine::Engine _engine;
+static app::RackScrollWidget rackScroll;
+static widget::ZoomWidget zoomWidget;
+
 static Context rack_context{
 	.scene = &_scene, .engine = &_engine, .window = &_window,
 	//event
@@ -22,7 +26,13 @@ Context *contextGet() {
 }
 
 void contextSet(Context *context) {
-	rack_context = *context;
+	rack_context.scene->rackScroll = &rackScroll;
+	rack_context.scene->rackScroll->zoomWidget = &zoomWidget;
+
+	if (context != nullptr) {
+		pr_warn("rack context changed!\n");
+		rack_context = *context;
+	}
 }
 
 } // namespace rack
