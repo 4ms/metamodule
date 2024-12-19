8faba58e00380e304915b06b0a7756c86145194c
Dan Green <danngreen1@gmail.com>
2024-12-19 14:53:52 -0800

    Split nanovg_pixbuf into helper files

--- firmware/vcv_plugin/internal/nanovg_pixbuf.cc
+++ firmware/vcv_plugin/internal/nanovg_pixbuf.cc
@@ -1,6 +1,8 @@
 #include "CoreModules/elements/units.hh"
 #include "gui/fonts/fonts.hh"
 #include "lvgl.h"
+#include "nanovg_pixbuf_drawctx.hh"
+#include "nanovg_pixbuf_util.hh"
 #include <algorithm>
 #include <cmath>
 #include <cstdlib>
@@ -10,162 +12,11 @@
 #include <string>
 #include <vector>
 
-#define NANOVG_PIXELBUFFER_IMPLEMENTATION
-#include <nanovg_gl.h>
-#include <nanovg_gl_utils.h>
-
 #include "console/pr_dbg.hh"
 #include "medium/debug_raw.h"
 
 namespace MetaModule::NanoVG
 {
-
-struct Texture {
-	std::vector<lv_color_t> pix;
-	size_t w{};
-	size_t h{};
-
-	lv_color_t px(size_t x, size_t y) const {
-		return pix[x + y * w];
-	}
-
-	lv_color_t &px(size_t x, size_t y) {
-		return pix[x + y * w];
-	}
-	//type?
-	//flags?
-};
-
-struct TextRenderCacheEntry {
-	float x;
-	float y;
-	int align;
-	lv_obj_t *label;
-
-	bool operator==(const TextRenderCacheEntry &other) const {
-		return x == other.x && y == other.y && align == other.align;
-	}
-};
-
-struct DrawContext {
-	lv_obj_t *canvas{};
-	lv_draw_line_dsc_t line_dsc{};
-	lv_draw_rect_dsc_t rect_dsc{};
-	lv_draw_label_dsc_t label_dsc{};
-
-	std::vector<Texture> textures;
-
-	std::vector<TextRenderCacheEntry> labels;
-
-	void *parent_ctx{}; // needed for deleting
-
-	DrawContext(lv_obj_t *canvas)
-		: canvas{canvas} {
-		lv_draw_line_dsc_init(&line_dsc);
-		line_dsc.width = 1;
-
-		lv_draw_rect_dsc_init(&rect_dsc);
-		rect_dsc.radius = 0;
-
-		lv_draw_label_dsc_init(&label_dsc);
-	}
-};
-
-constexpr lv_coord_t to_lv_coord(float x) {
-	return std::round(mm_to_px(to_mm(x), 240));
-}
-// 10px in rack dimensions => 6.325px in MM dimensions
-// static_assert(mm_to_px(to_mm(10.f), 240) == 6.325291928f);
-
-constexpr lv_point_t to_lv_point(NVGvertex vertex) {
-	return lv_point_t(to_lv_coord(vertex.x), to_lv_coord(vertex.y));
-}
-
-DrawContext *get_drawcontext(void *uptr) {
-	return (DrawContext *)(uptr);
-}
-
-lv_color_t to_lv_color(NVGcolor color) {
-	return lv_color_make(color.r * 255.f, color.g * 255.f, color.b * 255.f);
-}
-
-lv_color_t to_lv_text_color(NVGcolor color) {
-	auto hsv = lv_color_to_hsv(to_lv_color(color));
-	return lv_color_hsv_to_rgb(hsv.h, (hsv.s + 100) / 2, 100);
-}
-
-constexpr uint8_t to_lv_opa(NVGcolor color) {
-	return std::round(color.a * float(LV_OPA_100));
-}
-
-constexpr bool is_poly_concave(std::span<lv_point_t> points) {
-	auto n = points.size();
-	if (n < 3)
-		return false;
-
-	auto X_prod = [](lv_point_t O, lv_point_t A, lv_point_t B) {
-		return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
-	};
-
-	int sign = 0;
-	for (auto i = 0u; i < points.size(); i++) {
-		auto O = points[i];
-		auto A = points[(i + 1) % n];
-		auto B = points[(i + 2) % n];
-		auto cross = X_prod(O, A, B);
-		if (cross == 0)
-			continue;
-
-		if (sign == 0)
-			sign = cross;
-
-		if ((sign > 0 && cross < 0) || (sign < 0 && cross > 0))
-			return true;
-	}
-	return false;
-}
-
-// Not used:
-bool fix_poly_concave(std::span<lv_point_t> points) {
-	auto n = points.size();
-	if (n < 3)
-		return false;
-
-	auto X_prod = [](lv_point_t O, lv_point_t A, lv_point_t B) {
-		return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
-	};
-
-	int sign = 0;
-
-	int offender = -1;
-	lv_point_t fix{};
-
-	for (auto i = 0u; i < points.size() + 1; i++) {
-		auto O = points[i];
-		auto A = points[(i + 1) % n];
-		auto B = points[(i + 2) % n];
-		auto cross = X_prod(O, A, B);
-		if (cross == 0)
-			continue;
-
-		if (sign == 0)
-			sign = cross;
-
-		if ((sign > 0 && cross < 0) || (sign < 0 && cross > 0)) {
-			// fix = A;
-			// offender = (i + 2) % n;
-			fix = B;
-			offender = (i + 1) % n;
-		}
-	}
-
-	if (offender >= 0) {
-		points[offender] = fix;
-		return fix_poly_concave(points);
-	}
-
-	return true;
-}
 
 void renderFill(void *uptr,
 				NVGpaint *paint,
---
+++ firmware/vcv_plugin/internal/nanovg_pixbuf_drawctx.hh
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "lvgl.h"
+#include <vector>
+
+namespace MetaModule::NanoVG
+{
+
+struct Texture {
+	std::vector<lv_color_t> pix;
+	size_t w{};
+	size_t h{};
+
+	lv_color_t px(size_t x, size_t y) const {
+		return pix[x + y * w];
+	}
+
+	lv_color_t &px(size_t x, size_t y) {
+		return pix[x + y * w];
+	}
+	//type?
+	//flags?
+};
+
+struct TextRenderCacheEntry {
+	float x;
+	float y;
+	int align;
+	lv_obj_t *label;
+
+	bool operator==(const TextRenderCacheEntry &other) const {
+		return x == other.x && y == other.y && align == other.align;
+	}
+};
+
+struct DrawContext {
+	lv_obj_t *canvas{};
+	lv_draw_line_dsc_t line_dsc{};
+	lv_draw_rect_dsc_t rect_dsc{};
+	lv_draw_label_dsc_t label_dsc{};
+
+	std::vector<Texture> textures;
+
+	std::vector<TextRenderCacheEntry> labels;
+
+	void *parent_ctx{}; // needed for deleting
+
+	DrawContext(lv_obj_t *canvas)
+		: canvas{canvas} {
+		lv_draw_line_dsc_init(&line_dsc);
+		line_dsc.width = 1;
+
+		lv_draw_rect_dsc_init(&rect_dsc);
+		rect_dsc.radius = 0;
+
+		lv_draw_label_dsc_init(&label_dsc);
+	}
+};
+
+DrawContext *get_drawcontext(void *uptr) {
+	return (DrawContext *)(uptr);
+}
+
+} // namespace MetaModule::NanoVG
