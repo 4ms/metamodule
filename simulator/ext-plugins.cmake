67c6ab5435d704555e496619007aeb7617285893
Dan Green <danngreen1@gmail.com>
2024-12-23 11:27:39 -0800

    Allow building external plugins as built-in, for simulator
    Simulator builds assets image using firmware assets plus ext plugin assets
    
    Modify internal plugin manager to build Venom
    
    Ext plugin brand and path can be different
    
    Firmware ignores ext builtin plugins

---
+++ simulator/ext-plugins.cmake
@@ -0,0 +1,53 @@
+# list(APPEND ext_builtin_brand_paths "${CMAKE_CURRENT_LIST_DIR}/../../metamodule-plugin-examples/Bogaudio")
+# list(APPEND ext_builtin_brand_libname "BogaudioModules")
+
+list(APPEND ext_builtin_brand_paths "${CMAKE_CURRENT_LIST_DIR}/../../metamodule-plugin-examples/Venom")
+list(APPEND ext_builtin_brand_libname "Venom")
+
+
+
+#
+# Asset dir
+#
+
+cmake_path(SET ASSET_DIR "${CMAKE_CURRENT_BINARY_DIR}/assets")
+set(ASSET_IMG_FILENAME assets.uimg)
+cmake_path(APPEND ASSET_IMG_PATH "${CMAKE_CURRENT_BINARY_DIR}" "${ASSET_IMG_FILENAME}")
+
+message("set ASSET_DIR to ${ASSET_DIR}")
+message("set ASSET_IMG_PATH to ${ASSET_IMG_PATH}")
+
+add_custom_command(
+  OUTPUT ${ASSET_DIR}
+  COMMAND ${CMAKE_COMMAND} -E echo Copying "${FWDIR}/assets" to "${ASSET_DIR}"
+  COMMAND ${CMAKE_COMMAND} -E copy_directory "${FWDIR}/assets" "${ASSET_DIR}"
+  COMMENT "Copying assets/ dir from ${FWDIR}/assets to ${ASSET_DIR}"
+  VERBATIM USES_TERMINAL
+)
+
+add_custom_command(
+  OUTPUT ${ASSET_IMG_PATH}
+  COMMAND cd ${ASSET_DIR} && ${CMAKE_COMMAND} -E tar -cf ${ASSET_IMG_PATH}.tar .
+  COMMAND cd ${FWDIR} && flashing/uimg_header.py --name Assets ${ASSET_IMG_PATH}.tar ${ASSET_IMG_PATH}
+  COMMENT "Creating assets uimg file at ${ASSET_IMG_PATH}"
+  DEPENDS ${ASSET_DIR}
+  VERBATIM USES_TERMINAL
+)
+
+ add_custom_target(asset-image ALL 
+	DEPENDS ${ASSET_IMG_PATH}
+ )
+
+foreach(branddir brand IN ZIP_LISTS ext_builtin_brand_paths ext_builtin_brand_libname)
+	set(METAMODULE_SDK_DIR ${CMAKE_CURRENT_LIST_DIR})
+	add_subdirectory(${branddir} ${CMAKE_CURRENT_BINARY_DIR}/builtins/${brand})
+
+	target_link_libraries(${brand} PRIVATE metamodule::vcv-plugin-interface)
+	target_link_libraries(${brand} PRIVATE cpputil::cpputil)
+	target_compile_definitions(${brand} PRIVATE METAMODULE METAMODULE_BUILTIN)
+
+	target_link_libraries(_vcv_ports_internal PUBLIC ${brand})
+	add_dependencies(asset-image ${brand}-assets)
+
+endforeach()
+
---
+++ simulator/plugin.cmake
@@ -0,0 +1,39 @@
+
+# file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/replace.cmake"
+# [=[
+# file(READ "${SOURCE}" TEXT)
+# string(REPLACE "foo" "bar" TEXT "${TEXT}")
+# file(WRITE "${TARGET}" "${TEXT}")
+# ]=])
+
+# add_custom_command(
+#     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/plugin.cpp"
+#     COMMAND "${CMAKE_COMMAND}"
+#         "-DSOURCE=${CMAKE_CURRENT_SOURCE_DIR}/plugin.cpp"
+#         "-DTARGET=${CMAKE_CURRENT_BINARY_DIR}/plugin.cpp"
+#         -P "${CMAKE_CURRENT_BINARY_DIR}/replace.cmake"
+#     DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/plugin.cpp" "${CMAKE_CURRENT_BINARY_DIR}/replace.cmake"
+# )
+
+
+function(create_plugin)
+	message("Building ${brand} as built-in plugin (create_plugin)")
+
+	# target_sources(${PLUGIN_OPTIONS_SOURCE_LIB} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/plugin.cpp)
+
+    set(oneValueArgs SOURCE_LIB SOURCE_ASSETS DESTINATION PLUGIN_NAME PLUGIN_JSON)
+    cmake_parse_arguments(PLUGIN_OPTIONS "" "${oneValueArgs}" "" ${ARGN} )
+
+	add_custom_command(
+		OUTPUT "${ASSET_DIR}/${PLUGIN_OPTIONS_PLUGIN_NAME}" 
+		COMMAND ${CMAKE_COMMAND} -E copy_directory "${PLUGIN_OPTIONS_SOURCE_ASSETS}" "${ASSET_DIR}/${PLUGIN_OPTIONS_PLUGIN_NAME}"
+		COMMENT "Copying ${PLUGIN_OPTIONS_SOURCE_ASSETS} to ${ASSET_DIR}/${PLUGIN_OPTIONS_PLUGIN_NAME}"
+		VERBATIM
+	)
+
+	add_custom_target(${PLUGIN_OPTIONS_SOURCE_LIB}-assets
+		DEPENDS "${ASSET_DIR}/${PLUGIN_OPTIONS_PLUGIN_NAME}" 
+	)
+
+
+endfunction()
