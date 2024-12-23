7d194d0954301a125f04bc7b51f619204b618e47
Dan Green <danngreen1@gmail.com>
2024-12-23 14:25:54 -0800

    Allow building external plugins as built-in, for simulator
    Simulator builds assets image using firmware assets plus ext plugin assets
    
    Modify internal plugin manager to build Venom
    
    Ext plugin brand and path can be different
    
    Firmware ignores ext builtin plugins

--- simulator/ext-plugins.cmake
+++ simulator/ext-plugins.cmake
@@ -1,16 +1,15 @@
-# Three steps to build an external plugin into the simulator:
-# 1. Copy/paste the two list APPEND commands, puttting in the path to the
+# Two steps to build an external plugin into the simulator:
+#
+# 1. Copy/paste the two `list APPEND commands` below, puttting in the path to the
 #    plugin (where the CMakeLists.txt lives), and the plugin cmake library name
-# 2. Copy/paste the two lines per plugin in simulator/src/ext_plugin_builtin.hh.
-# 3. In the plugin, find the file where init(rack::Plugin*) is defined, and rename it to init_BRAND
+#
+# 2. In the plugin, find the file where init(rack::Plugin*) is defined, and rename it to init_Brand
 #    You can use #if defined(METAMODULE_BUILTIN) so that it's still called init() when building as a plugin for Rack or MM.
+#    `Brand` in init_Brand must match the plugin cmake library name you used in step 1.
 
-# list(APPEND ext_builtin_brand_paths "${CMAKE_CURRENT_LIST_DIR}/../../metamodule-plugin-examples/Bogaudio")
-# list(APPEND ext_builtin_brand_libname "BogaudioModules")
-
-list(APPEND ext_builtin_brand_paths "${CMAKE_CURRENT_LIST_DIR}/../../metamodule-plugin-examples/Venom")
-list(APPEND ext_builtin_brand_libname "Venom")
-
+# Example with Venom:
+# list(APPEND ext_builtin_brand_paths "${CMAKE_CURRENT_LIST_DIR}/../../metamodule-plugin-examples/Venom")
+# list(APPEND ext_builtin_brand_libname "Venom")
 
 
 #
@@ -45,6 +44,8 @@ add_custom_command(
 	DEPENDS ${ASSET_IMG_PATH}
  )
 
+ set(EXT_PLUGIN_INIT_CALLS "")
+
 foreach(branddir brand IN ZIP_LISTS ext_builtin_brand_paths ext_builtin_brand_libname)
 	set(METAMODULE_SDK_DIR ${CMAKE_CURRENT_LIST_DIR})
 	add_subdirectory(${branddir} ${CMAKE_CURRENT_BINARY_DIR}/builtins/${brand})
@@ -56,5 +57,9 @@ foreach(branddir brand IN ZIP_LISTS ext_builtin_brand_paths ext_builtin_brand_li
 	target_link_libraries(_vcv_ports_internal PUBLIC ${brand})
 	add_dependencies(asset-image ${brand}-assets)
 
+	string(APPEND EXT_PLUGIN_INIT_CALLS "\textern void init_${brand}(rack::plugin::Plugin *);\n\tinit_${brand}(&internal_plugins.emplace_back(\"${brand}\"));")
 endforeach()
+
+configure_file(src/ext_plugin_builtin.hh.in ${CMAKE_CURRENT_BINARY_DIR}/ext_plugin/ext_plugin_builtin.hh)
+target_include_directories(simulator PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/ext_plugin)
 
