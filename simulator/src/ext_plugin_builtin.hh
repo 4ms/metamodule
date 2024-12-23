9da8b32a15249f2339bf23d4fa2a6960904726e4
Dan Green <danngreen1@gmail.com>
2024-12-23 14:38:48 -0800

    Fix merge

--- simulator/src/ext_plugin_builtin.hh
+++
@@ -1,6 +0,0 @@
-#include <rack.hpp>
-
-inline void load_ext_builtin_plugins(std::list<rack::plugin::Plugin> &internal_plugins) {
-	extern void init_Venom(rack::plugin::Plugin *);
-	init_Venom(&internal_plugins.emplace_back("Venom"));
-}
