1068b9de8b7420d68b0359b60b80e218c0c63eb3
Dan Green <danngreen1@gmail.com>
2024-12-23 22:01:30 -0800

    NanoVG renders text as LVGL labels, caching them on XY pos/alignment
    Tried using native NanoVG method of drawing font to an image, then copying to frame buffer using triangles, but it's by far too slow

---
+++ firmware/vcv_plugin/export/nanovg/fontstash-wrapper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef struct FONScontext FONScontext;
+
+int fonsAddFont(FONScontext *s, const char *name, const char *path, int fontIndex);
+int fonsAddFontMem(FONScontext *s, const char *name, unsigned char *data, int ndata, int freeData, int fontIndex);
+int fonsGetFontByName(FONScontext *s, const char *name);
+const char *fonsGetFontNameByHandle(FONScontext *s, int handle);
+
+void fonsDeleteInternal(FONScontext *s);
+FONScontext *fonsCreateInternal();
+void fonsResetFallbackFont(FONScontext *stash, int base);
+int fonsAddFallbackFont(FONScontext *stash, int base, int fallback);
