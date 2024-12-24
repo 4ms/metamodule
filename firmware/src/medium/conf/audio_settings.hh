b7fc2abc73374efb2d0201c1068a6d8d00185449
Dan Green <danngreen1@gmail.com>
2024-12-24 11:30:06 -0800

    User setting for max number of audio overrun retries

--- firmware/src/medium/conf/audio_settings.hh
+++ firmware/src/medium/conf/audio_settings.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
 
@@ -9,30 +10,28 @@ namespace MetaModule
 struct AudioSettings {
 	static constexpr std::array ValidSampleRates = {24000u, 32000u, 48000u, 96000u};
 	static constexpr std::array ValidBlockSizes = {16u, 32u, 64u, 128u, 256u, 512u};
+	static constexpr std::array ValidOverrunRetries = {0u, 4u, 8u, 16u, 32u, 64u, 128u, 256u};
 
 	static constexpr uint32_t DefaultSampleRate = 48000;
 	static constexpr uint32_t DefaultBlockSize = 64;
+	static constexpr uint32_t DefaultOverrunRetries = 16;
 
 	uint32_t sample_rate = DefaultSampleRate;
 	uint32_t block_size = DefaultBlockSize;
+	uint32_t max_overrun_retries = DefaultOverrunRetries;
 
 	void make_valid() {
-
-		bool valid = false;
-		for (auto sr : ValidSampleRates) {
-			if (sample_rate == sr)
-				valid = true;
-		}
-		if (!valid)
+		if (std::ranges::find(ValidSampleRates, sample_rate) == std::end(ValidSampleRates)) {
 			sample_rate = DefaultSampleRate;
-
-		valid = false;
-		for (auto sz : ValidBlockSizes) {
-			if (block_size == sz)
-				valid = true;
 		}
-		if (!valid)
+
+		if (std::ranges::find(ValidBlockSizes, block_size) == std::end(ValidBlockSizes)) {
 			block_size = DefaultBlockSize;
+		}
+
+		if (std::ranges::find(ValidOverrunRetries, max_overrun_retries) == std::end(ValidOverrunRetries)) {
+			max_overrun_retries = DefaultOverrunRetries;
+		}
 	}
 };
 
