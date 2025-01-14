66fd1ba636fcfe5535a7dbc056175890a0555cc9
audiobird <cantrellhepner@gmail.com>
2025-01-13 16:03:18 -0800

    Run patches headless

--- headless/src/main.cc
+++ headless/src/main.cc
@@ -38,6 +38,8 @@ auto measure_time_ms(auto &&process) {
 
 } // namespace MetaModule::Headless
 
+std::atomic<bool> run{true};
+
 int main(int argc, char *argv[]) {
 	using namespace MetaModule;
 	using namespace MetaModule::Headless;
@@ -45,13 +47,7 @@ int main(int argc, char *argv[]) {
 	MetaModuleSim::Settings settings;
 	settings.parse(argc, argv);
 
-	const auto samples_to_run = settings.samples_to_run;
-	const float effective_play_time = samples_to_run / 48000.f;
-	printf("Run for %lu samples (%g sec @ 48kHz)\n", samples_to_run, effective_play_time);
-
 	auto patchdata = read_patch(settings.patch);
-
-	auto input = read_wav(settings.audio_in_file, samples_to_run);
 
 	// Load patch file
 	MetaModule::PatchPlayer player;
@@ -59,24 +55,15 @@ int main(int argc, char *argv[]) {
 
 	// Play patch
 	AudioWrapper audio{player};
-	std::vector<AudioConf::AudioOutFrame> output(samples_to_run);
 
-	auto duration = measure_time_ms([&] {
-		const auto inblocks = std::span{input};
-		const auto outblocks = std::span{output};
-		constexpr size_t block_size = 32;
+	if (!audio.start()) {
+		printf("Failed to start audio driver\n");
+	}
 
-		for (size_t i = 0; i < samples_to_run; i += block_size) {
-			auto in_block = inblocks.subspan(i, block_size);
-			auto out_block = outblocks.subspan(i, block_size);
+	while (run) {
+	}
 
-			audio.process(in_block, out_block);
-		}
-	});
-	printf("Effective load (single core): %g percent\n", (float)duration / (effective_play_time * 10));
-
-	// Write output to wav file
-	write_wav(output, settings.audio_out_file);
+	audio.stop();
 
 	return 0;
 }
