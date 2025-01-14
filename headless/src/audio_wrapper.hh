d5f52f03e8b7754a72957398a71592d025538993
audiobird <cantrellhepner@gmail.com>
2025-01-13 16:02:51 -0800

    Create jack client

--- headless/src/audio_wrapper.hh
+++ headless/src/audio_wrapper.hh
@@ -1,11 +1,13 @@
 #pragma once
 #include "patch_play/patch_player.hh"
 #include "stream_conf.hh"
+#include <jack/jack.h>
 
 namespace MetaModule::Headless
 {
 
 struct AudioWrapper {
+
 	AudioWrapper(PatchPlayer &patch_player)
 		: player{patch_player} {
 
@@ -14,29 +16,95 @@ struct AudioWrapper {
 
 		for (auto i = 0u; i < AudioConf::NumOutChans; i++)
 			player.set_output_jack_patched_status(i, true);
-	}
 
-	void process(AudioConf::AudioInBuffer in_buff, AudioConf::AudioOutBuffer out_buff) {
+		client = jack_client_open("MetaModule", {}, {});
+		if (!client) {
+			return;
+		}
 
-		for (unsigned i = 0; auto &out : out_buff) {
-			auto &in = in_buff[i++];
-
-			for (auto [i, injack] : enumerate(in.chan)) {
-				player.set_panel_input(i, injack);
+		for (auto i = 0u; i < out.size(); i++) {
+			const std::string name = "out_" + std::to_string(i);
+			out[i] = jack_port_register(client, name.c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+			if (!out[i]) {
+				return;
 			}
+		}
 
-			// Step patch
-			player.update_patch();
-
-			// Get outputs
-			for (auto [i, outjack] : enumerate(out.chan)) {
-				outjack = player.get_panel_output(i);
+		for (auto i = 0u; i < in.size(); i++) {
+			const std::string name = "in_" + std::to_string(i);
+			in[i] = jack_port_register(client, name.c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+			if (!in[i]) {
+				return;
 			}
+		}
+
+		if (jack_set_process_callback(client, onJackProcess, this)) {
+			return;
 		}
 	}
 
+	~AudioWrapper() {
+		for (auto &i : in) {
+			jack_port_unregister(client, i);
+		}
+
+		for (auto &o : out) {
+			jack_port_unregister(client, o);
+		}
+
+		jack_client_close(client);
+	}
+
+	bool start() {
+		if (jack_activate(client)) {
+			jack_client_close(client);
+			return false;
+		}
+		return true;
+	}
+
+	bool stop() {
+		if (jack_deactivate(client)) {
+			jack_client_close(client);
+			return false;
+		}
+		return true;
+	}
+
 private:
+	jack_client_t *client{};
+	std::array<jack_port_t *, AudioConf::NumOutChans> out;
+	std::array<jack_port_t *, AudioConf::NumInChans> in;
+
 	PatchPlayer &player;
+
+	void onJackProcess(jack_nframes_t nFrames) {
+		std::array<float *, 2> ip{};
+		std::array<float *, 2> op{};
+
+		for (auto i = 0u; i < in.size(); i++) {
+			ip[i] = reinterpret_cast<float *>(jack_port_get_buffer(in[i], nFrames));
+		}
+
+		for (auto i = 0u; i < out.size(); i++) {
+			op[i] = reinterpret_cast<float *>(jack_port_get_buffer(out[i], nFrames));
+		}
+
+		for (auto i = 0u; i < nFrames; ++i) {
+			player.set_panel_input(0, ip[0][i] * 10);
+			player.set_panel_input(1, ip[1][i] * 10);
+
+			player.update_patch();
+
+			op[0][i] = player.get_panel_output(0) / 12;
+			op[1][i] = player.get_panel_output(1) / 12;
+		}
+	}
+
+	static int onJackProcess(jack_nframes_t nFrames, void *args) {
+		static_cast<AudioWrapper *>(args)->onJackProcess(nFrames);
+		return 0;
+	}
 };
 
 } // namespace MetaModule::Headless
