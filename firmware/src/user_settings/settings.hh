a0d053819bc18610139f2b9388ce3d6797c5d4fd
Dan Green <danngreen1@gmail.com>
2025-01-16 21:07:09 -0800

    Move screensaver and view settings to user_settings/ dir

--- firmware/src/user_settings/settings.hh
+++ firmware/src/user_settings/settings.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include "conf/audio_settings.hh"
 #include "fs/volumes.hh"
+#include "user_settings/fs_settings.hh"
 #include "user_settings/plugin_autoload_settings.hh"
 #include "user_settings/screensaver_settings.hh"
 #include "user_settings/view_settings.hh"
