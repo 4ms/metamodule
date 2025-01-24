7fc654469f64fbdc2cf9be6f5cbab5dcec6dcf72
Dan Green <danngreen1@gmail.com>
2025-01-23 16:05:21 -0800

    fix libc_stubs in merge

--- firmware/src/core_m4/libc_stub.c
+++ firmware/src/core_m4/libc_stub.c
@@ -14,10 +14,10 @@ int _read(int fd, char *ptr, int len) {
 	return -1;
 }
 
-int _fstat(int x, void *p) {
+__attribute__((used)) int _fstat(int x, void *p) {
 	return 0;
 }
 
-int _isatty(int fd) {
-	return 0;
+__attribute__((used)) int _isatty(int x) {
+	return -1;
 }
