6c87fd5d66ae31fcbbc7045b4dacb45c1c091b03
Dan Green <danngreen1@gmail.com>
2025-01-31 15:20:29 -0800

    Implement async_osdialog_file

--- firmware/vcv_plugin/export/osdialog/async_filebrowser.cc
+++ firmware/vcv_plugin/export/osdialog/async_filebrowser.cc
@@ -1,5 +1,7 @@
+#include "async_filebrowser.hh"
 #include "gui/pages/file_browser/file_browser_adaptor.hh"
 #include <cstdio>
+#include <osdialog.h>
 
 namespace MetaModule
 {
@@ -14,6 +16,29 @@ void register_file_browser_vcv(FileBrowserDialog &file_browser) {
 }
 
 } // namespace MetaModule
+
+void async_osdialog_file(osdialog_file_action action,
+						 const char *path,
+						 const char *filename,
+						 osdialog_filters *filters,
+						 std::function<void(char *path)> &&action_function) {
+
+	using namespace MetaModule;
+
+	if (!action_function || !browser)
+		return;
+
+	if (action == OSDIALOG_SAVE) {
+		printf("Save file dialog box -- not supported\n");
+
+	} else if (action == OSDIALOG_OPEN) {
+		auto filter_string = stringify_osdialog_filters(filters);
+		show_file_browser(browser, filter_string.c_str(), path, "Open File:", action_function);
+
+	} else if (action == OSDIALOG_OPEN_DIR) {
+		show_file_browser(browser, "*/", path, "Open Folder:", action_function);
+	}
+}
 
 void async_dialog_filebrowser(const bool saving,
 							  const char *const nameOrExtensions,
