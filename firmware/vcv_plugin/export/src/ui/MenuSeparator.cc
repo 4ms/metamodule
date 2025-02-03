1aef34e2b7bff6034514514a4711c434a913f1f9
Dan Green <danngreen1@gmail.com>
2025-02-03 14:49:53 -0800

    Implement rack::MenuLabel/Separator

---
+++ firmware/vcv_plugin/export/src/ui/MenuSeparator.cc
@@ -0,0 +1,11 @@
+#include <ui/MenuSeparator.hpp>
+
+namespace rack::ui
+{
+
+MenuSeparator::MenuSeparator() = default;
+
+void MenuSeparator::draw(const DrawArgs &args) {
+}
+
+} // namespace rack::ui
