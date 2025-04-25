3f9762b2f940f8b5aa4c2531f019251af4d02b46
Dan Green <danngreen1@gmail.com>
2025-04-25 11:38:21 -0700

    CPU tests use normal allocators unless #define is set for enabling MemoryWatcher

--- firmware/src/load_test/test_modules.hh
+++ firmware/src/load_test/test_modules.hh
@@ -8,8 +8,6 @@
 namespace MetaModule::LoadTest
 {
 
-constexpr bool MM_LOADTEST_MEASURE_MEMORY = true;
-
 struct ModuleEntry {
 	static constexpr std::array<unsigned, 5> blocksizes{32, 64, 128, 256, 512};
 	std::string slug;
@@ -38,29 +36,30 @@ inline void test_all_modules(auto append_file) {
 		for (auto slug : slugs) {
 			ModuleEntry entry;
 			entry.slug = brand + ":" + slug;
-			pr_info("Testing %s\n", slug.c_str());
+			pr_info("Testing %s\n", entry.slug.c_str());
 			lv_label_set_text_fmt(ui_MainMenuNowPlaying, "Testing %s", entry.slug.c_str());
 
-			if constexpr (MM_LOADTEST_MEASURE_MEMORY) {
-				auto mem_tester = ModuleMemoryTester{entry.slug};
-				entry.mem_usage = mem_tester.run_test(ModuleMemoryTester::TestType::FirstRun);
-			}
+#ifdef MM_LOADTEST_MEASURE_MEMORY
+			auto mem_tester = ModuleMemoryTester{entry.slug};
+			pr_info("Running memory test\n");
+			entry.mem_usage = mem_tester.run_test(ModuleMemoryTester::TestType::FirstRun);
+#endif
 
 			for (auto i = 0u; auto blocksize : ModuleEntry::blocksizes) {
 				ModuleLoadTester tester(entry.slug);
 
-				pr_trace("Block size %u\n", blocksize);
+				pr_info("Block size %u\n", blocksize);
 
-				pr_trace("Running all unpatched test\n");
+				pr_info("Running all unpatched test\n");
 				entry.isolated[i] = tester.run_test(blocksize, KnobTestType::AllStill, JackTestType::NonePatched);
 
-				pr_trace("Running Zero'ed inputs test\n");
+				pr_info("Running Zero'ed inputs test\n");
 				entry.patched[i] = tester.run_test(blocksize, KnobTestType::AllStill, JackTestType::AllInputsZero);
 
-				pr_trace("Running LFO test\n");
+				pr_info("Running LFO test\n");
 				entry.cv_modulated[i] = tester.run_test(blocksize, KnobTestType::AllStill, JackTestType::AllInputsLFO);
 
-				pr_trace("Running audio-rate test\n");
+				pr_info("Running audio-rate test\n");
 				entry.audio_modulated[i] =
 					tester.run_test(blocksize, KnobTestType::AllStill, JackTestType::AllInputsAudio);
 
@@ -71,7 +70,7 @@ inline void test_all_modules(auto append_file) {
 		}
 	}
 
-	lv_label_set_text(ui_MainMenuNowPlaying, "");
+	lv_label_set_text(ui_MainMenuNowPlaying, "Finished load tests");
 }
 
 inline std::string csv_header() {
@@ -101,13 +100,13 @@ inline std::string csv_header() {
 		pr_info("InputsAudio-%u, ", blocksize);
 	}
 
-	if constexpr (MM_LOADTEST_MEASURE_MEMORY) {
-		s += "PeakStartupMem, PeakRunningMem, ";
-		// Not accurate, but sometimes a hint:
-		s += "LeakedMem?, ";
-		s += "DoubleFree?";
-		pr_info("PeakStartupMem, PeakRunningMem, LeakedMem, DoubleFree?, Valid");
-	}
+#ifdef MM_LOADTEST_MEASURE_MEMORY
+	s += "PeakStartupMem, PeakRunningMem, ";
+	// Not accurate, but sometimes a hint:
+	s += "LeakedMem?, ";
+	s += "DoubleFree?";
+	pr_info("PeakStartupMem, PeakRunningMem, LeakedMem, DoubleFree?, Valid");
+#endif
 
 	s += "\n";
 	pr_info("\n");
@@ -124,8 +123,8 @@ inline std::string entry_to_csv(ModuleEntry const &entry) {
 	pr_info("%s, ", entry.slug.c_str());
 
 	auto report_cpu = [&s](auto entryitem) {
-		char buf[8];
-		snprintf(buf, 8, "%.3f, ", entryitem.average_run_time / sampletime);
+		char buf[16];
+		snprintf(buf, 16, "%.3f, ", entryitem.average_run_time / sampletime);
 		s += buf;
 		pr_info("%.3f, ", entryitem.average_run_time / sampletime);
 	};
@@ -146,23 +145,23 @@ inline std::string entry_to_csv(ModuleEntry const &entry) {
 		report_cpu(entry.audio_modulated[i]);
 	}
 
-	if constexpr (MM_LOADTEST_MEASURE_MEMORY) {
-		if (entry.mem_usage.results_invalid) {
-			s += "CAN'T MEASURE, , , ";
-		} else {
-			s += std::to_string(entry.mem_usage.peak_mem_startup) + ", ";
-			s += std::to_string(entry.mem_usage.peak_running_mem) + ", ";
-			// Not accurate, but sometimes a hint:
-			s += std::to_string(entry.mem_usage.mem_leaked) + ", ";
-			s += entry.mem_usage.double_free ? "YES" : "n";
-		}
-		pr_info("%zu, %zu, %zu, %d, %s\n",
-				entry.mem_usage.peak_mem_startup,
-				entry.mem_usage.peak_running_mem,
-				entry.mem_usage.mem_leaked,
-				entry.mem_usage.double_free,
-				entry.mem_usage.results_invalid ? "TOOMANYALLOCS" : "ok");
+#ifdef MM_LOADTEST_MEASURE_MEMORY
+	if (entry.mem_usage.results_invalid) {
+		s += "CAN'T MEASURE, , , ";
+	} else {
+		s += std::to_string(entry.mem_usage.peak_mem_startup) + ", ";
+		s += std::to_string(entry.mem_usage.peak_running_mem) + ", ";
+		// Not accurate, but sometimes a hint:
+		s += std::to_string(entry.mem_usage.mem_leaked) + ", ";
+		s += entry.mem_usage.double_free ? "YES" : "n";
 	}
+	pr_info("%zu, %zu, %zu, %d, %s\n",
+			entry.mem_usage.peak_mem_startup,
+			entry.mem_usage.peak_running_mem,
+			entry.mem_usage.mem_leaked,
+			entry.mem_usage.double_free,
+			entry.mem_usage.results_invalid ? "TOOMANYALLOCS" : "ok");
+#endif
 
 	if (s.ends_with(", ")) {
 		s.pop_back();
