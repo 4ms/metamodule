9f4561b1f8f9859c65231536b89c9c7b3e3b6be3
Dan Green <danngreen1@gmail.com>
2025-04-29 16:51:14 -0700

    Merge main branch's handling of 0-byte files with v2.0-dev's write FlagAbort

--- firmware/src/fs/asset_drive/untar.cc
+++ firmware/src/fs/asset_drive/untar.cc
@@ -126,12 +126,15 @@ bool Archive::extract_files(std::function<uint32_t(std::string_view, std::span<c
 		auto name = entry.name;
 
 		if (entry.type == TarEntry::File) {
-			auto filedata = extract_file_entry(entry);
-			pr_dump("Extracted %zu bytes for %s\n", filedata.size(), name.c_str());
-			auto bytes_written = write(name, filedata);
+			if (auto filedata = extract_file_entry(entry)) {
+				pr_dump("Extracted %zu bytes for %s\n", filedata->size(), name.c_str());
+				auto bytes_written = write(name, *filedata);
 
-			if (bytes_written == FlagAbort) {
-				return false;
+				if (bytes_written == FlagAbort) {
+					return false;
+				}
+			} else {
+				pr_err("File too large to extract: %s\n", name.c_str());
 			}
 		} else {
 			pr_trace("Skipping non-file entry %s\n", name.c_str());
