bd87e7bb56aa48175b3329e1074f1b40478ca9ce
Dan Green <danngreen1@gmail.com>
2025-05-15 15:39:41 -0700

    Load tester clears previous results before running, and streams to in_progress file, then fills cpu_test file when done

--- firmware/src/core_a7/aux_core_main.cc
+++ firmware/src/core_a7/aux_core_main.cc
@@ -87,10 +87,18 @@ extern "C" void aux_core_main() {
 		if (should_run == "all\n" || Pin{ControlPins::but0, PinMode::Input, PinPull::Up, PinPolarity::Inverted}.is_on())
 		{
 			pr_info("A7 Core 2 running CPU load tests\n");
-			LoadTest::test_all_modules([&file_storage_proxy, &ui](std::string_view csv_line) {
-				FS::append_file(file_storage_proxy, csv_line, {"cpu_test.csv", Volume::USB});
-				ui.update_screen();
+
+			// clear previous results files
+			FS::write_file(file_storage_proxy, std::string("In progress"), {"cpu_test.csv", Volume::USB});
+			FS::write_file(file_storage_proxy, std::string(""), {"cpu_test_in_progress.csv", Volume::USB});
+
+			std::string results;
+			LoadTest::test_all_modules([&file_storage_proxy /*, &ui*/, &results](std::string_view csv_line) {
+				results += csv_line;
+				FS::append_file(file_storage_proxy, csv_line, {"cpu_test_in_progress.csv", Volume::USB});
+				// ui.update_screen();
 			});
+			FS::write_file(file_storage_proxy, results, {"cpu_test.csv", Volume::USB});
 		}
 
 		// Finish Tests
