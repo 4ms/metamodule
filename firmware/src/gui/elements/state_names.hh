5de67ecd9c93cb5f05cb153e7420268ff3a9ce4d
Eric Gao <ericxgao@gmail.com>
2025-05-26 23:45:05 -0700

    fix: crash due to accessing out of bounds pos names

--- firmware/src/gui/elements/state_names.hh
+++ firmware/src/gui/elements/state_names.hh
@@ -34,9 +34,12 @@ inline std::string get_element_value_string(Element const &element, float value,
 						   el.display_base == 0 && el.units.length() == 0) {
 						   unsigned v = std::round(std::clamp(value, 0.f, 1.f) * (float)(el.num_pos - 1));
 
-						   if (v >= 0 && v < el.num_pos && el.pos_names[v].size())
+						   if (v >= 0 && v < el.pos_names.size()) {
 							   s = el.pos_names[v];
-						} else {
+						   }
+						} 
+						
+						if (!s.size()) {
 						   // Scale it
 						   float v = value * (el.max_value - el.min_value) + el.min_value;
 						   if (el.integral)
