9ec0f407444609acd09167bb12083cd0d8eb616b
Dan Green <danngreen1@gmail.com>
2025-06-26 20:08:53 -0700

    Enable/disable sending MIDI feedback based on setting

--- firmware/src/core_a7/aux_core_player.hh
+++ firmware/src/core_a7/aux_core_player.hh
@@ -1,12 +1,9 @@
 #pragma once
-#include "midi.hpp"
-#include "midi/midi_queue.hh"
-#include "midi/midi_router.hh"
-#include "midi/midi_sync.hh"
 #include "core_a7/smp_api.hh"
 #include "drivers/interrupt.hh"
 #include "drivers/smp.hh"
 #include "gui/ui.hh"
+#include "midi/midi_sync.hh"
 #include "patch_play/patch_player.hh"
 #include "util/fixed_vector.hh"
 #include <atomic>
@@ -20,7 +17,7 @@ struct AuxPlayer {
 
 	FixedVector<unsigned, 64> module_ids;
 	unsigned midi_throttle_counter = 0;
-	
+
 	// MIDI sync instance
 	MidiSync midi_sync;
 
@@ -88,16 +85,21 @@ struct AuxPlayer {
 				}
 			}
 
-			for (auto &p : patch_player.watched_params().active_watched_params()) {
-				if (p.is_active()) {
-					auto value = patch_player.get_param(p.module_id, p.param_id);
-					auto map = MappedKnob{.panel_knob_id = p.panel_knob_id};
-					if (map.is_midi_cc()) {
-						midi_sync.sync_param_to_midi(value, p.midi_chan, map.cc_num());
-					} else if (map.is_midi_notegate()) {
-						midi_sync.sync_param_to_midi_notegate(value, p.midi_chan, map.notegate_num());
-					} else if (p.panel_knob_id == MidiPitchWheelJack) {
-						midi_sync.sync_param_to_midi_pitchwheel(value, p.midi_chan);
+			if (ui.midi_feedback_enabled()) {
+				for (auto &p : patch_player.watched_params().active_watched_params()) {
+					if (p.is_active()) {
+						auto value = patch_player.get_param(p.module_id, p.param_id);
+						auto map = MappedKnob{.panel_knob_id = p.panel_knob_id};
+
+						if (map.is_midi_cc()) {
+							midi_sync.sync_param_to_midi(value, p.midi_chan, map.cc_num());
+
+						} else if (map.is_midi_notegate()) {
+							midi_sync.sync_param_to_midi_notegate(value, p.midi_chan, map.notegate_num());
+
+						} else if (p.panel_knob_id == MidiPitchWheelJack) {
+							midi_sync.sync_param_to_midi_pitchwheel(value, p.midi_chan);
+						}
 					}
 				}
 			}
