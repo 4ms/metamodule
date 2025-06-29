93202420acf3d0326a49e2c3d227ada96d346478
Dan Green <danngreen1@gmail.com>
2025-06-28 19:26:10 -0700

    Control expander I2C bus runs at 400kHz

--- firmware/src/medium/conf/button_expander_conf.hh
+++ firmware/src/medium/conf/button_expander_conf.hh
@@ -4,6 +4,8 @@
 namespace MetaModule::ButtonExpander
 {
 
+constexpr uint8_t MaxAddresses = 8; // Number of addresses the chip supports
+
 // I2C for Aux header (not audio expander header)
 const mdrivlib::I2CConfig i2c_conf = {
 	.I2Cx = I2C2,
@@ -11,13 +13,19 @@ const mdrivlib::I2CConfig i2c_conf = {
 	.SDA = {mdrivlib::GPIO::B, mdrivlib::PinNum::_11, mdrivlib::PinAF::AltFunc4},
 	.timing =
 		{
-			.PRESC = 0x10,
-			.SCLDEL_SDADEL = 0x50,
-			.SCLH = 0x58,
-			.SCLL = 0x74,
+			// .PRESC = 0x00,
+			// .SCLDEL_SDADEL = 0x71,
+			// .SCLH = 0x40,
+			// .SCLL = 0x80,
+			.PRESC = 0x40,
+			.SCLDEL_SDADEL = 0x40,
+			.SCLH = 0x0A,
+			.SCLL = 0x1D,
 		},
 	.priority1 = 2,
 	.priority2 = 1,
+	.analog_filter = true,
+	.digital_filter = mdrivlib::I2CConfig::DigitalFilterLevel::_8,
 };
 
 constexpr mdrivlib::GPIO_expander_conf gpio_chip_conf = {
