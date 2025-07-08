6c95a79337484d86de0104ee58e88913a0f5145d
Dan Green <danngreen1@gmail.com>
2025-07-07 22:58:46 -0700

    Move random() implemenation to hardware_support library

--- simulator/stubs/random.cpp
+++ simulator/stubs/random.cpp
@@ -27,3 +27,23 @@ Xoroshiro128Plus &local() {
 }
 
 } // namespace rack::random
+
+#include <cstdint>
+#include <cstdlib>
+
+namespace MetaModule
+{
+
+bool hardware_random_ready() {
+	return true;
+}
+
+uint32_t hardware_random() {
+	return std::rand(); //fake it for the simulator
+}
+
+uint32_t random() {
+	return std::rand();
+}
+
+} // namespace MetaModule
