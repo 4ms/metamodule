13a3497de72ded7003a3271d1a1f4130476a7304
Dan Green <danngreen1@gmail.com>
2025-07-09 16:44:05 -0700

    rack::random::init() uses hardware_support library
    no longer need special vcv_hardware/ dir

--- firmware/src/hardware_support/random.cc
+++ firmware/src/hardware_support/random.cc
@@ -1,4 +1,4 @@
-#include "drivers/stm32xx.h"
+#include "drivers/rcc.hh"
 #include <cstdint>
 #include <cstdlib>
 
@@ -16,5 +16,17 @@ uint32_t hardware_random() {
 uint32_t random() {
 	return std::rand();
 }
+
+__attribute__((visibility("hidden"))) bool hardware_random_init() {
+	mdrivlib::RCC_Enable::RNG1_::set();
+
+	RNG1->CR = RNG1->CR | RNG_CED_DISABLE;
+	RNG1->CR |= RNG_CR_RNGEN;
+
+	return true;
+}
+
+// globally init the random peripheral once
+static auto s_hardware_random_init = hardware_random_init();
 
 } // namespace MetaModule
--- simulator/stubs/random.cpp
+++ simulator/stubs/random.cpp
@@ -1,33 +1,3 @@
-#include "random.hpp"
-#include <time.h>
-
-namespace rack::random
-{
-
-static Xoroshiro128Plus rng;
-
-void init() {
-	// Don't reset state if already seeded
-	if (rng.isSeeded())
-		return;
-
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-
-	rng.seed(ts.tv_sec, ts.tv_nsec);
-
-	// Shift state a few times due to low seed entropy
-	for (int i = 0; i < 4; i++) {
-		rng();
-	}
-}
-
-Xoroshiro128Plus &local() {
-	return rng;
-}
-
-} // namespace rack::random
-
 #include <cstdint>
 #include <cstdlib>
 
