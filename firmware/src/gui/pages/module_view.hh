d36c1d60d84acaaaf61dfa7225ac994daa051391
Dan Green <danngreen1@gmail.com>
2025-08-04 15:04:29 -0700

    Formatting

--- firmware/src/gui/pages/module_view.hh
+++ firmware/src/gui/pages/module_view.hh
@@ -227,7 +227,7 @@ struct ModuleViewPage : PageBase {
 			}
 
 			append_connected_jack_name(opts, gui_el.idx, gui_el.module_idx, *patch);
-			
+
 			// Add separator and parameter name
 			opts.append(" ");
 			opts.append(base.short_name.substr(0, base.short_name.find_first_of("\0\n")));
@@ -331,10 +331,12 @@ struct ModuleViewPage : PageBase {
 
 	void update() override {
 		if (gui_state.back_button.is_just_released()) {
-			
+
 			// If encoder is pressed, handle removal shortcuts directly here
 			bool encoder_is_pressed = metaparams.rotary_button.is_pressed();
-			if (encoder_is_pressed && mode == ViewMode::List && !action_menu.is_visible() && !settings_menu.is_visible()) {
+			if (encoder_is_pressed && mode == ViewMode::List && !action_menu.is_visible() &&
+				!settings_menu.is_visible())
+			{
 				handle_encoder_back_removal();
 			} else if (action_menu.is_visible()) {
 				action_menu.back();
@@ -640,7 +642,7 @@ private:
 		auto page = static_cast<ModuleViewPage *>(event->user_data);
 
 		auto cur_sel = lv_roller_get_selected(ui_ElementRoller);
-		
+
 		if (cur_sel >= page->roller_drawn_el_idx.size()) {
 			page->roller_hover.hide();
 			return;
@@ -689,7 +691,6 @@ private:
 
 		// Save current select in args so we can navigate back to this item
 		page->args.element_indices = page->drawn_elements[cur_idx].gui_element.idx;
-
 
 		page->unhighlight_component(prev_sel);
 		page->highlight_component(cur_idx);
@@ -780,7 +781,7 @@ private:
 			suppress_next_click = false;
 			return;
 		}
-		
+
 		auto &drawn_element = drawn_elements[drawn_idx];
 		args.element_indices = drawn_element.gui_element.idx;
 
@@ -957,34 +958,34 @@ private:
 
 	uint16_t selected_input_port = 0;
 	uint16_t selected_output_port = 0;
-	
+
 	bool suppress_next_click = false;
 
 	void handle_quick_assign() {
 		const DrawnElement *current_element = get_highlighted_element();
-		
+
 		if (!current_element) {
 			return;
 		}
-		
+
 		bool is_param = std::visit(overloaded{
-		    [](BaseElement const &el) { return false; },
-		    [](ParamElement const &el) { return true; },
-		    [](AltParamElement const &el) { return false; },
-	        },
-	        current_element->element);
-		                
+									   [](BaseElement const &el) { return false; },
+									   [](ParamElement const &el) { return true; },
+									   [](AltParamElement const &el) { return false; },
+								   },
+								   current_element->element);
+
 		bool is_input_jack = std::holds_alternative<JackInput>(current_element->element);
 		bool is_output_jack = std::holds_alternative<JackOutput>(current_element->element);
-		
+
 		bool is_jack = is_input_jack || is_output_jack;
-		
+
 		if (!is_param && !is_jack) {
 			return;
 		}
-		
+
 		bool encoder_is_pressed = metaparams.rotary_button.is_pressed();
-		
+
 		if (encoder_is_pressed) {
 			// Parameter quick assign: hold encoder + wiggle knob
 			if (is_param) {
@@ -996,7 +997,7 @@ private:
 					}
 					i++;
 				}
-				
+
 				// MIDI CC quick assign: hold encoder + send MIDI CC
 				for (unsigned ccnum = 0; auto &cc : params.midi_ccs) {
 					if (cc.changed) {
@@ -1007,7 +1008,7 @@ private:
 					}
 					ccnum++;
 				}
-				
+
 				// MIDI Note quick assign: hold encoder + send MIDI note
 				auto &note = params.last_midi_note;
 				if (note.changed) {
@@ -1016,9 +1017,8 @@ private:
 					roller_hover.force_redraw();
 					return;
 				}
-				
 			}
-			
+
 			// Jack quick assign: hold encoder + turn encoder
 			if (is_jack) {
 				if (auto motion = metaparams.rotary_pushed.use_motion(); motion != 0) {
@@ -1033,18 +1033,18 @@ private:
 
 	void handle_encoder_back_removal() {
 		const DrawnElement *current_element = get_highlighted_element();
-		
+
 		if (!current_element) {
 			return;
 		}
-		
+
 		bool is_param = std::visit(overloaded{
-		    [](BaseElement const &el) { return false; },
-		    [](ParamElement const &el) { return true; },
-		    [](AltParamElement const &el) { return false; },
-	        },
-	        current_element->element);
-		                
+									   [](BaseElement const &el) { return false; },
+									   [](ParamElement const &el) { return true; },
+									   [](AltParamElement const &el) { return false; },
+								   },
+								   current_element->element);
+
 		bool is_input_jack = std::holds_alternative<JackInput>(current_element->element);
 		bool is_output_jack = std::holds_alternative<JackOutput>(current_element->element);
 		bool is_jack = is_input_jack || is_output_jack;
@@ -1057,24 +1057,20 @@ private:
 			uint16_t module_id = (uint16_t)current_element->gui_element.module_idx;
 			uint16_t param_id = (uint16_t)current_element->gui_element.idx.param_idx;
 			remove_existing_mappings_for_param(module_id, param_id);
-			
+
 		} else if (is_jack) {
 			// Remove jack mappings
 			ElementType jack_type = is_input_jack ? ElementType::Input : ElementType::Output;
-			
+
 			Jack module_jack;
 			if (jack_type == ElementType::Input) {
-				module_jack = {
-					.module_id = (uint16_t)current_element->gui_element.module_idx,
-					.jack_id = (uint16_t)current_element->gui_element.idx.input_idx
-				};
+				module_jack = {.module_id = (uint16_t)current_element->gui_element.module_idx,
+							   .jack_id = (uint16_t)current_element->gui_element.idx.input_idx};
 			} else {
-				module_jack = {
-					.module_id = (uint16_t)current_element->gui_element.module_idx,
-					.jack_id = (uint16_t)current_element->gui_element.idx.output_idx
-				};
+				module_jack = {.module_id = (uint16_t)current_element->gui_element.module_idx,
+							   .jack_id = (uint16_t)current_element->gui_element.idx.output_idx};
 			}
-			
+
 			module_mods.put(RemoveJackMappings{.jack = module_jack, .type = jack_type});
 		}
 
@@ -1082,7 +1078,7 @@ private:
 		roller_hover.force_redraw();
 	}
 
-	const DrawnElement* get_highlighted_element() {
+	const DrawnElement *get_highlighted_element() {
 		if (auto drawn_idx = get_drawn_idx(cur_selected)) {
 			return &drawn_elements[*drawn_idx];
 		}
@@ -1091,11 +1087,11 @@ private:
 
 	void remove_existing_mappings_for_param(uint16_t module_id, uint16_t param_id) {
 		uint32_t target_knobset = page_list.get_active_knobset();
-		
+
 		// Remove ALL existing knob mappings for this parameter in this knobset (non-MIDI)
 		if (target_knobset != PatchData::MIDIKnobSet && target_knobset < patch->knob_sets.size()) {
 			auto &knobset = patch->knob_sets[target_knobset];
-			
+
 			for (const auto &mapping : knobset.set) {
 				if (mapping.module_id == module_id && mapping.param_id == param_id) {
 					module_mods.put(RemoveMapping{.map = mapping, .set_id = target_knobset});
@@ -1127,7 +1123,7 @@ private:
 				return;
 			}
 		}
-		
+
 		remove_existing_mappings_for_param(module_id, param_id);
 
 		auto map = MappedKnob{
@@ -1142,7 +1138,7 @@ private:
 		// update the patch data and call refresh() automatically
 		uint32_t target_knobset = page_list.get_active_knobset();
 		module_mods.put(AddMapping{.map = map, .set_id = target_knobset});
-		
+
 		// Suppress the next click to prevent unwanted mapping pane opening
 		suppress_next_click = true;
 	}
@@ -1168,7 +1164,7 @@ private:
 		};
 
 		module_mods.put(AddMidiMap{.map = map});
-		
+
 		// Suppress the next click to prevent unwanted mapping pane opening
 		suppress_next_click = true;
 	}
@@ -1218,18 +1214,14 @@ private:
 
 		Jack module_jack;
 		uint16_t selected_port;
-		
+
 		if (jack_type == ElementType::Input) {
-			module_jack = {
-				.module_id = (uint16_t)element->gui_element.module_idx,
-				.jack_id = (uint16_t)element->gui_element.idx.input_idx
-			};
+			module_jack = {.module_id = (uint16_t)element->gui_element.module_idx,
+						   .jack_id = (uint16_t)element->gui_element.idx.input_idx};
 			selected_port = selected_input_port;
 		} else {
-			module_jack = {
-				.module_id = (uint16_t)element->gui_element.module_idx,
-				.jack_id = (uint16_t)element->gui_element.idx.output_idx
-			};
+			module_jack = {.module_id = (uint16_t)element->gui_element.module_idx,
+						   .jack_id = (uint16_t)element->gui_element.idx.output_idx};
 			selected_port = selected_output_port;
 		}
 
@@ -1237,14 +1229,10 @@ private:
 		module_mods.put(RemoveJackMappings{.jack = module_jack, .type = jack_type});
 
 		// Create new mapping to the selected port
-		AddJackMapping mapping{
-			.panel_jack_id = selected_port,
-			.jack = module_jack,
-			.type = jack_type
-		};
+		AddJackMapping mapping{.panel_jack_id = selected_port, .jack = module_jack, .type = jack_type};
 
 		module_mods.put(mapping);
-		
+
 		// Suppress the next click to prevent unwanted mapping pane opening
 		suppress_next_click = true;
 	}
