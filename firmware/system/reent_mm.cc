d773c1f2f95fbde9d5083b40ff8ac2278a9652aa
Dan Green <danngreen1@gmail.com>
2025-08-11 15:01:04 -0700

    reent: detect active process better and include ReadPatchElement proc

--- firmware/system/reent_mm.cc
+++ firmware/system/reent_mm.cc
@@ -1,28 +1,54 @@
+#include "reent_mm.hh"
 #include "drivers/stm32xx.h"
+#include <array>
 #include <reent.h>
 #include <sys/reent.h>
 
-static struct _reent core0_reent;
-static struct _reent core1_reent;
-static struct _reent async0_reent;
-static struct _reent async1_reent;
+namespace MetaModule
+{
 
-static bool init_reents = [] {
-	core0_reent = _REENT_INIT(core0_reent);
-	core1_reent = _REENT_INIT(core1_reent);
-	async0_reent = _REENT_INIT(async0_reent);
-	async1_reent = _REENT_INIT(async1_reent);
+static std::array<struct _reent, NUM_PROCESSES> reents;
+
+// Immediately invoked lambda run at static init time
+static bool init_all_reents = [] {
+	for (auto &r : reents) {
+		r = _REENT_INIT(r);
+	}
 	return true;
 }();
 
-extern "C" struct _reent *__getreent(void) {
-	auto core = __get_MPIDR() & 0b1; //0 = CA7 Core 1, 1 = CA7 Core 2
-	auto is_async_thread0 = (core == 0) && LL_TIM_IsActiveFlag_UPDATE(TIM7);
-	auto is_async_thread1 = (core == 1) && LL_TIM_IsActiveFlag_UPDATE(TIM6);
+Processes get_current_proc() {
+	const auto core = __get_MPIDR() & 0b1; //0 = CA7 Core 1, 1 = CA7 Core 2
+	if (core == 1) {
+		if (GIC_GetIRQStatus(TIM6_IRQn) & 0b10) {
+			return Processes::Core1AsyncThread;
+		}
+		if (GIC_GetIRQStatus(SGI1_IRQn) & 0b10) { // SMPCommand::ReadPatchGuiElements
+			return Processes::Core1ReadPatchElements;
+		}
+		if (GIC_GetIRQStatus(SGI4_IRQn) & 0b10) { // SMPCommand::PlayModuleList
+			return Processes::Core1Audio;
+		}
+		return Processes::Core1Main;
 
-	return is_async_thread0 ? &async0_reent :
-		   is_async_thread1 ? &async1_reent :
-		   core == 0		? &core0_reent :
-		   core == 1		? &core1_reent :
-							  &core0_reent;
+	} else {
+		if (GIC_GetIRQStatus(TIM7_IRQn) & 0b10) {
+			return Processes::Core0AsyncThread;
+		}
+		if (GIC_GetIRQStatus(DMA2_Stream1_IRQn) & 0b10) { // Audio SAI TX DMA
+			return Processes::Core0Audio;
+		}
+		return Processes::Core0Main;
+	}
+}
+
+int get_current_proc_id() {
+	return static_cast<int>(get_current_proc());
+}
+
+} // namespace MetaModule
+
+extern "C" struct _reent *__getreent(void) {
+	auto proc_id = MetaModule::get_current_proc_id();
+	return &MetaModule::reents[proc_id];
 }
