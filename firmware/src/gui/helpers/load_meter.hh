845a1ee27c916861f40bc1aff37fb5b674109244
Dan Green <danngreen1@gmail.com>
2025-10-01 16:33:51 -0700

    Remove comments

--- firmware/src/gui/helpers/load_meter.hh
+++ firmware/src/gui/helpers/load_meter.hh
@@ -18,30 +18,6 @@ inline void update_load_text(MetaParams const &metaparams,
 	if (settings.show_samplerate) {
 		auto [cur_sr, cur_bs, _] = patch_playloader.get_audio_settings();
 
-		// std::array<char, 32> txt;
-		// char *p = txt.begin();
-
-		// auto [new_p, ec] = std::to_chars(p, txt.end(), metaparams.audio_load);
-		// p = new_p;
-
-		// *p++ = '%';
-		// *p++ = ' ';
-		// p = std::copy(Gui::grey_color_html.begin(), Gui::grey_color_html.end(), p);
-
-		// auto [new_p2, ec2] = std::to_chars(p, txt.end(), cur_sr / 1000);
-		// p = new_p2;
-
-		// *p++ = 'k';
-		// *p++ = '/';
-
-		// auto [new_p3, ec3] = std::to_chars(p, txt.end(), cur_bs);
-		// p = new_p3;
-
-		// *p++ = LV_TXT_COLOR_CMD[0];
-		// *p++ = '\0';
-
-		// lv_label_set_text(meter, txt.data());
-
 		lv_label_set_text_fmt(meter,
 							  "%d%% %s%uk/%u%c",
 							  metaparams.audio_load,
