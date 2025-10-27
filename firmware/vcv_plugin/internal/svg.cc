37e3e5f52d8b4e6c38dbb7495bfb4809302288aa
Dan Green <danngreen1@gmail.com>
2025-10-27 16:06:26 -0700

    vcv internal does not depend on draw_img

--- firmware/vcv_plugin/internal/svg.cc
+++ firmware/vcv_plugin/internal/svg.cc
@@ -1,12 +1,24 @@
-#include "gui/elements/draw_img.hh"
 #include "gui/images/paths.hh"
+#include "lvgl.h"
 #include <math.hpp>
 
 rack::math::Vec get_svg_size(std::string const &svg_filename) {
 
 	auto path = LV_FS_FATFS_LETTER + std::string(":2:/") + svg_filename;
 
-	if (auto sz = MetaModule::ElementDrawer::get_image_size(path); sz) {
+	struct ImageSize {
+		uint32_t w;
+		uint32_t h;
+	};
+	auto get_image_size = [](std::string_view img_path) -> std::optional<ImageSize> {
+		lv_img_header_t img_header;
+		if (lv_img_decoder_get_info(img_path.data(), &img_header) == LV_RES_OK)
+			return ImageSize{img_header.w, img_header.h};
+		else
+			return std::nullopt;
+	};
+
+	if (auto sz = get_image_size(path); sz) {
 		// pr_dbg("%s is %f x %f\n",
 		// 	   svg_filename.c_str(),
 		// 	   MetaModule::PNGpx_to_SVGpx(sz->w),
