057d3945434383e33bb26accc8690e1c5b1e923b
Dan Green <danngreen1@gmail.com>
2025-10-29 15:29:21 -0700

    Fix settings not being read if > 2048 bytes

--- firmware/src/user_settings/settings_file.cc
+++ firmware/src/user_settings/settings_file.cc
@@ -1,7 +1,5 @@
 #include "settings_file.hh"
-#include "delay.hh"
 #include "fs/general_io.hh"
-#include "memory/ram_buffer.hh"
 #include "patch_file/file_storage_proxy.hh"
 #include "settings_parse.hh"
 #include "settings_serialize.hh"
@@ -20,41 +18,16 @@ bool write_settings(FileStorageProxy &proxy, UserSettings const &settings, Volum
 }
 
 bool read_settings(FileStorageProxy &proxy, UserSettings *settings, Volume vol) {
+	std::string buffer;
 
-	auto rawmem = get_ram_buffer();
-	auto buffer = std::span{(char *)rawmem.data(), 2048};
+	if (auto bytes_read = FS::read_file(proxy, buffer, PatchLocation{"settings.yml", vol})) {
+		pr_dbg("Settings file loaded: %zu bytes, beginning parsing\n", *bytes_read);
 
-	uint32_t timeout = get_time();
-	while (!proxy.request_load_file("settings.yml", vol, buffer)) {
-		if (get_time() - timeout > 1000) {
-			pr_err("Settings file read request not made in 1 second\n");
-			return false;
-		}
-	}
-
-	timeout = get_time();
-	while (true) {
-		auto msg = proxy.get_message();
-
-		if (msg.message_type == FileStorageProxy::LoadFileOK) {
-			pr_dbg(
-				"Settings file loaded in %ums, %zu bytes, beginning parsing\n", get_time() - timeout, msg.bytes_read);
-
-			auto yaml = std::span<char>{buffer.data(), msg.bytes_read};
-
-			return parse(yaml, settings);
-		}
-
-		else if (msg.message_type == FileStorageProxy::LoadFileFailed)
-		{
-			pr_info("Settings file not found\n");
-			return false;
-		}
-
-		if (get_time() - timeout > 3000) {
-			pr_err("Settings file read request not responded to in 3 seconds\n");
-			return false;
-		}
+		auto yaml = std::span<char>{buffer.data(), *bytes_read};
+		return parse(yaml, settings);
+	} else {
+		pr_info("Error reading settings file\n");
+		return false;
 	}
 }
 
