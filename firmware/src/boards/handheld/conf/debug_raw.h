6b0112fddfbf09d7dc3e38ea4e96d08ed52cb247
Dan Green <danngreen1@gmail.com>
2025-11-06 13:29:01 -0800

    Debug Raw for handheld

--- firmware/src/boards/handheld/conf/debug_raw.h
+++ firmware/src/boards/handheld/conf/debug_raw.h
@@ -4,24 +4,34 @@
 #if !defined(SIMULATOR)
 //C and C++
 
-static inline void DebugPin1High() {
+static inline void DebugPin2High() {
 	volatile uint32_t *GPIOD_BSRR = (volatile uint32_t *)(0x50005018);
-	*GPIOD_BSRR = (1 << 11);
+	*GPIOD_BSRR = (1 << 0);
+}
+
+static inline void DebugPin2Low() {
+	volatile uint32_t *GPIOD_BSRR = (volatile uint32_t *)(0x50005018);
+	*GPIOD_BSRR = (1 << (0 + 16));
+}
+
+static inline void DebugPin1High() {
+	volatile uint32_t *GPIOE_BSRR = (volatile uint32_t *)(0x50006018);
+	*GPIOE_BSRR = (1 << 5);
 }
 
 static inline void DebugPin1Low() {
-	volatile uint32_t *GPIOD_BSRR = (volatile uint32_t *)(0x50005018);
-	*GPIOD_BSRR = (1 << (11 + 16));
+	volatile uint32_t *GPIOE_BSRR = (volatile uint32_t *)(0x50006018);
+	*GPIOE_BSRR = (1 << (5 + 16));
 }
 
 static inline void DebugPin0High() {
-	volatile uint32_t *GPIOE_BSRR = (volatile uint32_t *)(0x50006018);
-	*GPIOE_BSRR = (1 << 9);
+	volatile uint32_t *GPIOD_BSRR = (volatile uint32_t *)(0x50005018);
+	*GPIOD_BSRR = (1 << 7);
 }
 
 static inline void DebugPin0Low() {
-	volatile uint32_t *GPIOE_BSRR = (volatile uint32_t *)(0x50006018);
-	*GPIOE_BSRR = (1 << (9 + 16));
+	volatile uint32_t *GPIOD_BSRR = (volatile uint32_t *)(0x50005018);
+	*GPIOD_BSRR = (1 << (7 + 16));
 }
 
 #define HARDWARE_BKPT() asm volatile("bkpt")
