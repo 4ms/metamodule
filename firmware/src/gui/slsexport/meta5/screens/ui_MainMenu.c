351324c6984e69db84e38c656d130e0a751e7f34
Dan Green <danngreen1@gmail.com>
2025-11-17 07:05:47 -0800

    unify all load meters

--- firmware/src/gui/slsexport/meta5/screens/ui_MainMenu.c
+++ firmware/src/gui/slsexport/meta5/screens/ui_MainMenu.c
@@ -303,28 +303,4 @@ lv_obj_set_style_pad_right(ui_MenuLabelSettings, 0, LV_PART_MAIN| LV_STATE_DEFAU
 lv_obj_set_style_pad_top(ui_MenuLabelSettings, 12, LV_PART_MAIN| LV_STATE_DEFAULT);
 lv_obj_set_style_pad_bottom(ui_MenuLabelSettings, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
 
-ui_MainMenuLoadMeter = lv_label_create(ui_MainMenu);
-lv_obj_set_width( ui_MainMenuLoadMeter, LV_SIZE_CONTENT);
-lv_obj_set_height( ui_MainMenuLoadMeter, 20);
-lv_obj_set_x( ui_MainMenuLoadMeter, -4 );
-lv_obj_set_y( ui_MainMenuLoadMeter, 217 );
-lv_obj_set_align( ui_MainMenuLoadMeter, LV_ALIGN_TOP_RIGHT );
-lv_obj_set_flex_flow(ui_MainMenuLoadMeter,LV_FLEX_FLOW_ROW);
-lv_obj_set_flex_align(ui_MainMenuLoadMeter, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_SPACE_EVENLY);
-lv_label_set_text(ui_MainMenuLoadMeter,"1%");
-lv_obj_clear_flag( ui_MainMenuLoadMeter, LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_SCROLLABLE | LV_OBJ_FLAG_SCROLL_ELASTIC | LV_OBJ_FLAG_SCROLL_MOMENTUM | LV_OBJ_FLAG_SCROLL_CHAIN );    /// Flags
-lv_obj_set_scrollbar_mode(ui_MainMenuLoadMeter, LV_SCROLLBAR_MODE_OFF);
-lv_obj_set_style_text_color(ui_MainMenuLoadMeter, lv_color_hex(0xFD8B18), LV_PART_MAIN | LV_STATE_DEFAULT );
-lv_obj_set_style_text_opa(ui_MainMenuLoadMeter, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_letter_space(ui_MainMenuLoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_line_space(ui_MainMenuLoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_align(ui_MainMenuLoadMeter, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_font(ui_MainMenuLoadMeter, &ui_font_MuseoSansRounded50014, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_radius(ui_MainMenuLoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_left(ui_MainMenuLoadMeter, 1, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_right(ui_MainMenuLoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_top(ui_MainMenuLoadMeter, 2, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_bottom(ui_MainMenuLoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_label_set_recolor(ui_MainMenuLoadMeter, 1);
-
 }
--- firmware/src/gui/slsexport/meta5/screens/ui_PatchSelectorPage.c
+++ firmware/src/gui/slsexport/meta5/screens/ui_PatchSelectorPage.c
@@ -101,32 +101,6 @@ lv_obj_set_style_pad_right(ui_NowPlayingName, 4, LV_PART_MAIN| LV_STATE_DEFAULT)
 lv_obj_set_style_pad_top(ui_NowPlayingName, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
 lv_obj_set_style_pad_bottom(ui_NowPlayingName, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
 
-ui_LoadMeter = lv_label_create(ui_PatchSelectorTitlePanel);
-lv_obj_set_width( ui_LoadMeter, LV_SIZE_CONTENT);
-lv_obj_set_height( ui_LoadMeter, 20);
-lv_obj_set_x( ui_LoadMeter, 0 );
-lv_obj_set_y( ui_LoadMeter, 2 );
-lv_obj_set_align( ui_LoadMeter, LV_ALIGN_TOP_RIGHT );
-lv_obj_set_flex_flow(ui_LoadMeter,LV_FLEX_FLOW_ROW);
-lv_obj_set_flex_align(ui_LoadMeter, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_SPACE_EVENLY);
-lv_label_set_text(ui_LoadMeter,"1%");
-lv_obj_clear_flag( ui_LoadMeter, LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_SCROLLABLE | LV_OBJ_FLAG_SCROLL_ELASTIC | LV_OBJ_FLAG_SCROLL_MOMENTUM | LV_OBJ_FLAG_SCROLL_CHAIN );    /// Flags
-lv_obj_set_scrollbar_mode(ui_LoadMeter, LV_SCROLLBAR_MODE_OFF);
-lv_obj_set_style_text_color(ui_LoadMeter, lv_color_hex(0xFD8B18), LV_PART_MAIN | LV_STATE_DEFAULT );
-lv_obj_set_style_text_opa(ui_LoadMeter, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_letter_space(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_line_space(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_align(ui_LoadMeter, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_font(ui_LoadMeter, &ui_font_MuseoSansRounded50014, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_radius(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_bg_color(ui_LoadMeter, lv_color_hex(0x202328), LV_PART_MAIN | LV_STATE_DEFAULT );
-lv_obj_set_style_bg_opa(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_left(ui_LoadMeter, 1, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_right(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_top(ui_LoadMeter, 2, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_pad_bottom(ui_LoadMeter, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_label_set_recolor(ui_LoadMeter, 1);
-
 ui_DrivesPanel = lv_obj_create(ui_PatchSelectorPage);
 lv_obj_set_width( ui_DrivesPanel, 103);
 lv_obj_set_height( ui_DrivesPanel, 206);
