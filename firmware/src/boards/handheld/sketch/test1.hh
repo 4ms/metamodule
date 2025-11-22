65a090e8fd7f42e5a2b69bfc9348b8263410bb7a
Dan Green <danngreen1@gmail.com>
2025-11-21 17:05:29 -0800

    Test1

--- firmware/src/boards/handheld/sketch/test1.hh
+++ firmware/src/boards/handheld/sketch/test1.hh
@@ -12,49 +12,37 @@ public:
 
 	void setup() {
 		colorMode(RGB, 255, 255, 255);
-		rectMode(CENTER);
+		// rectMode(CENTER);
 	}
 
 	void draw() {
 		background(0);
 
-		// Test stroke width is centered:
-		stroke(0, 0, 255); //blue
-		fill(85);
-
 		strokeWeight(20);
 		fill(255, 0, 0); //red
-		ellipse(100, 100, 100, 100);
+		rect(100, 100, 100, 100);
 
 		strokeWeight(0);
 		fill(0, 255, 0); //green
-		ellipse(100, 200, 100, 100);
+		rect(100, 200, 100, 100);
 
-		// noStroke();
-		// fill(0, 0, 0);
-		// rect(0, 0, width, height);
+		strokeWeight(0);
+		fill(0, 255, 0); //green
+		rect(400, 200, 100, 100);
 
-		// fill(0, 0, 200);
-		// stroke(200, 0, 0);
-		// strokeWeight(4.f);
-		// ellipse((30 + ctr) % width, (60 + ctr * 2) % height, 50, 150);
+		stroke(200, 200, 200);
+		strokeWeight(20.f);
+		fill(23, 205, 40);
+		rect(30, 60, 100, 200);
 
-		// fill(200, 0, 200);
-		// noStroke();
-		// ellipse((30 + ctr) % width, (60 + ctr * 2) % height, 150, 50);
+		strokeWeight(2.f);
+		fill(122, 76, 16);
+		rect(630, 60, 200, 100);
 
-		// stroke(200, 200, 200);
-		// strokeWeight(20.f);
-		// fill(23, 205, 40);
-		// rect(30, 60, 100, 200);
-
-		// strokeWeight(2.f);
-		// fill(122, 76, 16);
-		// pushMatrix();
-		// rect(630, 60, 200, 100);
-		// rotate(((ctr % 20) / 20.f) * 2.f * 3.1416f);
-		// translate(ctr % width, 0);
-		// popMatrix();
+		// 		for (int i = 0; i < 100; i++) {
+		// 			fill(std::rand() & 0xFF, std::rand() & 0xFF, std::rand() & 0xFF);
+		// 			rect(std::rand() % 900, std::rand() % 400, std::rand() % 400, std::rand() % 200);
+		// 		}
 
 		// noFill();
 		// stroke(0, 255, 0);
@@ -66,33 +54,47 @@ public:
 		// endShape(OPEN);
 
 		// Draw a concave shape
-		fill(255, 0, 0);
-		stroke(0, 0, 255);
-		strokeWeight(40);
-		pushMatrix();
-		translate(width / 2, height / 2);
-		rotate(90.f / 360.f * TWO_PI);
-		beginShape();
-		vertex(0, -50);
-		vertex(50, 50);
-		vertex(0, 35);
-		vertex(-50, 50);
-		endShape(CLOSE);
-		popMatrix();
+		// fill(255, 0, 0);
+		// stroke(0, 0, 255);
+		// strokeWeight(40);
+		// pushMatrix();
+		// translate(width / 2, height / 2);
+		// rotate(90.f / 360.f * TWO_PI);
+		// beginShape();
+		// vertex(0, -50);
+		// vertex(50, 50);
+		// vertex(0, 35);
+		// vertex(-50, 50);
+		// endShape(CLOSE);
+		// popMatrix();
 
-		fill(255, 0, 0);
-		stroke(0, 0, 255);
-		strokeWeight(0);
-		pushMatrix();
-		translate(width / 2 + 100, height / 2);
-		rotate(90.f / 360.f * TWO_PI);
-		beginShape();
-		vertex(0, -50);
-		vertex(50, 50);
-		vertex(0, 35);
-		vertex(-50, 50);
-		endShape(CLOSE);
-		popMatrix();
+		// fill(255, 0, 0);
+		// stroke(0, 0, 255);
+		// strokeWeight(0);
+		// pushMatrix();
+		// translate(width / 2 + 100, height / 2);
+		// rotate(90.f / 360.f * TWO_PI);
+		// beginShape();
+		// vertex(0, -50);
+		// vertex(50, 50);
+		// vertex(0, 35);
+		// vertex(-50, 50);
+		// endShape(CLOSE);
+		// popMatrix();
+
+		// fill(255, 0, 0);
+		// stroke(0, 0, 255);
+		// strokeWeight(0);
+		// pushMatrix();
+		// translate(width / 2 - 100, height / 2);
+		// scale(1.5f);
+		// beginShape();
+		// vertex(0, -50);
+		// vertex(50, 50);
+		// vertex(0, 35);
+		// vertex(-50, 50);
+		// endShape(CLOSE);
+		// popMatrix();
 
 		// ctr += 4;
 	}
