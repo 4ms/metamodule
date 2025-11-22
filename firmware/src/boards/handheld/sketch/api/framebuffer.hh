b9dc4510c03900df6d4122b7b84c5b7e8fd3a4a0
Dan Green <danngreen1@gmail.com>
2025-11-21 17:05:37 -0800

    WIP optimize vlines

--- firmware/src/boards/handheld/sketch/api/framebuffer.hh
+++ firmware/src/boards/handheld/sketch/api/framebuffer.hh
@@ -1,4 +1,5 @@
 #include "../sketch.hh"
+#include <cstring>
 #include <span>
 
 namespace Handheld
@@ -13,14 +14,28 @@ inline void set_buffer(std::span<Color> buf) {
 inline auto buf(int x, int y) {
 	// Buffer is arranged in columns.
 	// That is, pixels touching each other vertically occupy consectutive memory addresses
-	constexpr int HEIGHT = 400;
-	auto pos = (x + 1) * HEIGHT - 1 - y;
+	// constexpr int HEIGHT = 400;
+	// auto pos = (x + 1) * HEIGHT - 1 - y;
+
+	auto pos = x * height + (height - 1 - y);
+
+	// auto pos = x * height + y;
 
 	return buffer.data() + pos;
 }
 
-inline void draw_vline(unsigned y_start, unsigned height, unsigned x, Color color) {
-	std::fill_n(buf(x, y_start), height, color);
+inline void draw_vline(int y_start, int h, int x, Color color) {
+	// uint32_t c = color << 16 | color;
+	// std::memset(buf(x, y_start), c, h / 2);
+	// if (h & 1) {
+	// 	*buf(x, y_start + h) = color;
+	// }
+
+	std::fill_n(buf(x, y_start), h, color);
+
+	// for (int i = 0; i < h; i++) {
+	// 	*buf(x, y_start + i) = color;
+	// }
 }
 
 } // namespace Handheld
