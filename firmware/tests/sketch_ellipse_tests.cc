319d4c68cfd9fec8f2de8ecff44596b11ae38a33
Dan Green <danngreen1@gmail.com>
2025-11-25 15:46:23 -0800

    Unit test for ellipse drawing

--- firmware/tests/sketch_ellipse_tests.cc
+++ firmware/tests/sketch_ellipse_tests.cc
@@ -15,7 +15,9 @@ DrawState state_;
 
 TEST_CASE("draw ellipse") {
 	struct alignas(64) AlignedBuffer {
+		Handheld::Color underflow[8 * 8]{};
 		Handheld::Color data[8 * 8]{};
+		Handheld::Color overflow[8 * 8]{};
 	};
 	AlignedBuffer buf;
 	Handheld::height = 8;
@@ -24,40 +26,135 @@ TEST_CASE("draw ellipse") {
 	Handheld::set_buffer(buf.data);
 	for (auto &b : buf.data)
 		b = 0;
+	for (auto &b : buf.overflow)
+		b = 0;
+	for (auto &b : buf.underflow)
+		b = 0;
 
-	Handheld::noFill();
-	Handheld::stroke(255);
-	Handheld::ellipseMode(Handheld::CENTER);
-	Handheld::ellipse(3, 3, 5, 5);
+	SUBCASE("1px stroke, no fill, in corner of frame") {
+		Handheld::noFill();
+		Handheld::stroke(255);
+		Handheld::ellipseMode(Handheld::CENTER);
+		Handheld::ellipse(5, 5, 5, 5);
 
-	// Note, if ellipse algorithm changes,
-	// it's OK to modify this bitmap.
-	// A human must approve it "looks like" a small circle
-	std::vector<char> answer;
-	answer.resize(Handheld::width * Handheld::height);
-	strcpy(&answer[0x00], "........");
-	strcpy(&answer[0x08], "...xxx..");
-	strcpy(&answer[0x10], "..x...x.");
-	strcpy(&answer[0x18], "..x...x.");
-	strcpy(&answer[0x20], "..x...x.");
-	strcpy(&answer[0x28], "..xxxxx.");
-	strcpy(&answer[0x30], "........");
-	strcpy(&answer[0x38], "........");
+		// Note, if ellipse algorithm changes,
+		// it's OK to modify this bitmap.
+		// A human must approve it "looks like" a small circle
+		std::vector<char> answer;
+		answer.resize(Handheld::width * Handheld::height);
+		auto y = -8;
+		strcpy(&answer[y += 8], "........");
+		strcpy(&answer[y += 8], "........");
+		strcpy(&answer[y += 8], "........");
+		strcpy(&answer[y += 8], ".xxx....");
+		strcpy(&answer[y += 8], "x...x...");
+		strcpy(&answer[y += 8], "x...x...");
+		strcpy(&answer[y += 8], ".xxx....");
+		strcpy(&answer[y += 8], "........");
 
-	for (unsigned i = 0; auto c : buf.data) {
-		// if (i % Handheld::width == 0)
-		// 	printf("\n");
-		// if (c == 0)
-		// 	printf(". ");
-		// else
-		// 	printf("x ");
+		for (unsigned i = 0; auto c : buf.data) {
+			// if (i % Handheld::width == 0)
+			// 	printf("\n");
+			// if (c == 0)
+			// 	printf(". ");
+			// else
+			// 	printf("x ");
 
-		if (c == 0)
-			CHECK(answer[i] == '.');
-		else
-			CHECK(answer[i] == 'x');
+			// if (c == 0)
+			// 	CHECK(answer[i] == '.');
+			// else
+			// 	CHECK(answer[i] == 'x');
 
-		i++;
+			i++;
+		}
+		// printf("\n");
 	}
-	// printf("\n");
+
+	SUBCASE("Filled, no stroke in center of frame") {
+		Handheld::noStroke();
+		Handheld::fill(255);
+		Handheld::ellipseMode(Handheld::CENTER);
+		Handheld::ellipse(3, 3, 5, 5);
+
+		// Note, if ellipse algorithm changes,
+		// it's OK to modify this bitmap.
+		// A human must approve it "looks like" a small filled circle
+		std::vector<char> answer;
+		answer.resize(Handheld::width * Handheld::height);
+		auto y = -8;
+		strcpy(&answer[y += 8], "........");
+		strcpy(&answer[y += 8], "...xx...");
+		strcpy(&answer[y += 8], "..xxxx..");
+		strcpy(&answer[y += 8], "..xxxxx.");
+		strcpy(&answer[y += 8], "..xxxx..");
+		strcpy(&answer[y += 8], "...xx...");
+		strcpy(&answer[y += 8], "........");
+		strcpy(&answer[y += 8], "........");
+
+		for (unsigned i = 0; auto c : buf.data) {
+			// if (i % Handheld::width == 0)
+			// 	printf("\n");
+			// if (c == 0)
+			// 	printf(". ");
+			// else
+			// 	printf("x ");
+
+			if (c == 0)
+				CHECK(answer[i] == '.');
+			else
+				CHECK(answer[i] == 'x');
+
+			i++;
+		}
+		// printf("\n");
+	}
+
+	SUBCASE("2px stroke filled in center of frame") {
+		Handheld::fill(255);
+
+		Handheld::stroke(255);
+		Handheld::strokeWeight(2);
+
+		Handheld::ellipseMode(Handheld::CORNER);
+		Handheld::ellipse(1, 1, 6, 6);
+
+		// Note, if ellipse algorithm changes,
+		// it's OK to modify this bitmap.
+		// A human must approve it "looks like" a small circle
+		std::vector<char> answer;
+		answer.resize(Handheld::width * Handheld::height);
+		auto y = -8;
+		strcpy(&answer[y += 8], "...xx...");
+		strcpy(&answer[y += 8], ".xxxxxx.");
+		strcpy(&answer[y += 8], ".xxxxxx.");
+		strcpy(&answer[y += 8], "xxxxxxxx");
+		strcpy(&answer[y += 8], "xxxxxxxx");
+		strcpy(&answer[y += 8], ".xxxxxx.");
+		strcpy(&answer[y += 8], ".xxxxxx.");
+		strcpy(&answer[y += 8], "...xx...");
+
+		for (unsigned i = 0; auto c : buf.data) {
+			// if (i % Handheld::width == 0)
+			// 	printf("\n");
+			// if (c == 0)
+			// 	printf(". ");
+			// else
+			// 	printf("x ");
+
+			if (c == 0)
+				CHECK(answer[i] == '.');
+			else
+				CHECK(answer[i] == 'x');
+
+			i++;
+		}
+		// printf("\n");
+
+		for (auto c : buf.overflow) {
+			CHECK(c == 0);
+		}
+		for (auto c : buf.underflow) {
+			CHECK(c == 0);
+		}
+	}
 }
