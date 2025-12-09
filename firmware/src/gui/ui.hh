2ef084c506fac346af2b2c660d6d2bc0418a1769
Dan Green <danngreen1@gmail.com>
2025-12-09 14:40:56 -0800

    Remove LVGL and ThorVG from ui

--- firmware/src/gui/ui.hh
+++ firmware/src/gui/ui.hh
@@ -1,18 +1,13 @@
 #pragma once
-#include "debug.hh"
-#include "drivers/fusb302.hh"
 #include "dynload/plugin_manager.hh"
-#include "dynload/preload_plugins.hh"
-#include "gui/button_exp_nav.hh"
 #include "gui/notify/notification.hh"
-#include "gui/pages/page_manager.hh"
 #include "params/params_dbg_print.hh"
 #include "params/params_state.hh"
 #include "params/sync_params.hh"
 #include "patch_file/file_storage_proxy.hh"
+#include "patch_play/patch_mod_queue.hh"
 #include "patch_play/patch_playloader.hh"
-#include "screen/lvgl_driver.hh"
-#include "thorvg.h"
+#include "user_settings/settings_file.hh"
 
 namespace MetaModule
 {
@@ -23,11 +18,9 @@ private:
 	PatchPlayLoader &patch_playloader;
 
 	NotificationQueue notify_queue;
-	PageManager page_manager;
 	ParamsMidiState params;
 	MetaParams metaparams;
 	UserSettings settings;
-	Screensaver screensaver{settings.screensaver};
 
 	ParamDbgPrint print_dbg_params{params, metaparams};
 
@@ -40,23 +33,9 @@ public:
 	   PluginManager &plugin_manager,
 	   FatFileIO &ramdisk)
 		: sync_params{sync_params}
-		, patch_playloader{patch_playloader}
-		, page_manager{patch_storage,
-					   open_patch_manager,
-					   patch_playloader,
-					   params,
-					   metaparams,
-					   notify_queue,
-					   patch_mod_queue,
-					   plugin_manager,
-					   settings,
-					   screensaver,
-					   ramdisk} {
+		, patch_playloader{patch_playloader} {
 		params.clear();
 		metaparams.clear();
-
-		MMDisplay::init(metaparams, screensaver);
-		Gui::init_lvgl_styles();
 
 		if (!Settings::read_settings(patch_storage, &settings, Volume::NorFlash)) {
 			settings = UserSettings{};
@@ -74,72 +53,34 @@ public:
 		patch_playloader.update_param_catchup_mode();
 
 		ModuleFactory::setModuleDisplayName("HubMedium", "Panel");
-
-		tvg::Initializer::init(0, tvg::CanvasEngine::Sw);
 	}
 
 	void update_screen() {
 		auto now = HAL_GetTick();
-		if ((now - last_lv_update_tm) > 2) {
-			last_lv_update_tm = now;
-			lv_timer_handler();
+		// Set the frame rate here:
+		if ((now - last_screen_update_tm) > 16) {
+			last_screen_update_tm = now;
+			// TODO: dump screen buffer via SPI DMA
 		}
 	}
 
 	void update_page() {
 		auto now = HAL_GetTick();
+		// Set the update rate here:
 		if ((now - last_page_update_tm) > 16) {
 			last_page_update_tm = now;
-			page_update_task();
+			read_latest_params();
+			handle_file_requests();
+			redraw_page();
 		}
-
-		// Uncomment to enable:
-		// print_dbg_params.output_debug_info(HAL_GetTick());
-		// print_dbg_params.output_load(HAL_GetTick());
 	}
 
-	TextDisplayWatcher &displays() {
-		return params.text_displays;
-	}
-
-	void preload_plugins(PluginManager &plugin_manager) {
-		lv_show(ui_MainMenuNowPlayingPanel);
-		lv_show(ui_MainMenuNowPlaying);
-
-		if (settings.plugin_preload.slugs.size())
-			delay_ms(600); //allow time for ???
-
-		auto preloader = PreLoader{plugin_manager, settings.plugin_preload.slugs};
-
-		while (true) {
-			auto status = preloader.process();
-
-			if (status.state == PreLoader::State::Error) {
-				notify_queue.put({status.message, Notification::Priority::Error, 2000});
-				break;
-
-			} else if (status.state == PreLoader::State::Warning) {
-				notify_queue.put({status.message, Notification::Priority::Error, 2000});
-
-			} else if (status.state == PreLoader::State::Done) {
-				break;
-
-			} else {
-				if (status.message.length()) {
-					lv_label_set_text(ui_MainMenuNowPlaying, status.message.c_str());
-				}
-			}
-
-			update_screen();
-			page_manager.handle_notifications();
-		}
-
-		lv_label_set_text(ui_MainMenuNowPlaying, "");
+	void redraw_page() {
+		// TODO:
+		// Here you can update the screen framebuffer
 	}
 
 	void load_initial_patch() {
-		page_manager.init();
-
 		if (settings.load_initial_patch) {
 			pr_dbg("Loading initial patch '%s' on vol %u\n",
 				   settings.initial_patch_name.c_str(),
@@ -154,10 +95,6 @@ public:
 		notify_queue.put({message, Notification::Priority::Error, 2000});
 	}
 
-	bool midi_feedback_enabled() const {
-		return settings.midi.midi_feedback == MidiSettings::MidiFeedback::Enabled;
-	}
-
 	UserSettings &get_settings() {
 		return settings;
 	}
@@ -166,24 +103,19 @@ public:
 		return notify_queue;
 	}
 
-	std::atomic<bool> new_patch_data = false;
-
 private:
-	void page_update_task() {
+	void read_latest_params() {
 		// Clear all accumulated knob change events
 		for (auto &knob : params.knobs) {
 			knob.changed = false;
 		}
 
+		// Copy params and metaparams from the audio thread
 		[[maybe_unused]] bool read_ok = sync_params.read_sync(params, metaparams);
+	}
 
-		// Experimental?
-		// button_expander_nav(metaparams);
-
-		page_manager.update_current_page();
-
-		new_patch_data.store(false, std::memory_order_release);
-
+	void handle_file_requests() {
+		// Handle file save/load requests
 		auto load_status = patch_playloader.handle_file_events();
 		if (!load_status.success) {
 			notify_queue.put({load_status.error_string, Notification::Priority::Error, 1500});
@@ -194,7 +126,7 @@ private:
 	}
 
 	uint32_t last_page_update_tm = 0;
-	uint32_t last_lv_update_tm = 0;
+	uint32_t last_screen_update_tm = 0;
 };
 
 } // namespace MetaModule
