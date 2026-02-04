c3050c8deb8a4098f4c71c5bc3b1834bad2c24cf
Dan Green <danngreen1@gmail.com>
2026-02-03 16:35:57 -0800

    Add CV to MIDI CC to build

--- firmware/vcv_ports/glue/RackCore/plugins.hh
+++ firmware/vcv_ports/glue/RackCore/plugins.hh
@@ -6,6 +6,7 @@ namespace rack::core
 
 extern rack::plugin::Model *modelMIDI_CV;
 extern rack::plugin::Model *modelCV_MIDI;
+extern rack::plugin::Model *modelCV_MIDICC;
 extern rack::plugin::Model *modelScope;
 
 } // namespace rack::core
