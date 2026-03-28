b978e4e18ff20207c14433e51e38cb9110bc4fb3
Dan Green <danngreen1@gmail.com>
2026-03-27 22:38:21 -0700

    VCV: mark_*_patched doesn't clear poly channel count

--- firmware/vcv_plugin/export/src/VCV_module_wrapper.cc
+++ firmware/vcv_plugin/export/src/VCV_module_wrapper.cc
@@ -84,7 +84,12 @@ void VCVModuleWrapper::mark_input_unpatched(int input_id) {
 }
 
 void VCVModuleWrapper::mark_input_patched(int input_id) {
-	if ((size_t)input_id < inputs.size())
+	if ((size_t)input_id >= inputs.size())
+		return;
+
+	// 0 (unpatched) -> 1 (patched mono)
+	// 1-N (mono/poly patched) => no change
+	if (inputs[input_id].channels == 0)
 		inputs[input_id].channels = 1;
 }
 
@@ -99,7 +104,12 @@ void VCVModuleWrapper::mark_output_unpatched(int output_id) {
 }
 
 void VCVModuleWrapper::mark_output_patched(int output_id) {
-	if ((size_t)output_id < outputs.size())
+	if ((size_t)output_id >= outputs.size())
+		return;
+
+	// 0 (unpatched) -> 1 (patched mono)
+	// 1-N (mono/poly patched) => no change
+	if (outputs[output_id].channels == 0)
 		outputs[output_id].channels = 1;
 }
 
