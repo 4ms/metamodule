31b66162ad5914c0151c2b70b6af759bc8c46155
Dan Green <danngreen1@gmail.com>
2026-03-31 06:47:17 -0700

    Implement poly ports without breaking sdk api
    Use dynamic cast in plugin_module.cc

--- firmware/src/patch_play/plugin_module.hh
+++ firmware/src/patch_play/plugin_module.hh
@@ -8,4 +8,8 @@ namespace MetaModule
 void plugin_module_init(std::unique_ptr<CoreProcessor> &module);
 void plugin_module_deinit(std::unique_ptr<CoreProcessor> &module);
 
+CoreProcessor::PolyPortBuffer plugin_module_get_poly_output_buffer(std::unique_ptr<CoreProcessor> &module,
+																   int output_id);
+CoreProcessor::PolyPortBuffer plugin_module_get_poly_input_buffer(std::unique_ptr<CoreProcessor> &module, int input_id);
+
 } // namespace MetaModule
