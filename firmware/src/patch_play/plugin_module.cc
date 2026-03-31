9cddfae26b7a6baaef826c120abe9fc851ce9f42
Dan Green <danngreen1@gmail.com>
2026-03-31 07:00:19 -0700

    implement poly for Source module in plugin_module

--- firmware/src/patch_play/plugin_module.cc
+++ firmware/src/patch_play/plugin_module.cc
@@ -2,6 +2,9 @@
 #include "CoreModules/CoreProcessor.hh"
 #include "engine/Module.hpp"
 
+//// !
+#include "CoreModules/4ms/core/SourceCore.cc"
+
 namespace MetaModule
 {
 
@@ -28,6 +31,14 @@ CoreProcessor::PolyPortBuffer plugin_module_get_poly_output_buffer(std::unique_p
 	if (auto rack_module = get_rack_plugin_module(module)) {
 		if (output_id < (int)rack_module->outputs.size())
 			return {rack_module->outputs[output_id].voltages.begin(), &rack_module->outputs[output_id].channels};
+	}
+
+	else if (auto m = dynamic_cast<SourceCore *>(module.get()))
+	{
+		if (output_id == 0)
+			return {m->out1s.data(), &m->out1_chans};
+		else
+			return {m->out2s.data(), &m->out2_chans};
 	}
 
 	return {nullptr, nullptr};
