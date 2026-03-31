5df5d0726305fcbb335c5f665a64ba008cd8252d
Dan Green <danngreen1@gmail.com>
2026-03-31 06:47:17 -0700

    Implement poly ports without breaking sdk api
    Use dynamic cast in plugin_module.cc

--- firmware/src/patch_play/plugin_module.cc
+++ firmware/src/patch_play/plugin_module.cc
@@ -1,5 +1,5 @@
 #include "plugin_module.hh"
-
+#include "CoreModules/CoreProcessor.hh"
 #include "engine/Module.hpp"
 
 namespace MetaModule
@@ -21,6 +21,25 @@ void plugin_module_deinit(std::unique_ptr<CoreProcessor> &module) {
 		rack::engine::Module::RemoveEvent rm{};
 		rack_module->onRemove(rm);
 	}
+}
+
+CoreProcessor::PolyPortBuffer plugin_module_get_poly_output_buffer(std::unique_ptr<CoreProcessor> &module,
+																   int output_id) {
+	if (auto rack_module = get_rack_plugin_module(module)) {
+		if (output_id < (int)rack_module->outputs.size())
+			return {rack_module->outputs[output_id].voltages.begin(), &rack_module->outputs[output_id].channels};
+	}
+
+	return {nullptr, nullptr};
+}
+
+CoreProcessor::PolyPortBuffer plugin_module_get_poly_input_buffer(std::unique_ptr<CoreProcessor> &module,
+																  int input_id) {
+	if (auto rack_module = get_rack_plugin_module(module)) {
+		if (input_id < (int)rack_module->inputs.size())
+			return {rack_module->inputs[input_id].voltages.begin(), &rack_module->inputs[input_id].channels};
+	}
+	return {nullptr, nullptr};
 }
 
 } // namespace MetaModule
