e7ab91e72edcff5420f88872ee4e9bec1a2774b5
Dan Green <danngreen1@gmail.com>
2026-03-31 14:43:25 -0700

    Cleanup Merge module: limit jacks to MaxPoly (evenly spaced), remove lights

--- firmware/vcv_ports/RackCore/Merge.cpp
+++ firmware/vcv_ports/RackCore/Merge.cpp
@@ -4,23 +4,22 @@ namespace rack::core
 {
 
 struct Merge : Module {
+	static constexpr unsigned MaxPoly = CoreProcessor::MaxPolyChannels;
+
 	enum ParamIds { NUM_PARAMS };
-	enum InputIds { ENUMS(MONO_INPUTS, 8), NUM_INPUTS };
+	enum InputIds { ENUMS(MONO_INPUTS, MaxPoly), NUM_INPUTS };
 	enum OutputIds { POLY_OUTPUT, NUM_OUTPUTS };
-	// enum LightIds { ENUMS(CHANNEL_LIGHTS, 8), NUM_LIGHTS };
 	enum DisplayIds { ChannelDisplay = 0 };
 
-	dsp::ClockDivider lightDivider;
 	int channels = -1;
 	int automaticChannels = 0;
 
 	Merge() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, 0);
-		for (int i = 0; i < 16; i++)
+		for (auto i = 0u; i < MaxPoly; i++)
 			configInput(MONO_INPUTS + i, string::f("Channel %d", i + 1));
 		configOutput(POLY_OUTPUT, "Polyphonic");
 
-		lightDivider.setDivision(512);
 		onReset();
 	}
 
@@ -30,7 +29,7 @@ struct Merge : Module {
 
 	void process(const ProcessArgs &args) override {
 		int lastChannel = -1;
-		for (int c = 0; c < 16; c++) {
+		for (auto c = 0u; c < MaxPoly; c++) {
 			float v = 0.f;
 			if (inputs[MONO_INPUTS + c].isConnected()) {
 				lastChannel = c;
@@ -73,22 +72,15 @@ struct MergeWidget : ModuleWidget {
 		addChild(createWidget<ThemedScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 41.995)), module, Merge::MONO_INPUTS + 0));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 52.155)), module, Merge::MONO_INPUTS + 1));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 62.315)), module, Merge::MONO_INPUTS + 2));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 72.475)), module, Merge::MONO_INPUTS + 3));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 82.635)), module, Merge::MONO_INPUTS + 4));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 92.795)), module, Merge::MONO_INPUTS + 5));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 102.955)), module, Merge::MONO_INPUTS + 6));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 113.115)), module, Merge::MONO_INPUTS + 7));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 41.995)), module, Merge::MONO_INPUTS + 8));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 52.155)), module, Merge::MONO_INPUTS + 9));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 62.315)), module, Merge::MONO_INPUTS + 10));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 72.475)), module, Merge::MONO_INPUTS + 11));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 82.635)), module, Merge::MONO_INPUTS + 12));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 92.795)), module, Merge::MONO_INPUTS + 13));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 102.955)), module, Merge::MONO_INPUTS + 14));
-		// addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(18.119, 113.115)), module, Merge::MONO_INPUTS + 15));
+		float first_col = (Merge::MaxPoly > 8) ? 7.3f : 12.7f;
+		float space_y = 81.3f / std::min(Merge::MaxPoly, 8u);
+		float first_row = (42.f - 10.16f / 2) + space_y / 2;
+
+		for (auto i = 0u; i < Merge::MaxPoly; i++)
+			addInput(createInputCentered<ThemedPJ301MPort>(
+				mm2px(Vec(first_col + 10.8f * int(i / 8), first_row + (i % 8) * space_y)),
+				module,
+				Merge::MONO_INPUTS + i));
 
 		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(7.281, 21.967)), module, Merge::POLY_OUTPUT));
 
