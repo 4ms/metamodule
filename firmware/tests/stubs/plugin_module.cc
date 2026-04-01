1e7416370f988a360fe02b534080e77f34136f8d
Dan Green <danngreen1@gmail.com>
2026-04-01 07:26:22 -0700

    Tests for poly and summed+poly

--- firmware/tests/stubs/plugin_module.cc
+++ firmware/tests/stubs/plugin_module.cc
@@ -1,4 +1,5 @@
 #include "CoreModules/CoreProcessor.hh"
+#include "test_poly_module.hh"
 #include <memory>
 
 namespace MetaModule
@@ -11,20 +12,20 @@ void plugin_module_deinit(std::unique_ptr<CoreProcessor> &module) {
 
 CoreProcessor::PolyPortBuffer plugin_module_get_poly_output_buffer(std::unique_ptr<CoreProcessor> &module,
 																   int output_id) {
-	// if (auto rack_module = get_rack_plugin_module(module)) {
-	// if (output_id < (int)rack_module->outputs.size())
-	// return {rack_module->outputs[output_id].voltages.begin(), &rack_module->outputs[output_id].channels};
-	// }
+	if (auto m = dynamic_cast<TestPolyModule *>(module.get())) {
+		if ((unsigned)output_id < TestPolyModule::NumOutputs)
+			return {m->output_poly[output_id].data(), &m->output_channels[output_id]};
+	}
 
 	return {nullptr, nullptr};
 }
 
 CoreProcessor::PolyPortBuffer plugin_module_get_poly_input_buffer(std::unique_ptr<CoreProcessor> &module,
 																  int input_id) {
-	// if (auto rack_module = get_rack_plugin_module(module)) {
-	// 	if (input_id < (int)rack_module->inputs.size())
-	// 		return {rack_module->inputs[input_id].voltages.begin(), &rack_module->inputs[input_id].channels};
-	// }
+	if (auto m = dynamic_cast<TestPolyModule *>(module.get())) {
+		if ((unsigned)input_id < TestPolyModule::NumInputs)
+			return {m->input_poly[input_id].data(), &m->input_channels[input_id]};
+	}
 	return {nullptr, nullptr};
 }
 
