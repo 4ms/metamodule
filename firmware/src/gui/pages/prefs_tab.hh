1a5f205cf5872ebf2d5e4ec1529d076d97b192c4
Dan Green <danngreen1@gmail.com>
2026-04-24 15:08:14 -0700

    Disabling USB Video skips frame buffer filling

--- firmware/src/gui/pages/prefs_tab.hh
+++ firmware/src/gui/pages/prefs_tab.hh
@@ -467,6 +467,7 @@ private:
 			video.enabled = video_enabled;
 			while (!DeviceSettingsProxy::send_video_mode(video_enabled))
 				;
+			UsbVideoBuffer::enable(video_enabled);
 			gui_state.do_write_settings = true;
 		}
 		if (video.mirror != video_mirror) {
