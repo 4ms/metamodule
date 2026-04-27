79c68a4969fb2dd9e308370a630fbb1c65dd2d23
Dan Green <danngreen1@gmail.com>
2026-04-27 13:54:24 -0700

    Fix Wifi UART ISRs repeatedly called on Overrun error

--- firmware/src/wifi/comm/BufferedUSART2.cpp
+++ firmware/src/wifi/comm/BufferedUSART2.cpp
@@ -23,7 +23,8 @@ void BufferedUSART2::init() {
 }
 
 void BufferedUSART2::deinit() {
-	LL_USART_DisableIT_RXNE_RXFNE(USART_PERIPH);
+	LL_USART_DisableIT_RXFT(USART_PERIPH);
+	LL_USART_DisableIT_IDLE(USART_PERIPH);
 }
 
 bool BufferedUSART2::setBaudrate(uint32_t baudRate) {
@@ -33,39 +34,57 @@ bool BufferedUSART2::setBaudrate(uint32_t baudRate) {
 void BufferedUSART2::initPeripheral() {
 	commMain.init();
 
-	mdrivlib::Interrupt::register_and_start_isr(USART_IRQ, USART_IRQ_PRIO, 0, []() {
-		if (LL_USART_IsActiveFlag_RXNE_RXFNE(USART_PERIPH)) {
-			do {
-				auto val = USART_PERIPH->RDR;
+	// CR3.RXFTCFG can only be programmed when UE=0.
+	LL_USART_Disable(USART_PERIPH);
+	LL_USART_SetRXFIFOThreshold(USART_PERIPH, LL_USART_FIFOTHRESHOLD_1_2);
+	LL_USART_Enable(USART_PERIPH);
 
-				auto result = queue.put(val);
-				if (!result) {
-					queue.reset();
-					pr_warn("USART2: RX Soft Overrun\n");
-					overrunDetected = true;
-					break;
-				}
-			} while (LL_USART_IsActiveFlag_RXNE(USART_PERIPH));
-		} else if (LL_USART_IsActiveFlag_ORE(USART_PERIPH)) {
-			queue.reset();
-			overrunDetected = true;
-			pr_warn("USART2: FIFO Overrun\n");
+	mdrivlib::Interrupt::register_and_start_isr(USART_IRQ, USART_IRQ_PRIO, 0, []() {
+		// Clear ORE first in case new data comes in during the IRQ handler.
+		if (LL_USART_IsActiveFlag_ORE(USART_PERIPH)) {
 			LL_USART_ClearFlag_ORE(USART_PERIPH);
-		} else {
-			pr_warn("USART2: ISR called with no flag (ISR=%x)\n", USART_PERIPH->ISR);
-			(void)USART_PERIPH->RDR;
+			overrunDetected = true;
+		}
+		// Not necessary to clear these, but makes debugging ISR register easier:
+		LL_USART_ClearFlag_FE(USART_PERIPH);
+		LL_USART_ClearFlag_NE(USART_PERIPH);
+		LL_USART_ClearFlag_PE(USART_PERIPH);
+		LL_USART_ClearFlag_IDLE(USART_PERIPH);
+
+		// Drain the FIFO unconditionally
+		while (LL_USART_IsActiveFlag_RXNE_RXFNE(USART_PERIPH)) {
+			auto val = USART_PERIPH->RDR;
+			if (!queue.put(val)) {
+				queue.reset();
+				overrunDetected = true;
+				break;
+			}
 		}
 	});
 
 	// read RX from hardware to clear RXNE flag
-	(void)USART_PERIPH->RDR;
+	[[maybe_unused]] auto _ = USART_PERIPH->RDR;
 
-	LL_USART_EnableIT_RXNE_RXFNE(USART_PERIPH);
+	// Trigger IRQ on FIFO-threshold (1/2 = 4 bytes), plus IDLE to flush the
+	// last 1-3 bytes of a burst. RXNE/RXFNE stays off so we aren't IRQ'd per byte.
+	LL_USART_EnableIT_RXFT(USART_PERIPH);
+	LL_USART_EnableIT_IDLE(USART_PERIPH);
 
 	LL_USART_DisableIT_CM(USART_PERIPH);
+	LL_USART_DisableIT_CTS(USART_PERIPH);
+	LL_USART_DisableIT_EOB(USART_PERIPH);
+	LL_USART_DisableIT_ERROR(USART_PERIPH);
+	LL_USART_DisableIT_LBD(USART_PERIPH);
 	LL_USART_DisableIT_PE(USART_PERIPH);
+	LL_USART_DisableIT_RTO(USART_PERIPH);
+	LL_USART_DisableIT_RXFF(USART_PERIPH);
+	LL_USART_DisableIT_RXNE_RXFNE(USART_PERIPH);
 	LL_USART_DisableIT_TC(USART_PERIPH);
-	LL_USART_DisableIT_TC(USART_PERIPH);
+	LL_USART_DisableIT_TCBGT(USART_PERIPH);
+	LL_USART_DisableIT_TXE_TXFNF(USART_PERIPH);
+	LL_USART_DisableIT_TXFE(USART_PERIPH);
+	LL_USART_DisableIT_TXFT(USART_PERIPH);
+	LL_USART_DisableIT_WKUP(USART_PERIPH);
 }
 
 void BufferedUSART2::transmit(uint8_t val) {
