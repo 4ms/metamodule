b085b18337975c9bef03d00d84079926254508c0
Dan Green <danngreen1@gmail.com>
2026-07-30 16:02:06 -0700

    Enable GUI selection of USB console mode

--- firmware/src/core_a7/aux_core_main.cc
+++ firmware/src/core_a7/aux_core_main.cc
@@ -71,8 +71,10 @@ extern "C" void aux_core_main() {
 			;
 	}
 
+	// Tell the M4 which USB device class to present (MIDI, Video, or the
+	// Debug Console). The M4 defaults to MIDI until this arrives.
 	auto usb_device_mode = ui.get_settings().usb_device_mode;
-	if (usb_device_mode != UsbDeviceMode::Cdc) {
+	if (usb_device_mode != UsbDeviceMode::Midi) {
 		while (!DeviceSettingsProxy::send_device_mode(usb_device_mode))
 			;
 	}
