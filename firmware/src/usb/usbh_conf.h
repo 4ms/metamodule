9201462fae902343cf2cde12096d4589138e6866
Dan Green <danngreen1@gmail.com>
2026-08-20 09:44:25 -0700

    Disable USBH debug log

--- firmware/src/usb/usbh_conf.h
+++ firmware/src/usb/usbh_conf.h
@@ -43,7 +43,7 @@ static inline void Error_Handler() {
 
 #define USBH_USER_LOG_OUTPUT 1
 #define USBH_ERR_LOG_OUTPUT 1
-#define USBH_DBG_LOG_OUTPUT 1
+#define USBH_DBG_LOG_OUTPUT 0
 
 enum { MidiStreamingBufferSize = 1024 };
 enum { HOST_HS = 0, HOST_FS = 1 };
