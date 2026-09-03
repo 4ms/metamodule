c5eaa1888f8d153fa6f756ff5f2b520d356a8b61
Dan Green <danngreen1@gmail.com>
2026-09-03 11:58:21 -0700

    Make Overrun retry options match actual retries

--- firmware/src/gui/slsexport/prefs_section_audio.cc
+++ firmware/src/gui/slsexport/prefs_section_audio.cc
@@ -39,8 +39,7 @@ void PrefsSectionAudio::create(lv_obj_t *parent) {
 	bs_override_check = lv_obj_get_child(bs_override_cont, 1);
 	lv_obj_set_style_text_font(bs_override_label, &ui_font_MuseoSansRounded50014, LV_PART_MAIN);
 
-	auto audio_retry_cont =
-		create_prefs_labeled_dropdown(parent, "Overrun retries:", "None\n4\n8\n16\n32\n64\n128\n256");
+	auto audio_retry_cont = create_prefs_labeled_dropdown(parent, "Overrun retries:", "None\n1\n2\n4\n8\n16\n32\n64");
 	overrun_retries = lv_obj_get_child(audio_retry_cont, 1);
 	lv_obj_set_width(overrun_retries, 75);
 
