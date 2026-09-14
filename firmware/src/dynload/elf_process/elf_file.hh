16bfe1390b4d8bf0be04175a31b3d397cf2d00e0
Dan Green <danngreen1@gmail.com>
2026-09-14 12:06:09 -0700

    Run .fini_array destructors (if found) when unloading a plugin

--- firmware/src/dynload/elf_process/elf_file.hh
+++ firmware/src/dynload/elf_process/elf_file.hh
@@ -63,14 +63,20 @@ struct Elf {
 		return highest_addr - lowest_addr + 1;
 	}
 
-	std::optional<ElfSection> find_section(std::string_view name) {
+	std::optional<ElfSection> try_find_section(std::string_view name) {
 		auto section = std::ranges::find_if(sections, [&](auto &sec) { return (sec.section_name() == name); });
 		if (section != sections.end())
 			return *section;
-		else {
-			pr_err("Section %.*s not found\n", (int)name.size(), name.data());
+		else
 			return {};
-		}
+	}
+
+	// same as try_find_section, but print an error if section not found
+	std::optional<ElfSection> find_section(std::string_view name) {
+		auto section = try_find_section(name);
+		if (!section)
+			pr_err("Section %.*s not found\n", (int)name.size(), name.data());
+		return section;
 	}
 
 	std::optional<ElfSection> get_section(size_t index) {
--- firmware/src/dynload/loaded_plugin.hh
+++ firmware/src/dynload/loaded_plugin.hh
@@ -10,11 +10,19 @@
 namespace MetaModule
 {
 
+// Location of the plugin's .fini_array within its code buffer.
+// The entries there are relocated (absolute) destructor pointers.
+struct PluginFiniArray {
+	uint32_t offset = 0;
+	uint32_t count = 0;
+};
+
 struct LoadedPlugin {
 	PluginFile fileinfo;
 	rack::plugin::Plugin rack_plugin;
 	std::vector<std::string> loaded_files;
 	CodeBuffer code;
+	PluginFiniArray fini_array;
 };
 
 using LoadedPluginList = std::list<LoadedPlugin>;
