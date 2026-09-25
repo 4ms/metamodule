d9e28ea1904c52299bfc562cde8ae9a10814036b
Dan Green <danngreen1@gmail.com>
2026-09-25 11:47:13 -0700

    M4 reports stdout as a character device

--- firmware/src/core_m4/libc_stub.c
+++ firmware/src/core_m4/libc_stub.c
@@ -1,3 +1,5 @@
+#include <sys/stat.h>
+
 int _open(const char *filename, int flags, int mode) {
 	return -1;
 }
@@ -14,10 +16,13 @@ int _read(int fd, char *ptr, int len) {
 	return -1;
 }
 
-__attribute__((used)) int _fstat(int x, void *p) {
+// stdout must look like a tty so newlib line-buffers it (flushing each newline).
+// Otherwise it gets fully buffered and printf() output is held back until 1kB accumulates
+__attribute__((used)) int _fstat(int x, struct stat *st) {
+	st->st_mode = S_IFCHR;
 	return 0;
 }
 
 __attribute__((used)) int _isatty(int x) {
-	return -1;
+	return 1;
 }
